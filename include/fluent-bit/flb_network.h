a602e00d288ec2346bfdc48ca92d7e67cbc3b534
Masaya YAMAMOTO <pandax381@gmail.com>
2015-05-27 00:27:48 +0900

    Core: handling IPv6 address on flb_network.
    Signed-off-by: Masaya YAMAMOTO <pandax381@gmail.com>

--- include/fluent-bit/flb_network.h
+++ include/fluent-bit/flb_network.h
@@ -33,7 +33,7 @@ int flb_net_socket_nonblocking(int sockfd);
 int flb_net_socket_tcp_fastopen(int sockfd);
 
 /* Socket handling */
-int flb_net_socket_create();
+int flb_net_socket_create(int family, int nonblock);
 int flb_net_tcp_connect(char *host, unsigned long port);
 int flb_net_server(char *port, char *listen_addr);
 int flb_net_bind(int socket_fd, const struct sockaddr *addr,
