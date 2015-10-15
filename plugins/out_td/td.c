c3e74c01a827f249289589b11ff20358cbb448ac
Eduardo Silva <eduardo@treasure-data.com>
2015-10-15 15:42:33 -0600

    out_td: parse and convert to new record format
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- plugins/out_td/td.c
+++ plugins/out_td/td.c
@@ -23,15 +23,105 @@
 #include <assert.h>
 #include <errno.h>
 
+#include <msgpack.h>
 #include <fluent-bit/flb_output.h>
 #include <fluent-bit/flb_utils.h>
 #include <fluent-bit/flb_network.h>
+#include <fluent-bit/flb_pack.h>
 
 #include "td.h"
 #include "td_http.h"
 #include "td_config.h"
 
 struct flb_output_plugin out_td_plugin;
+
+/*
+ * Convert the internal Fluent Bit data representation to the required
+ * one by Treasure Data cloud service.
+ *
+ * This function returns a new msgpack buffer and store the bytes length
+ * in the out_size variable.
+ */
+static char *td_format(void *data, size_t bytes, int *out_size)
+{
+    int i;
+    int ret;
+    int n_size;
+    size_t off = 0;
+    time_t atime;
+    struct msgpack_sbuffer mp_sbuf;
+    struct msgpack_packer mp_pck;
+    msgpack_unpacked result;
+    msgpack_object root;
+    msgpack_object map;
+
+    /* Initialize contexts for new output */
+    msgpack_sbuffer_init(&mp_sbuf);
+    msgpack_packer_init(&mp_pck, &mp_sbuf, msgpack_sbuffer_write);
+
+    /* Iterate the original buffer and perform adjustments */
+    msgpack_unpacked_init(&result);
+
+    /* Perform some format validation */
+    ret = msgpack_unpack_next(&result, data, bytes, &off);
+    if (!ret) {
+        return NULL;
+    }
+    if (result.data.type != MSGPACK_OBJECT_ARRAY) {
+        return NULL;
+    }
+
+    root = result.data;
+    if (root.via.array.size == 0) {
+        return NULL;
+    }
+
+    off = 0;
+    msgpack_unpacked_destroy(&result);
+    msgpack_unpacked_init(&result);
+    while (msgpack_unpack_next(&result, data, bytes, &off)) {
+        if (result.data.type != MSGPACK_OBJECT_ARRAY) {
+            continue;
+        }
+
+        /* Each array must have two entries: time and record */
+        root = result.data;
+        if (root.via.array.size != 2) {
+            continue;
+        }
+
+        atime = root.via.array.ptr[0].via.u64;
+        map   = root.via.array.ptr[1];
+
+        n_size = map.via.map.size + 1;
+        msgpack_pack_map(&mp_pck, n_size);
+        msgpack_pack_bin(&mp_pck, 4);
+        msgpack_pack_bin_body(&mp_pck, "time", 4);
+        msgpack_pack_int32(&mp_pck, atime);
+
+        for (i = 0; i < n_size - 1; i++) {
+            msgpack_pack_object(&mp_pck, map.via.map.ptr[i].key);
+            msgpack_pack_object(&mp_pck, map.via.map.ptr[i].val);
+        }
+    }
+    msgpack_unpacked_destroy(&result);
+
+    char *buf;
+    msgpack_sbuffer *sbuf;
+
+    sbuf = &mp_sbuf;
+    *out_size = sbuf->size;
+    buf = malloc(sbuf->size);
+    if (!buf) {
+        return NULL;
+    }
+
+    /* set a new buffer and re-initialize our MessagePack context */
+    memcpy(buf, sbuf->data, sbuf->size);
+    msgpack_sbuffer_destroy(&mp_sbuf);
+
+    return buf;
+}
 
 int cb_td_init(struct flb_config *config)
 {
@@ -60,14 +150,21 @@ int cb_td_flush(void *data, size_t bytes, void *out_context,
 {
     int n;
     int ret;
+    int bytes_out;
+    char *pack;
     size_t bytes_sent;
     char buf[1024];
-
     size_t len;
     char *request;
     struct flb_out_td_config *ctx = out_context;
 
-    request = td_http_request(data, bytes, &len, ctx, config);
+    /* Convert format */
+    pack = td_format(data, bytes, &bytes_out);
+    if (!pack) {
+        return -1;
+    }
+
+    request = td_http_request(pack, bytes_out, &len, ctx, config);
     ret = flb_io_write(&out_td_plugin, request, len, &bytes_sent);
     if (ret == -1) {
         perror("write");
@@ -79,6 +176,7 @@ int cb_td_flush(void *data, size_t bytes, void *out_context,
         buf[n] = '\0';
         flb_debug("[TD] API server response:\n%s", buf);
     }
+
     return bytes_sent;
 }
 
