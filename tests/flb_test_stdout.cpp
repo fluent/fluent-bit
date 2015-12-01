47dc5796d8aff42b55827a194f1730c3e27031bc
Eduardo Silva <eduardo@treasure-data.com>
2015-12-01 09:54:25 -0600

    tests: stdout: use new data samples
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- tests/flb_test_stdout.cpp
+++ tests/flb_test_stdout.cpp
@@ -2,10 +2,36 @@
 
 #include <gtest/gtest.h>
 #include <fluent-bit.h>
-#include "flb_data.h"
+#include "data/json_invalid.h"
+#include "data/json_small.h"
+#include "data/json_long.h"
+
+TEST(Outputs, json_invalid) {
+    int i;
+    int ret;
+    int total;
+    int bytes;
+    char *p = (char *) JSON_INVALID;
+    struct flb_lib_ctx *ctx;
+
+    ctx = flb_lib_init((char *) "stdout");
+    EXPECT_TRUE(ctx != NULL);
+
+    ret = flb_lib_start(ctx);
+    EXPECT_EQ(ret, 0);
+
+    total = 0;
+    for (i = 0; i < (int) sizeof(JSON_INVALID) - 1; i++) {
+        bytes = flb_lib_push(ctx, p + i, 1);
+        EXPECT_EQ(bytes, 1);
+        total++;
+    }
+    flb_lib_stop(ctx);
+    flb_lib_exit(ctx);
+}
 
 /* It writes a very long JSON map (> 100KB) byte by byte */
-TEST(Outputs, json_long_stdout) {
+TEST(Outputs, json_long) {
     int i;
     int ret;
     int total;
@@ -29,7 +55,7 @@ TEST(Outputs, json_long_stdout) {
     flb_lib_exit(ctx);
 }
 
-TEST(Outputs, json_small_stdout) {
+TEST(Outputs, json_small) {
     int i;
     int ret;
     int total;
@@ -45,30 +71,6 @@ TEST(Outputs, json_small_stdout) {
 
     total = 0;
     for (i = 0; i < (int) sizeof(JSON_SMALL) - 1; i++) {
-        bytes = flb_lib_push(ctx, p + i, 1);
-        EXPECT_EQ(bytes, 1);
-        total++;
-    }
-    flb_lib_stop(ctx);
-    flb_lib_exit(ctx);
-}
-
-TEST(Outputs, json_invalid) {
-    int i;
-    int ret;
-    int total;
-    int bytes;
-    char *p = (char *) JSON_INVALID;
-    struct flb_lib_ctx *ctx;
-
-    ctx = flb_lib_init((char *) "stdout");
-    EXPECT_TRUE(ctx != NULL);
-
-    ret = flb_lib_start(ctx);
-    EXPECT_EQ(ret, 0);
-
-    total = 0;
-    for (i = 0; i < (int) sizeof(JSON_INVALID) - 1; i++) {
         bytes = flb_lib_push(ctx, p + i, 1);
         EXPECT_EQ(bytes, 1);
         total++;
