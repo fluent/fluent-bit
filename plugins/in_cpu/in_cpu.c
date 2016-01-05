09381cc513fb9c99a5db5f99ec6fe7e40fec3a8f
Eduardo Silva <eduardo@treasure-data.com>
2016-01-04 23:39:14 -0600

    in_cpu: major improvement, include more metrics from the system
    The original in_cpu plugin allowed to retrieve overall CPU usage
    of the system in a fixed percentage value (abc%). This patch adds
    several metrics:
    
    - It now provides specific values per CPU core for the following
      metrics:
    
      user    : Time spent in user mode.
      nice    : Time spent in user mode with low priority (nice).
      system  : Time spent in system mode.
      idle    : Time spent in the idle task.
      iowait  : Time waiting for I/O to complete.
      irq     : Time servicing interrupts.
      softirq : Time servicing softirqs.
    
    In a JSON representation, the plugin should compose data in this
    format (time field have been removed in the example):
    
    {
      "cpu": 8.5,
      "cpu0.user": 882617.0,
      "cpu0.nice": 29932.0,
      "cpu0.system": 252175.0,
      "cpu0.idle": 20520087.0,
      "cpu0.iowait": 134709.0,
      "cpu0.irq": 0.0,
      "cpu0.softirq": 6949.0,
    
      "cpu1.user": 900223.0,
      "cpu1.nice": 29495.0,
      "cpu1.system": 245986.0,
      "cpu1.idle": 243352.0,
      "cpu1.iowait": 2304.0,
      "cpu1.irq": 0.0,
      "cpu1.softirq": 5354.0,
    
      "cpu2.user": 1161700.0,
      "cpu2.nice": 22536.0,
      "cpu2.system": 189551.0,
      "cpu2.idle": 237328.0,
      "cpu2.iowait": 1944.0,
      "cpu2.irq": 0.0,
      "cpu2.softirq": 6484.0,
    
      "cpu3.user": 1177862.0,
      "cpu3.nice": 21797.0,
      "cpu3.system": 203194.0,
      "cpu3.idle": 237128.0,
      "cpu3.iowait": 2495.0,
      "cpu3.irq": 0.0,
      "cpu3.softirq": 6238.0
    }
    
    I consider this a major improvement but I also think that all info may
    not be relevant for all users, the next series of patches I will introduce
    some filtering to let the user decide which specific data he wants.
    
    note: the first candidates at the moment to go away are the fields 'irq' and
    'softirq', I will do more research about real use cases where people needs those.
    
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- plugins/in_cpu/in_cpu.c
+++ plugins/in_cpu/in_cpu.c
@@ -32,39 +32,92 @@
 struct flb_input_plugin in_cpu_plugin;
 
 /* Retrieve CPU load from the system (through ProcFS) */
-static inline double proc_cpu_load()
+static inline double proc_cpu_load(int cpus, struct cpu_stats *cstats)
 {
+    int i;
     int ret;
+    int fd;
+    int cpu_id=-1;
     double total;
     double user, nice, system, idle, iowait, irq, softirq;
+    ssize_t read;
+    char *line = NULL;
+    size_t len = 0;
+    char *fmt;
     FILE *f;
+    struct cpu_snapshot *s;
 
     f = fopen("/proc/stat", "r");
     if (f == NULL) {
         return -1;
     }
 
-    ret = fscanf(f, " cpu %lf %lf %lf %lf %lf %lf %lf",
-                 &user, &nice, &system, &idle, &iowait, &irq, &softirq);
-    if (ret != 7) {
-        fclose(f);
-        return -1;
+    /* Always read (n_cpus + 1) lines */
+    for (i = 0; i <= cpus; i++) {
+        read = getline(&line, &len, f);
+        if (read == -1) {
+            break;
+        }
+
+        s = &cstats->info[i];
+        if (i == 0) {
+            fmt = " cpu  %lf %lf %lf %lf %lf %lf %lf";
+            ret = sscanf(line,
+                         fmt,
+                         &s->val_user,
+                         &s->val_nice,
+                         &s->val_system,
+                         &s->val_idle,
+                         &s->val_iowait,
+                         &s->val_irq,
+                         &s->val_softirq);
+        }
+        else {
+            fmt = " %s %lf %lf %lf %lf %lf %lf %lf";
+            ret = sscanf(line,
+                         fmt,
+                         &s->val_cpuid,
+                         &s->val_user,
+                         &s->val_nice,
+                         &s->val_system,
+                         &s->val_idle,
+                         &s->val_iowait,
+                         &s->val_irq,
+                         &s->val_softirq);
+        }
+
+        if (ret < 7) {
+            fclose(f);
+            return -1;
+        }
+    }
+
+    if (line) {
+        free(line);
     }
 
     fclose(f);
-    total = (user + nice + system);
+    s = &cstats->info[0];
+    total = (s->val_user + s->val_nice + s->val_system);
+
     return total;
 }
 
 /* Init CPU input */
 int in_cpu_init(struct flb_config *config)
 {
+    int i;
     int ret;
+    int len;
+    double total;
     struct flb_in_cpu_config *ctx;
+    struct cpu_stats *cstats;
+    struct cpu_snapshot *snap;
 
     /* Allocate space for the configuration */
     ctx = malloc(sizeof(struct flb_in_cpu_config));
     if (!ctx) {
+        perror("malloc");
         return -1;
     }
 
@@ -72,15 +125,37 @@ int in_cpu_init(struct flb_config *config)
     ctx->n_processors = sysconf(_SC_NPROCESSORS_ONLN);
     ctx->cpu_ticks    = sysconf(_SC_CLK_TCK);
 
-    /* We need to prepare our buffer */
-    ctx->data_idx   = 0;
+    /* Initialize buffers for CPU stats */
+    cstats = &ctx->cstats;
+    cstats->info = malloc(sizeof(struct cpu_snapshot) * (ctx->n_processors + 1));
+    if (!cstats->info) {
+        perror("malloc");
+        return -1;
+    }
+
+    for (i = 1; i <= ctx->n_processors; i++) {
+        snap = &cstats->info[i];
+
+        CPU_KEY_FORMAT(snap, user, i);
+        CPU_KEY_FORMAT(snap, nice, i);
+        CPU_KEY_FORMAT(snap, system, i);
+        CPU_KEY_FORMAT(snap, idle, i);
+        CPU_KEY_FORMAT(snap, iowait, i);
+        CPU_KEY_FORMAT(snap, irq, i);
+        CPU_KEY_FORMAT(snap, softirq, i);
+    }
+
 
     /* initialize MessagePack buffers */
     msgpack_sbuffer_init(&ctx->mp_sbuf);
     msgpack_packer_init(&ctx->mp_pck, &ctx->mp_sbuf, msgpack_sbuffer_write);
 
     /* Get CPU load, ready to be updated once fired the calc callback */
-    ctx->load_pre = proc_cpu_load();
+    total = proc_cpu_load(ctx->n_processors, &ctx->cstats);
+    if (total == -1) {
+        flb_utils_error_c("Could not obtain CPU data");
+    }
+    ctx->cstats.load_pre = total;
 
     /* Set the context */
     ret = flb_input_set_context("cpu", ctx, config);
@@ -101,23 +176,49 @@ int in_cpu_init(struct flb_config *config)
     return 0;
 }
 
+static inline int key_field(char *buf,
+                            char *cpu, int c_len,
+                            char *field, int f_len)
+{
+    int len = 0;
+
+    strncpy(buf, cpu, c_len);
+    len = c_len;
+
+    buf[len] = '.';
+    len++;
+
+    strncpy(buf + len, field, f_len);
+    len += f_len;
+
+    buf[len] = '\0';
+    return len;
+}
+
+
 /* Callback to gather CPU usage between now and previous snapshot */
 int in_cpu_collect(struct flb_config *config, void *in_context)
 {
+    int i;
+    int maps;
+    int len;
     double usage;
     double total;
     (void) config;
     struct flb_in_cpu_config *ctx = in_context;
+    struct cpu_stats *cstats = &ctx->cstats;
+    struct cpu_snapshot *s;
 
     /* Get the current CPU usage */
-    ctx->load_now = proc_cpu_load();
+    total = proc_cpu_load(ctx->n_processors, cstats);
+    cstats->load_now = total;
 
     /* Calculate the difference between the two samples */
-    usage = fabs(ctx->load_now - ctx->load_pre) / ctx->cpu_ticks;
+    usage = fabs(cstats->load_now - cstats->load_pre) / ctx->cpu_ticks;
     total = (usage * 100) / ctx->n_processors;
 
     /* Put current load back */
-    ctx->load_pre = ctx->load_now;
+    cstats->load_pre = cstats->load_now;
 
     /*
      * Store the new data into the MessagePack buffer,
@@ -125,13 +226,24 @@ int in_cpu_collect(struct flb_config *config, void *in_context)
     msgpack_pack_array(&ctx->mp_pck, 2);
     msgpack_pack_uint64(&ctx->mp_pck, time(NULL));
 
-    msgpack_pack_map(&ctx->mp_pck, 1);
+    msgpack_pack_map(&ctx->mp_pck, (ctx->n_processors * 7 ) + 1);
     msgpack_pack_bin(&ctx->mp_pck, 3);
     msgpack_pack_bin_body(&ctx->mp_pck, "cpu", 3);
     msgpack_pack_double(&ctx->mp_pck, total);
 
-    ctx->data_idx++;
-    flb_debug("[in_cpu] CPU %0.2f%% (buffer=%i)", total, ctx->data_idx - 1);
+    for (i = 1; i < ctx->n_processors + 1; i++) {
+        s = &cstats->info[i];
+
+        CPU_PACK_SNAP(s, user);
+        CPU_PACK_SNAP(s, nice);
+        CPU_PACK_SNAP(s, system);
+        CPU_PACK_SNAP(s, idle);
+        CPU_PACK_SNAP(s, iowait);
+        CPU_PACK_SNAP(s, irq);
+        CPU_PACK_SNAP(s, softirq);
+    }
+
+    flb_debug("[in_cpu] CPU %0.2f%%", total);
 
     flb_stats_update(in_cpu_plugin.stats_fd, 0, 1);
     return 0;
@@ -142,9 +254,6 @@ void *in_cpu_flush(void *in_context, int *size)
     char *buf;
     msgpack_sbuffer *sbuf;
     struct flb_in_cpu_config *ctx = in_context;
-
-    if (ctx->data_idx == 0)
-        return NULL;
 
     sbuf = &ctx->mp_sbuf;
     *size = sbuf->size;
--- plugins/in_cpu/in_cpu.h
+++ plugins/in_cpu/in_cpu.h
@@ -27,10 +27,50 @@
 /* Collection time: every 1 second (0 nanoseconds) */
 #define IN_CPU_COLLECT_SEC    1
 #define IN_CPU_COLLECT_NSEC   0
+#define IN_CPU_KEY_LEN       16
 
-struct in_cpu_data {
-    time_t time;
-    double cpu_usage;
+struct cpu_key {
+    size_t length;
+    char name[IN_CPU_KEY_LEN];
+};
+
+struct cpu_snapshot {
+    char   val_cpuid[8];
+    double val_user;
+    double val_nice;
+    double val_system;
+    double val_idle;
+    double val_iowait;
+    double val_irq;
+    double val_softirq;
+
+    /* necessary... */
+    struct cpu_key key_user;
+    struct cpu_key key_nice;
+    struct cpu_key key_system;
+    struct cpu_key key_idle;
+    struct cpu_key key_iowait;
+    struct cpu_key key_irq;
+    struct cpu_key key_softirq;
+};
+
+#define CPU_KEY_FORMAT(s, key, i)                                   \
+    s->key_##key.length = snprintf(s->key_##key.name,               \
+                                   IN_CPU_KEY_LEN,                  \
+                                   "cpu%i.%s", i - 1, #key);
+
+#define CPU_PACK_SNAP(s, key)                                           \
+    msgpack_pack_bin(&ctx->mp_pck, s->key_##key.length);                \
+    msgpack_pack_bin_body(&ctx->mp_pck, s->key_##key.name, s->key_##key.length); \
+    msgpack_pack_double(&ctx->mp_pck, s->val_##key)
+
+struct cpu_stats {
+    /* runtime data */
+    double load_now;    /* CPU load now               */
+    double load_pre;    /* CPU load previously        */
+
+    /* CPU snapshots */
+    struct cpu_snapshot *info;
 };
 
 /* CPU Input configuration & context */
@@ -39,16 +79,10 @@ struct flb_in_cpu_config {
     int n_processors;   /* number of core processors  */
     int cpu_ticks;      /* CPU ticks (Kernel setting) */
 
-    /* Tag: used to extend original tag */
-    int  tag_len;       /* The real string length     */
-    char tag[32];       /* Custom Tag for this input  */
-
-    /* runtime data */
-    double load_now;    /* CPU load now               */
-    double load_pre;    /* CPU load previously        */
-
     /* Buffered data */
     int data_idx;       /* next position available    */
+
+    struct cpu_stats cstats;
 
     /* MessagePack buffers */
     msgpack_packer  mp_pck;
