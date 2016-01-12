0ddf9a8b6984ceb9bf9d8ed6d5be8b55d5f7b586
Eduardo Silva <eduardo@treasure-data.com>
2016-01-12 12:03:20 -0600

    Examples: treasure data cpp: use new prototype for init callback
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- examples/td_cpp/td.cc
+++ examples/td_cpp/td.cc
@@ -35,7 +35,7 @@ int main(int argc, char **argv)
     }
 
     /* Initialize library */
-    ctx = flb_lib_init(NULL, (char *) "td");
+    ctx = flb_lib_init(NULL, (char *) "td", NULL);
     if (!ctx) {
         exit(EXIT_FAILURE);
     }
