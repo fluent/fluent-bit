cff01e79c559bf32891b571da94b330df8644aa4
Eduardo Silva <eduardo@treasure-data.com>
2016-01-18 14:45:59 -0800

    macros: remove deprecated colors
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- include/fluent-bit/flb_macros.h
+++ include/fluent-bit/flb_macros.h
@@ -23,25 +23,6 @@
 #define FLB_FALSE  0
 #define FLB_TRUE   !FLB_FALSE
 
-/* ANSI Colors */
-#define ANSI_RESET "\033[0m"
-#define ANSI_BOLD  "\033[1m"
-
-#define ANSI_CYAN          "\033[36m"
-#define ANSI_BOLD_CYAN     ANSI_BOLD ANSI_CYAN
-#define ANSI_MAGENTA       "\033[35m"
-#define ANSI_BOLD_MAGENTA  ANSI_BOLD ANSI_MAGENTA
-#define ANSI_RED           "\033[31m"
-#define ANSI_BOLD_RED      ANSI_BOLD ANSI_RED
-#define ANSI_YELLOW        "\033[33m"
-#define ANSI_BOLD_YELLOW   ANSI_BOLD ANSI_YELLOW
-#define ANSI_BLUE          "\033[34m"
-#define ANSI_BOLD_BLUE     ANSI_BOLD ANSI_BLUE
-#define ANSI_GREEN         "\033[32m"
-#define ANSI_BOLD_GREEN    ANSI_BOLD ANSI_GREEN
-#define ANSI_WHITE         "\033[37m"
-#define ANSI_BOLD_WHITE    ANSI_BOLD ANSI_WHITE
-
 #define FLB_INLINE inline __attribute__((always_inline))
 
 #endif
