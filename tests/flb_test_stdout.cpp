598f74c1b74ff2c932c5e5ecb1f073aeddbca59b
Eduardo Silva <eduardo@treasure-data.com>
2016-02-15 17:46:01 -0600

    tests: td: fix API usage
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- tests/flb_test_stdout.cpp
+++ tests/flb_test_stdout.cpp
@@ -12,22 +12,32 @@ TEST(Outputs, json_invalid) {
     int total;
     int bytes;
     char *p = (char *) JSON_INVALID;
-    struct flb_lib_ctx *ctx;
+    flb_ctx_t *ctx;
+    flb_input_t *input;
+    flb_output_t *output;
 
-    ctx = flb_lib_init(NULL, (char *) "stdout", NULL);
-    EXPECT_TRUE(ctx != NULL);
+    ctx = flb_create();
 
-    ret = flb_lib_start(ctx);
+    input = flb_input(ctx, (char *) "lib", NULL);
+    EXPECT_TRUE(input != NULL);
+    flb_input_set(input, "tag", "test");
+
+    output = flb_output(ctx, (char *) "stdout", NULL);
+    EXPECT_TRUE(output != NULL);
+    flb_output_set(output, "tag", "test");
+
+    ret = flb_start(ctx);
     EXPECT_EQ(ret, 0);
 
     total = 0;
     for (i = 0; i < (int) sizeof(JSON_INVALID) - 1; i++) {
-        bytes = flb_lib_push(ctx, p + i, 1);
+        bytes = flb_lib_push(input, p + i, 1);
         EXPECT_EQ(bytes, 1);
         total++;
     }
-    flb_lib_stop(ctx);
-    flb_lib_exit(ctx);
+
+    flb_stop(ctx);
+    flb_destroy(ctx);
 }
 
 /* It writes a very long JSON map (> 100KB) byte by byte */
@@ -37,22 +47,32 @@ TEST(Outputs, json_long) {
     int total;
     int bytes;
     char *p = (char *) JSON_LONG;
-    struct flb_lib_ctx *ctx;
+    flb_ctx_t *ctx;
+    flb_input_t *input;
+    flb_output_t *output;
 
-    ctx = flb_lib_init(NULL, (char *) "stdout", NULL);
-    EXPECT_TRUE(ctx != NULL);
+    ctx = flb_create();
 
-    ret = flb_lib_start(ctx);
+    input = flb_input(ctx, (char *) "lib", NULL);
+    EXPECT_TRUE(input != NULL);
+    flb_input_set(input, "tag", "test");
+
+    output = flb_output(ctx, (char *) "stdout", NULL);
+    EXPECT_TRUE(output != NULL);
+    flb_output_set(output, "tag", "test");
+
+    ret = flb_start(ctx);
     EXPECT_EQ(ret, 0);
 
     total = 0;
     for (i = 0; i < (int) sizeof(JSON_LONG) - 1; i++) {
-        bytes = flb_lib_push(ctx, p + i, 1);
+        bytes = flb_lib_push(input, p + i, 1);
         EXPECT_EQ(bytes, 1);
         total++;
     }
-    flb_lib_stop(ctx);
-    flb_lib_exit(ctx);
+
+    flb_stop(ctx);
+    flb_destroy(ctx);
 }
 
 TEST(Outputs, json_small) {
@@ -61,20 +81,30 @@ TEST(Outputs, json_small) {
     int total;
     int bytes;
     char *p = (char *) JSON_SMALL;
-    struct flb_lib_ctx *ctx;
+    flb_ctx_t *ctx;
+    flb_input_t *input;
+    flb_output_t *output;
 
-    ctx = flb_lib_init(NULL, (char *) "stdout", NULL);
-    EXPECT_TRUE(ctx != NULL);
+    ctx = flb_create();
 
-    ret = flb_lib_start(ctx);
+    input = flb_input(ctx, (char *) "lib", NULL);
+    EXPECT_TRUE(input != NULL);
+    flb_input_set(input, "tag", "test");
+
+    output = flb_output(ctx, (char *) "stdout", NULL);
+    EXPECT_TRUE(output != NULL);
+    flb_output_set(output, "tag", "test");
+
+    ret = flb_start(ctx);
     EXPECT_EQ(ret, 0);
 
     total = 0;
     for (i = 0; i < (int) sizeof(JSON_SMALL) - 1; i++) {
-        bytes = flb_lib_push(ctx, p + i, 1);
+        bytes = flb_lib_push(input, p + i, 1);
         EXPECT_EQ(bytes, 1);
         total++;
     }
-    flb_lib_stop(ctx);
-    flb_lib_exit(ctx);
+
+    flb_stop(ctx);
+    flb_destroy(ctx);
 }
--- tests/flb_test_td.cpp
+++ tests/flb_test_td.cpp
@@ -7,18 +7,28 @@
 /* It writes a big JSON message (> 3.5MB) */
 TEST(TD, json_long) {
     int ret;
-    struct flb_lib_ctx *ctx;
+    flb_ctx_t *ctx;
+    flb_input_t *input;
+    flb_output_t *output;
 
-    ctx = flb_lib_init(NULL, (char *) "td", NULL);
-    EXPECT_TRUE(ctx != NULL);
+    ctx = flb_create();
+
+    input = flb_input(ctx, (char *) "lib", NULL);
+    EXPECT_TRUE(input != NULL);
+    flb_input_set(input, "tag", "test");
+
+    output = flb_output(ctx, (char *) "td", NULL);
+    EXPECT_TRUE(output != NULL);
+    flb_output_set(output, "tag", "test");
 
     flb_config_verbose(FLB_TRUE);
     ret = flb_lib_config_file(ctx, (char *) "/tmp/td.conf");
 
-    ret = flb_lib_start(ctx);
+    ret = flb_start(ctx);
     EXPECT_EQ(ret, 0);
 
-    flb_lib_push(ctx, (char *) JSON_TD , (int) sizeof(JSON_TD) - 1);
-    flb_lib_stop(ctx);
-    flb_lib_exit(ctx);
+    flb_lib_push(input, (char *) JSON_TD , (int) sizeof(JSON_TD) - 1);
+
+    flb_stop(ctx);
+    flb_destroy(ctx);
 }
