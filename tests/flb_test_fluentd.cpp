8947467d236541532bfd149180f749f9875a902e
Eduardo Silva <eduardo@treasure-data.com>
2016-02-15 17:38:33 -0600

    tests: fluentd: fix API usage and change default TCP port
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- tests/flb_test_fluentd.cpp
+++ tests/flb_test_fluentd.cpp
@@ -7,15 +7,25 @@
 TEST(Outputs, json_long_fluentd) {
     int ret;
     int size = sizeof(JSON_LONG) - 1;
-    struct flb_lib_ctx *ctx;
+    flb_ctx_t *ctx;
+    flb_input_t *input;
+    flb_output_t *output;
 
-    ctx = flb_lib_init(NULL, (char *) "fluentd://127.0.0.1:12225", NULL);
-    EXPECT_TRUE(ctx != NULL);
+    ctx = flb_create();
 
-    ret = flb_lib_start(ctx);
+    input = flb_input(ctx, (char *) "lib", NULL);
+    EXPECT_TRUE(input != NULL);
+    flb_input_set(input, "tag", "test");
+
+    output = flb_output(ctx, (char *) "fluentd://127.0.0.1:24224", NULL);
+    EXPECT_TRUE(output != NULL);
+    flb_output_set(output, "tag", "test");
+
+    ret = flb_start(ctx);
     EXPECT_EQ(ret, 0);
 
-    flb_lib_push(ctx, (char *)JSON_LONG, size);
-    flb_lib_stop(ctx);
-    flb_lib_exit(ctx);
+    flb_lib_push(input, (char *)JSON_LONG, size);
+
+    flb_stop(ctx);
+    flb_destroy(ctx);
 }
