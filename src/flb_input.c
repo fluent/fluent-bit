28e75989ff3cc2be3dba3fe241da26429390efef
Eduardo Silva <eduardo@treasure-data.com>
2016-02-24 17:09:52 -0600

    input: do not assume default tag
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- src/flb_input.c
+++ src/flb_input.c
@@ -152,9 +152,6 @@ void flb_input_initialize_all(struct flb_config *config)
     mk_list_foreach(head, &config->inputs) {
         in = mk_list_entry(head, struct flb_input_instance, _head);
         p = in->p;
-        if (!in->tag) {
-            in->tag = strdup(in->p->name);
-        }
 
         /* Initialize the input */
         if (p->cb_init) {
