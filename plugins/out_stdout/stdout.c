29bea19633e5b44318fa2be93fd9e003b070c4b3
Eduardo Silva <eduardo@treasure-data.com>
2016-05-10 11:34:42 -0600

    out_stdout: use new prototype for flush callback
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- plugins/out_stdout/stdout.c
+++ plugins/out_stdout/stdout.c
@@ -37,6 +37,7 @@ int cb_stdout_init(struct flb_output_instance *ins, struct flb_config *config,
 }
 
 int cb_stdout_flush(void *data, size_t bytes,
+                    char *tag, int tag_len,
                     struct flb_input_instance *i_ins,
                     void *out_context,
                     struct flb_config *config)
@@ -75,8 +76,7 @@ int cb_stdout_flush(void *data, size_t bytes,
      */
     msgpack_unpacked_init(&result);
     while (msgpack_unpack_next(&result, data, bytes, &off)) {
-        /* FIXME: lazy output */
-        printf("[%zd] ", cnt++);
+        printf("[%zd] %s: ", cnt++, tag);
         msgpack_object_print(stdout, result.data);
         printf("\n");
     }
