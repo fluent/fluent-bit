42732a18210d898c4742c38397990b7540fdc584
Eduardo Silva <eduardo@treasure-data.com>
2016-06-02 14:13:15 -0600

    http_server: include key with build flags
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- include/fluent-bit/flb_http_server.h
+++ include/fluent-bit/flb_http_server.h
@@ -1,12 +1,14 @@
 #ifndef FLB_HTTP_H
 #define FLB_HTTP_H
 
+#include <fluent-bit/flb_info.h>
 #include <fluent-bit/flb_config.h>
 #include <fluent-bit/flb_version.h>
 
 #define FLB_HTTP_BANNER \
-  "{\n  \"service\": \"Fluent Bit\",\n" \
-  "  \"version\": \"v" FLB_VERSION_STR "\"\n}"
+  "{\n  \"service\"  : \"Fluent Bit\",\n" \
+  "  \"version\": \"v" FLB_VERSION_STR "\"\n" \
+  "  \"build_flags\": \"" FLB_INFO_FLAGS "\"\n}"
 
 int flb_http_server_start(struct flb_config *config);
 
