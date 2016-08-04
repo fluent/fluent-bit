1f73c867e0178adbe9bf4973c58aad856f7ecb31
Eduardo Silva <eduardo@treasure-data.com>
2016-08-03 21:14:37 -0600

    sha1: add missing sha1 wrapper
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

---
+++ src/flb_sha1.c
@@ -0,0 +1,35 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2015-2016 Treasure Data Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include <fluent-bit/flb_info.h>
+
+#ifdef FLB_HAVE_BUFFERING /* Only buffering uses SHA-1 at the moment */
+
+#include "sha1.h"
+
+void flb_sha1_encode(const void *data_in, unsigned long length,
+                     unsigned char *data_out)
+{
+    SHA_CTX sha;
+    SHA1_Init(&sha);
+    SHA1_Update(&sha, data_in, length);
+    SHA1_Final(data_out, &sha);
+}
+
+#endif
