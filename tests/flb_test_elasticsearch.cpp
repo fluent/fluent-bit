482997600273ba868ccd1cbd98dd677a69dc8c39
Eduardo Silva <eduardo@treasure-data.com>
2016-10-05 01:13:48 -0600

    tests: migrate tests to new API
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- tests/flb_test_elasticsearch.cpp
+++ tests/flb_test_elasticsearch.cpp
@@ -8,24 +8,24 @@ TEST(Outputs, json_es) {
     int ret;
     int size = sizeof(JSON_ES) - 1;
     flb_ctx_t *ctx;
-    flb_input_t *input;
-    flb_output_t *output;
+    int in_ffd;
+    int out_ffd;
 
     ctx = flb_create();
     EXPECT_TRUE(ctx != NULL);
 
-    input = flb_input(ctx, (char *) "lib", NULL);
-    EXPECT_TRUE(input != NULL);
-    flb_input_set(input, "tag", "test", NULL);
+    in_ffd = flb_input(ctx, (char *) "lib", NULL);
+    EXPECT_TRUE(in_ffd >= 0);
+    flb_input_set(ctx, in_ffd, "tag", "test", NULL);
 
-    output = flb_output(ctx, (char *) "es", NULL);
-    EXPECT_TRUE(output != NULL);
-    flb_output_set(output, "match", "test", NULL);
+    out_ffd = flb_output(ctx, (char *) "es", NULL);
+    EXPECT_TRUE(out_ffd >= 0);
+    flb_output_set(ctx, out_ffd, "match", "test", NULL);
 
     ret = flb_start(ctx);
     EXPECT_EQ(ret, 0);
 
-    flb_lib_push(input, (char *) JSON_ES, size);
+    flb_lib_push(ctx, in_ffd, (char *) JSON_ES, size);
 
     flb_stop(ctx);
     flb_destroy(ctx);
--- tests/flb_test_stdout.cpp
+++ tests/flb_test_stdout.cpp
@@ -13,25 +13,25 @@ TEST(Outputs, json_invalid) {
     int bytes;
     char *p = (char *) JSON_INVALID;
     flb_ctx_t *ctx;
-    flb_input_t *input;
-    flb_output_t *output;
+    int in_ffd;
+    int out_ffd;
 
     ctx = flb_create();
 
-    input = flb_input(ctx, (char *) "lib", NULL);
-    EXPECT_TRUE(input != NULL);
-    flb_input_set(input, "tag", "test", NULL);
+    in_ffd = flb_input(ctx, (char *) "lib", NULL);
+    EXPECT_TRUE(in_ffd >= 0);
+    flb_input_set(ctx, in_ffd, "tag", "test", NULL);
 
-    output = flb_output(ctx, (char *) "stdout", NULL);
-    EXPECT_TRUE(output != NULL);
-    flb_output_set(output, "match", "test", NULL);
+    out_ffd = flb_output(ctx, (char *) "stdout", NULL);
+    EXPECT_TRUE(out_ffd >= 0);
+    flb_output_set(ctx, out_ffd, "match", "test", NULL);
 
     ret = flb_start(ctx);
     EXPECT_EQ(ret, 0);
 
     total = 0;
     for (i = 0; i < (int) sizeof(JSON_INVALID) - 1; i++) {
-        bytes = flb_lib_push(input, p + i, 1);
+        bytes = flb_lib_push(ctx, in_ffd, p + i, 1);
         EXPECT_EQ(bytes, 1);
         total++;
     }
@@ -48,25 +48,25 @@ TEST(Outputs, json_long) {
     int bytes;
     char *p = (char *) JSON_LONG;
     flb_ctx_t *ctx;
-    flb_input_t *input;
-    flb_output_t *output;
+    int in_ffd;
+    int out_ffd;
 
     ctx = flb_create();
 
-    input = flb_input(ctx, (char *) "lib", NULL);
-    EXPECT_TRUE(input != NULL);
-    flb_input_set(input, "tag", "test", NULL);
+    in_ffd = flb_input(ctx, (char *) "lib", NULL);
+    EXPECT_TRUE(in_ffd >= 0);
+    flb_input_set(ctx, in_ffd, "tag", "test", NULL);
 
-    output = flb_output(ctx, (char *) "stdout", NULL);
-    EXPECT_TRUE(output != NULL);
-    flb_output_set(output, "match", "test", NULL);
+    out_ffd = flb_output(ctx, (char *) "stdout", NULL);
+    EXPECT_TRUE(out_ffd >= 0);
+    flb_output_set(ctx, out_ffd, "match", "test", NULL);
 
     ret = flb_start(ctx);
     EXPECT_EQ(ret, 0);
 
     total = 0;
     for (i = 0; i < (int) sizeof(JSON_LONG) - 1; i++) {
-        bytes = flb_lib_push(input, p + i, 1);
+        bytes = flb_lib_push(ctx, in_ffd, p + i, 1);
         EXPECT_EQ(bytes, 1);
         total++;
     }
@@ -82,25 +82,25 @@ TEST(Outputs, json_small) {
     int bytes;
     char *p = (char *) JSON_SMALL;
     flb_ctx_t *ctx;
-    flb_input_t *input;
-    flb_output_t *output;
+    int in_ffd;
+    int out_ffd;
 
     ctx = flb_create();
 
-    input = flb_input(ctx, (char *) "lib", NULL);
-    EXPECT_TRUE(input != NULL);
-    flb_input_set(input, "tag", "test", NULL);
+    in_ffd = flb_input(ctx, (char *) "lib", NULL);
+    EXPECT_TRUE(in_ffd >= 0);
+    flb_input_set(ctx, in_ffd, "tag", "test", NULL);
 
-    output = flb_output(ctx, (char *) "stdout", NULL);
-    EXPECT_TRUE(output != NULL);
-    flb_output_set(output, "match", "test", NULL);
+    out_ffd = flb_output(ctx, (char *) "stdout", NULL);
+    EXPECT_TRUE(out_ffd >= 0);
+    flb_output_set(ctx, out_ffd, "match", "test", NULL);
 
     ret = flb_start(ctx);
     EXPECT_EQ(ret, 0);
 
     total = 0;
     for (i = 0; i < (int) sizeof(JSON_SMALL) - 1; i++) {
-        bytes = flb_lib_push(input, p + i, 1);
+        bytes = flb_lib_push(ctx, in_ffd, p + i, 1);
         EXPECT_EQ(bytes, 1);
         total++;
     }
