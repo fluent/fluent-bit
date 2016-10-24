1b51b23a85ff98be8ccc16d600a2c18fddfaa718
Eduardo Silva <eduardo@treasure-data.com>
2016-10-24 13:48:19 -0600

    out_es: use flb_strdup() instead of strdup()
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- plugins/out_es/es.c
+++ plugins/out_es/es.c
@@ -275,7 +275,7 @@ int cb_es_init(struct flb_output_instance *ins,
 
     /* Get network configuration */
     if (!ins->host.name) {
-        ins->host.name = strdup("127.0.0.1");
+        ins->host.name = flb_strdup("127.0.0.1");
     }
 
     if (ins->host.port == 0) {
