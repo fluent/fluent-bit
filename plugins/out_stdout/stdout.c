dbb107fa745681d3ff34d434541f5797265083d7
Eduardo Silva <eduardo@treasure-data.com>
2016-11-01 15:25:39 -0600

    out_stdout: change flush callback return data type
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- plugins/out_stdout/stdout.c
+++ plugins/out_stdout/stdout.c
@@ -36,11 +36,11 @@ int cb_stdout_init(struct flb_output_instance *ins, struct flb_config *config,
     return 0;
 }
 
-int cb_stdout_flush(void *data, size_t bytes,
-                    char *tag, int tag_len,
-                    struct flb_input_instance *i_ins,
-                    void *out_context,
-                    struct flb_config *config)
+void cb_stdout_flush(void *data, size_t bytes,
+                     char *tag, int tag_len,
+                     struct flb_input_instance *i_ins,
+                     void *out_context,
+                     struct flb_config *config)
 {
     msgpack_unpacked result;
     size_t off = 0, cnt = 0;
