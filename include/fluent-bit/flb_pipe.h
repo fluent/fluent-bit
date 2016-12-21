2063f34fb56c0579248f21e7eefd9d31d82dd76e
Eduardo Silva <eduardo@treasure-data.com>
2016-12-21 16:08:38 -0600

    pipe: new Fluent Bit pipe interface (provide WIN32 support)
    Windows do not support selectable file descriptors and since Fluent Bit
    relies on unnamed Unix pipes for internal communication we need to
    provide a workaround.
    
    This patch implement a new pipe interface where on WIN32 it emulate
    unix pipes behavior through libevent creating two connected sockets (aka
    socketpair), on all different OS just use pipe(2).
    
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

---
+++ include/fluent-bit/flb_pipe.h
@@ -0,0 +1,34 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2015-2016 Treasure Data Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef FLB_PIPE_H
+#define FLB_PIPE_H
+
+#ifdef _WIN32
+#include <event.h>
+#define flb_pipefd_t evutil_socket_t
+#else
+#define flb_pipefd_t int
+#endif
+
+int flb_pipe_create(flb_pipefd_t pipefd[2]);
+void flb_pipe_destroy(flb_pipefd_t pipefd[2]);
+int flb_pipe_close(flb_pipefd_t fd);
+
+#endif
---
+++ src/flb_pipe.c
@@ -0,0 +1,94 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2015-2016 Treasure Data Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*
+ * Fluent Bit core uses unnamed Unix pipes for signaling and general
+ * communication across components. When building on Windows this is
+ * problematic because Windows pipes are not selectable and only
+ * sockets are.
+ *
+ * This file aims to wrap around the required backend calls depending
+ * of the operating system.
+ *
+ * This file provides 3 interfaces:
+ *
+ * - flb_pipe_create : create a pair of connected file descriptors or sockets.
+ * - flb_pipe_destroy: destroy a pair of connected fds or sockets.
+ * - flb_pipe_close  : close individual end of a pipe.
+ *
+ * we need to handle a 'closer' handler because for Windows a file descriptor
+ * is not a socket.
+ */
+
+#include <fluent-bit/flb_pipe.h>
+
+#ifdef _WIN32
+
+/*
+ * Building on Windows means that Monkey library (lib/monkey) and it
+ * core runtime have been build with 'libevent' backend support, that
+ * library provide an abstraction to create a socketpairs.
+ *
+ * Creating a pipe on Fluent Bit @Windows, means create a socket pair.
+ */
+
+int flb_pipe_create(flb_pipefd_t pipefd[2])
+{
+    if (evutil_socketpair(AF_UNIX, SOCK_STREAM, 0, pipefd) == -1) {
+        perror("socketpair");
+        return -1;
+    }
+
+    return 0;
+}
+
+void flb_pipe_destroy(flb_pipefd_t pipefd[2])
+{
+    socketclose(pipefd[0]);
+    socketclose(pipefd[1]);
+}
+
+int flb_pipe_close(flb_pipefd_t fd)
+{
+    return close(fd);
+}
+
+#else
+/* All other flavors of Unix/BSD are OK */
+
+#include <unistd.h>
+#include <stdint.h>
+
+int flb_pipe_create(flb_pipefd_t pipefd[2])
+{
+    return pipe(pipefd);
+}
+
+void flb_pipe_destroy(flb_pipefd_t pipefd[2])
+{
+    close(pipefd[0]);
+    close(pipefd[1]);
+}
+
+int flb_pipe_close(flb_pipefd_t fd)
+{
+    return close(fd);
+}
+
+#endif
