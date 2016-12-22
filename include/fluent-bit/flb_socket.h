7722778fd5e0c80f7446eb02a2881d091e2c6cb6
Eduardo Silva <eduardo@treasure-data.com>
2016-12-22 12:32:47 -0600

    socket: new header to wrap socket data type (WIN32)
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

---
+++ include/fluent-bit/flb_socket.h
@@ -0,0 +1,36 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2015-2016 Treasure Data Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef FLB_SOCKET_H
+#define FLB_SOCKET_H
+
+#ifdef _WIN32
+#include <event.h>
+#define flb_sockfd_t         evutil_socket_t
+#define flb_socket_close(fd) evutil_closesocket(fd)
+
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define flb_sockfd_t         int
+#define flb_socket_close(fd) close(fd)
+#endif
+
+#endif
