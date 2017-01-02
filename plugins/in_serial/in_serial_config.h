c12d9f65e39ea23627e5e79cbe3b4c39bf128be7
Eduardo Silva <eduardo@treasure-data.com>
2017-01-02 10:18:51 -0600

    plugins: update Copyrights (2015-2017)
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- plugins/in_serial/in_serial_config.h
+++ plugins/in_serial/in_serial_config.h
@@ -3,7 +3,7 @@
 /*  Serial input plugin for Fluent Bit
  *  ==================================
  *  Copyright (C) 2015-2016 Takeshi HASEGAWA
- *  Copyright (C) 2015-2016 Treasure Data Inc.
+ *  Copyright (C) 2015-2017 Treasure Data Inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
