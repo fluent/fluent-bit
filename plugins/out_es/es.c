24e664109ea04b993130e551328916b69301af0f
Eduardo Silva <eduardo@treasure-data.com>
2017-02-26 00:39:52 -0600

    out_es: use new http client api
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- plugins/out_es/es.c
+++ plugins/out_es/es.c
@@ -361,7 +361,7 @@ void cb_es_flush(void *data, size_t bytes,
 
     /* Compose HTTP Client request */
     c = flb_http_client(u_conn, FLB_HTTP_POST, "/_bulk",
-                        pack, bytes_out, NULL, 0, NULL);
+                        pack, bytes_out, NULL, 0, NULL, 0);
     flb_http_add_header(c, "User-Agent", 10, "Fluent-Bit", 10);
     flb_http_add_header(c, "Content-Type", 12, "application/json", 16);
 
