551ce70575d09bbb5036cad143d336d9da75de42
Takahiro YAMASHITA <nokute78@gmail.com>
2017-04-12 23:05:51 +0900

    filter_record_modifier: add new plugin
    Signed-off-by: Takahiro YAMASHITA <nokute78@gmail.com>

---
+++ plugins/filter_record_modifier/filter_modifier.h
@@ -0,0 +1,47 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2015-2017 Treasure Data Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef FLB_FILTER_RECORD_MODIFIER_H
+#define FLB_FILTER_RECORD_MODIFIER_H
+
+struct modifier_record {
+    char *key;
+    char *val;
+    int  key_len;
+    int  val_len;
+    struct mk_list _head;
+};
+
+struct modifier_key {
+    char *key;
+    int   key_len;
+    struct mk_list _head;
+};
+
+struct record_modifier_ctx {
+    int records_num;
+    int remove_keys_num;
+    int whitelist_keys_num;
+    struct mk_list records;
+    struct mk_list remove_keys;
+    struct mk_list whitelist_keys;
+};
+
+
+#endif /* FLB_FILTER_RECORD_MODIFIER_H */
