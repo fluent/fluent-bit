ec882576ee730eab187220ec6ae1f00676171c9e
Eduardo Silva <eduardo@treasure-data.com>
2017-06-01 17:25:11 +0900

    out_es: set conten-type for Bulk API (#286)
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- plugins/out_es/es.c
+++ plugins/out_es/es.c
@@ -358,6 +358,7 @@ void cb_es_flush(void *data, size_t bytes,
     c = flb_http_client(u_conn, FLB_HTTP_POST, "/_bulk",
                         pack, bytes_out, NULL, 0, NULL, 0);
     flb_http_add_header(c, "User-Agent", 10, "Fluent-Bit", 10);
+    flb_http_add_header(c, "Content-Type", 12, "application/x-ndjson", 20);
 
     if (ctx->http_user && ctx->http_passwd) {
         flb_http_basic_auth(c, ctx->http_user, ctx->http_passwd);
