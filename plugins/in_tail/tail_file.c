1839d53433b21d3839f4c9c7f4dcb9618e0e98d3
Eduardo Silva <eduardo@treasure-data.com>
2017-06-26 11:28:25 -0600

    in_tail: do not mark as rotated files when using relative paths
    Sometimes the file names given can be relative paths but when checking
    for rotation when "promoting from static to event" mode, the absolute
    path is used, generating a 'rotation' mark internally.
    
    For such situations where monitored files are provided with relative
    paths, an extra check is done through the check of inode number, this
    prevent messy internal rotation because of a user-call error.
    
    note: this is to workaround problems generated by a bad configuration.
    
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- plugins/in_tail/tail_file.c
+++ plugins/in_tail/tail_file.c
@@ -597,6 +597,7 @@ int flb_tail_file_to_event(struct flb_tail_file *file)
     int ret;
     char *name;
     struct stat st;
+    struct stat st_rotated;
 
     /* Check if the file promoted have pending bytes */
     ret = fstat(file->fd, &st);
@@ -611,9 +612,15 @@ int flb_tail_file_to_event(struct flb_tail_file *file)
     /* Check if this file have been rotated */
     name = flb_tail_file_name(file);
     if (strcmp(name, file->name) != 0) {
-        flb_trace("[in_tail] static file rotated: %s => to %s",
-                  file->name, name);
-        flb_tail_file_rotated(file);
+        ret = stat(name, &st_rotated);
+        if (ret == -1) {
+            flb_errno();
+        }
+        else if (st_rotated.st_ino != st.st_ino) {
+            flb_trace("[in_tail] static file rotated: %s => to %s",
+                      file->name, name);
+            flb_tail_file_rotated(file);
+        }
     }
     flb_free(name);
 
