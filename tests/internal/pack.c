3b3c544ca667f0e9eb009e4bd0504376f54faa37
Eduardo Silva <eduardo@treasure-data.com>
2017-07-31 16:47:32 -0600

    tests: internal: pack: add UTF-8 to JSON encoding tests
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- tests/internal/pack.c
+++ tests/internal/pack.c
@@ -4,12 +4,30 @@
 #include <fluent-bit/flb_mem.h>
 #include <fluent-bit/flb_pack.h>
 #include <fluent-bit/flb_error.h>
+#include <fluent-bit/flb_str.h>
 #include <monkey/mk_core.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <unistd.h>
 
 #include "flb_tests_internal.h"
 
+/* JSON iteration tests */
 #define JSON_SINGLE_MAP1 FLB_TESTS_DATA_PATH "/data/pack/json_single_map_001.json"
 #define JSON_SINGLE_MAP2 FLB_TESTS_DATA_PATH "/data/pack/json_single_map_002.json"
+
+/* Pack Samples path */
+#define PACK_SAMPLES     FLB_TESTS_DATA_PATH "/data/pack/"
+
+struct pack_test {
+    char *msgpack;
+    char *json;
+};
+
+/* If we get more than 256 tests, just update the size */
+struct pack_test pt[256];
 
 static inline void consume_bytes(char *buf, int bytes, int length)
 {
@@ -218,10 +236,152 @@ void test_json_pack_mult_iter()
     flb_free(buf);
 }
 
+/* Iterate data/pack/ directory and compose an array with files to test */
+static int utf8_tests_create()
+{
+    int i = 0;
+    int len;
+    int ret;
+    char ext_mp[PATH_MAX];
+    char ext_json[PATH_MAX];
+    DIR *dir;
+    struct pack_test *test;
+    struct dirent *entry;
+    struct stat st;
+
+    memset(pt, '\0', sizeof(pt));
+
+    dir = opendir(PACK_SAMPLES);
+    TEST_CHECK(dir != NULL);
+    if (dir == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type != DT_REG) {
+            continue;
+        }
+
+        len = strlen(entry->d_name);
+        if (strcmp(entry->d_name + (len - 3), ".mp") != 0) {
+            continue;
+        }
+
+        snprintf(ext_mp, sizeof(ext_mp) - 1,
+                 "%s%s", PACK_SAMPLES, entry->d_name);
+        len = snprintf(ext_json, sizeof(ext_json) - 1, "%s%s",
+                       PACK_SAMPLES, entry->d_name);
+        snprintf(ext_json + (len - 3), sizeof(ext_json) - len - 3,
+                 "%s", ".json");
+
+
+        /* Validate new paths */
+        ret = stat(ext_mp, &st);
+        if (ret == -1) {
+            printf("Unit test msgpack not found: %s\n", ext_mp);
+            exit(EXIT_FAILURE);
+        }
+
+        ret = stat(ext_json, &st);
+        if (ret == -1) {
+            printf("Unit test result JSON not found: %s\n", ext_json);
+            exit(EXIT_FAILURE);
+        }
+
+        /* Insert into table */
+        test = &pt[i];
+        test->msgpack = flb_strdup(ext_mp);
+        test->json    = flb_strdup(ext_json);
+        i++;
+    }
+
+    closedir(dir);
+    return i;
+}
+
+static void utf8_tests_destroy(int s)
+{
+    int i;
+    struct pack_test *test;
+
+    for (i = 0; i < s; i++) {
+        test = &pt[i];
+        flb_free(test->msgpack);
+        flb_free(test->json);
+    }
+}
+
+void test_utf8_to_json()
+{
+    int i;
+    int ret;
+    int n_tests;
+    char *file_msgp;
+    char *file_json;
+    char *out_buf;
+    size_t out_size;
+    size_t msgp_size;
+    size_t json_size;
+    struct stat st;
+    struct pack_test *test;
+
+    n_tests = utf8_tests_create();
+
+    /* Iterate unit tests table */
+    for (i = 0; i < n_tests; i++) {
+        test = &pt[i];
+        if (!test->msgpack) {
+            break;
+        }
+
+        file_msgp = mk_file_to_buffer(test->msgpack);
+        TEST_CHECK(file_msgp != NULL);
+        stat(test->msgpack, &st);
+        msgp_size = st.st_size;
+
+        file_json = mk_file_to_buffer(test->json);
+        TEST_CHECK(file_json != NULL);
+        if (!file_json) {
+            printf("Missing JSON file: %s\n", test->json);
+            flb_free(file_msgp);
+            continue;
+        }
+
+        json_size = strlen(file_json);
+
+        out_buf = NULL;
+        ret = flb_msgpack_raw_to_json_str(file_msgp, msgp_size,
+                                          &out_buf, &out_size);
+        TEST_CHECK(ret == 0);
+
+        ret = strcmp(file_json, out_buf);
+        if (ret != 0) {
+            TEST_CHECK(ret == 0);
+            printf("[test] %s\n", test->json);
+            printf("       EXPECTED => '%s'\n", file_json);
+            printf("       ENCODED  => '%s'\n", out_buf);
+        }
+
+        TEST_CHECK(out_size == json_size);
+
+        if (out_buf) {
+            flb_free(out_buf);
+        }
+        flb_free(file_msgp);
+        flb_free(file_json);
+    }
+
+    utf8_tests_destroy(n_tests);
+}
+
 TEST_LIST = {
+    /* JSON maps iteration */
     { "json_pack", test_json_pack },
     { "json_pack_iter", test_json_pack_iter},
     { "json_pack_mult", test_json_pack_mult},
     { "json_pack_mult_iter", test_json_pack_mult_iter},
+
+    /* Mixed bytes, check JSON encoding */
+    { "utf8_to_json", test_utf8_to_json},
     { 0 }
 };
