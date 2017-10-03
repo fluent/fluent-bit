1f0b15baad04b748b0731a8dc7220ef0cc1b8ea5
Eduardo Silva <eduardo@treasure-data.com>
2017-10-03 14:00:24 -0600

    build: cmake: new FindMonkey helper
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

---
+++ cmake/FindMonkey.cmake
@@ -0,0 +1,19 @@
+# Try to find Monkey HTTP Server core/library
+# ===========================================
+#
+#   http://monkey-project.com
+#
+# Definitions:
+#
+#  - MONKEY_FOUND      : source code found
+#  - MONKEY_INCLUDE_DIR: root include directory
+
+unset(MONKEY_INCLUDE_DIR CACHE)
+find_path(MONKEY_INCLUDE_DIR
+  NAMES monkey/mk_core.h monkey/mk_lib.h
+  PATHS ${PROJECT_SOURCE_DIR}/lib/monkey/include
+  )
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Monkey DEFAULT_MSG MONKEY_INCLUDE_DIR)
+include(FeatureSummary)
