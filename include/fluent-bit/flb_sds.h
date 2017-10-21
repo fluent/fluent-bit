65ca646293cfbc76532a794bcacfcb71ec92cae4
Eduardo Silva <eduardo@treasure-data.com>
2017-10-20 20:27:20 -0500

    sds: new dynamic string management interface
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

---
+++ include/fluent-bit/flb_sds.h
@@ -0,0 +1,69 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2015-2017 Treasure Data Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*
+ * The following SDS interface is a clone/strip-down version of the original
+ * SDS library created by Antirez at https://github.com/antirez/sds.
+ */
+
+#ifndef FLB_SDS_H
+#define FLB_SDS_H
+
+#include <fluent-bit/flb_info.h>
+
+#include <stdlib.h>
+#include <inttypes.h>
+
+#define FLB_SDS_HEADER_SIZE (sizeof(uint64_t) + sizeof(uint64_t))
+
+typedef char *flb_sds_t;
+
+struct flb_sds {
+    uint64_t len;        /* used */
+    uint64_t alloc;      /* excluding the header and null terminator */
+    char buf[];
+} __attribute__ ((__packed__));
+
+#define FLB_SDS_HEADER(s)  ((struct flb_sds *) (s - FLB_SDS_HEADER_SIZE))
+
+static inline size_t flb_sds_len(flb_sds_t s)
+{
+    return FLB_SDS_HEADER(s)->len;
+}
+
+static inline size_t flb_sds_alloc(flb_sds_t s)
+{
+    return FLB_SDS_HEADER(s)->alloc;
+}
+
+static inline size_t flb_sds_avail(flb_sds_t s)
+{
+    struct flb_sds *h;
+
+    h = FLB_SDS_HEADER(s);
+    return (h->alloc - h->len);
+}
+
+flb_sds_t flb_sds_create(char *str);
+flb_sds_t flb_sds_create_len(char *str, int len);
+flb_sds_t flb_sds_create_size(size_t size);
+flb_sds_t flb_sds_cat(flb_sds_t s, char *str, int len);
+int flb_sds_destroy(flb_sds_t s);
+
+#endif
---
+++ src/flb_sds.c
@@ -0,0 +1,145 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2015-2017 Treasure Data Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*
+ * The following SDS interface is a clone/strip-down version of the original
+ * SDS library created by Antirez at https://github.com/antirez/sds.
+ */
+
+#include <fluent-bit/flb_info.h>
+#include <fluent-bit/flb_mem.h>
+#include <fluent-bit/flb_log.h>
+#include <fluent-bit/flb_sds.h>
+
+static flb_sds_t sds_alloc(size_t size)
+{
+    void *buf;
+    flb_sds_t s;
+    struct flb_sds *head;
+
+    buf = flb_malloc(FLB_SDS_HEADER_SIZE + size + 1);
+    if (!buf) {
+        flb_errno();
+        return NULL;
+    }
+
+    head = buf;
+    head->len = 0;
+    head->alloc = size;
+
+    s = head->buf;
+    *s = '\0';
+
+    return s;
+}
+
+flb_sds_t flb_sds_create_len(char *str, int len)
+{
+    flb_sds_t s;
+    struct flb_sds *head;
+
+    s = sds_alloc(len);
+    if (str) {
+        memcpy(s, str, len);
+        s[len] = '\0';
+
+        head = FLB_SDS_HEADER(s);
+        head->len = len;
+    }
+    return s;
+}
+
+flb_sds_t flb_sds_create(char *str)
+{
+    size_t len;
+
+    if (!str) {
+        len = 0;
+    }
+    else {
+        len = strlen(str);
+    }
+
+    return flb_sds_create_len(str, len);
+}
+
+flb_sds_t flb_sds_create_size(size_t size)
+{
+    return sds_alloc(size);
+}
+
+/* Increase SDS buffer size 'len' bytes */
+flb_sds_t flb_sds_increase(flb_sds_t s, size_t len)
+{
+    size_t avail;
+    size_t new_size;
+    struct flb_sds *cur;
+    void *tmp;
+
+    avail = flb_sds_avail(s);
+    if (avail >= len) {
+        return s;
+    }
+
+    new_size = (FLB_SDS_HEADER_SIZE + flb_sds_len(s) + len + 1);
+    cur = FLB_SDS_HEADER(s);
+    tmp = flb_realloc(cur, new_size);
+    if (!tmp) {
+        flb_errno();
+        return NULL;
+    }
+
+    cur = tmp;
+    cur->alloc += len;
+    s = cur->buf;
+    return s;
+}
+
+flb_sds_t flb_sds_cat(flb_sds_t s, char *str, int len)
+{
+    size_t avail;
+    struct flb_sds *head;
+    flb_sds_t tmp;
+
+    avail = flb_sds_avail(s);
+    if (avail < len) {
+        tmp = flb_sds_increase(s, len);
+        if (!tmp) {
+            return NULL;
+        }
+        s = tmp;
+    }
+
+    head = FLB_SDS_HEADER(s);
+    memcpy(s + flb_sds_len(s), str, len);
+    head->len += len;
+    s[head->len] = '\0';
+
+    return s;
+}
+
+int flb_sds_destroy(flb_sds_t s)
+{
+    struct flb_sds *head;
+
+    head = FLB_SDS_HEADER(s);
+    flb_free(head);
+
+    return 0;
+}
