4e17598ac274793dad73c2f28e510f10871b092f
Eduardo Silva <eduardo@treasure-data.com>
2017-10-31 14:35:16 -0600

    in_tail: validate static file status on promotion (#409)
    the plugin have two main stages: static and events. On static mode
    it parse content files when it start, but then when it reach EOF it
    promote the files to 'events', meaning that will use the inotify
    mechanism to get file modifications, rotations, etc.
    
    If a monitored file in static mode is deleted, the promotion to events
    did not validate properly, upon error the expected behavior is to
    unregister the problematic file. This patch fix the issue.
    
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- plugins/in_tail/tail_file_internal.h
+++ plugins/in_tail/tail_file_internal.h
@@ -35,7 +35,7 @@ struct flb_tail_file {
     off_t offset;
     off_t last_line;
     ino_t inode;
-    char *name;
+    char *name;                 /* target file name */
     size_t name_len;
     time_t rotated;
     off_t pending_bytes;
