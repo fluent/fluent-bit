fc8169ff2ac8e41fa82e6842c9de6752b5e08205
Takahiro YAMASHITA <nokute78@gmail.com>
2017-11-02 09:32:24 +0900

    in_exec: add new plugin in_exec (#411)
    Signed-off-by: Takahiro YAMASHITA <nokute78@gmail.com>

---
+++ plugins/in_exec/in_exec.h
@@ -0,0 +1,40 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2015-2017 Treasure Data Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+#ifndef FLB_IN_EXEC_H
+#define FLB_IN_EXEC_H
+
+#include <fluent-bit/flb_config.h>
+#include <fluent-bit/flb_input.h>
+#include <fluent-bit/flb_utils.h>
+#include <fluent-bit/flb_parser.h>
+
+#include <msgpack.h>
+
+#define DEFAULT_BUF_SIZE      4096
+#define DEFAULT_INTERVAL_SEC  1
+#define DEFAULT_INTERVAL_NSEC 0
+
+struct flb_in_exec_config {
+    char  *cmd;
+    struct flb_parser  *parser;
+};
+
+extern struct flb_input_plugin in_exec_plugin;
+
+#endif /* FLB_IN_EXEC_H */
