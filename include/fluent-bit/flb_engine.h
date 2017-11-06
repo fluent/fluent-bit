42979e003a96fe40afc1f310f4e400c1b21ddaf6
Eduardo Silva <eduardo@treasure-data.com>
2017-11-06 10:48:24 -0600

    engine: new FLB_ENGINE_FAILED signal
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- include/fluent-bit/flb_engine.h
+++ include/fluent-bit/flb_engine.h
@@ -35,12 +35,14 @@
 
 /* Engine events: all engine events set the left 32 bits to '1' */
 #define FLB_ENGINE_EV_STARTED   FLB_BITS_U64_SET(1, 1) /* Engine started    */
-#define FLB_ENGINE_EV_STOP      FLB_BITS_U64_SET(1, 2) /* Requested to stop */
-#define FLB_ENGINE_EV_SHUTDOWN  FLB_BITS_U64_SET(1, 3) /* Engine shutdown   */
-#define FLB_ENGINE_EV_STATS     FLB_BITS_U64_SET(1, 4) /* Collect stats     */
+#define FLB_ENGINE_EV_FAILED    FLB_BITS_U64_SET(1, 2) /* Engine started    */
+#define FLB_ENGINE_EV_STOP      FLB_BITS_U64_SET(1, 3) /* Requested to stop */
+#define FLB_ENGINE_EV_SHUTDOWN  FLB_BITS_U64_SET(1, 4) /* Engine shutdown   */
+#define FLB_ENGINE_EV_STATS     FLB_BITS_U64_SET(1, 5) /* Collect stats     */
 
 /* Similar to engine events, but used as return values */
 #define FLB_ENGINE_STARTED      FLB_BITS_U64_LOW(FLB_ENGINE_EV_STARTED)
+#define FLB_ENGINE_FAILED       FLB_BITS_U64_LOW(FLB_ENGINE_EV_FAILED)
 #define FLB_ENGINE_STOP         FLB_BITS_U64_LOW(FLB_ENGINE_EV_STOP)
 #define FLB_ENGINE_SHUTDOWN     FLB_BITS_U64_LOW(FLB_ENGINE_EV_SHUTDOWN)
 #define FLB_ENGINE_STATS        FLB_BITS_U64_LOW(FLB_ENGINE_EV_STATS)
@@ -54,6 +56,7 @@
 #endif
 
 int flb_engine_start(struct flb_config *config);
+int flb_engine_failed(struct flb_config *config);
 int flb_engine_flush(struct flb_config *config,
                      struct flb_input_plugin *in_force);
 int flb_engine_exit(struct flb_config *config);
