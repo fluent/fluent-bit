6a28826801d9d387b5c9e5d4a6f0c548bd998087
Eduardo Silva <eduardo@treasure-data.com>
2017-11-07 11:38:37 -0600

    tests: runtime: filter_kubernetes: cleanup
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- tests/runtime/filter_kubernetes.c
+++ tests/runtime/filter_kubernetes.c
@@ -3,19 +3,6 @@
 #include <fluent-bit.h>
 #include "flb_tests_runtime.h"
 
-/* Test data */
-
-/* Test functions */
-void flb_test_filter_kubernetes_merged_json(void);
-void flb_test_filter_kubernetes_merged_json_with_invalid_json(void);
-
-/* Test list */
-TEST_LIST = {
-    {"kubernetes_merged_json", flb_test_filter_kubernetes_merged_json },
-    {"kubernetes_merged_json_with_invalid_json", flb_test_filter_kubernetes_merged_json_with_invalid_json },
-    {NULL, NULL}
-};
-
 pthread_mutex_t result_mutex = PTHREAD_MUTEX_INITIALIZER;
 char *output = NULL;
 
@@ -47,7 +34,7 @@ int callback_test(void* data, size_t size)
 }
 
 
-void flb_test_filter_kubernetes_merged_json(void)
+void flb_test_kube_merged_json()
 {
     int ret;
     int bytes;
@@ -59,24 +46,39 @@ void flb_test_filter_kubernetes_merged_json(void)
 
     ctx = flb_create();
 
-    in_ffd = flb_input(ctx, (char *) "lib", NULL);
-    TEST_CHECK(in_ffd >= 0);
-    flb_input_set(ctx, in_ffd, "Tag", "test", NULL);
-
-    out_ffd = flb_output(ctx, (char *) "lib", (void*)callback_test);
-    TEST_CHECK(out_ffd >= 0);
-    flb_output_set(ctx, out_ffd, "Match", "*", "format", "json", NULL);
-
+    /* Configure service */
     flb_service_set(ctx, "Flush", "1", "Log_Level", "debug", NULL);
 
+    /* Input */
+    in_ffd = flb_input(ctx, (char *) "lib", NULL);
+    TEST_CHECK(in_ffd >= 0);
+    flb_input_set(ctx, in_ffd,
+                  "Tag", "test",
+                  NULL);
+
+    /* Fiter */
     filter_ffd = flb_filter(ctx, (char *) "kubernetes", NULL);
     TEST_CHECK(filter_ffd >= 0);
-    ret = flb_filter_set(ctx, filter_ffd, "Match", "test", "dummy_meta", "true", "Merge_JSON_Log", "On", NULL);
+    ret = flb_filter_set(ctx, filter_ffd,
+                         "Match", "test",
+                         "dummy_meta", "true",
+                         "Merge_JSON_Log", "On",
+                         NULL);
     TEST_CHECK(ret == 0);
 
+    /* Output */
+    out_ffd = flb_output(ctx, (char *) "lib", (void*)callback_test);
+    TEST_CHECK(out_ffd >= 0);
+    flb_output_set(ctx, out_ffd,
+                   "Match", "*",
+                   "format", "json",
+                   NULL);
+
+    /* Start the engine */
     ret = flb_start(ctx);
     TEST_CHECK(ret == 0);
 
+    /* Ingest data */
     p = "[1448403340, {\"log\":\"{\\\"@timestamp\\\":\\\"2017-11-01T22:25:21.648+00:00\\\",\\\"message\\\":\\\"Started admin@53830483{HTTP/1.1,[http/1.1]}{0.0.0.0:8081}\\\",\\\"logger_name\\\":\\\"org.eclipse.jetty.server.AbstractConnector\\\",\\\"thread_name\\\":\\\"main\\\",\\\"level\\\":\\\"INFO\\\",\\\"level_value\\\":20000}\\n\",\"stream\":\"stdout\",\"time\":\"2017-11-01 T22:25:21.648509972Z\"}]";
     bytes = flb_lib_push(ctx, in_ffd, p, strlen(p));
     TEST_CHECK(bytes == strlen(p));
@@ -99,7 +101,7 @@ void flb_test_filter_kubernetes_merged_json(void)
     flb_destroy(ctx);
 }
 
-void flb_test_filter_kubernetes_merged_json_with_invalid_json(void)
+void flb_test_kube_merged_json_with_invalid_json()
 {
     int ret;
     int bytes;
@@ -146,3 +148,10 @@ void flb_test_filter_kubernetes_merged_json_with_invalid_json(void)
     flb_stop(ctx);
     flb_destroy(ctx);
 }
+
+
+TEST_LIST = {
+    {"kube_merged_json", flb_test_kube_merged_json },
+    {"kube_merged_json_with_invalid_json", flb_test_kube_merged_json_with_invalid_json },
+    {NULL, NULL}
+};
