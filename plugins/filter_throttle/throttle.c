a42fe5531f2fdae0271645700bfa3f8908de6f1c
Yaroslav Molochko <y.molochko@anchorfree.com>
2017-11-17 18:26:02 +0200

    Add periodict timer

--- plugins/filter_throttle/throttle.c
+++ plugins/filter_throttle/throttle.c
@@ -34,14 +34,37 @@
 #include "window.h"
 
 
-/* Given a msgpack record, do some filter action based on the defined rules */
-static inline int throttle_data(struct flb_filter_throttle_ctx *ctx, long timestamp)
+struct ticker {
+    struct flb_filter_throttle_ctx *ctx;
+    bool done;
+};
+
+void *time_ticker(void *args)
 {
-    if (timestamp == ctx->hash->current_timestamp && ctx->hash->total / ctx->hash->size > ctx->max_rate) {
+    struct ticker *t_ctx = args;
+    struct flb_time ftm;
+    long timestamp;
+
+    while (!t_ctx->done) {
+        flb_time_get(&ftm);
+        timestamp = flb_time_to_double(&ftm);
+        window_add(t_ctx->ctx->hash, timestamp, 0);
+
+        t_ctx->ctx->hash->current_timestamp = timestamp;
+
+        flb_error("current time is: %i", timestamp);
+        sleep(1);
+    }
+}
+
+/* Given a msgpack record, do some filter action based on the defined rules */
+static inline int throttle_data(struct flb_filter_throttle_ctx *ctx)
+{
+    if ( ctx->hash->total / ctx->hash->size > ctx->max_rate) {
         return THROTTLE_RET_DROP;
     }
 
-    window_add(ctx->hash, timestamp, 1);
+    window_add(ctx->hash, ctx->hash->current_timestamp, 1);
 
     flb_debug("[filter_throttle] limist is %f per sec in window %i sec, current rate is: %i per sec", ctx->max_rate, ctx->window_size, ctx->hash->total / ctx->hash->size);
 
@@ -80,6 +103,8 @@ static int cb_throttle_init(struct flb_filter_instance *f_ins,
 {
     int ret;
     struct flb_filter_throttle_ctx *ctx;
+    pthread_t tid;
+    struct ticker *ticker_ctx;
 
     /* Create context */
     ctx = flb_malloc(sizeof(struct flb_filter_throttle_ctx));
@@ -99,6 +124,11 @@ static int cb_throttle_init(struct flb_filter_instance *f_ins,
 
     /* Set our context */
     flb_filter_set_context(f_ins, ctx);
+
+    ticker_ctx = flb_malloc(sizeof(struct ticker));
+    ticker_ctx->ctx = ctx;
+    ticker_ctx->done = false;
+    pthread_create(&tid, NULL, &time_ticker, ticker_ctx);
     return 0;
 }
 
@@ -120,15 +150,10 @@ static int cb_throttle_filter(void *data, size_t bytes,
     msgpack_sbuffer tmp_sbuf;
     msgpack_packer tmp_pck;
 
-    struct flb_time ftm;
-    long timestamp;
-
     /* Create temporal msgpack buffer */
     msgpack_sbuffer_init(&tmp_sbuf);
     msgpack_packer_init(&tmp_pck, &tmp_sbuf, msgpack_sbuffer_write);
 
-    flb_time_get(&ftm);
-    timestamp = flb_time_to_double(&ftm);
 
     /* Iterate each item array and apply rules */
     msgpack_unpacked_init(&result);
@@ -140,7 +165,7 @@ static int cb_throttle_filter(void *data, size_t bytes,
 
         old_size++;
 
-        ret = throttle_data(context, timestamp);
+        ret = throttle_data(context);
         if (ret == THROTTLE_RET_KEEP) {
             msgpack_pack_object(&tmp_pck, root);
             new_size++;
