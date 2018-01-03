69c6e0e84279129cdf6f0460975a05b16d97590b
Eduardo Silva <eduardo@treasure-data.com>
2018-01-02 18:50:22 -0600

    core: update copyright header
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- src/flb_scheduler.c
+++ src/flb_scheduler.c
@@ -3,7 +3,7 @@
 
 /*  Fluent Bit
  *  ==========
- *  Copyright (C) 2015-2017 Treasure Data Inc.
+ *  Copyright (C) 2015-2018 Treasure Data Inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
