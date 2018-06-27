7a5b47eee6b3ab03acd81c26fe63c8e9317f78ac
Michiel Kalkman <michiel@nosuchtype.com>
2018-06-28 01:56:39 +1000

    filter_nest: Wildcards and prefix options (#654)
    This adds support to `filter_nest` for,
    
     - Multiple wildcards in a single filter instance
     - `Add_prefix` for both nest and lift operations
     - `Remove_prefix` for both nest and lift operations
    
    Signed-off-by: Michiel Kalkman <michiel@nosuchtype.com>

--- plugins/filter_nest/nest.h
+++ plugins/filter_nest/nest.h
@@ -28,18 +28,24 @@ enum FILTER_NEST_OPERATION {
 struct filter_nest_ctx
 {
     enum FILTER_NEST_OPERATION operation;
+    char *key;
+    int key_len;
+    char *prefix;
+    int prefix_len;
     // nest
-    char *nesting_key;
-    int nesting_key_len;
-    char *wildcard;
-    int wildcard_len;
-    bool wildcard_is_dynamic;
+    struct mk_list wildcards;
+    int wildcards_cnt;
+    bool remove_prefix;
     // lift
-    char *nested_under;
-    int nested_under_len;
-    char *prefix_with;
-    int prefix_with_len;
-    bool use_prefix;
+    bool add_prefix;
+};
+
+struct filter_nest_wildcard
+{
+    char *key;
+    int key_len;
+    bool key_is_dynamic;
+    struct mk_list _head;
 };
 
 #endif
