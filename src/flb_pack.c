d137d52619fe684ac6ecc9db85c7f55dab0fc4cc
Eduardo Silva <eduardo@treasure-data.com>
2018-08-06 10:18:26 -0600

    pack: deprecate flb_pack_json_valid() (#691)
    JSMN in strict mode cannot be used to validate a JSON string without
    filing the tokens, it behavior is unexpected, more details about this
    in the following jsmn pull request:
    
        https://github.com/zserge/jsmn/pull/119#issuecomment-410583885
    
    this patch remove the flb_pack_json_valid() function.
    
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- src/flb_pack.c
+++ src/flb_pack.c
@@ -211,20 +211,6 @@ int flb_pack_json(char *js, size_t len, char **buffer, size_t *size)
     return ret;
 }
 
-int flb_pack_json_valid(char *json, size_t len)
-{
-    int ret;
-    jsmn_parser parser;
-
-    jsmn_init(&parser);
-    ret = jsmn_parse(&parser, json, len, NULL, 0);
-    if (ret <= 0) {
-        return -1;
-    }
-
-    return 0;
-}
-
 /* Initialize a JSON packer state */
 int flb_pack_state_init(struct flb_pack_state *s)
 {
