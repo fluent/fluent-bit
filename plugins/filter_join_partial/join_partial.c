0cc8e9977a044f9f89587994c87d7920af11eb55
Jonas Pettersson <jonas.pettersson@sparebank1.no>
2018-10-31 08:57:26 +0100

    filter_join_partial: updated copyright
    Signed-off-by: Jonas Pettersson <jonas.pettersson@sparebank1.no>

--- plugins/filter_join_partial/join_partial.c
+++ plugins/filter_join_partial/join_partial.c
@@ -2,7 +2,7 @@
 
 /*  Fluent Bit
  *  ==========
- *  Copyright (C) 2018 SpareBank 1 Banksamarbeidet DA
+ *  Copyright (C) 2015-2018 Treasure Data Inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
