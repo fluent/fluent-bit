2896fccb6e49cce8886268a548d25cc6862164f2
Hiroshi Hatake <hatake@clear-code.com>
2018-12-05 11:21:04 +0900

    cio_meta: Avoid to use MREMAP_MAYMOVE and mremap on macOS
    Because they are not implemented on macOS.
    
    Signed-off-by: Hiroshi Hatake <hatake@clear-code.com>

--- lib/chunkio/src/cio_file.c
+++ lib/chunkio/src/cio_file.c
@@ -415,8 +415,16 @@ int cio_file_write(struct cio_chunk *ch, const void *buf, size_t count)
             return -1;
         }
 
+        /* OSX mman does not implement mremap or MREMAP_MAYMOVE. */
+#ifndef MREMAP_MAYMOVE
+        if (munmap(cf->data_size, av_size) == -1)
+            return -1;
+
+        tmp = mmap(0, new_size, PROT_READ | PROT_WRITE, MAP_SHARED, cf->fd, 0);
+#else
         tmp = mremap(cf->map, cf->alloc_size,
                      new_size, MREMAP_MAYMOVE);
+#endif
         if (tmp == MAP_FAILED) {
             cio_errno();
             cio_log_error(ch->ctx,
