7f8d5ddab49c8d02e328ee95c7bbc89aa7e354a7
Fujimoto Seiji <fujimoto@clear-code.com>
2018-12-12 12:35:41 +0900

    sosreport: make flb_sosreport() work for Windows
    Since we have neither uname() nor sysconf() on Windows, we need to
    use Win32 API to retrieve the equivalent information.
    
    With this patch, I can confirm that the following command runs fine
    on Windows.
    
        PS> fluent-bit -S
    
    Signed-off-by: Fujimoto Seiji <fujimoto@clear-code.com>

--- src/flb_sosreport.c
+++ src/flb_sosreport.c
@@ -28,7 +28,9 @@
 #include <fluent-bit/flb_version.h>
 #include <fluent-bit/flb_utils.h>
 
+#ifndef _MSC_VER
 #include <sys/utsname.h>
+#endif
 
 static void print_key(char *key)
 {
@@ -105,6 +107,62 @@ static void print_properties(struct mk_list *props)
     }
 }
 
+#ifdef _MSC_VER
+/* A definition table of SYSTEM_INFO.wProcessorArchitecture.
+ *
+ * This is a streight-forward translation of the official manual.
+ * https://docs.microsoft.com/en-us/windows/desktop/api/sysinfoapi/
+ */
+static const char* win32_arch(int archid)
+{
+    switch(archid)
+    {
+        case PROCESSOR_ARCHITECTURE_AMD64:
+            return "x64 (AMD or Intel)";
+        case PROCESSOR_ARCHITECTURE_ARM:
+            return "ARM";
+        case PROCESSOR_ARCHITECTURE_ARM64:
+            return "ARM64";
+        case PROCESSOR_ARCHITECTURE_IA64:
+            return "Intel Itanium-based";
+        case PROCESSOR_ARCHITECTURE_INTEL:
+            return "x86";
+        case PROCESSOR_ARCHITECTURE_UNKNOWN:
+        default:
+            return "unknown";
+    }
+}
+
+static void win32_operating_system_info()
+{
+    OSVERSIONINFOA win32os;
+
+    /* TODO Support "Application Manifest". Windows 10 reports a wrong
+     * version info if we do not manifest the supported OS.
+     * https://blogs.msdn.microsoft.com/chuckw/2013/09/10/manifest-madness/
+     */
+    win32os.dwOSVersionInfoSize = sizeof(OSVERSIONINFOA);
+    GetVersionExA(&win32os);
+
+    printf("[Operating System]\n");
+    printf("    Name\t\tWindows\n");
+    printf("    Version\t\t%i.%i\n", win32os.dwMajorVersion, win32os.dwMinorVersion);
+    printf("    Build\t\t%i\n", win32os.dwBuildNumber);
+    printf("\n");
+}
+
+static void win32_hardware_info()
+{
+    SYSTEM_INFO win32info;
+
+    GetNativeSystemInfo(&win32info);
+    printf("[Hardware]\n");
+    printf("    Architecture\t%s\n", win32_arch(win32info.wProcessorArchitecture));
+    printf("    Processors\t\t%i\n", win32info.dwNumberOfProcessors);
+    printf("\n");
+}
+#endif
+
 int flb_sosreport(struct flb_config *config)
 {
     char tmp[32];
@@ -117,7 +175,6 @@ int flb_sosreport(struct flb_config *config)
     struct flb_filter_instance *ins_filter;
     struct flb_output_instance *ins_out;
     struct flb_router_path *route;
-    struct utsname uts;
 
     printf("\n");
     printf("Fluent Bit Enterprise - SOS Report\n");
@@ -138,8 +195,11 @@ int flb_sosreport(struct flb_config *config)
     printf("    Built Flags\t\t%s\n", FLB_INFO_FLAGS);
     printf("\n");
 
-    /* Operating System */
+#ifndef _MSC_VER
+    struct utsname uts;
     uname(&uts);
+
+    /* Operating System */
     printf("[Operating System]\n");
     printf("    Name\t\t%s\n", uts.sysname);
     printf("    Release\t\t%s\n", uts.release);
@@ -151,6 +211,10 @@ int flb_sosreport(struct flb_config *config)
     printf("    Architecture\t%s\n", uts.machine);
     printf("    Processors\t\t%i\n",  (int) sysconf(_SC_NPROCESSORS_ONLN));
     printf("\n");
+#else
+    win32_operating_system_info();
+    win32_hardware_info();
+#endif
 
     /* Fluent Bit */
     printf("[Built Plugins]\n");
