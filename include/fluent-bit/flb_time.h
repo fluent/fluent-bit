b7f4757d73692851a0c3cbaa3fcdda20619871b5
Eduardo Silva <eduardo@treasure-data.com>
2019-01-08 16:33:08 -0600

    time: export flb_time_msleep()
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- include/fluent-bit/flb_time.h
+++ include/fluent-bit/flb_time.h
@@ -73,6 +73,7 @@ static inline int flb_time_equal(struct flb_time *t0, struct flb_time *t1) {
 }
 
 int flb_time_get(struct flb_time *tm);
+int flb_time_msleep(uint32_t ms);
 double flb_time_to_double(struct flb_time *tm);
 int flb_time_diff(struct flb_time *time1,
                   struct flb_time *time0, struct flb_time *result);
