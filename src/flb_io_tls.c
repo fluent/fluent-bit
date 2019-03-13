1731cfc87a964ba17bab77f553171a86a461533c
Fujimoto Seiji <fujimoto@clear-code.com>
2019-03-13 17:16:12 +0900

    io_tls: load CA certificates using Crypto API on Windows
    Previously, we tried to load certificates from /etc/ssl/certs if
    neither cert_file nor cert_path was specified. This simply does not
    work on Windows, since Windows stores certificates in the registry.
    
    This implemnts a Windows-specific routine to load system CA certs
    based on Wincrypt.h.
    
    Signed-off-by: Fujimoto Seiji <fujimoto@clear-code.com>

--- src/flb_io_tls.c
+++ src/flb_io_tls.c
@@ -26,6 +26,7 @@
 #include <mbedtls/error.h>
 
 #include <monkey/mk_core.h>
+#include <fluent-bit/flb_compat.h>
 #include <fluent-bit/flb_info.h>
 #include <fluent-bit/flb_mem.h>
 #include <fluent-bit/flb_io.h>
@@ -71,6 +72,51 @@ static inline int io_tls_event_switch(struct flb_upstream_conn *u_conn,
     return 0;
 }
 
+#ifdef _MSC_VER
+static int flb_tls_load_system_cert(struct flb_tls_context *ctx)
+{
+    int ret;
+    HANDLE h;
+    PCCERT_CONTEXT cert = NULL;
+
+    h = CertOpenSystemStoreA(NULL, "Root");
+    if (h == NULL) {
+        flb_error("[TLS] Cannot open cert store: %i", GetLastError());
+        return -1;
+    }
+
+    while (cert = CertEnumCertificatesInStore(h, cert)) {
+        if (cert->dwCertEncodingType & X509_ASN_ENCODING) {
+            ret = mbedtls_x509_crt_parse(&ctx->ca_cert,
+                                         cert->pbCertEncoded,
+                                         cert->cbCertEncoded);
+            if (ret) {
+                flb_debug("[TLS] cannot parse a certificate. skipping...");
+            }
+        }
+    }
+
+    if (!CertCloseStore(h, 0)) {
+        flb_error("[TLS] Cannot close cert store: %i", GetLastError());
+        return -1;
+    }
+    return 0;
+}
+#else
+static int flb_tls_load_system_cert(struct flb_tls_context *ctx)
+{
+    int ret;
+    const char ca_path[] = "/etc/ssl/certs/";
+
+    ret = mbedtls_x509_crt_parse_path(&ctx->ca_cert, ca_path);
+    if (ret < 0) {
+        flb_error("[TLS] Cannot read certificates from %s", ca_path);
+        return -1;
+    }
+    return 0;
+}
+#endif
+
 struct flb_tls_context *flb_tls_context_new(int verify,
                                             int debug,
                                             char *ca_path,
@@ -111,14 +157,17 @@ struct flb_tls_context *flb_tls_context_new(int verify,
             goto error;
         }
     }
-    else {
-        if (!ca_path) {
-            ca_path = "/etc/ssl/certs/";
-        }
+    else if (ca_file) {
         ret = mbedtls_x509_crt_parse_path(&ctx->ca_cert, ca_path);
         if (ret < 0) {
             io_tls_error(ret);
             flb_error("[TLS] error reading certificates from %s", ca_path);
+            goto error;
+        }
+    }
+    else {
+        ret = flb_tls_load_system_cert(ctx);
+        if (ret < 0) {
             goto error;
         }
     }
