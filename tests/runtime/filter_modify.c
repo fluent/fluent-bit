e8876784a87d8c6f02af75be6b64de6d2d634c9c
Eduardo Silva <eduardo@treasure-data.com>
2019-03-26 12:03:09 -0600

    tests: runtime: filter_modify: implement Commands unit tests
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- tests/runtime/filter_modify.c
+++ tests/runtime/filter_modify.c
@@ -3,88 +3,615 @@
 #include <fluent-bit.h>
 #include "flb_tests_runtime.h"
 
-pthread_mutex_t result_mutex = PTHREAD_MUTEX_INITIALIZER;
-char *output = NULL;
-
-/* Test data */
-
-/* Test functions */
-void flb_test_filter_nest_single(void);
-
-/* Test list */
-TEST_LIST = {
-    {"single", flb_test_filter_nest_single },
-    {NULL, NULL}
+struct filter_test {
+    flb_ctx_t *flb;    /* Fluent Bit library context */
+    int i_ffd;         /* Input fd  */
+    int f_ffd;         /* Filter fd */
 };
 
-
-void set_output(char *val)
+/* Callback to check expected results */
+static int cb_check_result(void *record, size_t size, void *data)
 {
-    pthread_mutex_lock(&result_mutex);
-    output = val;
-    pthread_mutex_unlock(&result_mutex);
+    char *p;
+    char *expected;
+    char *result;
+
+    expected = (char *) data;
+    result = (char *) record;
+
+    p = strstr(result, expected);
+    TEST_CHECK(p != NULL);
+
+    if (!p) {
+        flb_error("Expected to find: '%s' in result '%s'",
+                  expected, result);
+    }
+    flb_free(record);
+
+    /*
+     * If you want to debug your test
+     *
+     * printf("Expect: '%s' in result '%s'", expected, result);
+     */
+    return 0;
 }
 
-char *get_output(void)
+static struct filter_test *filter_test_create(struct flb_lib_out_cb *data)
 {
-    char *val;
+    int i_ffd;
+    int f_ffd;
+    int o_ffd;
+    struct filter_test *ctx;
 
-    pthread_mutex_lock(&result_mutex);
-    val = output;
-    pthread_mutex_unlock(&result_mutex);
+    ctx = flb_malloc(sizeof(struct filter_test));
+    if (!ctx) {
+        flb_errno();
+        return NULL;
+    }
 
-    return val;
+    /* Service config */
+    ctx->flb = flb_create();
+    flb_service_set(ctx->flb,
+                    "Flush", "0.200000000",
+                    "Grace", "1",
+                    NULL);
+
+    /* Input */
+    i_ffd = flb_input(ctx->flb, (char *) "lib", NULL);
+    TEST_CHECK(i_ffd >= 0);
+    flb_input_set(ctx->flb, i_ffd, "tag", "test", NULL);
+    ctx->i_ffd = i_ffd;
+
+    /* Filter configuration */
+    f_ffd = flb_filter(ctx->flb, (char *) "modify", NULL);
+    TEST_CHECK(f_ffd >= 0);
+    flb_filter_set(ctx->flb, f_ffd, "match", "*", NULL);
+    ctx->f_ffd = f_ffd;
+
+    /* Output */
+    o_ffd = flb_output(ctx->flb, (char *) "lib", (void *) data);
+    TEST_CHECK(o_ffd >= 0);
+    flb_output_set(ctx->flb, o_ffd,
+                   "match", "test",
+                   "format", "json",
+                   NULL);
+
+    return ctx;
 }
 
-void flb_test_filter_nest_single(void)
+static void filter_test_destroy(struct filter_test *ctx)
 {
-    int i;
+    sleep(0.5);
+    flb_stop(ctx->flb);
+    flb_destroy(ctx->flb);
+    flb_free(ctx);
+}
+
+/* Operation: SET / append new record */
+static void flb_test_op_set_append()
+{
+    int len;
     int ret;
     int bytes;
-    char *p, *output, *expected;
-    flb_ctx_t *ctx;
-    int in_ffd;
-    int out_ffd;
-    int filter_ffd;
+    char *p;
+    struct flb_lib_out_cb cb_data;
+    struct filter_test *ctx;
 
-    ctx = flb_create();
-
-    in_ffd = flb_input(ctx, (char *) "lib", NULL);
-    TEST_CHECK(in_ffd >= 0);
-    flb_input_set(ctx, in_ffd, "tag", "test", NULL);
-
-    out_ffd = flb_output(ctx, (char *) "stdout", NULL);
-    TEST_CHECK(out_ffd >= 0);
-    flb_output_set(ctx, out_ffd, "match", "test", NULL);
-    filter_ffd = flb_filter(ctx, (char *) "nest", NULL);
-    TEST_CHECK(filter_ffd >= 0);
-
-    ret = flb_filter_set(ctx, filter_ffd,
-        "Match", "*",
-        "Wildcard", "to_nest",
-        "Nest_under", "nested_key",
-        NULL);
-
-    TEST_CHECK(ret == 0);
-
-    ret = flb_start(ctx);
-    TEST_CHECK(ret == 0);
-
-    p = "[1448403340, {\"to_nest\":\"This is the data to nest\", \"extra\":\"Some more data\"}]";
-    bytes = flb_lib_push(ctx, in_ffd, p, strlen(p));
-    TEST_CHECK(bytes == strlen(p));
-
-    sleep(1); /* waiting flush */
-
-    output = get_output();
-    TEST_CHECK_(output != NULL, "Expected output to not be NULL");
-
-    if (output != NULL) {
-        expected = "\"tonest\":\"{\"to_nest\":\"This is the data to nest\"}\"";
-        TEST_CHECK_(strstr(output, expected) != NULL, "Expected output to contain '%s', got '%s'", expected, output);
-        free(output);
+    /* Create test context */
+    ctx = filter_test_create((void *) &cb_data);
+    if (!ctx) {
+        exit(EXIT_FAILURE);
     }
-    flb_stop(ctx);
-    flb_destroy(ctx);
+
+    /* Configure filter */
+    ret = flb_filter_set(ctx->flb, ctx->f_ffd,
+                         "set", "test_key test_value",
+                         NULL);
+    TEST_CHECK(ret == 0);
+
+    /* Prepare output callback with expected result */
+    cb_data.cb = cb_check_result;
+    cb_data.data = "\"test_key\":\"test_value\"";
+
+    /* Start the engine */
+    ret = flb_start(ctx->flb);
+    TEST_CHECK(ret == 0);
+
+    /* Ingest data samples */
+    p = "[0, {\"k\":\"sample\"}]";
+    len = strlen(p);
+    bytes = flb_lib_push(ctx->flb, ctx->i_ffd, p, len);
+    TEST_CHECK(bytes == len);
+
+    filter_test_destroy(ctx);
 }
 
+/* Operation: SET / replace value of existing key */
+static void flb_test_op_set_replace()
+{
+    int len;
+    int ret;
+    int bytes;
+    char *p;
+    struct flb_lib_out_cb cb_data;
+    struct filter_test *ctx;
+
+    /* Create test context */
+    ctx = filter_test_create((void *) &cb_data);
+    if (!ctx) {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Configure filter */
+    ret = flb_filter_set(ctx->flb, ctx->f_ffd,
+                         "set", "k test_value",
+                         NULL);
+    TEST_CHECK(ret == 0);
+
+    /* Prepare output callback with expected result */
+    cb_data.cb = cb_check_result;
+    cb_data.data = "\"k\":\"test_value\"";
+
+    /* Start the engine */
+    ret = flb_start(ctx->flb);
+    TEST_CHECK(ret == 0);
+
+    /* Ingest data samples */
+    p = "[0, {\"k\":\"sample\"}]";
+    len = strlen(p);
+    bytes = flb_lib_push(ctx->flb, ctx->i_ffd, p, len);
+    TEST_CHECK(bytes == len);
+
+    filter_test_destroy(ctx);
+}
+
+/* Operation: REMOVE */
+static void flb_test_op_remove()
+{
+    int len;
+    int ret;
+    int bytes;
+    char *p;
+    struct flb_lib_out_cb cb_data;
+    struct filter_test *ctx;
+
+    /* Create test context */
+    ctx = filter_test_create((void *) &cb_data);
+    if (!ctx) {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Configure filter */
+    ret = flb_filter_set(ctx->flb, ctx->f_ffd,
+                         "remove", "remove",
+                         NULL);
+    TEST_CHECK(ret == 0);
+
+    /* Prepare output callback with expected result */
+    cb_data.cb = cb_check_result;
+    cb_data.data = "\"k\":\"sample\"";
+
+    /* Start the engine */
+    ret = flb_start(ctx->flb);
+    TEST_CHECK(ret == 0);
+
+    /* Ingest data samples */
+    p = "[0, {\"k\":\"sample\",\"remove\":\"sample to remove\"}]";
+    len = strlen(p);
+    bytes = flb_lib_push(ctx->flb, ctx->i_ffd, p, len);
+    TEST_CHECK(bytes == len);
+
+    filter_test_destroy(ctx);
+}
+
+/* Operation: REMOVE_WILDCARD */
+static void flb_test_op_remove_wildcard()
+{
+    int len;
+    int ret;
+    int bytes;
+    char *p;
+    struct flb_lib_out_cb cb_data;
+    struct filter_test *ctx;
+
+    /* Create test context */
+    ctx = filter_test_create((void *) &cb_data);
+    if (!ctx) {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Configure filter */
+    ret = flb_filter_set(ctx->flb, ctx->f_ffd,
+                         "remove_wildcard", "k",
+                         NULL);
+    TEST_CHECK(ret == 0);
+
+    /* Prepare output callback with expected result */
+    cb_data.cb = cb_check_result;
+    cb_data.data = "\"a3\":\"sample3\"";
+
+    /* Start the engine */
+    ret = flb_start(ctx->flb);
+    TEST_CHECK(ret == 0);
+
+    /* Ingest data samples */
+    p = "[0, {\"k1\":\"sample1\",\"k2\":\"sample2\",\"a3\":\"sample3\"}]";
+    len = strlen(p);
+    bytes = flb_lib_push(ctx->flb, ctx->i_ffd, p, len);
+    TEST_CHECK(bytes == len);
+
+    filter_test_destroy(ctx);
+}
+
+/* Operation: REMOVE_REGEX */
+static void flb_test_op_remove_regex()
+{
+    int len;
+    int ret;
+    int bytes;
+    char *p;
+    struct flb_lib_out_cb cb_data;
+    struct filter_test *ctx;
+
+    /* Create test context */
+    ctx = filter_test_create((void *) &cb_data);
+    if (!ctx) {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Configure filter */
+    ret = flb_filter_set(ctx->flb, ctx->f_ffd,
+                         "remove_regex", "^[a-z][0-9]$",
+                         NULL);
+    TEST_CHECK(ret == 0);
+
+    /* Prepare output callback with expected result */
+    cb_data.cb = cb_check_result;
+    cb_data.data = "\"A3\":\"sample3\"";
+
+    /* Start the engine */
+    ret = flb_start(ctx->flb);
+    TEST_CHECK(ret == 0);
+
+    /* Ingest data samples */
+    p = "[0, {\"k1\":\"sample1\",\"k2\":\"sample2\",\"A3\":\"sample3\"}]";
+    len = strlen(p);
+    bytes = flb_lib_push(ctx->flb, ctx->i_ffd, p, len);
+    TEST_CHECK(bytes == len);
+
+    filter_test_destroy(ctx);
+}
+
+/*
+ * Operation: RENAME / Try to rename Key where 'renamed' key already
+ * exists: do nothing.
+ */
+static void flb_test_op_rename_exists()
+{
+    int len;
+    int ret;
+    int bytes;
+    char *p;
+    struct flb_lib_out_cb cb_data;
+    struct filter_test *ctx;
+
+    /* Create test context */
+    ctx = filter_test_create((void *) &cb_data);
+    if (!ctx) {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Configure filter */
+    ret = flb_filter_set(ctx->flb, ctx->f_ffd,
+                         "rename", "A3 k2",
+                         NULL);
+    TEST_CHECK(ret == 0);
+
+    /* Prepare output callback with expected result */
+    cb_data.cb = cb_check_result;
+    cb_data.data = "\"k1\":\"sample1\", \"k2\":\"sample2\", \"A3\":\"sample3\"";
+
+    /* Start the engine */
+    ret = flb_start(ctx->flb);
+    TEST_CHECK(ret == 0);
+
+    /* Ingest data samples */
+    p = "[0, {\"k1\":\"sample1\",\"k2\":\"sample2\",\"A3\":\"sample3\"}]";
+    len = strlen(p);
+    bytes = flb_lib_push(ctx->flb, ctx->i_ffd, p, len);
+    TEST_CHECK(bytes == len);
+
+    filter_test_destroy(ctx);
+}
+
+/* Operation: RENAME / Rename when key DON'T exists */
+static void flb_test_op_rename_no_exists()
+{
+    int len;
+    int ret;
+    int bytes;
+    char *p;
+    struct flb_lib_out_cb cb_data;
+    struct filter_test *ctx;
+
+    /* Create test context */
+    ctx = filter_test_create((void *) &cb_data);
+    if (!ctx) {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Configure filter */
+    ret = flb_filter_set(ctx->flb, ctx->f_ffd,
+                         "rename", "A3 a3",
+                         NULL);
+    TEST_CHECK(ret == 0);
+
+    /* Prepare output callback with expected result */
+    cb_data.cb = cb_check_result;
+    cb_data.data = "\"k1\":\"sample1\", \"k2\":\"sample2\", \"a3\":\"sample3\"";
+    /* Start the engine */
+    ret = flb_start(ctx->flb);
+    TEST_CHECK(ret == 0);
+
+    /* Ingest data samples */
+    p = "[0, {\"k1\":\"sample1\",\"k2\":\"sample2\",\"A3\":\"sample3\"}]";
+    len = strlen(p);
+    bytes = flb_lib_push(ctx->flb, ctx->i_ffd, p, len);
+    TEST_CHECK(bytes == len);
+
+    filter_test_destroy(ctx);
+}
+
+/*
+ * Operation: HARD_RENAME / Try to rename Key where 'renamed' key already
+ * exists: do nothing.
+ */
+static void flb_test_op_hard_rename_exists()
+{
+    int len;
+    int ret;
+    int bytes;
+    char *p;
+    struct flb_lib_out_cb cb_data;
+    struct filter_test *ctx;
+
+    /* Create test context */
+    ctx = filter_test_create((void *) &cb_data);
+    if (!ctx) {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Configure filter */
+    ret = flb_filter_set(ctx->flb, ctx->f_ffd,
+                         "hard_rename", "k2 k1",
+                         NULL);
+    TEST_CHECK(ret == 0);
+
+    /* Prepare output callback with expected result */
+    cb_data.cb = cb_check_result;
+    cb_data.data = "\"k1\":\"sample2\"";
+
+    /* Start the engine */
+    ret = flb_start(ctx->flb);
+    TEST_CHECK(ret == 0);
+
+    /* Ingest data samples */
+    p = "[0, {\"k1\":\"sample1\",\"k2\":\"sample2\"}]";
+    len = strlen(p);
+    bytes = flb_lib_push(ctx->flb, ctx->i_ffd, p, len);
+    TEST_CHECK(bytes == len);
+
+    filter_test_destroy(ctx);
+}
+
+/* Operation: HARD_RENAME / Rename when key DON'T exists */
+static void flb_test_op_hard_rename_no_exists()
+{
+    int len;
+    int ret;
+    int bytes;
+    char *p;
+    struct flb_lib_out_cb cb_data;
+    struct filter_test *ctx;
+
+    /* Create test context */
+    ctx = filter_test_create((void *) &cb_data);
+    if (!ctx) {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Configure filter */
+    ret = flb_filter_set(ctx->flb, ctx->f_ffd,
+                         "hard_rename", "k2 k3",
+                         NULL);
+    TEST_CHECK(ret == 0);
+
+    /* Prepare output callback with expected result */
+    cb_data.cb = cb_check_result;
+    cb_data.data = "\"k3\":\"sample2\"";
+
+    /* Start the engine */
+    ret = flb_start(ctx->flb);
+    TEST_CHECK(ret == 0);
+
+    /* Ingest data samples */
+    p = "[0, {\"k1\":\"sample1\",\"k2\":\"sample2\"}]";
+    len = strlen(p);
+    bytes = flb_lib_push(ctx->flb, ctx->i_ffd, p, len);
+    TEST_CHECK(bytes == len);
+
+    filter_test_destroy(ctx);
+}
+
+/* Operation: COPY / Target key already exists, do nothing */
+static void flb_test_op_copy_exists()
+{
+    int len;
+    int ret;
+    int bytes;
+    char *p;
+    struct flb_lib_out_cb cb_data;
+    struct filter_test *ctx;
+
+    /* Create test context */
+    ctx = filter_test_create((void *) &cb_data);
+    if (!ctx) {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Configure filter */
+    ret = flb_filter_set(ctx->flb, ctx->f_ffd,
+                         "copy", "k1 k2",
+                         NULL);
+    TEST_CHECK(ret == 0);
+
+    /* Prepare output callback with expected result */
+    cb_data.cb = cb_check_result;
+    cb_data.data = "\"k1\":\"sample1\", \"k2\":\"sample2\"";
+
+    /* Start the engine */
+    ret = flb_start(ctx->flb);
+    TEST_CHECK(ret == 0);
+
+    /* Ingest data samples */
+    p = "[0, {\"k1\":\"sample1\",\"k2\":\"sample2\"}]";
+    len = strlen(p);
+    bytes = flb_lib_push(ctx->flb, ctx->i_ffd, p, len);
+    TEST_CHECK(bytes == len);
+
+    filter_test_destroy(ctx);
+}
+
+/* Operation: COPY / Target key no exists, make a copy */
+static void flb_test_op_copy_no_exists()
+{
+    int len;
+    int ret;
+    int bytes;
+    char *p;
+    struct flb_lib_out_cb cb_data;
+    struct filter_test *ctx;
+
+    /* Create test context */
+    ctx = filter_test_create((void *) &cb_data);
+    if (!ctx) {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Configure filter */
+    ret = flb_filter_set(ctx->flb, ctx->f_ffd,
+                         "copy", "k1 k3",
+                         NULL);
+    TEST_CHECK(ret == 0);
+
+    /* Prepare output callback with expected result */
+    cb_data.cb = cb_check_result;
+    cb_data.data = "\"k3\":\"sample1\"";
+
+    /* Start the engine */
+    ret = flb_start(ctx->flb);
+    TEST_CHECK(ret == 0);
+
+    /* Ingest data samples */
+    p = "[0, {\"k1\":\"sample1\",\"k2\":\"sample2\"}]";
+    len = strlen(p);
+    bytes = flb_lib_push(ctx->flb, ctx->i_ffd, p, len);
+    TEST_CHECK(bytes == len);
+
+    filter_test_destroy(ctx);
+}
+
+/* Operation: HARD_COPY / if target key exists, replace value */
+static void flb_test_op_hard_copy_exists()
+{
+    int len;
+    int ret;
+    int bytes;
+    char *p;
+    struct flb_lib_out_cb cb_data;
+    struct filter_test *ctx;
+
+    /* Create test context */
+    ctx = filter_test_create((void *) &cb_data);
+    if (!ctx) {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Configure filter */
+    ret = flb_filter_set(ctx->flb, ctx->f_ffd,
+                         "hard_copy", "k1 k2",
+                         NULL);
+    TEST_CHECK(ret == 0);
+
+    /* Prepare output callback with expected result */
+    cb_data.cb = cb_check_result;
+    cb_data.data = "\"k1\":\"sample1\", \"k2\":\"sample1\"";
+
+    /* Start the engine */
+    ret = flb_start(ctx->flb);
+    TEST_CHECK(ret == 0);
+
+    /* Ingest data samples */
+    p = "[0, {\"k1\":\"sample1\",\"k2\":\"sample2\"}]";
+    len = strlen(p);
+    bytes = flb_lib_push(ctx->flb, ctx->i_ffd, p, len);
+    TEST_CHECK(bytes == len);
+
+    filter_test_destroy(ctx);
+}
+
+/* Operation: HARD_COPY / if key don't exists make a copy */
+static void flb_test_op_hard_copy_no_exists()
+{
+    int len;
+    int ret;
+    int bytes;
+    char *p;
+    struct flb_lib_out_cb cb_data;
+    struct filter_test *ctx;
+
+    /* Create test context */
+    ctx = filter_test_create((void *) &cb_data);
+    if (!ctx) {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Configure filter */
+    ret = flb_filter_set(ctx->flb, ctx->f_ffd,
+                         "hard_copy", "k1 k3",
+                         NULL);
+    TEST_CHECK(ret == 0);
+
+    /* Prepare output callback with expected result */
+    cb_data.cb = cb_check_result;
+    cb_data.data = "\"k3\":\"sample1\"";
+
+    /* Start the engine */
+    ret = flb_start(ctx->flb);
+    TEST_CHECK(ret == 0);
+
+    /* Ingest data samples */
+    p = "[0, {\"k1\":\"sample1\",\"k2\":\"sample2\"}]";
+    len = strlen(p);
+    bytes = flb_lib_push(ctx->flb, ctx->i_ffd, p, len);
+    TEST_CHECK(bytes == len);
+
+    filter_test_destroy(ctx);
+}
+
+TEST_LIST = {
+    {"op_set_append"            , flb_test_op_set_append },
+    {"op_set_replace"           , flb_test_op_set_replace },
+    {"op_remove"                , flb_test_op_remove },
+    {"op_remove_wildcard"       , flb_test_op_remove_wildcard },
+    {"op_remove_regex"          , flb_test_op_remove_regex },
+    {"op_rename_exists"         , flb_test_op_rename_exists },
+    {"op_rename_no_exists"      , flb_test_op_rename_no_exists },
+    {"op_hard_rename_exists"    , flb_test_op_hard_rename_exists },
+    {"op_hard_rename_no_exists" , flb_test_op_hard_rename_no_exists },
+    {"op_copy_exists"           , flb_test_op_copy_exists },
+    {"op_copy_no_exists"        , flb_test_op_copy_no_exists },
+    {"op_hard_copy_exists"      , flb_test_op_hard_copy_exists },
+    {"op_hard_copy_no_exists"   , flb_test_op_hard_copy_no_exists },
+    {NULL, NULL}
+};
