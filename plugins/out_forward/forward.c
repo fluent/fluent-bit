affc29ee851f2ad8ce1b0ee3c418f75523ce1846
Eduardo Silva <eduardo@treasure-data.com>
2019-03-27 10:41:58 -0600

    revert: out_forward: Don't assumed 'secure' mode just because TLS is on (#1228)
    - This reverts commit d89dc36926404f90b992f43e34c49d3763f2efdc.
    - This commit breaks TLS functionality when connecting to Fluentd.
    - stunnel is designed to be used with daemons not-aware of TLS, that's not the case
      of Fluentd. In Fluent Bit we have the pending functionality in in_forward
      that will be fixed this year. Running Fluent Bit securely must use Forward Protocol
      and further Forward handshake.

--- plugins/out_forward/forward.c
+++ plugins/out_forward/forward.c
@@ -428,14 +428,15 @@ static int forward_config_ha(char *upstream_file,
         }
         fc->secured = FLB_FALSE;
 
+        /* Is TLS enabled ? */
+        if (node->tls_enabled == FLB_TRUE) {
+            fc->secured = FLB_TRUE;
+        }
+
         /* Shared key (secure_forward) */
         tmp = flb_upstream_node_get_property("shared_key", node);
         if (tmp) {
             fc->shared_key = flb_sds_create(tmp);
-	    /* Is TLS enabled ? */
-	    if (node->tls_enabled == FLB_TRUE) {
-		fc->secured = FLB_TRUE;
-	    }
         }
         else {
             fc->shared_key = NULL;
@@ -499,9 +500,7 @@ static int forward_config_simple(struct flb_forward *ctx,
 #ifdef FLB_HAVE_TLS
     if (ins->use_tls == FLB_TRUE) {
         io_flags = FLB_IO_TLS;
-	if (fc->shared_key) {
-	    fc->secured = FLB_TRUE;
-	}
+        fc->secured = FLB_TRUE;
     }
     else {
         io_flags = FLB_IO_TCP;
