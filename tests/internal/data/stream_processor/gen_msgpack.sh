ad49a94ce40bbda0be371cccb48d4eb099ea2dfa
Eduardo Silva <eduardo@treasure-data.com>
2019-05-25 15:01:54 -0600

    tests: internal: sp: samples: add new subkeys data samples
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- tests/internal/data/stream_processor/gen_msgpack.sh
+++ tests/internal/data/stream_processor/gen_msgpack.sh
@@ -4,10 +4,11 @@ echo "=== Just hit ctrl-c after 2 seconds... ==="
 echo
 
 # Delete old samples file
-rm -rf samples.mp
+rm -rf samples.mp samples-subkeys.mp
 
 # Generate new msgpack file using as a source samples.json
-../../../../build/bin/fluent-bit -R ../../../../conf/parsers.conf \
- -i tail -p path=samples.json -p parser=json \
- -o file -p format=msgpack -p path=samples.mp -f 1
-
+../../../../build/bin/fluent-bit -R ../../../../conf/parsers.conf              \
+ -i tail -t samples         -p path=samples.json          -p parser=json       \
+ -i tail -t samples-subkeys -p path=samples-subkeys.json  -p parser=json       \
+ -o file -m samples         -p format=msgpack  -p path=samples.mp              \
+ -o file -m samples-subkeys -p format=msgpack  -p path=samples-subkeys.mp -f 1
