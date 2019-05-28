986d4fc1e3f37e8b8ce5a514604612340281565c
Yann Soubeyrand <yann.soubeyrand@gmx.fr>
2019-05-28 11:25:14 +0200

    out_counter: add const in functions signatures and limit casts
    Signed-off-by: Yann Soubeyrand <yann.soubeyrand@gmx.fr>

--- plugins/out_counter/counter.c
+++ plugins/out_counter/counter.c
@@ -56,8 +56,8 @@ int cb_counter_init(struct flb_output_instance *ins,
     return 0;
 }
 
-void cb_counter_flush(void *data, size_t bytes,
-                      char *tag, int tag_len,
+void cb_counter_flush(const void *data, size_t bytes,
+                      const char *tag, int tag_len,
                       struct flb_input_instance *i_ins,
                       void *out_context,
                       struct flb_config *config)
