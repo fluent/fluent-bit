20c9cda33c33073ec3d5c0868c1a2ca5e80b5fcb
Eduardo Silva <eduardo@treasure-data.com>
2019-06-10 12:30:05 -0600

    in_forward: fix buffer condition that cleans consumed record (#1375)
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- plugins/in_forward/fw_prot.c
+++ plugins/in_forward/fw_prot.c
@@ -130,12 +130,11 @@ int fw_prot_process(struct fw_conn *conn)
             msgpack_unpacked_destroy(&result);
 
             /* Adjust buffer data */
-            if (conn->buf_len > all_used && all_used > 0) {
+            if (conn->buf_len >= all_used && all_used > 0) {
                 memmove(conn->buf, conn->buf + all_used,
                         conn->buf_len - all_used);
                 conn->buf_len -= all_used;
             }
-
             return 0;
         }
 
