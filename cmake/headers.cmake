5165363261bb2140c7693074840476b1f98d3d2f
Eduardo Silva <eduardo@treasure-data.com>
2019-06-21 13:56:04 -0600

    cmake: add headers, libraries and macros cmake files
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

---
+++ cmake/headers.cmake
@@ -0,0 +1,24 @@
+if(NOT FLB_PATH_ROOT_SOURCE)
+  set(FLB_PATH_ROOT_SOURCE ${FLB_ROOT})
+endif()
+
+include_directories(
+  ${FLB_PATH_ROOT_SOURCE}/include/
+  ${FLB_PATH_ROOT_SOURCE}/lib/
+  ${FLB_PATH_ROOT_SOURCE}/${FLB_PATH_LIB_CO}
+  ${FLB_PATH_ROOT_SOURCE}/${FLB_PATH_LIB_RBTREE}
+  ${FLB_PATH_ROOT_SOURCE}/${FLB_PATH_LIB_MSGPACK}/include
+  ${FLB_PATH_ROOT_SOURCE}/${FLB_PATH_LIB_CHUNKIO}/include
+  ${FLB_PATH_ROOT_SOURCE}/${FLB_PATH_LIB_LUAJIT}/src
+  ${FLB_PATH_ROOT_SOURCE}/${FLB_PATH_LIB_MONKEY}/include
+  ${FLB_PATH_ROOT_SOURCE}/${FLB_PATH_LIB_MBEDTLS}/include
+  ${FLB_PATH_ROOT_SOURCE}/${FLB_PATH_LIB_SQLITE}
+  )
+
+# On Windows, the core uses libevent
+if(CMAKE_SYSTEM_NAME MATCHES "Windows")
+  include_directories(
+    lib/monkey/mk_core/deps/libevent/include
+    ${PROJECT_BINARY_DIR}/lib/monkey/mk_core/deps/libevent/include
+    )
+endif()
