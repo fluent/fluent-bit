b7f562f1568e7679e94d72c89f304ba8245c0b42
Masoud Koleini <masoud.koleini@arm.com>
2019-07-17 13:33:16 -0500

    sp: add tests for Stream Processor hopping window
    Signed-off-by: Masoud Koleini <masoud.koleini@arm.com>

--- tests/internal/data/stream_processor/gen_msgpack.sh
+++ tests/internal/data/stream_processor/gen_msgpack.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "=== Just hit ctrl-c after 2 seconds... ==="
+echo "=== Hit ctrl-c if it runs forever!... ==="
 echo
 
 # Delete old samples file
@@ -11,4 +11,30 @@ rm -rf samples.mp samples-subkeys.mp
  -i tail -t samples         -p path=samples.json          -p parser=json       \
  -i tail -t samples-subkeys -p path=samples-subkeys.json  -p parser=json       \
  -o file -m samples         -p format=msgpack  -p path=samples.mp              \
- -o file -m samples-subkeys -p format=msgpack  -p path=samples-subkeys.mp -f 1
+ -o file -m samples-subkeys -p format=msgpack  -p path=samples-subkeys.mp -f 1 &
+ pid=$!
+ sleep 2
+ kill -9 $pid
+
+ # Generate new msgpack files for hopping window
+ files=$(find "samples-hw/" -type f -name "*.mp")
+
+ for file in $files
+ do
+     rm -f $file
+ done
+
+ files=$(find "samples-hw/" -type f -name "*.json")
+
+ for file in $files
+ do
+     # Generate new msgpack files for hopping window
+     echo ""
+     echo "=== Generating MessagePack file for $file ... ==="
+     ../../../../build/bin/fluent-bit -R ../../../../conf/parsers.conf   \
+      -i tail -p path=$file  -p parser=json                              \
+      -o file -p format=msgpack  -p path=$(echo $file | sed s/json/mp/) -f 1 &
+      pid=$!
+      sleep 2
+      kill -9 $pid
+ done
