fb1efc30f3921ef8c26b3da9e41493109f4c9483
Eduardo Silva <eduardo@treasure-data.com>
2019-08-05 17:29:56 -0600

    router: use sds string for match string
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- src/flb_router.c
+++ src/flb_router.c
@@ -21,6 +21,7 @@
 #include <fluent-bit/flb_info.h>
 #include <fluent-bit/flb_mem.h>
 #include <fluent-bit/flb_str.h>
+#include <fluent-bit/flb_sds.h>
 #include <fluent-bit/flb_input.h>
 #include <fluent-bit/flb_output.h>
 #include <fluent-bit/flb_config.h>
@@ -157,7 +158,7 @@ int flb_router_io_set(struct flb_config *config)
             ) {
             flb_debug("[router] default match rule %s:%s",
                       i_ins->name, o_ins->name);
-            o_ins->match = flb_strdup("*");
+            o_ins->match = flb_sds_create_len("*", 1);
             flb_router_connect(i_ins, o_ins);
             return 0;
         }
