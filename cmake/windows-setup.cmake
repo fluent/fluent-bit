eb563ec6234da4d0df0a92020a0ff2c362126fe9
okkez <okkez000@gmail.com>
2019-09-17 15:06:49 +0900

    filter_geoip2: Add a new filter plugin for GeoIP2
    This adds a new filter plugin 'filter_geoip2' that enriches
    incoming stream with location data from GeoIP2 database.
    
    Suppose you have the following data:
    
        {"remote_addr"=>"8.8.8.8"}
    
    ... you can set up `filter_geoip2` as follows
    
        [FILTER]
          Name geoip2
          Match *
          Database GeoLite2-City.mmdb
          Lookup_key remote_addr
          Record country remote_addr %{country.names.en}
    
    ... which will produce a record like this:
    
        {"remote_addr"=>"8.8.8.8", "country"=>"United States"}
    
    The database 'GeoLige2-City.mmdb' is available from MaxMind:
    
        https://dev.maxmind.com/geoip/geoip2/geolite2/
    
    This patch was originally contributed by okkez, and submitted
    after several fixup by Fujimoto.
    
    Signed-off-by: okkez <okkez000@gmail.com>
    Signed-off-by: Fujimoto Seiji <fujimoto@ceptord.net>

--- cmake/windows-setup.cmake
+++ cmake/windows-setup.cmake
@@ -83,6 +83,7 @@ set(FLB_FILTER_NEST           Yes)
 set(FLB_FILTER_LUA            Yes)
 set(FLB_FILTER_RECORD_MODIFIER Yes)
 set(FLB_FILTER_REWRITE_TAG    Yes)
+set(FLB_FILTER_GEOIP2         Yes)
 
 # Search bison and flex executables
 if(CMAKE_SYSTEM_NAME MATCHES "Windows")
---
+++ plugins/filter_geoip2/geoip2.h
@@ -0,0 +1,51 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2019-2020 The Fluent Bit Authors
+ *  Copyright (C) 2015-2018 Treasure Data Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include <maxminddb.h>
+
+#ifndef FLB_FILTER_GEOIP2_H
+#define FLB_FILTER_GEOIP2_H
+
+struct geoip2_lookup_key {
+    char *key;
+    int key_len;
+    struct mk_list _head;
+};
+
+struct geoip2_record {
+    char *lookup_key;
+    char *key;
+    char *val;
+    int lookup_key_len;
+    int key_len;
+    int val_len;
+    struct mk_list _head;
+};
+
+struct geoip2_ctx {
+    MMDB_s *mmdb;
+    int lookup_keys_num;
+    int records_num;
+    struct mk_list lookup_keys;
+    struct mk_list records;
+    struct flb_filter_instance *ins;
+};
+
+#endif
