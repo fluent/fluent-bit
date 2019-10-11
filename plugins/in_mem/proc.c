34670e487707cf5e6d42ed85907832afbc43335b
Takahiro YAMASHITA <nokute78@gmail.com>
2019-10-12 08:32:43 +0900

    in_mem: don't discard return value of human_readable_size(#1625)
    Signed-off-by: Takahiro YAMASHITA <nokute78@gmail.com>

--- plugins/in_mem/proc.c
+++ plugins/in_mem/proc.c
@@ -169,6 +169,11 @@ struct proc_task *proc_stat(pid_t pid, int page_size)
     /* Internal conversion */
     t->proc_rss    = (t->rss * page_size);
     t->proc_rss_hr = human_readable_size(t->proc_rss);
+    if ( t->proc_rss_hr == NULL ) {
+        flb_free(buf);
+        flb_free(t);
+        return NULL;
+    }
 
     flb_free(buf);
     return t;
