93d4d5301a84b7e284c7c89caf5be1a8d249ffc2
Fujimoto Seiji <fujimoto@clear-code.com>
2019-12-24 07:03:45 +0900

    in_tcp: add Windows support (#1831)
    I know some Windows users use Fluentd's `in_tcp` plugin to accumulate
    data from internal services. This is very useful -- e.g. to collect
    logs from .NET applications running in internal network.
    
    This should allow users to do the same thing with Fluent Bit.
    
    Signed-off-by: Fujimoto Seiji <fujimoto@clear-code.com>

--- plugins/in_tcp/tcp_conn.c
+++ plugins/in_tcp/tcp_conn.c
@@ -198,8 +198,8 @@ int tcp_conn_event(void *data)
         }
 
         /* Read data */
-        bytes = read(conn->fd,
-                     conn->buf_data + conn->buf_len, available);
+        bytes = recv(conn->fd,
+                     conn->buf_data + conn->buf_len, available, 0);
         if (bytes <= 0) {
             flb_trace("[in_tcp] fd=%i closed connection", event->fd);
             tcp_conn_del(conn);
@@ -299,7 +299,7 @@ struct tcp_conn *tcp_conn_add(int fd, struct flb_in_tcp_config *ctx)
     conn->buf_data = flb_malloc(ctx->chunk_size);
     if (!conn->buf_data) {
         flb_errno();
-        close(fd);
+        flb_socket_close(fd);
         flb_error("[in_tcp] could not allocate new connection");
         flb_free(conn);
         return NULL;
@@ -317,7 +317,7 @@ struct tcp_conn *tcp_conn_add(int fd, struct flb_in_tcp_config *ctx)
     ret = mk_event_add(ctx->evl, fd, FLB_ENGINE_EV_CUSTOM, MK_EVENT_READ, conn);
     if (ret == -1) {
         flb_error("[in_tcp] could not register new connection");
-        close(fd);
+        flb_socket_close(fd);
         flb_free(conn->buf_data);
         flb_free(conn);
         return NULL;
@@ -342,7 +342,7 @@ int tcp_conn_del(struct tcp_conn *conn)
 
     /* Release resources */
     mk_list_del(&conn->_head);
-    close(conn->fd);
+    flb_socket_close(conn->fd);
     flb_free(conn->buf_data);
     flb_free(conn);
 
