3127a4d5e0277571574c1121b90787d16eb15db2
Hassan Farid <hassan.seth@gmail.com>
2019-12-27 11:28:55 -0500

    adding parsing to strip tag string

--- scripts/append_tag.lua
+++ scripts/append_tag.lua
@@ -1,5 +1,5 @@
 function append_tag(tag, timestamp, record)
     new_record = record
-    new_record["tag"] = tag
+    new_record["tag"] = string.gsub(tag, "([%w_]+)/([%w_]+)/([%w_]+)(.*)", "%1/%2/%3")
     return 1, timestamp, new_record
 end
