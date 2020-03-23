c903ec0caef24856e00a7c250d8bea11aa448b7e
ezra-at-lumedic <56045315+ezra-at-lumedic@users.noreply.github.com>
2020-03-23 15:24:58 -0700

    Support Solaris 11
    Per https://github.com/zlib-ng/zlib-ng/commit/8348a181e671865e22b1bdf77c1d51780cd0c0d9

--- lib/chunkio/deps/crc32/crc32.c
+++ lib/chunkio/deps/crc32/crc32.c
@@ -33,6 +33,21 @@
 #  define le16toh(x) (x)
 #elif defined(__FreeBSD__)
 #  include <sys/endian.h>
+#elif defined(__sun) || defined(sun)
+# include <sys/byteorder.h>
+# if !defined(LITTLE_ENDIAN)
+#  define LITTLE_ENDIAN 4321
+# endif
+# if !defined(BIG_ENDIAN)
+#  define BIG_ENDIAN 1234
+# endif
+# if !defined(BYTE_ORDER)
+#  if defined(_BIG_ENDIAN)
+#   define BYTE_ORDER BIG_ENDIAN
+#  else
+#   define BYTE_ORDER LITTLE_ENDIAN
+#  endif
+# endif
 #else
 #  include <endian.h>
 #endif
