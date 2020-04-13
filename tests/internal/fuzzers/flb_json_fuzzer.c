bc8e29d1bda477052edbdea3c3f3ca78c776e9cd
davkor <david@adalogics.com>
2020-04-13 19:27:31 +0100

    Minor switch in includes to sign off.
    Signed-off-by: davkor <david@adalogics.com>

--- tests/internal/fuzzers/flb_json_fuzzer.c
+++ tests/internal/fuzzers/flb_json_fuzzer.c
@@ -15,8 +15,8 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-#include <stdint.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 int flb_pack_json(char*, int, char**, size_t*, int*);
 
