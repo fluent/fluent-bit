1ac0956ac03e0556e8813d198c72b13eff375789
Takahiro YAMASHITA <nokute78@gmail.com>
2020-04-16 17:02:48 +0900

    time: add new function flb_time_add to add timestamp offset
    Signed-off-by: Takahiro YAMASHITA <nokute78@gmail.com>

--- include/fluent-bit/flb_time.h
+++ include/fluent-bit/flb_time.h
@@ -75,6 +75,8 @@ static inline int flb_time_equal(struct flb_time *t0, struct flb_time *t1) {
 int flb_time_get(struct flb_time *tm);
 int flb_time_msleep(uint32_t ms);
 double flb_time_to_double(struct flb_time *tm);
+int flb_time_add(struct flb_time *base, struct flb_time *duration,
+                 struct flb_time *result);
 int flb_time_diff(struct flb_time *time1,
                   struct flb_time *time0, struct flb_time *result);
 int flb_time_append_to_msgpack(struct flb_time *tm, msgpack_packer *pk, int fmt);
