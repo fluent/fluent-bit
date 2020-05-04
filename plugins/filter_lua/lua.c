5501e0ee4a774103f42fc20f19fd4731f66047d2
Takahiro Yamashita <nokute78@gmail.com>
2020-05-05 08:22:24 +0900

    filter_lua: support protected mode (#2148) (#2154)
    Signed-off-by: Takahiro YAMASHITA <nokute78@gmail.com>
    Signed-off-by: Robert Stephens <rstephens@synamedia.com>

--- plugins/filter_lua/lua.c
+++ plugins/filter_lua/lua.c
@@ -418,7 +418,21 @@ static int cb_lua_filter(const void *data, size_t bytes,
         lua_pushstring(ctx->lua->state, tag);
         lua_pushnumber(ctx->lua->state, ts);
         lua_pushmsgpack(ctx->lua->state, p);
-        lua_call(ctx->lua->state, 3, 3);
+        if (ctx->protected_mode) {
+            ret = lua_pcall(ctx->lua->state, 3, 3, 0);
+            if (ret != 0) {
+                flb_plg_error(ctx->ins, "error code %d: %s",
+                              ret, lua_tostring(ctx->lua->state, -1));
+                lua_pop(ctx->lua->state, 1);
+                msgpack_sbuffer_destroy(&tmp_sbuf);
+                msgpack_sbuffer_destroy(&data_sbuf);
+                msgpack_unpacked_destroy(&result);
+                return FLB_FILTER_NOTOUCH;
+            }
+        }
+        else {
+            lua_call(ctx->lua->state, 3, 3);
+        }
 
         /* Initialize Return values */
         l_code = 0;
