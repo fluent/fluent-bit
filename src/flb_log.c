d38a6f9c6e2f9056ccb9e41ed42a46b661401ce2
Fujimoto Seiji <fujimoto@ceptord.net>
2020-06-17 08:22:09 +0900

    log: fix a segmentation fault issue in logging worker
    Sean Fausett reported an out-of-bounds read in log_push() on Windows.
    Evidently `log_message` struct can be corrupted in some cases, and
    it confuses the subsequent write() to overrun the buffer.
    
    This fixes up the crash issue with a safety check: ensure that
    msg->size does not exceeds the message buffer size.
    
    Signed-off-by: Fujimoto Seiji <fujimoto@ceptord.net>

--- src/flb_log.c
+++ src/flb_log.c
@@ -94,6 +94,9 @@ static inline int log_read(flb_pipefd_t fd, struct flb_log *log)
         perror("bytes");
         return -1;
     }
+    if (msg.size > sizeof(msg.msg)) {
+        return -1;
+    }
     log_push(&msg, log);
 
     return bytes;
