07a3cbd64c08faf50c52125b5b020bb1354917ae
Vladimir Vasilev <38783428+vlvasilev@users.noreply.github.com>
2020-06-30 11:03:34 -0700

    filter: filter_throttle_size: new throttle size filter and it's unit tests (#1107)
    * Plugin: add size throttle filter and it's unit tests
    
    The Size Throttle Filter plugin sets the average Rate of messages size per
    Interval, based on a leaky bucket and sliding window algorithm.
    In the case of overflood, it will leak within a certain rate. There can be multiple
    buckets as each bucket are made for a different value of name_field.
    Name_field could be ,for exaple, the source (pod_name, contaner_name).
    Thus, for pod kube-apiserver and alertmanager there will be two different
    buckets. We can monitor only one field of the log by setting log_field,
    for example, to "log". Thus we will take into account only the size of
    the log field. Because after a time of no encountered new data an unused bucket
    will be deleted to avoid frequent deletion we can set window_time_duration.
    When we have nested fields we separate the keys by '|'.
    example:
    [FILTER]
    	Name     		size_throttle
    	Match	 		*
    	Rate     		1M
    	Window   		30
    	Interval 		3s
    	Log_field	 	log
    	Name_field		kubernetes|pod_name
    	Window_time_duration 	360s
    
    input:
        [123456, {"log":"20kmessage", "kubernetes":{"pod_name":"kube-apiserver"}}]
        [123490, {"log":"30kmessage", "kubernetes":{"pod_name":"alermanager"}}]
    
    This plugin will make two buckets (one for "kube-apiserver" and one for
    "alertmanager) where will take into account only the size of the "log" field.
    Log_field can be omitted, thus all of the message will be calculated.
    So if the kube-apiserver starts to flood the plugin will drop the messages
    only of kube-apiserver pod, not alertmanager's ones.
    
    Signed-off-by: I330716 <vladimir.vasilev@sap.com>
    
    * size_throttle_plugin: Add flb_hash as hash map replasing uthash.h
    
    Signed-off-by: I330716 <vladimir.vasilev@sap.com>
    
    * add support for windows
    
    Signed-off-by: I330716 <vladimir.vasilev@sap.com>
    
    * flb_filter_throttle_size: rename filter from size_throttle ro throttle_size
    
    Signed-off-by: I330716 <vladimir.vasilev@sap.com>

---
+++ plugins/filter_throttle_size/size_window.h
@@ -0,0 +1,141 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2019      The Fluent Bit Authors
+ *  Copyright (C) 2015-2018 Treasure Data Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef FLB_SIZE_WINDOW_H
+#define FLB_SIZE_WINDOW_H
+
+#include <fluent-bit/flb_hash.h>
+
+#define FLB_SIZE_WINDOW_HASH_MAX_ENTRIES 100000
+
+struct throttle_size_pane
+{
+    long timestamp;
+    unsigned long size;
+};
+
+struct throttle_size_window
+{
+    char *name;
+    unsigned size;
+    unsigned long total;
+    long timestamp;
+    int head;
+    int tail;
+    struct throttle_size_pane *table;
+};
+
+struct throttle_size_table
+{
+    struct flb_hash *windows;
+    void *lock;
+};
+
+struct throttle_size_table *create_throttle_size_table();
+
+struct throttle_size_window *size_window_create(const char *name,
+                                                unsigned name_length,
+                                                unsigned int size);
+
+/*This function adds new pane on top of the pane stack by overwriting the oldes one
+  which @timestamp and load size of 0 bytes. The oldes pane's amount of load size
+  is subtracted of the total amount.*/
+inline static void add_new_pane(struct throttle_size_window *stw,
+                                long timestamp)
+{
+    unsigned long tail_size = 0;
+    tail_size = stw->table[stw->tail].size;
+    if (stw->size - 1 == stw->head) {
+        /* the head will exceed the end of the inner array end must be put at the begging.  */
+        stw->head = -1;
+    }
+    stw->head += 1;
+    stw->table[stw->head].timestamp = timestamp;
+    stw->table[stw->head].size = 0;
+    stw->total -= tail_size;
+    if (stw->size - 1 == stw->tail) {
+        /* the tail will exceed the end of the inner array end must be put at the begging. */
+        stw->tail = -1;
+    }
+    stw->tail += 1;
+}
+
+/*This function adds @load to the latest pane which is on top of the pane stack.
+  @load is added to the total amount of the size throttling window.
+  If @load is not 0 then the size throttling window's timestamp will be updated to the
+  one which is on top of the pane stack(latest)*/
+inline static void add_load(struct throttle_size_window *stw,
+                            unsigned long load)
+{
+    stw->table[stw->head].size += load;
+    stw->total += load;
+    if (load) {
+        stw->timestamp = stw->table[stw->head].timestamp;
+    }
+}
+
+inline static void free_stw_content(struct throttle_size_window *stw)
+{
+    flb_free(stw->name);
+    flb_free(stw->table);
+}
+
+inline static void free_stw(struct throttle_size_window *stw)
+{
+    free_stw_content(stw);
+    flb_free(stw);
+}
+
+inline static struct throttle_size_window *find_throttle_size_window(struct
+                                                                     throttle_size_table
+                                                                     *table,
+                                                                     char
+                                                                     *name,
+                                                                     unsigned
+                                                                     name_length)
+{
+    char *window = NULL;
+    size_t out_size;
+    if (flb_hash_get(table->windows, name, name_length, (const char **)&window, &out_size) >=
+        0) {
+        if (out_size < sizeof(struct throttle_size_window)) {
+            flb_error("Malformed data in size window hashtable");
+            return NULL;
+        }
+        return (struct throttle_size_window *) window;
+    }
+    return NULL;
+}
+
+inline static void add_throttle_size_window(struct throttle_size_table
+                                            *table,
+                                            struct throttle_size_window
+                                            *window)
+{
+    flb_hash_add(table->windows, window->name, strlen(window->name),
+                 (char *) window, sizeof(struct throttle_size_window));
+}
+
+void destroy_throttle_size_table(struct throttle_size_table *table);
+
+void lock_throttle_size_table(struct throttle_size_table *ht);
+void unlock_throttle_size_table(struct throttle_size_table *ht);
+
+#endif
