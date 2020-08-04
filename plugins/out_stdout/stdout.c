c537af51e156e694fc5c34eba914b0acead97080
Eduardo Silva <eduardo@treasure-data.com>
2020-08-04 16:33:19 -0600

    out_stdout: support 'false' value to disable json_date_key
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- plugins/out_stdout/stdout.c
+++ plugins/out_stdout/stdout.c
@@ -64,6 +64,16 @@ static int cb_stdout_init(struct flb_output_instance *ins,
         }
     }
 
+    /* Date key */
+    ctx->date_key = ctx->json_date_key;
+    tmp = flb_output_get_property("json_date_key", ins);
+    if (tmp) {
+        /* Just check if we have to disable it */
+        if (flb_utils_bool(tmp) == FLB_FALSE) {
+            ctx->date_key = NULL;
+        }
+    }
+
     /* Date format for JSON output */
     ctx->json_date_format = FLB_PACK_JSON_DATE_DOUBLE;
     tmp = flb_output_get_property("json_date_format", ins);
@@ -104,7 +114,7 @@ static void cb_stdout_flush(const void *data, size_t bytes,
         json = flb_pack_msgpack_to_json_format(data, bytes,
                                                ctx->out_format,
                                                ctx->json_date_format,
-                                               ctx->json_date_key);
+                                               ctx->date_key);
         write(STDOUT_FILENO, json, flb_sds_len(json));
         flb_sds_destroy(json);
 
