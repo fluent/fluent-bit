01f5488eced28d533152a14c26551ea8dcf70417
Eduardo Silva <eduardo@treasure-data.com>
2020-10-14 15:16:47 -0600

    out_loki: new Loki output plugin! (#994)
    This patch adds a new plugin to deliver records to Loki (grafana).
    
    It supports the following configuration options:
    
      host                    Loki hostname or IP address
    
      port                    Loki TCP port
    
      tenant_id               Tenant ID used by default to push logs to Loki. If
                              omitted or empty it assumes Loki is running in
                              single-tenant mode and no X-Scope-OrgID header is sent.
                              > type: string
    
      labels                  labels for API requests.
                              > default: job="fluent-bit", type: multiple comma delimited strings
    
      auto_kubernetes_labels  If set to true, it will add all Kubernetes labels to
                              Loki labels.
                              > default: false, type: boolean
    
      label_keys              Comma separated list of keys to use as stream labels.
                              > type: multiple comma delimited strings
    
    One of the interesting features is that 'labels' and 'label_keys' support record
    accessor mode, e.g:
    
        [OUTPUT]
            name        loki
            match       *
            labels      job=fluentbit, rec=$sub['field']
            label_keys  $somekey
    
    Note that 'label_keys' must be prefixed with '$' since it's a record accessor pattern.
    
    This plugin is still in development, community feedback is welcome.
    
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

---
+++ plugins/out_loki/loki.h
@@ -0,0 +1,69 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2019-2020 The Fluent Bit Authors
+ *  Copyright (C) 2015-2018 Treasure Data Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef FLB_OUT_LOKI_H
+#define FLB_OUT_LOKI_H
+
+#include <fluent-bit/flb_output_plugin.h>
+#include <fluent-bit/flb_record_accessor.h>
+#include <fluent-bit/flb_upstream.h>
+
+#define FLB_LOKI_CT              "Content-Type"
+#define FLB_LOKI_CT_JSON         "application/json"
+#define FLB_LOKI_URI             "/loki/api/v1/push"
+#define FLB_LOKI_HOST            "127.0.0.1"
+#define FLB_LOKI_PORT            3100
+#define FLB_LOKI_HEADER_SCOPE    "X-Scope-OrgID"
+
+#define FLB_LOKI_KV_STR    0     /* sds string */
+#define FLB_LOKI_KV_RA     1     /* record accessor */
+#define FLB_LOKI_KV_K8S    2     /* kubernetes label */
+
+struct flb_loki_kv {
+    int val_type;                       /* FLB_LOKI_KV_STR or FLB_LOKI_KV_RA */
+    flb_sds_t key;                      /* string key */
+    flb_sds_t str_val;                  /* string value */
+    struct flb_record_accessor *ra_key; /* record accessor key context */
+    struct flb_record_accessor *ra_val; /* record accessor value context */
+    struct mk_list _head;               /* link to flb_loki->labels_list */
+};
+
+struct flb_loki {
+    /* Public configuration properties */
+    int auto_kubernetes_labels;
+    flb_sds_t tenant_id;
+    struct mk_list *labels;
+    struct mk_list *label_keys;
+
+    /* Private */
+    int tcp_port;
+    char *tcp_host;
+    flb_sds_t cache_labels;
+    struct flb_record_accessor *ra_k8s;
+    struct mk_list labels_list;     /* list of flb_loki_kv nodes */
+
+    /* Upstream Context */
+    struct flb_upstream *u;
+
+    /* Plugin instance */
+    struct flb_output_instance *ins;
+};
+
+#endif
