b5c326cc19d77cafeb439474771482699115add3
DavidKorczynski <david@adalogics.com>
2020-10-15 04:14:31 +0100

    tests: internal: fuzzer: add http-client, strptime and others refactorings  (#2685)
    Signed-off-by: davkor <david@adalogics.com>

--- tests/internal/fuzzers/parser_fuzzer.c
+++ tests/internal/fuzzers/parser_fuzzer.c
@@ -5,24 +5,12 @@
 #include <fluent-bit/flb_parser.h>
 #include <fluent-bit/flb_parser_decoder.h>
 
+#include "flb_fuzz_header.h"
+
 #define TYPES_LEN 5
-#define GET_MOD_EQ(max, idx) (data[0] % max) == idx
-#define MOVE_INPUT(offset) data += offset; size -= offset;
 
-char *get_null_terminated(size_t size, char **data, size_t *total_data_size)
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
-  char *tmp = flb_malloc(size+1);
-  memcpy(tmp, *data, size);
-  tmp[size] = '\0';
-
-  // Modify the fuzz variables
-  *total_data_size -= size;
-  *data += size;
-
-  return tmp;
-}
-
-int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size){
     char *format      = NULL;
     char *time_fmt    = NULL;
     char *time_key    = NULL;
