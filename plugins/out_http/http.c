fc299a925bb840b408b4a4c2de60a0949ab4aaff
Yu Yi <yiyu@google.com>
2020-11-05 23:37:29 +0000

    http_client: address PR comments related to readability
    Signed-off-by: Yu Yi <yiyu@google.com>

--- plugins/out_http/http.c
+++ plugins/out_http/http.c
@@ -114,7 +114,8 @@ static int http_post(struct flb_out_http *ctx,
                         ctx->proxy, 0);
 
 
-    flb_debug("[http_client] proxy host: %s port: %i", c->proxy.host, c->proxy.port);
+    flb_plg_debug(ctx->ins, "[http_client] proxy host: %s port: %i",
+                  c->proxy.host, c->proxy.port);
 
     /* Allow duplicated headers ? */
     flb_http_allow_duplicated_headers(c, ctx->allow_dup_headers);
