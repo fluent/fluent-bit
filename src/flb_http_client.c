d75062d7cd6935cab4aab8e971ec6a91a50688d5
Eduardo Silva <eduardo@treasure-data.com>
2020-11-07 08:27:39 -0600

    http_client: on proxy parse exception, do not release linked buf (oss-fuzz 27279)
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- src/flb_http_client.c
+++ src/flb_http_client.c
@@ -717,7 +717,6 @@ struct flb_http_client *flb_http_client(struct flb_upstream_conn *u_conn,
         ret = proxy_parse(proxy, c);
         if (ret != 0) {
             flb_debug("[http_client] Something wrong with the http_proxy parsing");
-            flb_free(buf);
             flb_http_client_destroy(c);
             return NULL;
         }
