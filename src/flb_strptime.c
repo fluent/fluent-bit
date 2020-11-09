fluent/fluent-bit

commit 163af0b9c0513f31cc4cbfe8aeee26d22513163c

strptime: fix bounds check (oss-fuzz 27241)
Signed-off-by: davkor <david@adalogics.com>

src/flb_strptime.c
@@ -168,6 +168,13 @@ _flb_strptime(const char *buf, const char *fmt, struct tm *tm, int initialize)
 			continue;
 		}
 
+        /*
+         * Having increased bp we need to ensure we are not
+         * moving beyond bounds.
+         */
+        if (*bp == '\0')
+           return (NULL);
+
 		if ((c = *fmt++) != '%')
 			goto literal;
 
@@ -177,6 +184,13 @@ again:		switch (c = *fmt++) {
 literal:
 		if (c != *bp++)
 			return (NULL);
+
+        /*
+         * Having increased bp we need to ensure we are not
+         * moving beyond bounds.
+         */
+        if (*bp == '\0')
+           return (NULL);
 
 		break;
 

commit 4748db6b4c74fb39d3a9d9aab5dade2254aaaf9d

strptime: fix regression
This commit https://github.com/fluent/fluent-bit/commit/163af0b9c0513f31cc4cbfe8aeee26d22513163c introduced a regression that caused parser tests to fail.

We only need bounds checking in the first place added by this commit, not the second.

src/flb_strptime.c
@@ -185,13 +185,6 @@ literal:
 		if (c != *bp++)
 			return (NULL);
 
-        /*
-         * Having increased bp we need to ensure we are not
-         * moving beyond bounds.
-         */
-        if (*bp == '\0')
-           return (NULL);
-
 		break;
 
 		/*