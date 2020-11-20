e2dac397a8f47b96c61c5221cee58c5c8ba2b7b5
Eduardo Silva <eduardo@treasure-data.com>
2020-11-20 11:30:59 -0600

    lib: libco: sync changes for FreeBSD and non-gcc compilers
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- lib/flb_libco/settings.h
+++ lib/flb_libco/settings.h
@@ -42,6 +42,8 @@
   #define text_section __declspec(allocate(".text"))
 #elif defined(__APPLE__) && defined(__MACH__)
   #define text_section __attribute__((section("__TEXT,__text")))
+#elif defined(__clang__)
+  #define text_section __attribute__((section(".text")))
 #else
   #define text_section __attribute__((section(".text#")))
 #endif
