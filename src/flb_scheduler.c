a56a90d0a544d93d96f5e63f2284c21855dafaab
Palle Girgensohn <girgen@pingpong.net>
2020-11-20 18:35:52 +0100

    scheduler: fix problem with SIGSEGV on FreeBSD 12+ (#2781)
    This is related to https://github.com/fluent/fluent-bit/pull/2463 for MacOS.
    
    In flb_pipe_r() function is returning 0 on FreeBSD 12+ and that brings up
    errors similar to "[error] [src/flb_scheduler.c:52 errno=0] No error: 0". This
    patch will fix it the same way as it was previously fixed for MacOS.
    
    This patch was tested on FreeBSD 11.4 and 12.2 amd64.  The resulting binaries
    was tested with "elfdump -a" to contain only one ".text" section and their
    startup logs were tested running "fluent-bit -v -i dummy -o stdout".
    
    See also: https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=250825
    Submitted by: Yuri Pankov and Artyom Davidov
    Signed-off-by: Palle Girgensohn <girgen@FreeBSD.org>
    
    Co-authored-by: Palle Girgensohn <girgen@FreeBSD.org>

--- src/flb_scheduler.c
+++ src/flb_scheduler.c
@@ -44,7 +44,7 @@ static inline int consume_byte(flb_pipefd_t fd)
 
     /* We need to consume the byte */
     ret = flb_pipe_r(fd, &val, sizeof(val));
-#ifdef __APPLE__
+#if defined(__APPLE__) || __FreeBSD__ >= 12
     if (ret < 0) {
 #else
     if (ret <= 0) {
