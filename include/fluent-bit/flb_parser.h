a47b75f060f3a9113c8c977a5a1bcbbd712e0add
Fujimoto Seiji <fujimoto@ceptord.net>
2020-11-27 16:50:23 +0900

    parser: Add a new option 'Time_Strict'
    The old behaviour was that it accepts time data only if the
    format is specified exactly. If the format does not match, it
    simply returns an error.
    
    However, with the new 'Time_Strict' option, a time parser can
    behave in "best effort" mode, and be permissive on heterogeneous
    time data.
    
    For example, suppose you set up the following time format:
    
        Time_Format %Y-%m-%dT%H:%M:%S.%L
    
    ... and received the record like this:
    
        {"time":"2020-11-10T11:11:26+00:00"}
    
    With `Time_Strict` turned off, you'll get the following result:
    
        "2020-11-10T11:11:26+00:00" => 2020-11-10T11:11:26.000000Z
    
    Signed-off-by: Fujimoto Seiji <fujimoto@ceptord.net>

--- include/fluent-bit/flb_parser.h
+++ include/fluent-bit/flb_parser.h
@@ -49,6 +49,7 @@ struct flb_parser {
     char *time_key;       /* field name that contains the time */
     int time_offset;      /* fixed UTC offset */
     int time_keep;        /* keep time field */
+    int time_strict;      /* parse time field strictly */
     char *time_frac_secs; /* time format have fractional seconds ? */
     struct flb_parser_types *types; /* type casting */
     int types_len;
@@ -92,6 +93,7 @@ struct flb_parser *flb_parser_create(const char *name, const char *format,
                                      const char *time_fmt, const char *time_key,
                                      const char *time_offset,
                                      int time_keep,
+                                     int time_strict,
                                      struct flb_parser_types *types,
                                      int types_len,
                                      struct mk_list *decoders,
