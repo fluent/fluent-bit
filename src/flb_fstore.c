b0996648ec8578b26ead91fd2181f1ccd4a14182
Wesley Pettit <wppttt@amazon.com>
2021-01-07 21:54:47 -0600

    fstore: fix wrong data type and missing initialization of structure (#2909)
    Author: Wesley Pettit <wppttt@amazon.com>
    Co-authored-by: Eduardo Silva <eduardo@treasure-data.com>

--- src/flb_fstore.c
+++ src/flb_fstore.c
@@ -162,6 +162,7 @@ struct flb_fstore_file *flb_fstore_file_create(struct flb_fstore *fs,
     }
 
     fsf->chunk = chunk;
+    fsf->stream = fs_stream->stream;
     mk_list_add(&fsf->_head, &fs_stream->files);
 
     return fsf;
