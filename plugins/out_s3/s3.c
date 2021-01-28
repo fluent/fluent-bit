f0f7365c14f5500f8885818ad42866501a70406d
Wesley Pettit <wppttt@amazon.com>
2021-01-27 17:34:46 -0800

    out_s3: use new credential api for mutli worker
    Signed-off-by: Wesley Pettit <wppttt@amazon.com>

--- plugins/out_s3/s3.c
+++ plugins/out_s3/s3.c
@@ -463,7 +463,8 @@ static int cb_s3_init(struct flb_output_instance *ins,
                                                        ctx->region,
                                                        ctx->sts_endpoint,
                                                        NULL,
-                                                       flb_aws_client_generator());
+                                                       flb_aws_client_generator(),
+                                                       ctx->ins);
 
     if (!ctx->provider) {
         flb_plg_error(ctx->ins, "Failed to create AWS Credential Provider");
@@ -512,11 +513,12 @@ static int cb_s3_init(struct flb_output_instance *ins,
                                                 ctx->region,
                                                 ctx->sts_endpoint,
                                                 NULL,
-                                                flb_aws_client_generator());
+                                                flb_aws_client_generator(),
+                                                ctx->ins);
         flb_free(session_name);
         if (!ctx->provider) {
             flb_plg_error(ctx->ins, "Failed to create AWS STS Credential "
-                         "Provider");
+                          "Provider");
             return -1;
         }
 
