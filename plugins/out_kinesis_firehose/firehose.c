a5ec3bf4536f09dfdd19eefd6c1395ee59b020f3
Wesley Pettit <wppttt@amazon.com>
2021-01-27 17:35:49 -0800

    out_kinesis_firehose: use new cred api for multi worker
    Signed-off-by: Wesley Pettit <wppttt@amazon.com>

--- plugins/out_kinesis_firehose/firehose.c
+++ plugins/out_kinesis_firehose/firehose.c
@@ -171,7 +171,8 @@ static int cb_firehose_init(struct flb_output_instance *ins,
                                                            (char *) ctx->region,
                                                            ctx->sts_endpoint,
                                                            NULL,
-                                                           flb_aws_client_generator());
+                                                           flb_aws_client_generator(),
+                                                           ctx->ins);
     if (!ctx->aws_provider) {
         flb_plg_error(ctx->ins, "Failed to create AWS Credential Provider");
         goto error;
@@ -211,7 +212,8 @@ static int cb_firehose_init(struct flb_output_instance *ins,
                                                     (char *) ctx->region,
                                                     ctx->sts_endpoint,
                                                     NULL,
-                                                    flb_aws_client_generator());
+                                                    flb_aws_client_generator(),
+                                                    ctx->ins);
         if (!ctx->aws_provider) {
             flb_plg_error(ctx->ins,
                           "Failed to create AWS STS Credential Provider");
