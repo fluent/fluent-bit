11b1e497f9419fdc93594a8b635ff4fdbfa60ea8
Eduardo Silva <eduardo@treasure-data.com>
2021-02-12 21:17:02 -0600

    filter_rewrite_tag: validate new tag (CID 316594)
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- plugins/filter_rewrite_tag/rewrite_tag.c
+++ plugins/filter_rewrite_tag/rewrite_tag.c
@@ -322,6 +322,11 @@ static int process_record(const char *tag, int tag_len, msgpack_object map,
     /* Release any capture info from 'results' */
     flb_regex_results_release(&result);
 
+    /* Validate new outgoing tag */
+    if (!out_tag) {
+        return FLB_FALSE;
+    }
+
     /* Emit record with new tag */
     ret = in_emitter_add_record(out_tag, flb_sds_len(out_tag), buf, buf_size,
                                 ctx->ins_emitter);
