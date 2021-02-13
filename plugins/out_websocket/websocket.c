ee8d191165382c0ddbfbc39d4bc89a72f0af3e21
Eduardo Silva <eduardo@treasure-data.com>
2021-02-12 21:19:09 -0600

    out_websocket: fix leak on exception (CID 316420)
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- plugins/out_websocket/websocket.c
+++ plugins/out_websocket/websocket.c
@@ -49,7 +49,7 @@ static int flb_ws_handshake(struct flb_upstream_conn *u_conn,
         flb_error("[output_ws] upstream connection error");
         return -1;
     }
-	
+
     /* Compose HTTP Client request */
     c = flb_http_client(u_conn, FLB_HTTP_GET, ctx->uri,
                         NULL, 0, NULL, 0, NULL, 0);
@@ -63,10 +63,10 @@ static int flb_ws_handshake(struct flb_upstream_conn *u_conn,
     flb_http_add_header(c, "Connection", 10, "Upgrade", 7);
     flb_http_add_header(c, "Sec-WebSocket-Key", 17, "dGhlIHNhbXBsZSBub25jZQ==", 24);
     flb_http_add_header(c, "Sec-WebSocket-Version", 21, "13", 2);
-	
+
     /* Perform request*/
     ret = flb_http_do(c, &bytes_sent);
-	
+
     if (ret != 0 || c->resp.status != 101) {
         if (c->resp.payload_size > 0) {
             flb_debug("[output_ws] Websocket Server Response\n%s",
@@ -89,17 +89,17 @@ static void flb_ws_mask(char *data, int len, char *mask)
     }
 }
 
-static int flb_ws_sendDataFrameHeader(struct flb_upstream_conn *u_conn, 
+static int flb_ws_sendDataFrameHeader(struct flb_upstream_conn *u_conn,
                                       struct flb_out_ws *ctx, const void *data, size_t bytes)
-{		
-    int ret = -1;	
+{
+    int ret = -1;
     char* data_frame_head;
     size_t bytes_sent;
     int data_frame_head_len = 0;
     //TODO use random function to generate masking_key
     char masking_key[4] = {0x12, 0x34, 0x56, 0x78};
     unsigned long long payloadSize = bytes;
-    
+
     flb_ws_mask((char *)data, payloadSize, masking_key);
     if (payloadSize < 126) {
         data_frame_head = (char *)flb_malloc(6);
@@ -163,14 +163,14 @@ static int flb_ws_sendDataFrameHeader(struct flb_upstream_conn *u_conn,
 
 error:
     flb_free(data_frame_head);
-    return -1;	
+    return -1;
 }
 
 static int cb_ws_init(struct flb_output_instance *ins,
                       struct flb_config *config, void *data)
 {
     struct flb_out_ws *ctx = NULL;
-    
+
     ctx = flb_ws_conf_create(ins, config);
     if (!ctx) {
         return -1;
@@ -206,7 +206,7 @@ static void cb_ws_flush(const void *data, size_t bytes,
     /* Get upstream context and connection */
     u = ctx->u;
     u_conn = flb_upstream_conn_get(u);
-    
+
     if (!u_conn) {
         flb_error("[out_ws] no upstream connections available to %s:%i", u->tcp_host, u->tcp_port);
         ctx->handshake = 1;
@@ -215,13 +215,13 @@ static void cb_ws_flush(const void *data, size_t bytes,
 
     now = time(NULL);
 
-    //TODO how to determine the interval? conn disconnet is about 30 sec, so we set 20 ssecnds here. 
+    //TODO how to determine the interval? conn disconnet is about 30 sec, so we set 20 ssecnds here.
     flb_debug("[out_ws] interval is  %ld and handshake is %d", now - ctx->last_input_timestamp, ctx->handshake);
     if ((now - ctx->last_input_timestamp > ctx->idle_interval) && (ctx->handshake == 0)) {
         ctx->handshake = 1;
         flb_upstream_conn_release(u_conn);
         FLB_OUTPUT_RETURN(FLB_RETRY);
-    } 
+    }
     ctx->last_input_timestamp = now;
 
     if (ctx->handshake == 1) {
@@ -241,7 +241,7 @@ static void cb_ws_flush(const void *data, size_t bytes,
                                                ctx->out_format,
                                                ctx->json_date_format,
                                                ctx->json_date_key);
-        
+
         if (!json) {
             flb_error("[out_ws] error formatting JSON payload");
             flb_upstream_conn_release(u_conn);
@@ -256,9 +256,13 @@ static void cb_ws_flush(const void *data, size_t bytes,
     else {
         ret = flb_ws_sendDataFrameHeader(u_conn, ctx, json, flb_sds_len(json));
     }
+
     if (ret == -1) {
         flb_error("[out_ws] dataFrameHeader sent failed");
         ctx->handshake = 1;
+        if (json) {
+            flb_sds_destroy(json);
+        }
         flb_upstream_conn_release(u_conn);
         FLB_OUTPUT_RETURN(FLB_RETRY);
     }
@@ -278,7 +282,7 @@ static void cb_ws_flush(const void *data, size_t bytes,
         flb_upstream_conn_release(u_conn);
         FLB_OUTPUT_RETURN(FLB_RETRY);
     }
-	
+
     /* Release the connection */
     flb_upstream_conn_release(u_conn);
     FLB_OUTPUT_RETURN(FLB_OK);
