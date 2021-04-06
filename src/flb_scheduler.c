e0865a8d856c01e82cff906c81fe80b84a3c9f1d
Eduardo Silva <edsiper@gmail.com>
2021-04-06 16:42:02 -0600

    scheduler: fix double close of timer_fd (#3058 #3326)
    When a retry is requested, a timer is created and a 'request' for a
    'retry' associated to it. Upon 'request' context destroy, the code
    is wrongly closing the file descriptor of the timer, and the timer
    handler closing it again leading to file descriptor re-use and
    further corruptions on the other components of the engine.
    
    Huge thanks to @hsmatulisgoogle @qingling128 @igorpeshansky @quentinmit
    @davidbtucker and @agup006 for helping to troubleshooting this
    issue!
    
    Signed-off-by: Eduardo Silva <edsiper@gmail.com>

--- src/flb_scheduler.c
+++ src/flb_scheduler.c
@@ -336,11 +336,6 @@ int flb_sched_request_destroy(struct flb_sched_request *req)
      */
     flb_sched_timer_invalidate(timer);
 
-#ifndef FLB_SYSTEM_WINDOWS
-    /* Close pipe after invalidating timer */
-    flb_pipe_close(req->fd);
-#endif
-
     /* Remove request */
     flb_free(req);
 
