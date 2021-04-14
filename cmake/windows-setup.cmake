ef9b6e493d33ed2769f0e39e43482919a41ca3f2
Eduardo Silva <edsiper@gmail.com>
2021-04-14 12:28:47 -0600

    build: windows: enable HTTP server and metrics
    Signed-off-by: Eduardo Silva <edsiper@gmail.com>

--- cmake/windows-setup.cmake
+++ cmake/windows-setup.cmake
@@ -11,6 +11,7 @@ set(FLB_PARSER                Yes)
 set(FLB_TLS                   Yes)
 set(FLB_AWS                   Yes)
 set(FLB_HTTP_SERVER            No)
+set(FLB_METRICS               Yes)
 
 # INPUT plugins
 # =============
