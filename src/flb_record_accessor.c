1e23dbbe3fdd569533cc60fed573b4dda920ca7c
davkor <david@adalogics.com>
2021-05-05 10:50:31 +0100

    record_accessor: limit length check only to floats.
    Signed-off-by: davkor <david@adalogics.com>

--- src/flb_record_accessor.c
+++ src/flb_record_accessor.c
@@ -455,21 +455,16 @@ static flb_sds_t ra_translate_keymap(struct flb_ra_parser *rp, flb_sds_t buf,
     }
     else if (v->type == FLB_RA_INT) {
         len = snprintf(str, sizeof(str) - 1, "%" PRId64, v->val.i64);
-        /* We need to check size is not above str length */
-        if (len >= 32) {
-            *found = FLB_FALSE;
-            return buf;
-        }
         tmp = flb_sds_cat(buf, str, len);
     }
     else if (v->type == FLB_RA_FLOAT) {
         len = snprintf(str, sizeof(str) - 1, "%f", v->val.f64);
-        /* We need to check size is not above str length */
-        if (len >= 32) {
-            *found = FLB_FALSE;
-            return buf;
+        if (len >= sizeof(str)) {
+            tmp = flb_sds_cat(buf, str, sizeof(str)-1);
         }
-        tmp = flb_sds_cat(buf, str, len);
+        else {
+            tmp = flb_sds_cat(buf, str, len);
+        }
     }
     else if (v->type == FLB_RA_STRING) {
         tmp = flb_sds_cat(buf, v->val.string, flb_sds_len(v->val.string));
