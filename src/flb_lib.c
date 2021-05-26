6c446e35319193de9968fd653ece152cbfa7c73d
Drew Zhang <zhayuzhu@amazon.com>
2021-05-26 13:48:13 -0700

    lib: add support for aws customsized error reporting logger
    Signed-off-by: Drew Zhang <zhayuzhu@amazon.com>

--- src/flb_lib.c
+++ src/flb_lib.c
@@ -40,6 +40,12 @@
 #include <mcheck.h>
 #endif
 
+#ifdef FLB_HAVE_AWS_ERROR_REPORTER
+#include <fluent-bit/aws/flb_aws_error_reporter.h>
+
+struct flb_aws_error_reporter *error_reporter;
+#endif
+
 /* thread initializator */
 static pthread_once_t flb_lib_once = PTHREAD_ONCE_INIT;
 
@@ -189,6 +195,12 @@ flb_ctx_t *flb_create()
         return NULL;
     }
 
+    #ifdef FLB_HAVE_AWS_ERROR_REPORTER
+    if (is_error_reporting_enabled()) {
+        error_reporter = flb_aws_error_reporter_create();
+    }
+    #endif
+
     return ctx;
 }
 
@@ -214,6 +226,12 @@ void flb_destroy(flb_ctx_t *ctx)
         }
         flb_config_exit(ctx->config);
     }
+
+    #ifdef FLB_HAVE_AWS_ERROR_REPORTER
+    if (is_error_reporting_enabled()) {
+        flb_aws_error_reporter_destroy(error_reporter);
+    }
+    #endif
 
     flb_free(ctx);
     ctx = NULL;
