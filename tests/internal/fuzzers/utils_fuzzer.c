7a3a7149b7e5484f23f43fee9fe52a27696cd53a
DavidKorczynski <david@adalogics.com>
2021-05-31 23:07:14 +0100

    tests: internal: fuzzers: add timeout checks and add license. (#3560)
    Signed-off-by: davkor <david@adalogics.com>

--- tests/internal/fuzzers/utils_fuzzer.c
+++ tests/internal/fuzzers/utils_fuzzer.c
@@ -1,3 +1,21 @@
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2019-2021 The Fluent Bit Authors
+ *  Copyright (C) 2015-2018 Treasure Data Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
@@ -9,10 +27,12 @@
 #include <fluent-bit/flb_uri.h>
 #include <fluent-bit/flb_sha512.h>
 #include <fluent-bit/flb_regex.h>
+#include "flb_fuzz_header.h"
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
-    /* We need a bit of data in this fuzzer */
+    TIMEOUT_GUARD
+
     if (size < 600) {
         return 0;
     }
