55c4a4cdb14d71c8a6a1da728e2d5fc593097845
Shikugawa <Shikugawa@gmail.com>
2021-06-17 19:30:16 +0900

    out-http: fix potential crash after failed to create HTTP client
    Signed-off-by: Shikugawa <Shikugawa@gmail.com>

--- plugins/out_http/http.c
+++ plugins/out_http/http.c
@@ -112,7 +112,10 @@ static int http_post(struct flb_out_http *ctx,
                         payload_buf, payload_size,
                         ctx->host, ctx->port,
                         ctx->proxy, 0);
-
+    if (!c) {
+        flb_plg_error(ctx->ins, "[http_client] failed to create HTTP client");
+        return FLB_RETRY;
+    }
 
     if (c->proxy.host) {
         flb_plg_debug(ctx->ins, "[http_client] proxy host: %s port: %i",
