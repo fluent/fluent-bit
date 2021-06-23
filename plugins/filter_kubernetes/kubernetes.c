dbf8992ad67c3172751b996ad34dfae7b37be255
Zhonghui Hu <zh0512xx@gmail.com>
2021-06-23 16:35:29 -0700

    filter_kubernetes: add more detailed description for kube_meta_cache_ttl
    Signed-off-by: Zhonghui Hu <zh0512xx@gmail.com>

--- plugins/filter_kubernetes/kubernetes.c
+++ plugins/filter_kubernetes/kubernetes.c
@@ -839,7 +839,12 @@ static struct flb_config_map config_map[] = {
     {
      FLB_CONFIG_MAP_TIME, "kube_meta_cache_ttl", "0",
      0, FLB_TRUE, offsetof(struct flb_kube, kube_meta_cache_ttl),
-     "configurable TTL for K8s cached metadata"
+     "configurable TTL for K8s cached metadata. " 
+     "By default, it is set to 0 which means TTL for cache entries is disabled and " 
+     "cache entries are evicted at random when capacity is reached. " 
+     "In order to enable this option, you should set the number to a time interval. " 
+     "For example, set this value to 60 or 60s and cache entries " 
+     "which have been created more than 60s will be evicted"
     },
     /* EOF */
     {0}
