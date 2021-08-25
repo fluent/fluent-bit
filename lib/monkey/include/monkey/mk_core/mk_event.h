86a3eb162d978b9c690f07e4b07f024036506ba6
Eduardo Silva <eduardo@calyptia.com>
2021-08-24 20:30:09 -1000

    lib: monkey: sync dev changes
    Signed-off-by: Eduardo Silva <eduardo@calyptia.com>

--- lib/monkey/include/monkey/mk_core/mk_event.h
+++ lib/monkey/include/monkey/mk_core/mk_event.h
@@ -131,6 +131,7 @@ int mk_event_add(struct mk_event_loop *loop, int fd,
 int mk_event_del(struct mk_event_loop *loop, struct mk_event *event);
 int mk_event_timeout_create(struct mk_event_loop *loop,
                             time_t sec, long nsec,void *data);
+int mk_event_timeout_disable(struct mk_event_loop *loop, void *data);
 int mk_event_timeout_destroy(struct mk_event_loop *loop, void *data);
 int mk_event_channel_create(struct mk_event_loop *loop,
                             int *r_fd, int *w_fd, void *data);
