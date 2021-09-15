a06faa58e0f19a39686b8d2397cb2748ff94ecd1
Philip Soares <philips@netisense.com>
2021-09-15 09:56:28 -0700

    out_s3: add back flb_sds_destroy() lost in 8cd50f8 (#4038)
    Fixes the memory loss incurred without it.
    
    Fixes #4038.
    
    Signed-off-by: Philip Soares <philips@netisense.com>

--- plugins/out_s3/s3.c
+++ plugins/out_s3/s3.c
@@ -1547,6 +1547,7 @@ static int send_upload_request(void *out_context, flb_sds_t chunk,
 
     /* Create buffer to upload to S3 */
     ret = construct_request_buffer(ctx, chunk, upload_file, &buffer, &buffer_size);
+    flb_sds_destroy(chunk);
     if (ret < 0) {
         flb_plg_error(ctx->ins, "Could not construct request buffer for %s",
                       upload_file->file_path);
