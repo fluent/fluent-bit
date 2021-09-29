f395ce09c78316ae5d4d46849c1a366b716d590e
Matthew Fala <falamatt@amazon.com>
2021-09-29 12:36:21 -0700

    out_cloudwatch_logs: resolve unpaired trailing backslash sanitization failure
    When logs are truncated to 262118 bytes, an escaped backslash may be
    truncated into an unescaped backslash.
    
    This trailing unescaped backslash escapes the preceding character in
    the cloudwatch PUT payload invalidating the JSON's well-formed
    structure and triggering AWS cloudwatch api to throw a
    'SerializationException'
    
    Signed-off-by: Matthew Fala <falamatt@amazon.com>

--- plugins/out_cloudwatch_logs/cloudwatch_api.c
+++ plugins/out_cloudwatch_logs/cloudwatch_api.c
@@ -353,6 +353,33 @@ static void set_stream_time_span(struct log_stream *stream, struct cw_event *eve
     }
 }
 
+/*
+ * Truncate log if needed. If truncated, only `written` is modified
+ * returns FLB_TRUE if truncated
+ */
+static int truncate_log(const struct flb_cloudwatch *ctx, const char *log_buffer,
+                         size_t *written) {
+    size_t trailing_backslash_count = 0;
+
+    if (*written > MAX_EVENT_LEN) {
+        flb_plg_warn(ctx->ins, "[size=%zu] Truncating event which is larger than "
+                        "max size allowed by CloudWatch", *written);
+        *written = MAX_EVENT_LEN;
+
+        /* remove trailing unescaped backslash if inadvertently synthesized */
+        while (trailing_backslash_count < *written &&
+                log_buffer[(*written - 1) - trailing_backslash_count] == '\\') {
+            trailing_backslash_count++;
+        }
+        if (trailing_backslash_count % 2 == 1) {
+            /* odd number of trailing backslashes, remove unpaired backslash */
+            (*written)--;
+        }
+        return FLB_TRUE;
+    }
+    return FLB_FALSE;
+}
+
 
 /*
  * Processes the msgpack object
@@ -422,24 +449,13 @@ int process_event(struct flb_cloudwatch *ctx, struct cw_flush *buf,
             return 1;
         }
 
-        if (written > MAX_EVENT_LEN) {
-            flb_plg_warn(ctx->ins, "[size=%zu] Truncating event which is larger than "
-                         "max size allowed by CloudWatch", written);
-            written = MAX_EVENT_LEN;
-        }
+        /* truncate log, if needed */
+        truncate_log(ctx, buf->event_buf, &written);
 
         /* copy serialized json to tmp_buf */
         if (!strncpy(tmp_buf_ptr, buf->event_buf, written)) {
             return -1;
         }
-
-        buf->tmp_buf_offset += written;
-        event = &buf->events[buf->event_index];
-        event->json = tmp_buf_ptr;
-        event->len = written;
-        event->timestamp = (unsigned long long) (tms->tm.tv_sec * 1000 +
-                                                 tms->tm.tv_nsec/1000000);
-
     }
     else {
         /*
@@ -448,21 +464,20 @@ int process_event(struct flb_cloudwatch *ctx, struct cw_flush *buf,
          * and last character
          */
         written -= 2;
-
-        if (written > MAX_EVENT_LEN) {
-            flb_plg_warn(ctx->ins, "[size=%zu] Truncating event which is larger than "
-                         "max size allowed by CloudWatch", written);
-            written = MAX_EVENT_LEN;
-        }
-
         tmp_buf_ptr++; /* pass over the opening quote */
-        buf->tmp_buf_offset += (written + 1);
-        event = &buf->events[buf->event_index];
-        event->json = tmp_buf_ptr;
-        event->len = written;
-        event->timestamp = (unsigned long long) (tms->tm.tv_sec * 1000 +
-                                                 tms->tm.tv_nsec/1000000);
+        buf->tmp_buf_offset++; /* advance tmp_buf past opening quote */
+
+        /* truncate log, if needed */
+        truncate_log(ctx, tmp_buf_ptr, &written);
     }
+
+    /* add log to events list */
+    buf->tmp_buf_offset += written;
+    event = &buf->events[buf->event_index];
+    event->json = tmp_buf_ptr;
+    event->len = written;
+    event->timestamp = (unsigned long long) (tms->tm.tv_sec * 1000ull +
+                                                tms->tm.tv_nsec/1000000);
 
     return 0;
 }
