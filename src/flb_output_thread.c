7a95c13d1446011a18c24a1d551c36fe8d77b72a
Eduardo Silva <eduardo@calyptia.com>
2021-11-14 22:09:39 -0600

    output: new flush callback prototype and renaming
    The following patch introduces 2 relevant changes:
    
    1. flush callback prototype change
    
       On this patch we change the callback prototype, before we used to pass
       different parameters as msgpack buffer, it size, tag, tag, len, input
       instance, output context and Fluent Bit context.
    
       While extending Fluent Bit some plugins might need access to other type
       of information, but changing the prototype of the function is very
       expensive in terms of maintenance; for simplicity reasons now we pass
       two new 'structure' arguments that consolidate the information (and also
       help us to add more info later without general plugins maintenance cost.
    
       The new prototype is:
    
       void (*cb_flush) (struct flb_event_chunk *,    // new struct flb_event_chunk
                         struct flb_output_flush *,   // new struct flb_output_flush
                         struct flb_input_instance *, // origin input instance
                         void *,                      // plugin custom context
                         struct flb_config *);        // Fluent Bit context
    
       The structure flb_event_chunk concentrate most of the chunk information plus
       other items such as counter for the number of events and a Tag in sds format.
    
       The other flb_output_flush argument is added and explained below.
    
    2. struct flb_output_flush
    
       Before invoking a flush callback, we used to create a context called
       struct flb_output_coro, that contained a 'coroutine' context (coro) plus
       other information. This was a bit confusing which did not abstract the internals
       properly.
    
       On this patch the structure has been renamed to 'struct flb_output_flush' and other
       internal lists are renamed from 'coros' to 'flush_list', and 'coros_destroy' to
       'flush_list_destroy'.
    
       With this new change we create a new concept of "flushing context" that now is
       passed to the flush callback. This context aims to concentrate information
       associated to the output instance.
    
    At this level, the output plugins prototypes has not yet been migrated. Next patch
    series fixes that.
    
    Signed-off-by: Eduardo Silva <eduardo@calyptia.com>

--- src/flb_output_thread.c
+++ src/flb_output_thread.c
@@ -178,9 +178,9 @@ static void output_thread(void *data)
     struct flb_task *task;
     struct flb_upstream_conn *u_conn;
     struct flb_output_instance *ins;
-    struct flb_output_coro *out_coro;
+    struct flb_output_flush *out_flush;
     struct flb_out_thread_instance *th_ins = data;
-    struct flb_out_coro_params *params;
+    struct flb_out_flush_params *params;
     struct flb_net_dns dns_ctx;
 
     /* Register thread instance */
@@ -284,17 +284,14 @@ static void output_thread(void *data)
                 }
 
                 /* Start the co-routine with the flush callback */
-                out_coro = flb_output_coro_create(task,
-                                                  task->i_ins,
-                                                  th_ins->ins,
-                                                  th_ins->config,
-                                                  task->buf, task->size,
-                                                  task->tag,
-                                                  task->tag_len);
-                if (!out_coro) {
+                out_flush = flb_output_flush_create(task,
+                                                    task->i_ins,
+                                                    th_ins->ins,
+                                                    th_ins->config);
+                if (!out_flush) {
                     continue;
                 }
-                flb_coro_resume(out_coro->coro);
+                flb_coro_resume(out_flush->coro);
             }
             else if (event->type == FLB_ENGINE_EV_CUSTOM) {
                 event->handler(event);
@@ -332,7 +329,7 @@ static void output_thread(void *data)
         flb_sched_timer_cleanup(sched);
 
         /* Check if we should stop the event loop */
-        if (stopping == FLB_TRUE && mk_list_size(&th_ins->coros) == 0) {
+        if (stopping == FLB_TRUE && mk_list_size(&th_ins->flush_list) == 0) {
             /*
              * If there are no busy network connections (and no coroutines) its
              * safe to stop it.
@@ -359,7 +356,7 @@ static void output_thread(void *data)
     mk_event_loop_destroy(th_ins->evl);
 
     flb_sched_destroy(sched);
-    params = FLB_TLS_GET(out_coro_params);
+    params = FLB_TLS_GET(out_flush_params);
     if (params) {
         flb_free(params);
     }
@@ -431,10 +428,10 @@ int flb_output_thread_pool_create(struct flb_config *config,
 
         th_ins->config = config;
         th_ins->ins = ins;
-        th_ins->coro_id = 0;
-        mk_list_init(&th_ins->coros);
-        mk_list_init(&th_ins->coros_destroy);
-        pthread_mutex_init(&th_ins->coro_mutex, NULL);
+        th_ins->flush_id = 0;
+        mk_list_init(&th_ins->flush_list);
+        mk_list_init(&th_ins->flush_list_destroy);
+        pthread_mutex_init(&th_ins->flush_mutex, NULL);
         mk_list_init(&th_ins->upstreams);
 
         upstream_thread_create(th_ins, ins);
@@ -500,9 +497,9 @@ int flb_output_thread_pool_coros_size(struct flb_output_instance *ins)
 
         th_ins = th->params.data;
 
-        pthread_mutex_lock(&th_ins->coro_mutex);
-        n = mk_list_size(&th_ins->coros);
-        pthread_mutex_unlock(&th_ins->coro_mutex);
+        pthread_mutex_lock(&th_ins->flush_mutex);
+        n = mk_list_size(&th_ins->flush_list);
+        pthread_mutex_unlock(&th_ins->flush_mutex);
 
         size += n;
     }
