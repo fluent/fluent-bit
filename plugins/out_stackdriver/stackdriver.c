af8097fe6c34afaabe8b6aa26cabade1d87acf4a
Eduardo Silva <eduardo@calyptia.com>
2021-11-15 14:47:41 -0600

    out_stackdriver: migrate to new flush callback prototype
    In a recent change the prototype of the flush function (callback)
    for output plugins has changed:
    
      --
      On this patch we change the callback prototype, before we used to pass
      different parameters as msgpack buffer, it size, tag, tag, len, input
      instance, output context and Fluent Bit context.
    
      While extending Fluent Bit some plugins might need access to other type
      of information, but changing the prototype of the function is very
      expensive in terms of maintenance; for simplicity reasons now we pass
      two new 'structure' arguments that consolidate the information (and also
      help us to add more info later without general plugins maintenance cost.
    
      The new prototype is:
    
      void (*cb_flush) (struct flb_event_chunk *,    // new struct flb_event_chunk
                        struct flb_output_flush *,   // new struct flb_output_flush
                        struct flb_input_instance *, // origin input instance
                        void *,                      // plugin custom context
                        struct flb_config *);        // Fluent Bit context
    
      The structure flb_event_chunk concentrate most of the chunk information plus
      other items such as counter for the number of events and a Tag in sds format.
    
      The other flb_output_flush argument is added and explained below.
      --
    
    This patch modifies the current plugin flush callback prototype and adjust the
    internals to use the new structure flb_event_chunk to gather message type,
    buffer, size and tag.
    
    Signed-off-by: Eduardo Silva <eduardo@calyptia.com>

--- plugins/out_stackdriver/stackdriver.c
+++ plugins/out_stackdriver/stackdriver.c
@@ -2072,8 +2072,8 @@ static int stackdriver_format(struct flb_config *config,
     return 0;
 }
 
-static void cb_stackdriver_flush(const void *data, size_t bytes,
-                                 const char *tag, int tag_len,
+static void cb_stackdriver_flush(struct flb_event_chunk *event_chunk,
+                                 struct flb_output_flush *out_flush,
                                  struct flb_input_instance *i_ins,
                                  void *out_context,
                                  struct flb_config *config)
@@ -2112,8 +2112,8 @@ static void cb_stackdriver_flush(const void *data, size_t bytes,
     /* Reformat msgpack to stackdriver JSON payload */
     ret = stackdriver_format(config, i_ins,
                              ctx, NULL,
-                             tag, tag_len,
-                             data, bytes,
+                             event_chunk->tag, flb_sds_len(event_chunk->tag),
+                             event_chunk->data, event_chunk->size,
                              &out_buf, &out_size);
     if (ret != 0) {
 #ifdef FLB_HAVE_METRICS
