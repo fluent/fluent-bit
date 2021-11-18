31d26a2c90407407ec34b34b7051a8899a129b64
Eduardo Silva <eduardo@calyptia.com>
2021-11-17 22:42:56 -0600

    in_forward: fix handling of TCP connections on SIGTERM (#2610)
    When Fluent Bit receives a SIGTERM, internally it was only taking
    care of the server socket leaving the active connections in an
    open state.
    
    This patch changes that behavior and if the plugin is paused or
    if the engine receives SIGTERM (which will trigger a pause), now
    the agent will drop the active TCP connections.
    
    Signed-off-by: Eduardo Silva <eduardo@calyptia.com>

--- plugins/in_forward/fw.c
+++ plugins/in_forward/fw.c
@@ -186,6 +186,8 @@ static void in_fw_pause(void *data, struct flb_config *config)
      */
     if (config->is_ingestion_active == FLB_FALSE) {
         mk_event_closesocket(ctx->server_fd);
+        fw_conn_del_all(ctx);
+
     }
 }
 
@@ -197,11 +199,11 @@ static int in_fw_exit(void *data, struct flb_config *config)
     struct flb_in_fw_config *ctx = data;
     struct fw_conn *conn;
 
-    mk_list_foreach_safe(head, tmp, &ctx->connections) {
-        conn = mk_list_entry(head, struct fw_conn, _head);
-        fw_conn_del(conn);
+    if (!ctx) {
+        return 0;
     }
 
+    fw_conn_del_all(ctx);
     fw_config_destroy(ctx);
     return 0;
 }
