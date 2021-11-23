676a693c47050d702f23fdada6156045725c9316
Phillip Whelan <phil@calyptia.com>
2021-11-23 15:48:29 -0300

    input chunk: remove call to llabs in flb_input_chunk_append_raw.
    Signed-off-by: Phillip Whelan <phil@calyptia.com>

--- src/flb_input_chunk.c
+++ src/flb_input_chunk.c
@@ -1440,7 +1440,7 @@ int flb_input_chunk_append_raw(struct flb_input_instance *in,
     }
 
     real_size = flb_input_chunk_get_real_size(ic);
-    real_diff = llabs(real_size - pre_real_size);
+    real_diff = real_size - pre_real_size;
     if (real_diff != 0) {
         flb_debug("[input chunk] update output instances with new chunk size diff=%d",
                   real_diff);
