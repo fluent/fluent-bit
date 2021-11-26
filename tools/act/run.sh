586f1350640b42abd8758f80d45f00f080e7310a
Patrick Stephens <pat@calyptia.com>
2021-11-26 12:53:10 +0000

    Review actions

--- tools/act/run.sh
+++ tools/act/run.sh
@@ -3,6 +3,9 @@ set -eux
 
 # release-build-docker-images
 JOB=${JOB:-release-build-distro-packages}
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-minioadmin}
+AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-minioadmin}
+AWS_S3_BUCKET_STAGING=${AWS_S3_BUCKET_STAGING:-fluentbit-ci-testbucket}
 
 act --privileged \
     -P ubuntu-latest=nektos/act-environments-ubuntu:18.04 \
@@ -11,4 +14,7 @@ act --privileged \
     -s GITHUB_TOKEN="${GITHUB_TOKEN}" \
     -s DOCKERHUB_TOKEN="$DOCKERHUB_TOKEN" \
     -s DOCKERHUB_USERNAME="$DOCKERHUB_USERNAME" \
+    -s AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
+    -s AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
+    -s AWS_S3_BUCKET_STAGING="$AWS_S3_BUCKET_STAGING" \
     -j "${JOB}"
