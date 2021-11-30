fc52cea8014d26d0ab23ace434a109a47bad2c20
Patrick Stephens <pat@calyptia.com>
2021-11-30 14:15:32 +0000

    Initial attempt at repo update script

---
+++ packaging/update-repos.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -eux
+# VERSION must be defined
+VERSION=${VERSION:-$1}
+# Where the base of all the repos is
+BASE_PATH=${BASE_PATH:-$2}
+
+RPM_REPO_PATHS=("amazonlinux/2/" "centos/7/")
+
+for RPM_REPO in "${RPM_REPO_PATHS[@]}"; do
+    echo "Updating $RPM_REPO"
+    for ARCH in x86_64 aarch64; do
+        REPO_DIR="$BASE_PATH/$RPM_REPO/$ARCH"
+        RPM="$REPO_DIR/td-agent-bit-${VERSION}-1.$ARCH.rpm"
+        [[ ! -d "$REPO_DIR" ]] && continue
+        [[ ! -f "$RPM" ]] && continue
+        echo "Updating $RPM_REPO/$ARCH"
+        # Sign the RPM
+        rpm --add-sign "$RPM"
+        # Full repo
+        # createrepo -dp "$REPO_DIR"
+        # Latest version only
+        createrepo -n "$RPM" "$REPO_DIR"
+    done
+done
+
+DEB_REPO_PATHS=( "debian/jessie"
+                 "debian/stretch"
+                 "debian/buster"
+                 "ubuntu/xenial"
+                 "ubuntu/bionic"
+                 "ubuntu/focal"
+                 "raspbian/jessie"
+                 "raspbian/stretch"
+                 "raspbian/buster" )
+
+for DEB_REPO in "${DEB_REPO_PATHS[@]}"; do
+    echo "Updating $DEB_REPO"
+    REPO_DIR="$BASE_PATH/$DEB_REPO"
+
+    debsigs --sign=origin -k "$GPG_KEY" "$REPO_DIR/td-agent-bit_${VERSION}_*.deb"
+    dpkg-scanpackages -m "$REPO_DIR" | gzip -c > "$REPO_DIR"/Packages.gz
+
+    # REPO_NAME="flb-${DEB_REPO/\//-}"
+    # VERSIONED_REPO_NAME="fluent-bit-${DEB_REPO//\//-}-$VERSION"
+    # DISTRBUTION=${DEB_REPO##*/}
+
+    # # Full repo
+    # # aptly repo add "$REPO_NAME" "$REPO_DIR/*.deb"
+
+    # # Latest version only
+    # aptly repo add "$REPO_NAME" "$REPO_DIR/td-agent-bit_${VERSION}_*.deb"
+    # aptly snapshot create "$VERSIONED_REPO_NAME" from repo "$REPO_NAME"
+    # aptly publish snapshot -distribution="${DISTRBUTION}" "$VERSIONED_REPO_NAME" \
+    #     "filesystem:${DEB_REPO}:"
+done
--- tools/act/run.sh
+++ tools/act/run.sh
@@ -5,12 +5,20 @@ set -eux
 JOB=${JOB:-release-build-distro-packages}
 AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-minioadmin}
 AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-minioadmin}
-AWS_S3_BUCKET_STAGING=${AWS_S3_BUCKET_STAGING:-fluentbit-ci-testbucket}
+AWS_S3_BUCKET_STAGING=${AWS_S3_BUCKET_STAGING:-fluentbit-ci-staging}
+AWS_S3_BUCKET_RELEASE=${AWS_S3_BUCKET_RELEASE:-fluentbit-ci-release}
+
+FLUENTBITIO_HOST=${FLUENTBITIO_HOST:-localhost}
+FLUENTBITIO_USERNAME=${FLUENTBITIO_USERNAME:-$USER}
+FLUENTBITIO_SSHKEY=${FLUENTBITIO_SSHKEY:-$HOME/.ssh/id_ed25519.pub}
+
+FLUENTBITIO_DIR=$(mktemp -d)
+echo "FLUENTBITIO_DIR=$FLUENTBITIO_DIR"
 
 rm -rf workflow/
 mkdir -p workflow/
 
-act --privileged --bind \
+act --privileged --bind --verbose \
     -P ubuntu-latest=nektos/act-environments-ubuntu:18.04 \
     -P ubuntu-18.04=nektos/act-environments-ubuntu:18.04 \
     --rm \
@@ -21,5 +29,7 @@ act --privileged --bind \
     -s AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
     -s AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
     -s AWS_S3_BUCKET_STAGING="$AWS_S3_BUCKET_STAGING" \
+    -s AWS_S3_BUCKET_RELEASE="$AWS_S3_BUCKET_RELEASE" \
     --env AWS_S3_ENDPOINT="http://localhost:9000" \
+    --env FLUENTBITIO_DIR="$FLUENTBITIO_DIR" \
     -j "${JOB}"
