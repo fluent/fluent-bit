cf0ddb894d6216096977e1cbc7788927131dd51a
Wesley Pettit <wppttt@amazon.com>
2021-12-01 11:08:28 -0800

    filter_tensorflow: add input instance to filter callback
    Signed-off-by: Wesley Pettit <wppttt@amazon.com>

--- plugins/filter_tensorflow/tensorflow.c
+++ plugins/filter_tensorflow/tensorflow.c
@@ -224,6 +224,7 @@ static int cb_tf_filter(const void *data, size_t bytes,
                         const char *tag, int tag_len,
                         void **out_buf, size_t *out_bytes,
                         struct flb_filter_instance *f_ins,
+                        struct flb_input_instance *i_ins,
                         void *filter_context,
                         struct flb_config *config)
 {
@@ -234,6 +235,7 @@ static int cb_tf_filter(const void *data, size_t bytes,
     (void) out_buf;
     (void) out_bytes;
     (void) f_ins;
+    (void) i_ins;
     int map_size;
 
     msgpack_object root;
