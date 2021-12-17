527b72928c8b23caea8ec73421943bfe31fc0b80
Patrick Stephens <pat@calyptia.com>
2021-12-17 15:14:21 +0000

    workflows: update build.sh
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- packaging/build.sh
+++ packaging/build.sh
@@ -1,12 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+set -eu
 
-# Never rely on PWD
+# Never rely on PWD so we can invoke from anywhere
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-FLB_BRANCH=""
-FLB_PREFIX=""
-FLB_VERSION=""
-FLB_DISTRO=""
+# Allow us to specify in the caller or pass variables
+FLB_BRANCH=${FLB_BRANCH:-}
+FLB_PREFIX=${FLB_PREFIX:-}
+FLB_VERSION=${FLB_VERSION:-}
+FLB_DISTRO=${FLB_DISTRO:-}
+FLB_OUT_DIR=${FLB_OUT_DIR:-}
+FLB_TARGZ=${FLB_TARGZ:-}
 
 while getopts "v:d:b:t:o:" option
 do
@@ -33,6 +37,12 @@ if [ -z "$FLB_BRANCH" ]; then
     FLB_PREFIX="v"
 fi
 
+if [[ ! -d "$SCRIPT_DIR/distros/$FLB_DISTRO" ]]; then
+    echo "Requested distro: $FLB_DISTRO"
+    echo "Mising directory: $SCRIPT_DIR/distros/$FLB_DISTRO"
+    exit 1
+fi
+
 # Validate 'Base Docker' image used to build the package
 FLB_BASE=flb-build-base-$FLB_DISTRO
 
@@ -43,7 +53,7 @@ if [ -f "$SCRIPT_DIR/distros/$FLB_DISTRO/Dockerfile.base" ]; then
         if ! docker build --no-cache \
             -t "$FLB_BASE" \
             -f "$SCRIPT_DIR/distros/$FLB_DISTRO/Dockerfile.base" \
-            distros/"$FLB_DISTRO"/
+            "$SCRIPT_DIR/distros/$FLB_DISTRO"/
         then
             echo "Error building base docker image"
             exit 1
@@ -78,24 +88,34 @@ if [ -n "$(docker images -q "$MAIN_IMAGE")" ]; then
 fi
 
 # Set tarball as an argument (./build.sh VERSION DISTRO/CODENAME -t something.tar.gz)
+FLB_ARG=""
 if [ -n "$FLB_TARGZ" ]; then
-    # Create sources directory if it don't exists
-    if [ ! -d "distros/$FLB_DISTRO/sources" ]; then
-        mkdir "distros/$FLB_DISTRO/sources"
+    # Check if we have a local or URL tarball
+    if [[ ! -f "$FLB_TARGZ" ]]; then
+        if curl --output /dev/null --silent --head --fail "$FLB_TARGZ"; then
+            echo "Unable to find tarball: $FLB_TARGZ"
+            exit 1
+        fi
+    fi
+
+    # Create sources directory if it does not exist
+    if [ ! -d "$SCRIPT_DIR/distros/$FLB_DISTRO/sources" ]; then
+        mkdir "$SCRIPT_DIR/distros/$FLB_DISTRO/sources"
     fi
 
     # Set build argument and copy tarball
     FLB_ARG="--build-arg FLB_SRC=$FLB_TARGZ"
-    cp "$FLB_TARGZ" "distros/$FLB_DISTRO/sources/"
+    cp "$FLB_TARGZ" "$SCRIPT_DIR/distros/$FLB_DISTRO/sources/"
 fi
 
-# Build the main image
+# Build the main image - we do want word splitting
+# shellcheck disable=SC2086
 if ! docker build \
     --no-cache \
     --build-arg FLB_VERSION="$FLB_VERSION" \
     --build-arg FLB_PREFIX=$FLB_PREFIX \
-    "$FLB_ARG" \
-    -t "$MAIN_IMAGE" "distros/$FLB_DISTRO"
+    $FLB_ARG \
+    -t "$MAIN_IMAGE" "$SCRIPT_DIR/distros/$FLB_DISTRO"
 then
     echo "Error building main docker image $MAIN_IMAGE"
     exit 1
