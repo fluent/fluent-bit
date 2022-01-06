3d0477ca1af3702e910dac9d0cce1cebde0b77f8
Patrick Stephens <pat@calyptia.com>
2022-01-06 19:30:40 +0000

    packaging: update local test doc
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- packaging/local-build-all.sh
+++ packaging/local-build-all.sh
@@ -2,7 +2,17 @@
 set -eux
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# Simple script to test a build of all supported targets
+# Simple script to test a build of all supported targets.
+# To build multi-arch, QEMU can be used and ideally buildkit support in Docker.
+#
+# Follow the relevant instructions to do this for your OS, e.g. for Ubuntu it may be:
+# $ sudo apt-get install qemu binfmt-support qemu-user-static # Install the qemu packages
+# $ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+# Confirm you can run a non-native architecture image, e.g.:
+# $ docker run --rm -t arm64v8/ubuntu uname -m # Run an executable made for aarch64 on x86_64
+# WARNING: The requested image's platform (linux/arm64/v8) does not match the detected host platform (linux/amd64) and no specific platform was requested
+# aarch64
+#
 
 # Ensure this is updated for new targets
 declare -a TARGETS=("centos/7" "centos/7.arm64v8" "centos/8" "centos/8.arm64v8"
