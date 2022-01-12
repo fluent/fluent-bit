2f61224b3262c7e2c3307c589daf094166bd6042
Patrick Stephens <pat@calyptia.com>
2022-01-12 16:05:19 +0000

    packaging: add test script
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

---
+++ packaging/testing/smoke/packages/local-test-all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eux
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Simple script to test all supported targets easily.
+
+PACKAGE_TEST=${PACKAGE_TEST:-centos7}
+RELEASE_URL=${RELEASE_URL:-https://packages.fluentbit.io}
+AWS_URL=${AWS_URL:-https://fluentbit-staging.s3.amazonaws.com}
+
+for DOCKERFILE in "$SCRIPT_DIR"/Dockerfile.*; do
+    DISTRO=${DOCKERFILE##*.}
+    echo "Testing $DISTRO"
+    PACKAGE_TEST="$DISTRO" RELEASE_URL="$RELEASE_URL" AWS_URL="$AWS_URL" "$SCRIPT_DIR"/run-package-tests.sh
+done
