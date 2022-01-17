addc4089f3dc147c96bceac5b8260cd45941d6c4
Thiago Padilha <thiago@calyptia.com>
2022-01-17 15:20:11 -0300

    in_kafka: initial implementation
    Signed-off-by: Thiago Padilha <thiago@calyptia.com>

--- cmake/headers.cmake
+++ cmake/headers.cmake
@@ -25,7 +25,7 @@ include_directories(
   ${CMAKE_CURRENT_BINARY_DIR}/include
   )
 
-if(FLB_OUT_KAFKA)
+if(FLB_IN_KAFKA OR FLB_OUT_KAFKA)
     include_directories(${FLB_PATH_ROOT_SOURCE}/${FLB_PATH_LIB_RDKAFKA}/src/)
 endif()
 
--- src/flb_kafka.c
+++ src/flb_kafka.c
@@ -21,6 +21,8 @@
 #include "fluent-bit/flb_config.h"
 #include "fluent-bit/flb_mem.h"
 #include "fluent-bit/flb_str.h"
+#include "fluent-bit/flb_utils.h"
+#include "monkey/mk_core/mk_list.h"
 #include <fluent-bit/flb_kafka.h>
 #include <fluent-bit/flb_kv.h>
 
@@ -93,6 +95,122 @@ rd_kafka_conf_t *flb_kafka_conf_create(struct flb_kafka *kafka,
 err:
     if (kafka_cfg) {
         flb_free(kafka_cfg);
+    }
+    return NULL;
+}
+
+static int add_topic_partitions(rd_kafka_topic_partition_list_t *list,
+                                const char *topic_str,
+                                const char *partitions_str)
+{
+    int ret = -1;
+    struct mk_list *split;
+    char *str, *end;
+    int start, stop;
+    size_t len;
+    split = flb_utils_split(partitions_str, '-', -1);
+    if (!split) {
+        flb_error("[flb_kafka] Failed to split partitions string");
+        goto end;
+    }
+
+    len = mk_list_size(split);
+    if (len == 1) {
+        str = mk_list_entry(split->next, struct flb_split_entry, _head)->value;
+        start = strtol(str, &end, 10);
+        if (end == str || *end != '\0') {
+            flb_error("[flb_kafka] invalid partition \"%s\"", str);
+            goto end;
+        }
+        // single partition
+        rd_kafka_topic_partition_list_add(list, topic_str, start);
+    } else if (len == 2) {
+        str = mk_list_entry(split->next, struct flb_split_entry, _head)->value;
+        start = strtol(str, &end, 10);
+        if (end == str || *end != '\0') {
+            flb_error("[flb_kafka] invalid partition \"%s\"", str);
+            goto end;
+        }
+        str = mk_list_entry(split->next->next, struct flb_split_entry, _head)->value;
+        stop = strtol(str, &end, 10);
+        if (end == str || *end != '\0') {
+            flb_error("[flb_kafka] invalid partition \"%s\"", str);
+            goto end;
+        }
+        rd_kafka_topic_partition_list_add_range(list, topic_str, start, stop);
+    } else {
+        flb_error("[flb_kafka] invalid partition range string \"%s\"", partitions_str);
+        goto end;
+    }
+
+    ret = 0;
+
+end:
+    if (split) {
+        flb_utils_split_free(split);
+    }
+    return ret;
+}
+
+rd_kafka_topic_partition_list_t *flb_kafka_parse_topics(const char *topics_str)
+{
+    rd_kafka_topic_partition_list_t *ret;
+    struct mk_list *split = NULL;
+    struct mk_list *partitions = NULL;
+    struct mk_list *curr;
+    struct flb_split_entry *entry;
+    struct flb_split_entry *topic_entry;
+    struct flb_split_entry *partitions_entry;
+    size_t len;
+
+    ret = rd_kafka_topic_partition_list_new(1);
+    if (!ret) {
+        flb_error("[flb_kafka] Failed to allocate topic list");
+        goto err;
+    }
+
+    split = flb_utils_split(topics_str, ',', -1);
+    if (!split) {
+        flb_error("[flb_kafka] Failed to split topics string");
+        goto err;
+    }
+
+    mk_list_foreach(curr, split) {
+        entry = mk_list_entry(curr, struct flb_split_entry, _head);
+        partitions = flb_utils_split(entry->value, ':', -1);
+        if (!partitions) {
+            flb_error("[flb_kafka] Failed to split topic string");
+            goto err;
+        }
+        len = mk_list_size(partitions);
+        if (len == 1) {
+            rd_kafka_topic_partition_list_add(ret, entry->value, 0);
+        } else if (len == 2) {
+            topic_entry = mk_list_entry(
+                    partitions->next, struct flb_split_entry, _head);
+            partitions_entry = mk_list_entry(
+                    partitions->next->next, struct flb_split_entry, _head);
+            if (add_topic_partitions(ret, topic_entry->value, partitions_entry->value)) {
+                goto err;
+            }
+        } else {
+            flb_error("[flb_kafka] Failed to parse topic/partition string");
+            goto err;
+        }
+        flb_utils_split_free(partitions);
+    }
+    flb_utils_split_free(split);
+    return ret;
+
+err:
+    if (ret) {
+        rd_kafka_topic_partition_list_destroy(ret);
+    }
+    if (split) {
+        flb_utils_split_free(split);
+    }
+    if (partitions) {
+        flb_utils_split_free(partitions);
     }
     return NULL;
 }
