eceeb5fe7b550a6a6069b79850b635f985f5ac0a
Eduardo Silva <eduardo@calyptia.com>
2022-02-01 18:23:39 -0600

    core: update Copyright banners
    - adjust all files to "2015-2022 The Fluent Bit Authors"
    
    Signed-off-by: Eduardo Silva <eduardo@calyptia.com>

--- src/proxy/go/go.c
+++ src/proxy/go/go.c
@@ -2,8 +2,7 @@
 
 /*  Fluent Bit
  *  ==========
- *  Copyright (C) 2019-2021 The Fluent Bit Authors
- *  Copyright (C) 2015-2018 Treasure Data Inc.
+ *  Copyright (C) 2015-2022 The Fluent Bit Authors
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -34,7 +33,7 @@
 /*
  * Go Plugin phases
  * ================
- *  Copyright (C) 2019      The Fluent Bit Authors
+ *  Copyright (C) 2015-2022 The Fluent Bit Authors
  *
  *  1. FLBPluginRegister(context)
  *  2. Inside FLBPluginRegister, it needs to register it self using Fluent Bit API
