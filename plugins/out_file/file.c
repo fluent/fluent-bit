30bfd8e10fb3ac603be7ab0c4f70c39f9cb48296
Eduardo Silva <eduardo@calyptia.com>
2022-02-16 19:54:06 -0600

    out_file: set 1 worker by default
    Signed-off-by: Eduardo Silva <eduardo@calyptia.com>

--- plugins/out_file/file.c
+++ plugins/out_file/file.c
@@ -647,6 +647,7 @@ struct flb_output_plugin out_file_plugin = {
     .cb_flush     = cb_file_flush,
     .cb_exit      = cb_file_exit,
     .flags        = 0,
+    .workers      = 1,
     .event_type   = FLB_OUTPUT_LOGS | FLB_OUTPUT_METRICS,
     .config_map   = config_map,
 };
