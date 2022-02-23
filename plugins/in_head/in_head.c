c9ac830f3f21f2d9f6c5c7d2ee65936d797d0b6b
Phillip Whelan <phil@calyptia.com>
2022-02-23 12:16:14 -0300

    in_head: add generic error when unable to load configuration.
    Signed-off-by: Phillip Whelan <phil@calyptia.com>

--- plugins/in_head/in_head.c
+++ plugins/in_head/in_head.c
@@ -259,6 +259,7 @@ static int in_head_config_read(struct flb_in_head_config *ctx,
     /* Load the config map */
     ret = flb_input_config_map_set(in, (void *)ctx);
     if (ret == -1) {
+        flb_plg_error(in, "unable to load configuration");
         return -1;
     }
 
