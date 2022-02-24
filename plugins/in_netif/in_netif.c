c146937d07105c6a587f233e0433e0291e33a7dd
Phillip Whelan <phil@calyptia.com>
2022-02-24 10:31:57 -0300

    in_netif: log error when unable to load configuration.
    Signed-off-by: Phillip Whelan <phil@calyptia.com>

--- plugins/in_netif/in_netif.c
+++ plugins/in_netif/in_netif.c
@@ -102,6 +102,7 @@ static int configure(struct flb_in_netif_config *ctx,
     /* Load the config map */
     ret = flb_input_config_map_set(in, (void *)ctx);
     if (ret == -1) {
+        flb_plg_error(in, "unable to load configuration");
         return -1;
     }
 
