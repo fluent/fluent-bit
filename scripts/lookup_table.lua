e04f5d2b44d9aed1c941681b60862a144aa69c1e
kadooster <33422579+kadooster@users.noreply.github.com>
2022-03-26 00:16:23 -0300

    Create lookup_table.lua
    lookup table feature in Lua to enrich logs with selected values from a text/csv file source

---
+++ scripts/lookup_table.lua
@@ -0,0 +1,62 @@
+--[[
+    This script runs through a csv file and, using regexes, links a KEY ID from a record to desired columns in a csv table.
+    In this example, it increments the record with plus 3 values up to 6 values
+    so record with data key M:1 gets NAME 1 - LAT 1 - COUNTRY 1 values, key M:4 gets NAME 4 - LAT 4 - COUNTRY 4 and so on.
+
+    With illustrative purposes, we created a basic CSV table with 6 columns and a sample record with timestamp and data fields only:
+       timestamp: 1530239065.807368
+       data:      IP:192.168.0.1 - M:4 - I:Any logged data - S:IGNORED_EVENT
+
+    We will use M:4 as key, but you can change the regex to match any value you desire.
+    
+    WARNING: Make sure you have ONLY UNIQUE KEYS in your key columns at your CSV or you will always get the first value found.
+--]]
+
+--sample record in: [1530239065.807368, {"data":"IP:192.168.0.1 - M:4 - I:Any logged data - S:IGNORED_EVENT"}]
+record = {}
+record["datetime"] = 1530239065.807368
+record["data"] = "IP:192.168.0.1 - M:4 - I:Any logged data - S:IGNORED_EVENT"
+
+--comma separated csv lookup table example that you can point to your csv file
+file = [[1,"John Johnz",40.431,116.570,"Mutian Valley","Beijing",China
+2,"Clark Kent",-25.694,-54.435,"Iguaçu","Paraná",Brazil
+3,"Bruce Wayne",25.197,55.274,"Downtown","Dubai","United Emirates"
+4,"Diana Prince",37.971,23.726,"Partenon","Atena",Greece]]
+
+--regex to get key value from record data so we have a field to search for
+key_regex = "[^M]+M:([0-9]+)"
+
+--regex with latin chars to break csv example fields
+csv_regex = ",.([%aáÁãÃâÂéÉêÊíÍôÔõÕúÚçÇ ]+).,([0-9-.]+),([0-9-.]+),.([%aáÁãÃâÂéÉêÊíÍôÔõÕúÚçÇ ]+).,.([%aáÁãÃâÂéÉêÊíÍôÔõÕúÚçÇ ]+).,\"?([%aáÁãÃâÂéÉêÊíÍôÔõÕúÚçÇ ]+)"
+
+--function that gets an id, scans csv file and breaks the result groups into table fields
+function get_matches(key_id)
+         for line in string.gmatch(file, "[^\r\n]+") do
+                  --merges key with csv_regex
+                  id_regex = key_id .. csv_regex
+                  name,lat,lon,city,state,country = string.match(line,id_regex)
+                  if name ~= nil then
+                          record["heroname"] = name
+                          record["secret_lat"] = lat
+                          record["country"] = country
+                          return record
+                  end
+         end
+end
+
+--function that finds an key from a record using a regex
+function extract_id(record)
+         key_id = string.match(record["data"], key_regex)
+         --condition to skip record if regex with lookup table key value returns null so we save processing
+         if key_id ~= nil then
+                  get_matches(key_id)
+        	        --print(record["data"],record["heroname"],record["country"])
+                  return 2, record["datetime"], record
+         else
+                 -- print("Couldn't find ID in this record so we'll keep it original")
+                 return 0, 0, 0 
+         end
+end
+
+extract_id(record)
+--sample record out: [1530239065.807368, {"data"=>"IP:192.168.0.1 - M:4 - I:Any logged data - S:IGNORED_EVENT", "heroname"=>"Diana Prince", "secret_lat"=>"37.971", "country"=>"Greece"}]
