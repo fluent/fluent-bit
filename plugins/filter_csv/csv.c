bfe6a7565cf4a91d7000b0446eeb522815316163
Thiago Padilha <thiago@calyptia.com>
2022-04-04 21:15:37 -0300

    filter_csv: Implement filter for processing CSV
    Signed-off-by: Thiago Padilha <thiago@calyptia.com>

---
+++ plugins/filter_csv/csv.c
@@ -0,0 +1,603 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2015-2022 The Fluent Bit Authors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include <fluent-bit/flb_info.h>
+#include <fluent-bit/flb_filter.h>
+#include <fluent-bit/flb_filter_plugin.h>
+#include <fluent-bit/flb_csv.h>
+#include <fluent-bit/flb_utils.h>
+#include <fluent-bit/flb_sds.h>
+#include <fluent-bit/flb_file.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <avro.h>
+
+#include "avro/basics.h"
+#include "avro/errors.h"
+#include "avro/io.h"
+#include "avro/schema.h"
+#include "avro/src/avro/io.h"
+#include "avro/src/avro/value.h"
+#include "avro/src/schema.h"
+#include "avro/src/st.h"
+#include "avro/value.h"
+#include "csv.h"
+#include "fluent-bit/flb_mem.h"
+#include "fluent-bit/flb_str.h"
+#include "fluent-bit/flb_time.h"
+#include "mpack/mpack.h"
+
+static int cb_csv_init(struct flb_filter_instance *f_ins,
+                       struct flb_config *config,
+                       void *data)
+{
+    (void) data;
+    struct filter_csv *ctx;
+
+    /* Create context */
+    ctx = flb_malloc(sizeof *ctx);
+    if (!ctx) {
+        flb_error("[filter_csv] filter cannot be loaded");
+        return -1;
+    }
+
+    memset(ctx, 0, sizeof(*ctx));
+
+    if (flb_filter_config_map_set(f_ins, (void*)ctx)) {
+        flb_errno();
+        flb_plg_error(f_ins, "configuration error");
+        flb_free(ctx);
+        return -1;
+    }
+
+    /* Set context */
+    flb_filter_set_context(f_ins, ctx);
+    ctx->ins = f_ins;
+
+    ctx->packbuf = flb_sds_create_size(1024);
+    if (!ctx->packbuf) {
+        flb_error("[filter_csv] failed to allocate packbuf");
+        return -1;
+    }
+
+    return 0;
+}
+
+static struct filter_csv_tag_state *get_tag_state(
+        struct filter_csv *ctx,
+        const char *tag) {
+    int i;
+    char fname[1024];
+
+    for (i = 0; i < FILTER_CSV_MAX_TAG_COUNT; i++) {
+        struct filter_csv_tag_state *state = ctx->states + i;
+        if (state->used && !strcmp(state->tag, tag)) {
+            return state;
+        }
+    }
+    /* not found, initialize for first use */
+    for (i = 0; i < FILTER_CSV_MAX_TAG_COUNT; i++) {
+        struct filter_csv_tag_state *state = ctx->states + i;
+        if (!state->used) {
+            state->used = true;
+            state->tag = flb_strdup(tag);
+            state->row_buffer = flb_sds_create("");
+            flb_csv_init(&state->state, NULL, NULL);
+
+            /* read avro schema */
+            snprintf(fname, sizeof(fname), "%s.json", tag);
+            state->avro_schema_json = flb_file_read(fname);
+
+            if (avro_schema_from_json_length(state->avro_schema_json,
+                        flb_sds_len(state->avro_schema_json), &state->aschema)) {
+                flb_plg_error(ctx->ins,
+                        "Unable to parse aobject schema:%s:error:%s:\n",
+                        state->avro_schema_json,
+                        avro_strerror());
+                return NULL;
+            }
+
+            state->aclass = avro_generic_class_from_schema(state->aschema);
+            if (!state->aclass) {
+                flb_plg_error(
+                        ctx->ins,
+                        "Unable to instantiate class from schema:%s:\n",
+                        avro_strerror());
+                return NULL;
+            }
+
+            if (avro_generic_value_new(state->aclass, &state->record)) {
+                flb_plg_error(
+                        ctx->ins,
+                        "Unable to allocate new avro value:%s:\n", avro_strerror(),
+                        avro_strerror());
+                return NULL;
+            }
+
+            state->avro_write_buffer_size = 1024;
+            state->avro_write_buffer = flb_malloc(state->avro_write_buffer_size);
+            if (!state->avro_write_buffer) {
+                flb_plg_error(ctx->ins, "Unable to allocate avro write buffer");
+                return NULL;
+            }
+
+            state->awriter = avro_writer_memory(
+                    state->avro_write_buffer,
+                    state->avro_write_buffer_size);
+            if (!state->awriter) {
+                flb_plg_error(ctx->ins, "failed to allocate avro writer");
+                return NULL;
+            }
+
+            return state;
+        }
+    }
+
+    return NULL;
+}
+
+static void mpack_buffer_flush(mpack_writer_t* writer, const char* buffer, size_t count)
+{
+    struct filter_csv *ctx = writer->context;
+    flb_sds_cat_safe(&ctx->packbuf, buffer, count);
+}
+
+static int collect_lines(
+        struct filter_csv *ctx,
+        struct filter_csv_tag_state *state,
+        const char *data,
+        size_t bytes)
+{
+    struct flb_time t;
+    mpack_reader_t reader;
+    mpack_tag_t mtag;
+    size_t strl;
+
+    mpack_reader_init_data(&reader, data, bytes);
+
+
+    while (bytes > 0) {
+        const char *record_start = reader.data;
+        size_t record_size = 0;
+
+        if (flb_time_pop_from_mpack(&t, &reader)) {
+            /* failed to parse */
+            return FLB_FILTER_NOTOUCH;
+        }
+
+        mtag = mpack_read_tag(&reader);
+        if (mtag.type != mpack_type_map) {
+            /* failed to parse */
+            return FLB_FILTER_NOTOUCH;
+        }
+
+        mtag = mpack_read_tag(&reader);
+        if (mtag.type != mpack_type_str) {
+            /* failed to parse */
+            return FLB_FILTER_NOTOUCH;
+        }
+
+        reader.data += mpack_tag_bytes(&mtag);
+        mtag = mpack_read_tag(&reader);
+        if (mtag.type != mpack_type_str) {
+            /* failed to parse */
+            return FLB_FILTER_NOTOUCH;
+        }
+        strl = mpack_tag_bytes(&mtag);
+        flb_sds_cat_safe(&state->row_buffer, reader.data, strl);
+        reader.data += strl;
+        flb_sds_cat_safe(&state->row_buffer, "\n", 1);
+        record_size = reader.data - record_start;
+        bytes -= record_size;
+    }
+
+    return 0;
+}
+
+static void write_mpack_field(void *data, const char *field, size_t field_len)
+{
+    mpack_writer_t *writer = data;
+    mpack_write_str(writer, field, field_len);
+}
+
+static int csv_to_mpack(struct filter_csv *ctx, struct filter_csv_tag_state *state)
+{
+    int ret;
+    struct flb_time t = {0};
+    char *bufptr;
+    char *bufptrstart;
+    size_t buflen;
+    size_t buflenstart;
+    size_t field_count;
+    char writebuf[1024];
+    mpack_writer_t writer;
+
+    mpack_writer_init(&writer, writebuf, sizeof(writebuf));
+    mpack_writer_set_context(&writer, ctx);
+    mpack_writer_set_flush(&writer, mpack_buffer_flush);
+
+    bufptr = state->row_buffer;
+    buflen = flb_sds_len(state->row_buffer);
+    /* parse all csv records */
+    while (buflen) {
+        bufptrstart = bufptr;
+        buflenstart = buflen;
+        /* We parse two times. First we do a simple pass to calculate the
+         * csv field count */
+        ret = flb_csv_parse_record(&state->state, &bufptr, &buflen,
+                &field_count);
+        if (ret) {
+            break;
+        }
+
+        mpack_write_tag(&writer, mpack_tag_array(2));
+        flb_time_append_to_mpack(&writer, &t, 0);
+        mpack_write_tag(&writer, mpack_tag_map(1));
+        mpack_write_cstr(&writer, "csv");
+        mpack_write_tag(&writer, mpack_tag_array(field_count));
+
+        /* Now that we created the array tag, run again to write the field
+         * values */
+        state->state.field_callback = write_mpack_field;
+        state->state.data = &writer;
+        ret = flb_csv_parse_record(&state->state, &bufptrstart, &buflenstart,
+                &field_count);
+        state->state.field_callback = NULL;
+        state->state.data = NULL;
+    }
+
+    mpack_writer_flush_message(&writer);
+    mpack_writer_destroy(&writer);
+
+    if (ret == FLB_CSV_EOF) {
+        /* move the incomplete csv record to the beginning of the buffer */
+        memmove(state->row_buffer, bufptrstart, buflenstart);
+        flb_sds_len_set(state->row_buffer, buflenstart);
+    } else {
+        flb_sds_len_set(state->row_buffer, 0);
+    }
+
+    return 0;
+}
+
+static int extract_union_type_it(int i, avro_schema_t schema, void *arg)
+{
+    int *type_disc = arg;
+    int type = avro_typeof(schema);
+
+    switch (type) {
+        case AVRO_NULL:
+            return ST_CONTINUE;
+        default:
+            *type_disc = (type << 16) + i;
+            return ST_STOP;
+    }
+}
+
+static int extract_union_type(avro_value_t *value)
+{
+    int type_disc;
+    avro_schema_t schema = avro_value_get_schema(value);
+    struct avro_union_schema_t *uschema = avro_schema_to_union(schema);
+    st_foreach(uschema->branches, HASH_FUNCTION_CAST extract_union_type_it,
+            (st_data_t)&type_disc);
+    avro_value_set_branch(value, type_disc & 0xffff, value);
+    return type_disc >> 16;
+}
+
+static int extract_avro_type(avro_value_t *value)
+{
+    int type = avro_value_get_type(value);
+    switch (type) {
+        case AVRO_STRING:
+        case AVRO_BYTES:
+        case AVRO_BOOLEAN:
+        case AVRO_INT32:
+        case AVRO_INT64:
+        case AVRO_FLOAT:
+        case AVRO_FIXED:
+        case AVRO_DOUBLE:
+        case AVRO_NULL:
+            return type;
+        case AVRO_UNION:
+            return extract_union_type(value);
+        default:
+            flb_error("unsupported avro type");
+            return -1;
+    }
+}
+
+static int parse_int64(const char *in, int64_t *out)
+{
+    char *end;
+    int64_t val;
+
+    errno = 0;
+    val = strtol(in, &end, 10);
+    if (end == in || *end != 0 || errno)  {
+        return -1;
+    }
+
+    *out = val;
+    return 0;
+}
+
+static int parse_double(const char *in, double *out)
+{
+    char *end;
+    double val;
+    errno = 0;
+    val = strtod(in, &end);
+    if (end == in || *end != 0 || errno) {
+        return -1;
+    }
+    *out = val;
+    return 0;
+}
+
+static void write_avro_int_value(const char *field, size_t field_len,
+        avro_value_t *avalue)
+{
+    char buf[256];
+    int64_t val;
+
+    memcpy(buf, field, field_len);
+    buf[field_len] = 0;
+    parse_int64(buf, &val);
+
+    switch (avro_value_get_type(avalue)) {
+        case AVRO_INT32:
+            avro_value_set_int(avalue, val);
+            break;
+        case AVRO_INT64:
+            avro_value_set_long(avalue, val);
+            break;
+    }
+}
+
+static void write_avro_float_value(const char *field, size_t field_len,
+        avro_value_t *avalue)
+{
+    char buf[256];
+    double val;
+
+    memcpy(buf, field, field_len);
+    buf[field_len] = 0;
+    parse_double(buf, &val);
+
+    switch (avro_value_get_type(avalue)) {
+        case AVRO_FLOAT:
+            avro_value_set_float(avalue, val);
+            break;
+        case AVRO_DOUBLE:
+            avro_value_set_double(avalue, val);
+            break;
+    }
+}
+
+static void write_avro_field(void *data, const char *field, size_t field_len)
+{
+    int ret;
+    bool boolean_val;
+    avro_value_t avalue;
+    struct filter_csv_tag_state *state = data;
+
+    ret = avro_value_get_by_index(
+            &state->record,
+            state->record_field_index,
+            &avalue,
+            NULL);
+
+    switch (extract_avro_type(&avalue)) {
+        case AVRO_STRING:
+        case AVRO_BYTES:
+            avro_value_set_string_len(&avalue, field, field_len + 1);
+            break;
+        case AVRO_BOOLEAN:
+            boolean_val = field_len == 4 && !strncmp(field, "true", 4);
+            avro_value_set_boolean(&avalue, boolean_val);
+            break;
+        case AVRO_INT32:
+        case AVRO_INT64:
+            write_avro_int_value(field, field_len, &avalue);
+            break;
+        case AVRO_FLOAT:
+        case AVRO_DOUBLE:
+            write_avro_float_value(field, field_len, &avalue);
+            break;
+        default:
+            flb_error("unsupported avro type");
+            break;
+    }
+    state->record_field_index++;
+}
+
+static int write_avro_value(
+        struct filter_csv *ctx,
+        struct filter_csv_tag_state *state,
+        mpack_writer_t *writer)
+{
+    size_t payload_size;
+
+    if (avro_value_write(state->awriter, &state->record)) {
+        flb_plg_error(ctx->ins, "failed to write avro value: %s", avro_strerror());
+        return -1;
+    }
+
+    payload_size = avro_writer_tell(state->awriter);
+    mpack_write_bin(writer, state->avro_write_buffer, payload_size);
+    avro_writer_memory_set_dest(state->awriter, state->avro_write_buffer,
+            state->avro_write_buffer_size);
+
+    return 0;
+}
+
+
+static int csv_to_avro(
+        struct filter_csv *ctx,
+        struct filter_csv_tag_state *state)
+{
+    int ret;
+    struct flb_time t = {0};
+    char *bufptr;
+    char *bufptrstart;
+    size_t buflen;
+    size_t buflenstart;
+    size_t field_count;
+    char writebuf[1024];
+    mpack_writer_t writer;
+
+    state->state.field_callback = write_avro_field;
+    state->state.data = state;
+
+    mpack_writer_init(&writer, writebuf, sizeof(writebuf));
+    mpack_writer_set_context(&writer, ctx);
+    mpack_writer_set_flush(&writer, mpack_buffer_flush);
+
+    bufptr = state->row_buffer;
+    buflen = flb_sds_len(state->row_buffer);
+    /* parse all csv records */
+    while (buflen) {
+        bufptrstart = bufptr;
+        buflenstart = buflen;
+        /* We parse two times. First we do a simple pass to calculate the
+         * csv field count */
+        ret = flb_csv_parse_record(&state->state, &bufptr, &buflen,
+                &field_count);
+        if (ret) {
+            break;
+        }
+
+        mpack_write_tag(&writer, mpack_tag_array(2));
+        flb_time_append_to_mpack(&writer, &t, 0);
+        mpack_write_tag(&writer, mpack_tag_map(1));
+        mpack_write_cstr(&writer, "payload");
+        if (write_avro_value(ctx, state, &writer)) {
+            return FLB_FILTER_NOTOUCH;
+        }
+        
+        state->record_field_index = 0;
+    }
+
+    mpack_writer_flush_message(&writer);
+    mpack_writer_destroy(&writer);
+
+    if (ret == FLB_CSV_EOF) {
+        /* move the incomplete csv record to the beginning of the buffer */
+        memmove(state->row_buffer, bufptrstart, buflenstart);
+        flb_sds_len_set(state->row_buffer, buflenstart);
+    } else {
+        flb_sds_len_set(state->row_buffer, 0);
+    }
+
+    return 0;
+}
+
+static int cb_csv_filter(const void *data, size_t bytes,
+                         const char *tag, int tag_len,
+                         void **out_buf, size_t *out_bytes,
+                         struct flb_filter_instance *f_ins,
+                         void *filter_context,
+                         struct flb_config *config)
+{
+    int ret;
+    struct filter_csv *ctx;
+    struct filter_csv_tag_state *state;
+    char *outbuf;
+
+    ctx = filter_context;
+    flb_sds_len_set(ctx->packbuf, 0);
+
+    state = get_tag_state(ctx, tag);
+
+    if (!state) {
+        flb_plg_error(ctx->ins, "max number of tag exceeded");
+        return FLB_FILTER_NOTOUCH;
+    }
+
+    collect_lines(ctx, state, data, bytes);
+
+    if (ctx->convert_to_avro) {
+        ret = csv_to_avro(ctx, state);
+    } else {
+        ret = csv_to_mpack(ctx, state);
+    }
+
+    if (ret) {
+        return ret;
+    }
+
+    /* allocate outbuf that contains the modified chunks */
+    outbuf = flb_malloc(flb_sds_len(ctx->packbuf));
+    if (!outbuf) {
+        flb_plg_error(ctx->ins, "failed to allocate outbuf");
+        return FLB_FILTER_NOTOUCH;
+    }
+    memcpy(outbuf, ctx->packbuf, flb_sds_len(ctx->packbuf));
+    /* link new buffer */
+    *out_buf   = outbuf;
+    *out_bytes = flb_sds_len(ctx->packbuf);
+    return FLB_FILTER_MODIFIED;
+}
+
+static int cb_csv_exit(void *data, struct flb_config *config)
+{
+    int i;
+    struct filter_csv *ctx = data;
+
+    for (i = 0; i < FILTER_CSV_MAX_TAG_COUNT; i++) {
+        struct filter_csv_tag_state *state = ctx->states + i;
+        if (state->used) {
+            flb_csv_destroy(&state->state);
+            flb_free(state->tag);
+            flb_sds_destroy(state->row_buffer);
+            flb_sds_destroy(state->avro_schema_json);
+            avro_value_decref(&state->record);
+            avro_value_iface_decref(state->aclass);
+            avro_schema_decref(state->aschema);
+            flb_free(state->avro_write_buffer);
+            avro_writer_free(state->awriter);
+        }
+    }
+
+    flb_sds_destroy(ctx->packbuf);
+    flb_free(ctx);
+
+    return 0;
+}
+
+static struct flb_config_map config_map[] = {
+    {
+     FLB_CONFIG_MAP_BOOL, "convert_to_avro", "false",
+     0, FLB_TRUE, offsetof(struct filter_csv, convert_to_avro),
+     "If enabled, convert CSV records into avro, using tag path to find schema "
+    },
+    {0}
+};
+
+struct flb_filter_plugin filter_csv_plugin = {
+    .name         = "csv",
+    .description  = "CSV filter plugin",
+    .cb_init      = cb_csv_init,
+    .cb_filter    = cb_csv_filter,
+    .cb_exit      = cb_csv_exit,
+    .config_map   = config_map,
+    .flags        = 0
+};
---
+++ plugins/filter_csv/csv.h
@@ -0,0 +1,53 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2015-2022 The Fluent Bit Authors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef FLB_FILTER_CSV_H
+#define FLB_FILTER_CSV_H
+
+#include <fluent-bit/flb_info.h>
+#include <fluent-bit/flb_filter.h>
+#include <fluent-bit/flb_sds.h>
+#include <fluent-bit/flb_csv.h>
+#include <avro.h>
+
+#define FILTER_CSV_MAX_TAG_COUNT 1024
+
+struct filter_csv_tag_state {
+    struct flb_csv_state state;
+    char *tag;
+    flb_sds_t row_buffer;
+    avro_schema_t aschema;
+    avro_value_iface_t *aclass;
+    avro_value_t record;
+    char *avro_write_buffer;
+    size_t avro_write_buffer_size;
+    avro_writer_t awriter;
+    size_t record_field_index;
+    flb_sds_t avro_schema_json;
+    bool used;
+};
+
+struct filter_csv {
+    struct filter_csv_tag_state states[FILTER_CSV_MAX_TAG_COUNT];
+    bool convert_to_avro;
+    struct flb_filter_instance *ins;
+    flb_sds_t packbuf;
+};
+
+#endif
