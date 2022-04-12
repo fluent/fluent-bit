69cda86d1588de240fbd088d67a0c5b51f52ef50
Hiroshi Hatake <hatake@calyptia.com>
2022-04-12 13:47:58 +0900

    build: macos: Split macOS specific building rules into macos-setup.cmake
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>
    Signed-off-by: a445943 <csancheztejeda@santandertecnologia.com.ar>

---
+++ cmake/macos-setup.cmake
@@ -0,0 +1,30 @@
+execute_process(
+  COMMAND brew --prefix
+  RESULT_VARIABLE HOMEBREW
+  OUTPUT_VARIABLE HOMEBREW_PREFIX
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+if (HOMEBREW EQUAL 0 AND EXISTS "${HOMEBREW_PREFIX}")
+  message(STATUS "Found Homebrew at ${HOMEBREW_PREFIX}")
+  include(cmake/homebrew.cmake)
+endif()
+
+# Create rootcert on macOS
+set(MACOS_ROOT_CERT ${CMAKE_CURRENT_BINARY_DIR}/certs/rootcert.pem)
+execute_process(
+  COMMAND security find-certificate -a -p /Library/Keychains/System.keychain
+  RESULT_VARIABLE SECURITY_SYSTEM_RESULT
+  OUTPUT_VARIABLE SECURITY_SYSTEM_CERTS
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+file(WRITE ${MACOS_ROOT_CERT} ${SECURITY_SYSTEM_CERTS})
+
+execute_process(
+  COMMAND security find-certificate -a -p /System/Library/Keychains/SystemRootCertificates.keychain
+  RESULT_VARIABLE SECURITY_ROOT_RESULT
+  OUTPUT_VARIABLE SECURITY_ROOT_CERTS
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+file(APPEND ${MACOS_ROOT_CERT} ${SECURITY_ROOT_CERTS})
+
+install(FILES ${MACOS_ROOT_CERT} COMPONENT binary DESTINATION ${CMAKE_INSTALL_SYSCONFDIR}/certs)
