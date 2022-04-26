d5e9e0e9ecbf7b638d85200a2a0875e57291b136
Takahiro Yamashita <nokute78@gmail.com>
2022-04-26 13:04:53 +0900

    filter_lua: add return for discarding all records (#5251) (#5271)
    * tests: runtime: filter_lua: add test case for #5251
    
    If a length of ctx->packbuf is 0, it means all records are dropped.
    In this case, the plugin should return FLB_FILTER_MODIFIED and out_bytes should be 0.
    
    Signed-off-by: Takahiro Yamashita <nokute78@gmail.com>
    Signed-off-by: a445943 <csancheztejeda@santandertecnologia.com.ar>

--- tests/runtime/filter_lua.c
+++ tests/runtime/filter_lua.c
@@ -28,6 +28,7 @@
 
 pthread_mutex_t result_mutex = PTHREAD_MUTEX_INITIALIZER;
 char *output = NULL;
+int  num_output = 0;
 
 void set_output(char *val)
 {
@@ -45,6 +46,45 @@ char *get_output(void)
     pthread_mutex_unlock(&result_mutex);
 
     return val;
+}
+
+static void clear_output_num()
+{
+    pthread_mutex_lock(&result_mutex);
+    num_output = 0;
+    pthread_mutex_unlock(&result_mutex);
+}
+
+static int get_output_num()
+{
+    int ret;
+    pthread_mutex_lock(&result_mutex);
+    ret = num_output;
+    pthread_mutex_unlock(&result_mutex);
+
+    return ret;
+}
+
+static int cb_count_msgpack_events(void *record, size_t size, void *data)
+{
+    msgpack_unpacked result;
+    size_t off = 0;
+
+    if (!TEST_CHECK(data != NULL)) {
+        flb_error("data is NULL");
+    }
+
+    /* Iterate each item array and apply rules */
+    msgpack_unpacked_init(&result);
+    while (msgpack_unpack_next(&result, record, size, &off) == MSGPACK_UNPACK_SUCCESS) {
+        pthread_mutex_lock(&result_mutex);
+        num_output++;
+        pthread_mutex_unlock(&result_mutex);
+    }
+    msgpack_unpacked_destroy(&result);
+
+    flb_free(record);
+    return 0;
 }
 
 int callback_test(void* data, size_t size, void* cb_data)
@@ -511,7 +551,8 @@ void flb_test_array_contains_null(void)
     flb_destroy(ctx);
 }
 
-void flb_test_code(void)
+/* https://github.com/fluent/fluent-bit/issues/5251 */
+void flb_test_drop_all_records(void)
 {
     int ret;
     flb_ctx_t *ctx;
@@ -520,33 +561,32 @@ void flb_test_code(void)
     int filter_ffd;
     char *output = NULL;
     char *input = "[0, {\"key\":\"val\"}]";
-    char *result;
     struct flb_lib_out_cb cb_data;
 
     char *script_body = ""
       "function lua_main(tag, timestamp, record)\n"
-      "    new_record = record\n"
-      "    new_record[\"lua_array\"] = {};\n"
-      "    new_record[\"lua_array2\"] = {1,2,3};\n"
-      "    return 1, timestamp, new_record\n"
+      "    return -1, 0, 0\n"
       "end\n";
+
+    clear_output_num();
 
     /* Create context, flush every second (some checks omitted here) */
     ctx = flb_create();
     flb_service_set(ctx, "flush", "1", "grace", "1", NULL);
 
     /* Prepare output callback context*/
-    cb_data.cb = callback_test;
+    cb_data.cb = cb_count_msgpack_events;
     cb_data.data = NULL;
 
+    ret = create_script(script_body, strlen(script_body));
+    TEST_CHECK(ret == 0);
     /* Filter */
     filter_ffd = flb_filter(ctx, (char *) "lua", NULL);
     TEST_CHECK(filter_ffd >= 0);
     ret = flb_filter_set(ctx, filter_ffd,
                          "Match", "*",
                          "call", "lua_main",
-                         "code", script_body,
-                         "type_array_key", "lua_array lua_array2",
+                         "script", TMP_LUA_PATH,
                          NULL);
 
     /* Input */
@@ -559,26 +599,22 @@ void flb_test_code(void)
     TEST_CHECK(out_ffd >= 0);
     flb_output_set(ctx, out_ffd,
                    "match", "test",
-                   "format", "json",
                    NULL);
 
     ret = flb_start(ctx);
     TEST_CHECK(ret==0);
 
     flb_lib_push(ctx, in_ffd, input, strlen(input));
-    sleep(1);
-    output = get_output();
-    result = strstr(output, "\"lua_array\":[]");
-    if(!TEST_CHECK(result != NULL)) {
-        TEST_MSG("output:%s\n", output);
-    }
-    result = strstr(output, "\"lua_array2\":[1,2,3]");
-    if(!TEST_CHECK(result != NULL)) {
-        TEST_MSG("output:%s\n", output);
+    flb_time_msleep(1500); /* waiting flush */
+
+    ret = get_output_num();
+    if (!TEST_CHECK(ret == 0)) {
+        TEST_MSG("error. got %d expect 0", ret);
     }
 
     /* clean up */
     flb_lib_free(output);
+    delete_script();
 
     flb_stop(ctx);
     flb_destroy(ctx);
@@ -591,7 +627,6 @@ TEST_LIST = {
     {"type_int_key_multi", flb_test_type_int_key_multi},
     {"type_array_key", flb_test_type_array_key},
     {"array_contains_null", flb_test_array_contains_null},
-    {"code", flb_test_code},
-
+    {"drop_all_records", flb_test_drop_all_records},
     {NULL, NULL}
 };
