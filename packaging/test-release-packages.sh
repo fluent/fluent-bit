4933b26808ecd4a9b64a69522fa5942e4d4cf0e4
sarad <sarad.mohanan@gmail.com>
2022-05-04 15:05:25 +0530

    adding tests and updating github actions
    Signed-off-by: sarad <sarad.mohanan@gmail.com>

--- packaging/test-release-packages.sh
+++ packaging/test-release-packages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eux
 # Verify package install for a latest release version
+docker run --rm -it ubuntu:22.04 sh -c "apt-get update && apt-get install -y sudo gpg curl;curl https://raw.githubusercontent.com/fluent/fluent-bit/master/install.sh | sh"
 docker run --rm -it ubuntu:20.04 sh -c "apt-get update && apt-get install -y sudo gpg curl;curl https://raw.githubusercontent.com/fluent/fluent-bit/master/install.sh | sh"
 docker run --rm -it ubuntu:18.04 sh -c "apt-get update && apt-get install -y sudo gpg curl;curl https://raw.githubusercontent.com/fluent/fluent-bit/master/install.sh | sh"
 docker run --rm -it debian:10 sh -c "apt-get update && apt-get install -y sudo gpg curl;curl https://raw.githubusercontent.com/fluent/fluent-bit/master/install.sh | sh"
