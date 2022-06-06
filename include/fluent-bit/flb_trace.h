7b8e9b9003313ffc4adf22f0bb45b11f9338a457
Phillip Whelan <phil@calyptia.com>
2022-06-06 15:50:36 -0400

    several minor fixes...

--- include/fluent-bit/flb_trace.h
+++ include/fluent-bit/flb_trace.h
@@ -5,4 +5,4 @@
 #define FLB_TRACE_TYPE_OUTPUT 3
 
 int flb_trace_input_write(struct flb_input_chunk *ic, int trace_id);
-int flb_trace_filter_write(void *pfilter, void *input_chunk);
+int flb_trace_filter_write(void *pfilter, void *input_chunk, size_t offset);
