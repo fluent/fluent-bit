427b5b91cd83665b0ba2981cca680b2e7214d293
Eduardo Silva <eduardo@calyptia.com>
2022-06-12 13:47:35 -0600

    tests: move timeout test from internal to runtime
    Signed-off-by: Eduardo Silva <eduardo@calyptia.com>

--- tests/runtime/core-timeout.c
+++ tests/runtime/core-timeout.c
@@ -2,7 +2,7 @@
 
 /*  Fluent Bit
  *  ==========
- *  Copyright (C) 2015-2016 Treasure Data Inc.
+ *  Copyright (C) 2015-2022 The Fluent Bit Authors
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -18,12 +18,7 @@
  */
 
 #include <fluent-bit.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <string.h>
-#include "flb_tests_internal.h"
+#include "flb_tests_runtime.h"
 
 #define ELAPSED_TIME_LIMIT 9
 
