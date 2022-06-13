92e44939b755c99f78fad95340a58358a1a6c5cd
Phillip Whelan <phil@calyptia.com>
2022-06-13 16:10:58 -0400

    trace: push traces as records to an output plugin.
    Use a trace context at the input level that pushes traces as records
    to a private instance of output using an emitter input plugin.
    
    Still need a way to circumvent filters for this route.
    
    Signed-off-by: Phillip Whelan <phil@calyptia.com>

--- include/fluent-bit/flb_trace_chunk.h
+++ include/fluent-bit/flb_trace_chunk.h
@@ -21,17 +21,22 @@ struct flb_trace_chunk_filter_record {
 	size_t buf_size;
 };
 
-struct flb_trace_chunk {
-	struct flb_input_chunk *ic;
-	int trace_id;
-	int tracer_versions;
-	struct flb_trace_chunk_input_record input;
-	int num_filters;
-	struct flb_trace_chunk_filter_record *filters;
+struct flb_trace_chunk_context {
+	/* avoid cyclical include ... */
+	void *input;
+	void *output;
+	int trace_count;
 };
 
+struct flb_trace_chunk {
+	struct flb_input_chunk *ic;
+	struct flb_trace_chunk_context *ctxt;
+	int trace_id;
+	int tracer_versions;
+};
+
+struct flb_trace_chunk_context *flb_trace_chunk_context_new(struct flb_config *config);
 struct flb_trace_chunk *flb_trace_chunk_new(struct flb_input_chunk *chunk);
-int flb_trace_chunk_input(struct flb_trace_chunk *trace, void *pinput);
-int flb_trace_chunk_filter(struct flb_trace_chunk *trace, void *pfilter);
-int flb_trace_chunk_flush(struct flb_trace_chunk *trace, int offset);
+int flb_trace_chunk_input(struct flb_trace_chunk *trace, char *buf, int buf_size);
+int flb_trace_chunk_filter(struct flb_trace_chunk *trace, void *pfilter, char *buf, int buf_size);
 void flb_trace_chunk_free(struct flb_trace_chunk *trace);
--- src/flb_trace_chunk.c
+++ src/flb_trace_chunk.c
@@ -10,33 +10,6 @@
 #include <fluent-bit/flb_pack.h>
 #include <fluent-bit/flb_base64.h>
 
-
-static int record_resize(msgpack_packer *mp_pck, msgpack_sbuffer *mp_sbuf, void *buf, size_t buf_size, int add_size)
-{
-    msgpack_unpacked result;
-    int rc = -1;
-    int ret;
-    size_t off = 0;
-    int i;
-    
-
-    msgpack_unpacked_init(&result);
-    ret = msgpack_unpack_next(&result, buf, buf_size, &off);
-    if (ret != MSGPACK_UNPACK_SUCCESS && ret != MSGPACK_UNPACK_CONTINUE) {
-        goto unpack_error;
-    }
-    
-    msgpack_pack_array(mp_pck, result.data.via.array.size + add_size);
-    for (i = 0; i < result.data.via.array.size; i++) {
-        msgpack_pack_object(mp_pck, result.data.via.array.ptr[i]);
-    }
-
-    rc = 0;
-unpack_error:
-    msgpack_unpacked_destroy(&result);
-    return rc;
-}
-
 struct flb_trace_chunk *flb_trace_chunk_new(struct flb_input_chunk *chunk)
 {
     struct flb_trace_chunk *trace;
@@ -48,124 +21,194 @@ struct flb_trace_chunk *flb_trace_chunk_new(struct flb_input_chunk *chunk)
     }
 
     trace->ic = chunk;
-    trace->trace_id = f_ins->chunk_trace_count++;
+    trace->trace_id = f_ins->trace_ctxt->trace_count++;
 
-    chunk->chunk_trace = (void *)trace;
     return trace;
 }
 
 void flb_trace_chunk_free(struct flb_trace_chunk *trace)
 {
-    if (trace->filters) {
-        flb_free(trace->filters);
-    }
     flb_free(trace);
 }
 
-int flb_trace_chunk_input(struct flb_trace_chunk *tracer, void *pinput)
+struct flb_trace_chunk_context *flb_trace_chunk_context_new(struct flb_config *config)
 {
-    if (tracer == NULL) {
+    struct flb_input_instance *input;
+    struct flb_output_instance *output;
+    struct flb_trace_chunk_context *ctx;
+    int ret;
+
+
+    input = (void *)flb_input_new(config, "emitter", NULL, FLB_FALSE);
+    if (input == NULL) {
+        flb_error("could not load trace emitter");
+        return NULL;
+    }
+    ret = flb_input_set_property(input, "alias", "trace-emitter");
+    if (ret != 0) {
+        flb_error("unable to set alias for trace emitter");
+        flb_input_instance_destroy(input);
+        return NULL;
+    }
+    ret = flb_input_instance_init(input, config);
+    if (ret == -1) {
+        flb_error("cannot initialize trace emitter");
+        flb_input_instance_destroy(input);
+        return -1;
+    }
+    /* Storage context */
+    ret = flb_storage_input_create(config->cio, input);
+    if (ret == -1) {
+        return -1;
+    }
+    output = flb_output_new(config, (char *)"stdout", NULL, 0);
+    if (output == NULL) {
+        flb_error("could not create trace output");
+        //flb_free(input);
+        return NULL;
+    }
+    flb_output_set_property(output, "match", "*");
+    ret = flb_output_instance_init(output, config);
+    if (ret == -1) {
+        flb_error("cannot initialize trace emitter output");
+        flb_output_instance_destroy(output);
         return -1;
     }
 
-    flb_time_get(&tracer->input.t);
-    tracer->input.input = pinput;
-    cio_chunk_get_content(tracer->ic->chunk, 
-                          &tracer->input.buf,
-                  &tracer->input.buf_size);
-    return 0;
+    ret = flb_router_connect_direct(input, output);
+    if (ret != 0) {
+        flb_error("unable to route traces");
+        return NULL;
+    }
+
+    flb_router_connect(input, output);
+
+    ctx = flb_calloc(1, sizeof(struct flb_trace_chunk_context));
+    ctx->output = (void *)output;
+    ctx->input = (void *)input;
+
+    return ctx;
 }
 
-int flb_trace_chunk_filter(struct flb_trace_chunk *tracer, void *pfilter)
+int flb_trace_chunk_input(struct flb_trace_chunk *trace, char *buf, int buf_size)
 {
-    if (tracer == NULL) {
-        return -1;
-    }
-
-    tracer->filters = flb_realloc(tracer->filters,
-                      sizeof(struct flb_trace_chunk_filter_record) * (tracer->num_filters+1));
-    flb_time_get(&tracer->filters[tracer->num_filters].t);
-    tracer->filters[tracer->num_filters].filter =  pfilter;
-    cio_chunk_get_content(tracer->ic->chunk,
-                          &tracer->filters[tracer->num_filters].buf,
-                  &tracer->filters[tracer->num_filters].buf_size);
-    tracer->filters[tracer->num_filters].trace_version = tracer->tracer_versions++;
-    tracer->num_filters++;
-    return 0;
-}
-
-int flb_trace_chunk_flush(struct flb_trace_chunk *tracer, int offset)
-{
-    if (tracer == NULL) {
-        return -1;
-    }
-
     msgpack_packer mp_pck;
     msgpack_sbuffer mp_sbuf;
-    struct flb_input_instance *input;
-    struct flb_filter_instance *filter;
-    char *buf;
-    size_t buf_size;
+    struct flb_input_instance *input = (struct flb_input_instance *)trace->ic->in;
+    struct flb_output_instance *output = (struct flb_output_instance *)input->trace_ctxt->output;
     int rc = -1;
     int slen;
-    int i;
+    flb_sds_t tag = flb_sds_create("trace");
     unsigned char b64enc[102400];
     size_t bc64enclen;
     char trace_id_buf[256];
+    
 
-
-    input = (struct flb_input_instance *)tracer->input.input;
 
     msgpack_sbuffer_init(&mp_sbuf);
     msgpack_packer_init(&mp_pck, &mp_sbuf, msgpack_sbuffer_write);
 
-    cio_chunk_get_content(tracer->ic->chunk, &buf, &buf_size);
-    record_resize(&mp_pck, &mp_sbuf, buf, buf_size, 5 + (tracer->num_filters * 6));
-
     slen = snprintf(trace_id_buf, sizeof(trace_id_buf)-1, "%s.%d", 
-           input->name, tracer->trace_id);
+           input->name, trace->trace_id);
     if (slen <= 0) {
         goto sbuffer_error;
     }
 
+    msgpack_pack_array(&mp_pck, 2);
+    flb_pack_time_now(&mp_pck);
+    msgpack_pack_map(&mp_pck, 4);
+
+    msgpack_pack_str_with_body(&mp_pck, "type", 4);
     msgpack_pack_int(&mp_pck, FLB_TRACE_CHUNK_TYPE_INPUT);
-    flb_time_append_to_msgpack(&tracer->input.t, &mp_pck, FLB_TIME_ETFMT_INT);
+
+    msgpack_pack_str_with_body(&mp_pck, "trace_id", strlen("trace_id"));
     msgpack_pack_str_with_body(&mp_pck, trace_id_buf, slen);
+
+    msgpack_pack_str_with_body(&mp_pck, "input_instance", strlen("input_instance"));
     msgpack_pack_str_with_body(&mp_pck, input->name, strlen(input->name));
 
+    msgpack_pack_str_with_body(&mp_pck, "content", strlen("content"));
     flb_base64_encode(b64enc, sizeof(b64enc)-1, &bc64enclen,
-                    (unsigned char *)tracer->input.buf,
-                    tracer->input.buf_size);
+                    (unsigned char *)buf, buf_size);
     msgpack_pack_str_with_body(&mp_pck, b64enc, bc64enclen);
 
-    for (i = 0; i < tracer->num_filters; i++) {
-        filter = (struct flb_filter_instance *)tracer->filters[i].filter;
-
-        rc = msgpack_pack_int(&mp_pck, FLB_TRACE_CHUNK_TYPE_FILTER);
-        if (rc == -1) {
-            goto sbuffer_error;
-        }
-
-        flb_time_append_to_msgpack(&tracer->filters[i].t, &mp_pck, FLB_TIME_ETFMT_INT);
-        msgpack_pack_str_with_body(&mp_pck, trace_id_buf, slen);
-        rc = msgpack_pack_str_with_body(&mp_pck, filter->name, strlen(filter->name));
-        if (rc == -1) {
-            goto sbuffer_error;
-        }
-        rc = msgpack_pack_int(&mp_pck, tracer->filters[i].trace_version);
-        if (rc == -1) {
-            goto sbuffer_error;
-        }
-
-        flb_base64_encode(b64enc, sizeof(b64enc)-1, &bc64enclen,
-                    (unsigned char *)tracer->filters[i].buf, 
-                    tracer->filters[i].buf_size);
-        msgpack_pack_str_with_body(&mp_pck, b64enc, bc64enclen);
-    }
-
-    rc = mp_sbuf.size;
-    flb_input_chunk_write_at(tracer->ic, offset, mp_sbuf.data, mp_sbuf.size);
+    in_emitter_add_record(tag, flb_sds_len(tag), mp_sbuf.data, mp_sbuf.size,
+                          trace->ic->in->trace_ctxt->input);
 sbuffer_error:
     msgpack_sbuffer_destroy(&mp_sbuf);
+    return rc;
+}
+
+int flb_trace_chunk_filter(struct flb_trace_chunk *tracer, void *pfilter, char *buf, int buf_size)
+{
+    msgpack_packer mp_pck;
+    msgpack_sbuffer mp_sbuf;
+    int rc = -1;
+    struct flb_input_instance *input = (struct flb_input_instance *)tracer->ic->in;
+    struct flb_filter_instance *filter = (struct flb_filter_instance *)pfilter;
+    flb_sds_t tag = flb_sds_create("trace");
+    struct flb_time tm;
+    char trace_id_buf[256];
+    unsigned char b64enc[102400];
+    size_t bc64enclen;
+
+
+    if (tracer == NULL) {
+        goto tracer_error;
+    }
+
+    msgpack_sbuffer_init(&mp_sbuf);
+    msgpack_packer_init(&mp_pck, &mp_sbuf, msgpack_sbuffer_write);
+
+    msgpack_pack_array(&mp_pck, 2);
+    flb_pack_time_now(&mp_pck);
+    msgpack_pack_map(&mp_pck, 6);
+
+    msgpack_pack_str_with_body(&mp_pck, "type", strlen("type"));
+    rc = msgpack_pack_int(&mp_pck, FLB_TRACE_CHUNK_TYPE_FILTER);
+    if (rc == -1) {
+        goto sbuffer_error;
+    }
+
+    flb_time_get(&tm);
+    msgpack_pack_str_with_body(&mp_pck, "time", strlen("time"));
+    msgpack_pack_double(&mp_pck, flb_time_to_double(&tm));
+
+
+    rc = snprintf(trace_id_buf, sizeof(trace_id_buf)-1, "%s.%d", 
+           input->name, tracer->trace_id);
+    if (rc <= 0) {
+        rc = -1;
+        goto sbuffer_error;
+    }
+    msgpack_pack_str_with_body(&mp_pck, "trace_id", strlen("trace_id"));
+    msgpack_pack_str_with_body(&mp_pck, trace_id_buf, strlen(trace_id_buf));
+
+    
+    msgpack_pack_str_with_body(&mp_pck, "filter_instance", strlen("filter_instance"));
+    rc = msgpack_pack_str_with_body(&mp_pck, filter->name, strlen(filter->name));
+    if (rc == -1) {
+        goto sbuffer_error;
+    }
+    
+    msgpack_pack_str_with_body(&mp_pck, "record_version", strlen("record_version"));
+    rc = msgpack_pack_int(&mp_pck, tracer->tracer_versions++);
+    if (rc == -1) {
+        goto sbuffer_error;
+    }
+
+    flb_base64_encode(b64enc, sizeof(b64enc)-1, &bc64enclen, (unsigned char *)buf, buf_size);
+    msgpack_pack_str_with_body(&mp_pck, "record", strlen("record"));
+    msgpack_pack_str_with_body(&mp_pck, b64enc, bc64enclen);
+
+    in_emitter_add_record(tag, flb_sds_len(tag), mp_sbuf.data, mp_sbuf.size,
+                          tracer->ic->in->trace_ctxt->input);
+    
+    rc = 0;
+
+sbuffer_error:
+    msgpack_sbuffer_destroy(&mp_sbuf);
+tracer_error:
+    flb_sds_destroy(tag);
     return rc;
 }
