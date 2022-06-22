24b79eadeb31b2719dbdb090df6002a3aed415ba
Patrick Stephens <pat@calyptia.com>
2022-06-22 14:45:11 +0100

    examples: update with embedded container to enable
    Signed-off-by: Patrick Stephens <pat@calyptia.com>
    Signed-off-by: Phillip Whelan <phil@calyptia.com>

--- examples/tracing/enable-tracing.sh
+++ examples/tracing/enable-tracing.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -eu
-
+REMOTE_HOST=${REMOTE_HOST:-127.0.0.1}
 if command -v http; then
-    http -v 127.0.0.1:2020/api/v1/trace input=dummy.0 output=stdout prefix=trace. enable:=true params:='{"format":"json"}'
+    http -v $REMOTE_HOST:2020/api/v1/trace input=dummy.0 output=stdout prefix=trace. enable:=true params:='{"format":"json"}'
 elif command -v curl; then
-    curl --header 'Content-Type: application/json' --data '{"enable":true, "input": "dummy.0", "output": "stdout", "params": { "format": "json" }, "prefix": "trace."}' '127.0.0.1:2020/api/v1/trace'
+    curl --header 'Content-Type: application/json' --data '{"enable":true, "input": "dummy.0", "output": "stdout", "params": { "format": "json" }, "prefix": "trace."}' "$REMOTE_HOST":2020/api/v1/trace
 else
     echo "No curl or httpie installed"
-    exit 1
+    apt-get update
+    apt-get install -y curl
+    curl --header 'Content-Type: application/json' --data '{"enable":true, "input": "dummy.0", "output": "stdout", "params": { "format": "json" }, "prefix": "trace."}' "$REMOTE_HOST":2020/api/v1/trace
 fi
