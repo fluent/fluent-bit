5750f1fe199e204cabe97e8a8fc90573d19f0685
Chris Burr <chrisburr@users.noreply.github.com>
2022-07-06 16:37:06 +0200

    Fix name of oneshot parameter in Exec input plugin (#5127)
    Signed-off-by: Chris Burr <christopher.burr@cern.ch>

--- plugins/in_exec/in_exec.c
+++ plugins/in_exec/in_exec.c
@@ -329,7 +329,7 @@ static struct flb_config_map config_map[] = {
       "Set the buffer size"
     },
     {
-      FLB_CONFIG_MAP_BOOL, "bool", "false",
+      FLB_CONFIG_MAP_BOOL, "oneshot", "false",
       0, FLB_TRUE, offsetof(struct flb_exec, oneshot),
       "execute the command only once"
     },
