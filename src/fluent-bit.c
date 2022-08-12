325ab34210e2589872463c0ebafa17fd4887edd7
Leonardo Alminana <leonardo@calyptia.com>
2022-08-12 13:41:56 +0200

    dummy: dummy dum dum
    Signed-off-by: Leonardo Alminana <leonardo@calyptia.com>

--- src/fluent-bit.c
+++ src/fluent-bit.c
@@ -15,6 +15,7 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
+ *  This meaningless change is just here to trigger CI
  */
 
 #define _GNU_SOURCE
