52191c0e54880ec77bc7def37b7e8e6cd27b0f7d
Leonardo Alminana <leonardo@calyptia.com>
2022-08-23 19:28:52 +0200

    plugins: in_opentelemetry: transitioned from legacy TCP listener to                            downstream
    Signed-off-by: Leonardo Alminana <leonardo@calyptia.com>

--- plugins/in_opentelemetry/http_conn.c
+++ plugins/in_opentelemetry/http_conn.c
@@ -36,11 +36,19 @@ static int opentelemetry_conn_event(void *data)
     char *tmp;
     char *request_end;
     size_t request_len;
-    struct http_conn *conn = data;
+    struct http_conn *conn;
     struct mk_event *event;
-    struct flb_opentelemetry *ctx = conn->ctx;
+    struct flb_opentelemetry *ctx;
+    struct flb_connection *connection;
 
-    event = &conn->event;
+    connection = (struct flb_connection *) data;
+
+    conn = connection->user_data;
+
+    ctx = conn->ctx;
+
+    event = &connection->event;
+
     if (event->mask & MK_EVENT_READ) {
         available = (conn->buf_size - conn->buf_len) - 1;
         if (available < 1) {
@@ -67,7 +75,10 @@ static int opentelemetry_conn_event(void *data)
         }
 
         /* Read data */
-        bytes = recv(conn->fd, conn->buf_data + conn->buf_len, available, 0);
+        bytes = flb_io_net_read(connection,
+                                (void *) &conn->buf_data[conn->buf_len],
+                                available);
+
         if (bytes <= 0) {
             flb_plg_trace(ctx->ins, "fd=%i closed connection", event->fd);
             opentelemetry_conn_del(conn);
@@ -198,34 +209,33 @@ static void opentelemetry_conn_request_init(struct mk_http_session *session,
     request->session = session;
 }
 
-struct http_conn *opentelemetry_conn_add(int fd, struct flb_opentelemetry *ctx)
+struct http_conn *opentelemetry_conn_add(struct flb_connection *connection,
+                                         struct flb_opentelemetry *ctx)
 {
-    int ret;
     struct http_conn *conn;
-    struct mk_event *event;
+    int               ret;
 
     conn = flb_calloc(1, sizeof(struct http_conn));
     if (!conn) {
         flb_errno();
         return NULL;
     }
+    conn->connection = connection;
 
     /* Set data for the event-loop */
-    event = &conn->event;
-    MK_EVENT_NEW(event);
-    event->fd      = fd;
-    event->type    = FLB_ENGINE_EV_CUSTOM;
-    event->handler = opentelemetry_conn_event;
+    MK_EVENT_NEW(&connection->event);
+
+    connection->user_data     = conn;
+    connection->event.type    = FLB_ENGINE_EV_CUSTOM;
+    connection->event.handler = opentelemetry_conn_event;
 
     /* Connection info */
-    conn->fd      = fd;
     conn->ctx     = ctx;
     conn->buf_len = 0;
 
     conn->buf_data = flb_malloc(ctx->buffer_chunk_size);
     if (!conn->buf_data) {
         flb_errno();
-        flb_socket_close(fd);
         flb_plg_error(ctx->ins, "could not allocate new connection");
         flb_free(conn);
         return NULL;
@@ -233,17 +243,20 @@ struct http_conn *opentelemetry_conn_add(int fd, struct flb_opentelemetry *ctx)
     conn->buf_size = ctx->buffer_chunk_size;
 
     /* Register instance into the event loop */
-    ret = mk_event_add(ctx->evl, fd, FLB_ENGINE_EV_CUSTOM, MK_EVENT_READ, conn);
+    ret = mk_event_add(ctx->evl,
+                       connection->fd,
+                       FLB_ENGINE_EV_CUSTOM,
+                       MK_EVENT_READ,
+                       &connection->event);
     if (ret == -1) {
         flb_plg_error(ctx->ins, "could not register new connection");
-        flb_socket_close(fd);
         flb_free(conn->buf_data);
         flb_free(conn);
         return NULL;
     }
 
     /* Initialize HTTP Session: this is a custom context for Monkey HTTP */
-    opentelemetry_conn_session_init(&conn->session, ctx->server, conn->fd);
+    opentelemetry_conn_session_init(&conn->session, ctx->server, connection->fd);
 
     /* Initialize HTTP Request: this is the initial request and it will be reinitialized
      * automatically after the request is handled so it can be used for the next one.
@@ -257,17 +270,17 @@ struct http_conn *opentelemetry_conn_add(int fd, struct flb_opentelemetry *ctx)
 
 int opentelemetry_conn_del(struct http_conn *conn)
 {
-    struct flb_opentelemetry *ctx;
-
-    ctx = conn->ctx;
-
     if (conn->session.channel != NULL) {
         mk_channel_release(conn->session.channel);
     }
 
-    mk_event_del(ctx->evl, &conn->event);
+    /* The downstream unregisters the file descriptor from the event-loop
+     * so there's nothing to be done by the plugin
+     */
+    flb_downstream_conn_release(conn->connection);
+
     mk_list_del(&conn->_head);
-    flb_socket_close(conn->fd);
+
     flb_free(conn->buf_data);
     flb_free(conn);
 
