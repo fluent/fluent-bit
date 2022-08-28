ec19e2a23e163d4bb3854b88f467eedad3d35f24
Leonardo Alminana <leonardo@calyptia.com>
2022-08-28 20:55:56 +0200

    tests: runtime_shell: added test cases for the downstream layer These tests use syslog because it's a simple input plugin that supports all the transports and feature combinations implemented in downstream.
    Signed-off-by: Leonardo Alminana <leonardo@calyptia.com>

---
+++ tests/runtime_shell/in_syslog_uds_dgram_plaintext_expect.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+. ${FLB_RUNTIME_SHELL_PATH}/in_syslog_common.sh
+
+input_generator() {
+    result=$(wait_for_fluent_bit)
+
+    if test "$result" -eq "0"
+    then
+        logger -u $SOCKET_PATH 'Hello!'
+    fi
+}
+
+test_in_syslog_uds_stream_plaintext_filter_expect() {
+    export SOCKET_PATH=/tmp/fluent_bit_syslog_uds_stream.sock
+
+    input_generator &
+
+    $FLB_BIN -c $FLB_RUNTIME_SHELL_CONF/in_syslog_uds_dgram_plaintext_expect.conf
+}
+
+# The following command launch the unit test
+. $FLB_RUNTIME_SHELL_PATH/runtime_shell.env
