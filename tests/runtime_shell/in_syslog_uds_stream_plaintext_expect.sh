f00634efc2d7b4338ae9f1c670806e3b8e81b224
Leonardo Alminana <leonardo@calyptia.com>
2022-09-01 11:44:55 +0200

    tests: runtime_shell: fixed macos compatibility issues
    Signed-off-by: Leonardo Alminana <leonardo@calyptia.com>

--- tests/runtime_shell/in_syslog_uds_stream_plaintext_expect.sh
+++ tests/runtime_shell/in_syslog_uds_stream_plaintext_expect.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-. ${FLB_RUNTIME_SHELL_PATH}/in_syslog_common.sh
+. ${FLB_RUNTIME_SHELL_PATH}/common.sh
 
 input_generator() {
-    result=$(wait_for_fluent_bit)
+    result=$(wait_for_fluent_bit ${SIGNAL_FILE_PATH})
 
     if test "$result" -eq "0"
     then
-        logger -u $SOCKET_PATH 'Hello!'
+        echo '<13>Jan  1 00:00:00 testuser:  Hello!' | nc -w 1 -U $SOCKET_PATH
     fi
 }
 
 test_in_syslog_uds_stream_plaintext_filter_expect() {
+    export SIGNAL_FILE_PATH="/tmp/fb_signal_$$"
     export SOCKET_PATH=/tmp/fluent_bit_syslog_uds_stream.sock
 
     input_generator &
