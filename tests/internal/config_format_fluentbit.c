7fd5aaff2c701fc8a01c65c4ecad68e03613256d
Eduardo Silva <eduardo@calyptia.com>
2022-09-03 09:54:05 -0600

    Revert "tests: internal: config_format_fluentbit: added config initialization to prevent macos crashes."
    This reverts commit 6007d7a96e83ce0c65dd19042b75b68fc31b2cb4.

--- tests/internal/config_format_fluentbit.c
+++ tests/internal/config_format_fluentbit.c
@@ -15,19 +15,6 @@
 
 #define ERROR_LOG "fluentbit_conf_error.log"
 
-static void initialization_crutch()
-{
-    struct flb_config *config;
-
-    config = flb_config_init();
-
-    if (config == NULL) {
-        return;
-    }
-
-    flb_config_exit(config);
-}
-
 /* data/config_format/fluent-bit.conf */
 void test_basic()
 {
@@ -35,8 +22,6 @@ void test_basic()
 	struct flb_cf *cf;
     struct flb_cf_section *s;
     struct flb_cf_group *g;
-
-    initialization_crutch();
 
     cf = flb_cf_fluentbit_create(NULL, FLB_000, NULL, 0);
     TEST_CHECK(cf != NULL);
@@ -146,8 +131,6 @@ void missing_value()
                                 .size = sizeof(expected_strs)/sizeof(char*),
                                 .lists = &expected_strs[0],
     };
-
-    initialization_crutch();
 
     unlink(ERROR_LOG);
 
