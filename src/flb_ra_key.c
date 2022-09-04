518876c2e13b1ad7de171c40aa36ccb422b6dacc
David Korczynski <david@adalogics.com>
2022-09-04 17:52:10 +0100

    ra_key: handle error case
    flb_sds_create_len may fail and this needs to be handled.
    
    Fixes: https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=46082
    
    Signed-off-by: David Korczynski <david@adalogics.com>

--- src/flb_ra_key.c
+++ src/flb_ra_key.c
@@ -55,6 +55,11 @@ static int msgpack_object_to_ra_value(msgpack_object o,
         result->type = FLB_RA_STRING;
         result->val.string = flb_sds_create_len((char *) o.via.str.ptr,
                                                 o.via.str.size);
+
+        /* Handle cases where flb_sds_create_len fails */
+        if (result->val.string == NULL) {
+          return -1;
+        }
         return 0;
     }
     else if (o.type == MSGPACK_OBJECT_MAP) {
