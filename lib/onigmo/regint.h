c9be01fe631a8563cacdf4481706dc0da9e288f3
Hiroshi Hatake <hatake@calyptia.com>
2022-09-26 22:27:06 +0900

    build: lib: onigmo: Bundle the patched fluent/onigmo
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- lib/onigmo/regint.h
+++ lib/onigmo/regint.h
@@ -5,7 +5,7 @@
 **********************************************************************/
 /*-
  * Copyright (c) 2002-2013  K.Kosako  <sndgk393 AT ybb DOT ne DOT jp>
- * Copyright (c) 2011-2016  K.Takata  <kentkt AT csc DOT jp>
+ * Copyright (c) 2011-2019  K.Takata  <kentkt AT csc DOT jp>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -81,13 +81,12 @@
 /* #define USE_OP_PUSH_OR_JUMP_EXACT */
 #define USE_QTFR_PEEK_NEXT
 #define USE_ST_LIBRARY
-#define USE_SUNDAY_QUICK_SEARCH
 
 #define INIT_MATCH_STACK_SIZE                     160
 #define DEFAULT_MATCH_STACK_LIMIT_SIZE              0 /* unlimited */
 #define DEFAULT_PARSE_DEPTH_LIMIT                4096
 
-#define OPT_EXACT_MAXLEN   24
+#define OPT_EXACT_MAXLEN   24	/* This must be smaller than ONIG_CHAR_TABLE_SIZE. */
 
 /* check config */
 #if defined(USE_PERL_SUBEXP_CALL) || defined(USE_CAPITAL_P_NAMED_GROUP)
@@ -192,6 +191,14 @@
 # define st_nothing_key_free            onig_st_nothing_key_free
 /* */
 # define onig_st_is_member              st_is_member
+
+# ifdef __GNUC__
+#  define RB_GNUC_EXTENSION __extension__
+#  define RB_GNUC_EXTENSION_BLOCK(x) __extension__ ({ x; })
+# else
+#  define RB_GNUC_EXTENSION
+#  define RB_GNUC_EXTENSION_BLOCK(x) (x)
+# endif
 
 #endif /* RUBY */
 
