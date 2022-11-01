4ff80f79424187af35dce9e75f0da639a290d039
Hiroshi Hatake <hatake@calyptia.com>
2022-11-01 18:24:25 +0900

    out_forward: Make forwardable for msgpack payloads of ctraces and cmetrics
    Just using to pack `msgpack_pack_bin` is not suitable for them.
    Instead, we should format as forward mode compatible array with:
    
    ```
    [time, [{map of msgpack payloads}]]
    ```
    
    to be able to handle with ordinary msgpack handler for format of forward mode.
    
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- plugins/out_forward/forward.c
+++ plugins/out_forward/forward.c
@@ -1049,6 +1049,22 @@ static int flush_message_mode(struct flb_forward *ctx,
     return FLB_OK;
 }
 
+/* pack payloads of cmetrics or ctraces */
+static int pack_metricses_payload(msgpack_packer *mp_pck, const void *data, size_t bytes) {
+    int entries;
+    struct flb_time tm;
+
+    /* Format with forward mode representation of entries: [time, [{entries map}] */
+    msgpack_pack_array(mp_pck, 1);
+    msgpack_pack_array(mp_pck, 2);
+    flb_time_get(&tm);
+    flb_time_append_to_msgpack(&tm, mp_pck, 0);
+    entries = flb_mp_count(data, bytes);
+    msgpack_pack_array(mp_pck, entries);
+
+    return 0;
+}
+
 #include <fluent-bit/flb_pack.h>
 /*
  * Forward Mode: this is the generic mechanism used in Fluent Bit, it takes
@@ -1116,7 +1132,7 @@ static int flush_forward_mode(struct flb_forward *ctx,
         }
         else {
             /* FLB_EVENT_TYPE_METRICS and FLB_EVENT_TYPE_TRACES */
-            msgpack_pack_bin(&mp_pck, bytes);
+            pack_metricses_payload(&mp_pck, data, bytes);
         }
     }
 
