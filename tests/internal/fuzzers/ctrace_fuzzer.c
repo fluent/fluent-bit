4f2f456264f4e7c4761487442350fbf02899cc7e
David Korczynski <david@adalogics.com>
2022-11-28 02:21:45 -0800

    tests: internal: fuzzers: fix offset
    Fixes: https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=53103
    
    Signed-off-by: David Korczynski <david@adalogics.com>

--- tests/internal/fuzzers/ctrace_fuzzer.c
+++ tests/internal/fuzzers/ctrace_fuzzer.c
@@ -3,7 +3,7 @@
 #include <ctraces/ctr_decode_msgpack.h>
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size){
-    size_t off;
+    size_t off = 0;
     struct ctrace *ctr = NULL;
     ctr_decode_msgpack_create(&ctr, data, size, &off);
     if (ctr != NULL) {
