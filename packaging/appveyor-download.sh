713b1170d41ff4b0816af89299ed8404ef9edd00
Patrick Stephens <pat@calyptia.com>
2023-01-12 14:19:24 +0000

    packaging: add comments
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- packaging/appveyor-download.sh
+++ packaging/appveyor-download.sh
@@ -10,17 +10,20 @@ OUTPUT_DIR=${OUTPUT_DIR:-$PWD}
 APPVEYOR_BUILD_VERSION=$(curl -sSfL --header "Content-type: application/json" "$URL/projects/$PROJECT_SLUG/history?recordsNumber=100"|jq -cr ".builds[]|select(.isTag)|select(.tag == \"$TAG\").version")
 APPVEYOR_BUILD_INFO=$(curl -sSfL --header "Content-type: application/json" "$URL/projects/$PROJECT_SLUG/build/${APPVEYOR_BUILD_VERSION}")
 
+# Assuming two jobs - Win32/64
 JOB_ID1=$(echo "$APPVEYOR_BUILD_INFO"| jq -cr .build.jobs[0].jobId)
 JOB_ID2=$(echo "$APPVEYOR_BUILD_INFO"| jq -cr .build.jobs[1].jobId)
 
 ARTIFACTS_JOB1=$(curl -sSfL --header "Content-type: application/json" "$URL/buildjobs/${JOB_ID1}/artifacts")
 ARTIFACTS_JOB2=$(curl -sSfL --header "Content-type: application/json" "$URL/buildjobs/${JOB_ID2}/artifacts")
 
+# Assuming two artefacts per job - fluent-bit (no td-agent-bit) zip/exe
 JOB1_FILE1=$(echo "$ARTIFACTS_JOB1"| jq -cr .[0].fileName)
 JOB1_FILE2=$(echo "$ARTIFACTS_JOB1"| jq -cr .[1].fileName)
 JOB2_FILE1=$(echo "$ARTIFACTS_JOB2"| jq -cr .[0].fileName)
 JOB2_FILE2=$(echo "$ARTIFACTS_JOB2"| jq -cr .[1].fileName)
 
+# Download all the artefacts now
 mkdir -p "$OUTPUT_DIR"
 pushd "$OUTPUT_DIR"
 curl -sSfLO "$URL/buildjobs/${JOB_ID1}/artifacts/$JOB1_FILE1"
