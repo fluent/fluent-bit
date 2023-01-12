a057abb5ecd8e6d3e0e072301629d49526576444
Patrick Stephens <pat@calyptia.com>
2023-01-12 12:05:40 +0000

    workflows: update to push to new bucket
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- packaging/update-source-packages.sh
+++ packaging/update-source-packages.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -eux
 
-SOURCE_DIR=${SOURCE_DIR:-$HOME/apt}
 WINDOWS_SOURCE_DIR=${WINDOWS_SOURCE_DIR:-ignore}
 VERSION=${VERSION:-$1}
 MAJOR_VERSION=${MAJOR_VERSION:-}
+SOURCE_DIR=${SOURCE_DIR:?}
+TARGET_DIR=${TARGET_DIR:?}
 
 if [[ -z "$VERSION" ]]; then
     echo "Missing VERSION value"
@@ -15,21 +16,19 @@ if [[ -z "$MAJOR_VERSION" ]]; then
     MAJOR_VERSION=${VERSION%.*}
 fi
 
-echo "Publishing source and Windows packages for $VERSION (major: $MAJOR_VERSION)"
-
 if [[ ! -d "$SOURCE_DIR" ]]; then
     echo "Missing source directory: $SOURCE_DIR"
+    exit 1
 fi
 
-RELEASES_DIR=${RELEASES_DIR:-/var/www/releases.fluentbit.io}
-if [[ -d "$RELEASES_DIR/releases" ]]; then
-    echo "Using legacy releases linkage"
-    RELEASES_DIR="$RELEASES_DIR/releases" 
+if [[ ! -d "$TARGET_DIR" ]]; then
+    echo "Missing target directory: $TARGET_DIR"
+    exit 1
 fi
 
 # Handle the JSON schema by copying in the new versions (if they exist).
 echo "Updating JSON schema"
-find "$SOURCE_DIR/" -iname "fluent-bit-schema*$VERSION*.json" -exec cp -vf "{}" "$RELEASES_DIR/$MAJOR_VERSION/" \;
+find "$SOURCE_DIR/" -iname "fluent-bit-schema*$VERSION*.json" -exec cp -vf "{}" "$TARGET_DIR/$MAJOR_VERSION/" \;
 
 # Intended for AppVeyor usage
 if [[ -d "$WINDOWS_SOURCE_DIR" ]]; then
@@ -48,13 +47,13 @@ if [[ -d "$WINDOWS_SOURCE_DIR" ]]; then
         done
     popd
     # shellcheck disable=SC2086
-    cp -vf "$WINDOWS_SOURCE_DIR"/*$VERSION* "$RELEASES_DIR/$MAJOR_VERSION/"
+    cp -vf "$WINDOWS_SOURCE_DIR"/*$VERSION* "$TARGET_DIR/$MAJOR_VERSION/"
 else
     # Windows - we do want word splitting and ensure some files exist
     if compgen -G "$SOURCE_DIR/windows/*$VERSION*" > /dev/null; then
         echo "Copying Windows artefacts"
         # shellcheck disable=SC2086
-        cp -vf "$SOURCE_DIR"/windows/*$VERSION* "$RELEASES_DIR/$MAJOR_VERSION/"
+        cp -vf "$SOURCE_DIR"/windows/*$VERSION* "$TARGET_DIR/$MAJOR_VERSION/"
     else
         echo "Missing Windows builds"
     fi
@@ -64,7 +63,7 @@ fi
 if compgen -G "$SOURCE_DIR/source/*$VERSION*" > /dev/null; then
     echo "Copying source artefacts"
     # shellcheck disable=SC2086
-    cp -vf "$SOURCE_DIR"/source/*$VERSION* "$RELEASES_DIR/$MAJOR_VERSION/"
+    cp -vf "$SOURCE_DIR"/source/*$VERSION* "$TARGET_DIR/$MAJOR_VERSION/"
 else
     echo "Missing source artefacts"
 fi
