7cb40e43c1f00ba356fa5500ee649566d68a0694
Patrick Stephens <pat@calyptia.com>
2023-01-12 14:11:18 +0000

    packaging: add automated AppVeyor download and release
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

---
+++ packaging/appveyor-download.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -eux
+
+TAG=${TAG:?}
+URL=${URL:-https://ci.appveyor.com/api}
+PROJECT_SLUG=${PROJECT_SLUG:-fluent/fluent-bit-2e87g}
+OUTPUT_DIR=${OUTPUT_DIR:-$PWD}
+
+# Search the history for the version built using our tag
+APPVEYOR_BUILD_VERSION=$(curl -sSfL --header "Content-type: application/json" "$URL/projects/$PROJECT_SLUG/history?recordsNumber=100"|jq -cr ".builds[]|select(.isTag)|select(.tag == \"$TAG\").version")
+APPVEYOR_BUILD_INFO=$(curl -sSfL --header "Content-type: application/json" "$URL/projects/$PROJECT_SLUG/build/${APPVEYOR_BUILD_VERSION}")
+
+JOB_ID1=$(echo "$APPVEYOR_BUILD_INFO"| jq -cr .build.jobs[0].jobId)
+JOB_ID2=$(echo "$APPVEYOR_BUILD_INFO"| jq -cr .build.jobs[1].jobId)
+
+ARTIFACTS_JOB1=$(curl -sSfL --header "Content-type: application/json" "$URL/buildjobs/${JOB_ID1}/artifacts")
+ARTIFACTS_JOB2=$(curl -sSfL --header "Content-type: application/json" "$URL/buildjobs/${JOB_ID2}/artifacts")
+
+JOB1_FILE1=$(echo "$ARTIFACTS_JOB1"| jq -cr .[0].fileName)
+JOB1_FILE2=$(echo "$ARTIFACTS_JOB1"| jq -cr .[1].fileName)
+JOB2_FILE1=$(echo "$ARTIFACTS_JOB2"| jq -cr .[0].fileName)
+JOB2_FILE2=$(echo "$ARTIFACTS_JOB2"| jq -cr .[1].fileName)
+
+mkdir -p "$OUTPUT_DIR"
+pushd "$OUTPUT_DIR"
+curl -sSfLO "$URL/buildjobs/${JOB_ID1}/artifacts/$JOB1_FILE1"
+curl -sSfLO "$URL/buildjobs/${JOB_ID1}/artifacts/$JOB1_FILE2"
+curl -sSfLO "$URL/buildjobs/${JOB_ID2}/artifacts/$JOB2_FILE1"
+curl -sSfLO "$URL/buildjobs/${JOB_ID2}/artifacts/$JOB2_FILE2"
+popd
