580703a4583b0ededa7c755d23102f2fd7a2911f
Hiroshi Hatake <hatake@calyptia.com>
2023-01-19 12:51:04 +0900

    build: windows: Enable in_elasticsearch by default
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- cmake/windows-setup.cmake
+++ cmake/windows-setup.cmake
@@ -49,7 +49,7 @@ if(FLB_WINDOWS_DEFAULTS)
   set(FLB_IN_STATSD             Yes)
   set(FLB_IN_STORAGE_BACKLOG    Yes)
   set(FLB_IN_EMITTER            Yes)
-  set(FLB_IN_ELASTICSEARCH       No)
+  set(FLB_IN_ELASTICSEARCH      Yes)
 
   # OUTPUT plugins
   # ==============
