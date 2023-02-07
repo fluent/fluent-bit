e40be487f09c7b64e52c9f33e6857b4194e0c9e6
Hiroshi Hatake <hatake@calyptia.com>
2023-02-07 14:31:30 +0900

    reload: Reconstruct config format context
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- src/flb_reload.c
+++ src/flb_reload.c
@@ -27,6 +27,11 @@
 #include <fluent-bit/flb_config_format.h>
 #include <fluent-bit/flb_utils.h>
 
+#include <cfl/cfl.h>
+#include <cfl/cfl_sds.h>
+#include <cfl/cfl_variant.h>
+#include <cfl/cfl_kvlist.h>
+
 /*
  * Hot reload
  * ----------
@@ -36,6 +41,73 @@
  *     this will check that all configuration properties are valid.
  */
 
+static int recreate_cf_section(struct flb_cf_section *s, struct flb_cf *cf)
+{
+    struct mk_list *head;
+    struct cfl_list *p_head;
+    struct cfl_kvpair *kv;
+    struct flb_cf_group *g;
+    struct flb_cf_section *new_s;
+    struct flb_cf_group *new_g;
+    struct cfl_variant *var = NULL;
+
+    new_s = flb_cf_section_create(cf, s->name, flb_sds_len(s->name));
+    if (cfl_list_size(&s->properties->list) > 0) {
+        cfl_list_foreach(p_head, &s->properties->list) {
+            var = NULL;
+            kv = cfl_list_entry(p_head, struct cfl_kvpair, _head);
+            var = flb_cf_section_property_add(cf, new_s->properties,
+                                              kv->key, cfl_sds_len(kv->key),
+                                              kv->val->data.as_string, cfl_sds_len(kv->val->data.as_string));
+
+            if (var == NULL) {
+                flb_error("[reload] recreating section '%s' property '%s' is failed", s->name, kv->key);
+                return -1;
+            }
+        }
+    }
+
+    if (mk_list_size(&s->groups) <= 0) {
+        return 0;
+    }
+
+    mk_list_foreach(head, &s->groups) {
+        g = mk_list_entry(head, struct flb_cf_group, _head);
+        new_g = flb_cf_group_create(cf, new_s, g->name, flb_sds_len(g->name));
+
+        if (cfl_list_size(&g->properties->list) > 0) {
+            cfl_list_foreach(p_head, &g->properties->list) {
+                var = NULL;
+                kv = cfl_list_entry(p_head, struct cfl_kvpair, _head);
+                var = flb_cf_section_property_add(cf, new_g->properties,
+                                                  kv->key, cfl_sds_len(kv->key),
+                                                  kv->val->data.as_string, cfl_sds_len(kv->val->data.as_string));
+                if (var == NULL) {
+                    flb_error("[reload] recreating group '%s' property '%s' is failed", g->name, kv->key);
+                    return -1;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
+static int flb_reload_reconstruct_cf(struct flb_cf *old_cf, struct flb_cf *new_cf)
+{
+    struct mk_list *head;
+    struct flb_cf_section *s;
+
+    mk_list_foreach(head, &old_cf->sections) {
+        s = mk_list_entry(head, struct flb_cf_section, _head);
+        if (recreate_cf_section(s, new_cf) != 0) {
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
 int flb_reload(flb_ctx_t *ctx, struct flb_cf *cf)
 {
     int ret;
@@ -44,6 +116,17 @@ int flb_reload(flb_ctx_t *ctx, struct flb_cf *cf)
     struct flb_config *new_config;
     flb_ctx_t *new_ctx;
     struct flb_cf *new_cf;
+    struct flb_cf *original_cf;
+
+    /* Normally, we should create a service section before using this cf
+     * context. However, this context of config format will be used
+     * for copying contents from other one. So, we just need to create
+     * a new cf instance here.
+     */
+    new_cf = flb_cf_create();
+    if (!new_cf) {
+        return -1;
+    }
 
     flb_info("reloading instance pid=%lu tid=%i", getpid(), pthread_self());
 
@@ -52,6 +135,14 @@ int flb_reload(flb_ctx_t *ctx, struct flb_cf *cf)
 
     if (old_config->conf_path_file) {
         file = flb_sds_create(old_config->conf_path_file);
+    }
+    else {
+        /* FIXME: How to handle when specifying conf file and
+         * arguments case? */
+        if (flb_reload_reconstruct_cf(old_config->cf_main, new_cf) != 0) {
+            flb_error("[reload] reconstruct cf failed");
+            return -1;
+        }
     }
 
     /* FIXME: validate incoming 'cf' is valid before stopping current service */
@@ -63,30 +154,35 @@ int flb_reload(flb_ctx_t *ctx, struct flb_cf *cf)
     new_ctx = flb_create();
 
     new_config = new_ctx->config;
-    new_cf = new_config->cf_main;
+
+    /* Delete the original context of config format */
+    original_cf = new_config->cf_main;
+    flb_cf_destroy(original_cf);
 
     /* Create another config format context */
     if (file != NULL) {
-        new_cf = flb_cf_create_from_file(new_config->cf_main, file);
+        new_cf = flb_cf_create_from_file(new_cf, file);
 
         if (!new_cf) {
             flb_sds_destroy(file);
 
             return -1;
         }
-
-        ret = flb_config_load_config_format(new_config, new_cf);
-        if (ret != 0) {
-            flb_sds_destroy(file);
-
-            return -1;
-        }
     }
+
+    /* Load the new config format context to config context. */
+    ret = flb_config_load_config_format(new_config, new_cf);
+    if (ret != 0) {
+        flb_sds_destroy(file);
+
+        return -1;
+    }
+    new_config->cf_main = new_cf;
 
     if (file != NULL) {
         new_config->conf_path_file = file;
     }
-    new_config->cf_main = new_cf;
+
 
     /* FIXME: DEBUG */
     printf("[POS STOP DUMP]\n");
