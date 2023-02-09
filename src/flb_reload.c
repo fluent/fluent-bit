d3fca07a2a74cfb3d31575d032ca1ecc8fb14fe0
Hiroshi Hatake <hatake@calyptia.com>
2023-02-09 17:04:33 +0900

    reload: Validate all type of plugin's config_map
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- src/flb_reload.c
+++ src/flb_reload.c
@@ -23,6 +23,9 @@
 #include <fluent-bit/flb_lib.h>
 #include <fluent-bit/flb_mem.h>
 #include <fluent-bit/flb_input.h>
+#include <fluent-bit/flb_filter.h>
+#include <fluent-bit/flb_output.h>
+#include <fluent-bit/flb_custom.h>
 #include <fluent-bit/flb_config.h>
 #include <fluent-bit/flb_config_format.h>
 #include <fluent-bit/flb_utils.h>
@@ -31,6 +34,189 @@
 #include <cfl/cfl_sds.h>
 #include <cfl/cfl_variant.h>
 #include <cfl/cfl_kvlist.h>
+
+static int flb_input_propery_check_all(struct flb_config *config)
+{
+    int ret;
+    struct mk_list *tmp;
+    struct mk_list *head;
+    struct flb_input_instance *ins;
+    struct flb_input_plugin *p;
+
+    /* Iterate all active input instance plugins */
+    mk_list_foreach_safe(head, tmp, &config->inputs) {
+        ins = mk_list_entry(head, struct flb_input_instance, _head);
+        p = ins->p;
+
+        /* Skip pseudo input plugins */
+        if (!p) {
+            continue;
+        }
+
+        /* Check net property */
+        ret = flb_input_net_property_check(ins, config);
+        if (ret == -1) {
+            return -1;
+        }
+
+        /* Check plugin property */
+        ret = flb_input_plugin_property_check(ins, config);
+        if (ret == -1) {
+            return -1;
+        }
+
+        /* destroy net config map (will be recreated at flb_start) */
+        if (ins->net_config_map) {
+            flb_config_map_destroy(ins->net_config_map);
+        }
+
+        /* destroy config map (will be recreated at flb_start) */
+        if (ins->config_map) {
+            flb_config_map_destroy(ins->config_map);
+        }
+    }
+
+    return 0;
+}
+
+static int flb_output_propery_check_all(struct flb_config *config)
+{
+    int ret;
+    struct mk_list *tmp;
+    struct mk_list *head;
+    struct flb_output_instance *ins;
+
+    /* Iterate all active input instance plugins */
+    mk_list_foreach_safe(head, tmp, &config->outputs) {
+        ins = mk_list_entry(head, struct flb_output_instance, _head);
+
+        /* Check net property */
+        ret = flb_output_net_property_check(ins, config);
+        if (ret == -1) {
+            return -1;
+        }
+
+        /* Check plugin property */
+        ret = flb_output_plugin_property_check(ins, config);
+        if (ret == -1) {
+            return -1;
+        }
+
+        /* destroy net config map (will be recreated at flb_start) */
+        if (ins->net_config_map) {
+            flb_config_map_destroy(ins->net_config_map);
+        }
+
+        /* destroy config map (will be recreated at flb_start) */
+        if (ins->config_map) {
+            flb_config_map_destroy(ins->config_map);
+        }
+    }
+
+    return 0;
+}
+
+static int flb_filter_propery_check_all(struct flb_config *config)
+{
+    int ret;
+    struct mk_list *tmp;
+    struct mk_list *head;
+    struct flb_filter_instance *ins;
+
+    /* Iterate all active input instance plugins */
+    mk_list_foreach_safe(head, tmp, &config->filters) {
+        ins = mk_list_entry(head, struct flb_filter_instance, _head);
+
+        if (!ins->match
+#ifdef FLB_HAVE_REGEX
+            && !ins->match_regex
+#endif
+            ) {
+            flb_warn("[filter] NO match rule for %s filter instance, unloading.",
+                     ins->name);
+            flb_filter_instance_destroy(ins);
+            continue;
+        }
+
+        /* Check plugin property */
+        ret = flb_filter_plugin_property_check(ins, config);
+        if (ret == -1) {
+            return -1;
+        }
+
+        /* destroy config map (will be recreated at flb_start) */
+        if (ins->config_map) {
+            flb_config_map_destroy(ins->config_map);
+        }
+    }
+
+    return 0;
+}
+
+static int flb_custom_propery_check_all(struct flb_config *config)
+{
+    int ret;
+    struct mk_list *tmp;
+    struct mk_list *head;
+    struct flb_custom_instance *ins;
+
+    /* Iterate all active input instance plugins */
+    mk_list_foreach_safe(head, tmp, &config->customs) {
+        ins = mk_list_entry(head, struct flb_custom_instance, _head);
+
+        /* Check plugin property */
+        ret = flb_custom_plugin_property_check(ins, config);
+        if (ret == -1) {
+            return -1;
+        }
+
+        /* destroy config map (will be recreated at flb_start) */
+        if (ins->config_map) {
+            flb_config_map_destroy(ins->config_map);
+        }
+    }
+
+    return 0;
+}
+
+int flb_reload_property_check_all(struct flb_config *config)
+{
+    int ret = 0;
+
+    /* Check properties of custom plugins */
+    ret = flb_custom_propery_check_all(config);
+    if (ret == -1) {
+        flb_error("[reload] check properties for custom plugins is failed");
+
+        return -1;
+    }
+
+    /* Check properties of input plugins */
+    ret = flb_input_propery_check_all(config);
+    if (ret == -1) {
+        flb_error("[reload] check properties for input plugins is failed");
+
+        return -1;
+    }
+
+    /* Check properties of filter plugins */
+    ret = flb_filter_propery_check_all(config);
+    if (ret == -1) {
+        flb_error("[reload] check properties for filter plugins is failed");
+
+        return -1;
+    }
+
+    /* Check properties of output plugins */
+    ret = flb_output_propery_check_all(config);
+    if (ret == -1) {
+        flb_error("[reload] check properties for output plugins is failed");
+
+        return -1;
+    }
+
+    return 0;
+}
 
 /*
  * Hot reload
@@ -175,13 +361,26 @@ int flb_reload(flb_ctx_t *ctx, struct flb_cf *cf_opts)
 
         return -1;
     }
+
+    /* Validate plugin properites before fluent-bit stops the old context. */
+    ret = flb_reload_property_check_all(new_config);
+    if (ret != 0) {
+        flb_sds_destroy(file);
+        flb_cf_destroy(new_cf);
+        flb_stop(new_ctx);
+        flb_destroy(new_ctx);
+
+        flb_error("[reload] reloaded config is invalid. Reloading is halted");
+
+        return -1;
+    }
+
     new_config->cf_main = new_cf;
 
     if (file != NULL) {
         new_config->conf_path_file = file;
     }
 
-    /* FIXME: validate incoming 'cf' is valid before stopping current service */
     flb_info("[reload] stop everything of the old context");
     flb_stop(ctx);
     flb_destroy(ctx);
