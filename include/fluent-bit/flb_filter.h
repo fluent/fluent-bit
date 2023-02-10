dfcc3cd541b10b3344a8b463c28e5d59c7ec9b61
Hiroshi Hatake <hatake@calyptia.com>
2023-02-10 11:54:28 +0900

    filter: Extract as a function for checking existence of match properties
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- include/fluent-bit/flb_filter.h
+++ include/fluent-bit/flb_filter.h
@@ -135,6 +135,7 @@ void flb_filter_do(struct flb_input_chunk *ic,
                    struct flb_config *config);
 const char *flb_filter_name(struct flb_filter_instance *ins);
 
+int flb_filter_match_property_existence(struct flb_filter_instance *ins);
 int flb_filter_plugin_property_check(struct flb_filter_instance *ins,
                                      struct flb_config *config);
 int flb_filter_init(struct flb_config *config, struct flb_filter_instance *ins);
