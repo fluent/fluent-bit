0edb7b8b717db806efcb4b753c6e93372b9d222f
Wesley Pettit <wppttt@amazon.com>
2023-02-13 16:33:00 -0800

    in_mqtt: init mk_event with MK_EVENT_ZERO
    Signed-off-by: Wesley Pettit <wppttt@amazon.com>

--- plugins/in_mqtt/mqtt_conn.c
+++ plugins/in_mqtt/mqtt_conn.c
@@ -81,6 +81,7 @@ struct mqtt_conn *mqtt_conn_add(int fd, struct flb_in_mqtt_config *ctx)
 
     /* Set data for the event-loop */
     event = &conn->event;
+    MK_EVENT_ZERO(event);
     event->fd           = fd;
     event->type         = FLB_ENGINE_EV_CUSTOM;
     event->mask         = MK_EVENT_EMPTY;
