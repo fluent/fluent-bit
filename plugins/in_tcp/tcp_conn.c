35e05ab34e69058fee00efbab86bfebbc93c2e78
Leonardo Alminana <leonardo@calyptia.com>
2023-02-24 12:15:31 +0100

    in_tcp: fixed event loop bugs in threaded mode
    Removed a stored reference to the main event loop in favor of obtaining
    the current event loop pointer from the TLS which is required to prevent
    client sockets from being registered in the main thread when the plugin
    instance is running in its own thread.
    
    Additionally, a call to flb_input_downstream_set was added in order to
    enable thread safety and move the downstream to the plugin instance
    downstream list if needed in order to ensure that the downstream is
    processed by the timeout callback runing in the plugins thread instead
    of the main thread.
    
    Signed-off-by: Leonardo Alminana <leonardo@calyptia.com>

--- plugins/in_tcp/tcp_conn.c
+++ plugins/in_tcp/tcp_conn.c
@@ -327,7 +327,7 @@ struct tcp_conn *tcp_conn_add(struct flb_connection *connection,
     }
 
     /* Register instance into the event loop */
-    ret = mk_event_add(ctx->evl,
+    ret = mk_event_add(flb_engine_evl_get(),
                        connection->fd,
                        FLB_ENGINE_EV_CUSTOM,
                        MK_EVENT_READ,
