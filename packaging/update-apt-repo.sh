1f04c0b399df6785e51718a2d1b3947a14cd89fa
Pat <pat@calyptia.com>
2023-04-05 15:07:23 +0100

    workflows: fix automation for release (#7125)
    * workflows: fix automation for release
    * workflows: fix codename extraction for Ubuntu
    * testing: updates for AL2023 testing
    
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- packaging/update-apt-repo.sh
+++ packaging/update-apt-repo.sh
@@ -11,6 +11,9 @@ fi
 # "debian/bookworm" "debian/bullseye" "debian/buster" "ubuntu/xenial" "ubuntu/bionic" "ubuntu/focal" "ubuntu/jammy" "raspbian/buster" "raspbian/bullseye"
 DEB_REPO=${DEB_REPO:?}
 
+# Set true to prevent signing
+DISABLE_SIGNING=${DISABLE_SIGNING:-false}
+
 REPO_DIR=$(realpath -sm "$BASE_PATH/$DEB_REPO" )
 if [[ ! -d "$REPO_DIR" ]] ; then
     echo "ERROR: missing $REPO_DIR"
