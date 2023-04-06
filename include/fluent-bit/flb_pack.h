00f75521cacbe4980782f45ccca27e9ce2799fa5
Leonardo Alminana <leonardo@calyptia.com>
2023-04-06 19:50:52 +0200

    pack: added output parameter to return consumed size on json conversion
    pack: added a helper function to get the metadata (needs to be updated)
    
    Signed-off-by: Leonardo Alminana <leonardo@calyptia.com>

--- include/fluent-bit/flb_pack.h
+++ include/fluent-bit/flb_pack.h
@@ -76,9 +76,10 @@ int flb_pack_init(struct flb_config *config);
 int flb_json_tokenise(const char *js, size_t len, struct flb_pack_state *state);
 
 
-int flb_pack_json(const char *js, size_t len, char **buffer, size_t *size, int *root_type);
+int flb_pack_json(const char *js, size_t len, char **buffer, size_t *size,
+                  int *root_type, size_t *consumed);
 int flb_pack_json_recs(const char *js, size_t len, char **buffer, size_t *size,
-                       int *root_type, int *out_records);
+                       int *root_type, int *out_records, size_t *consumed);
 
 int flb_pack_state_init(struct flb_pack_state *s);
 void flb_pack_state_reset(struct flb_pack_state *s);
