c5a668cc534579604e50a73ef0fc9b90a7cd5747
Hiroshi Hatake <hatake@calyptia.com>
2023-04-08 23:04:05 +0900

    reload: test: Add reloading with YAML processor case
    Instead, we should check reloading with processor configuration.
    
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- tests/internal/reload.c
+++ tests/internal/reload.c
@@ -170,8 +170,80 @@ void test_reload()
     flb_destroy(ctx);
 }
 
+/* data/reload/yaml/processor.yaml */
+void test_reload_yaml()
+{
+    struct flb_cf *cf = NULL;
+    struct flb_cf *cf_opts;
+    struct flb_cf_section *section;
+    struct cfl_variant *ret;
+    flb_ctx_t *ctx;
+    int status;
+
+    /* create context */
+    cf_opts = flb_cf_create();
+    TEST_CHECK(cf_opts != NULL);
+
+    /* add a valid section (input) */
+    section = flb_cf_section_create(cf_opts, "INPUT", 5);
+    TEST_CHECK(section != NULL);
+
+    /* add property to the section recently created */
+    ret = flb_cf_section_property_add(cf_opts, section->properties, "name", 0, "dummy", 0);
+    TEST_CHECK(ret != NULL);
+
+    TEST_CHECK(mk_list_size(&cf_opts->inputs) == 1);
+
+    ctx = flb_create();
+    if (!TEST_CHECK(ctx != NULL)) {
+        TEST_MSG("flb_create failed");
+        exit(EXIT_FAILURE);
+    }
+
+    cf = ctx->config->cf_main;
+
+    status = flb_reload_reconstruct_cf(cf_opts, cf);
+    TEST_CHECK(status == 0);
+
+    /* Mimic operation like as service_configure() */
+    cf = flb_cf_create_from_file(cf, FLB_YAML);
+    TEST_CHECK(cf != NULL);
+
+    ctx->config->conf_path_file = flb_sds_create(FLB_YAML);
+    ctx->config->enable_hot_reload = FLB_TRUE;
+
+    status = flb_config_load_config_format(ctx->config, cf);
+    TEST_CHECK(status == 0);
+
+    /* Start the engine */
+    status = flb_start(ctx);
+    TEST_CHECK(status == 0);
+    TEST_CHECK(mk_list_size(&ctx->config->inputs) == 2);
+
+    sleep(2);
+
+    status = flb_reload(ctx, cf_opts);
+    TEST_CHECK(status == 0);
+
+    sleep(2);
+
+    /* flb context should be replaced with flb_reload() */
+    ctx = flb_context_get();
+
+    TEST_CHECK(mk_list_size(&ctx->config->cf_opts->inputs) == 1);
+    TEST_CHECK(mk_list_size(&ctx->config->inputs) == 2);
+    TEST_CHECK(mk_list_size(&ctx->config->filters) == 0);
+    TEST_CHECK(mk_list_size(&ctx->config->outputs) == 1);
+
+    flb_cf_destroy(cf_opts);
+
+    flb_stop(ctx);
+    flb_destroy(ctx);
+}
+
 TEST_LIST = {
     { "reconstruct_cf" , test_reconstruct_cf},
     { "reload"         , test_reload},
+    { "reload_yaml"    , test_reload_yaml},
     { 0 }
 };
