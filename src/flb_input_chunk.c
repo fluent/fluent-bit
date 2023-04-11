eb61bdde3b25fedbdd00e0839893c866eb58c6d1
lecaros <lecaros@calyptia.com>
2023-04-11 16:17:16 -0400

    flb_input_chunk: make message easily followable
    Signed-off-by: lecaros <lecaros@calyptia.com>

--- src/flb_input_chunk.c
+++ src/flb_input_chunk.c
@@ -1394,7 +1394,7 @@ int flb_input_chunk_append_raw(struct flb_input_instance *in,
      */
     ic = input_chunk_get(in, tag, tag_len, buf_size, &set_down);
     if (!ic) {
-        flb_error("[input chunk] no available chunk");
+        flb_error("[input chunk] chk: %s - no available chunk", flb_input_chunk_get_name(ic));
         return -1;
     }
 
