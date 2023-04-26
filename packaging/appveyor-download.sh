36e00546d2d9095ffc0550182481ff6d6cc4feca
Patrick Stephens <pat@calyptia.com>
2023-04-26 11:47:10 +0100

    packaging: handle multiple AppVeyor builds for downloading
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- packaging/appveyor-download.sh
+++ packaging/appveyor-download.sh
@@ -6,8 +6,10 @@ URL=${URL:-https://ci.appveyor.com/api}
 PROJECT_SLUG=${PROJECT_SLUG:-fluent/fluent-bit-2e87g}
 OUTPUT_DIR=${OUTPUT_DIR:-$PWD}
 
-# Search the history for the version built using our tag
-APPVEYOR_BUILD_VERSION=$(curl -sSfL --header "Content-type: application/json" "$URL/projects/$PROJECT_SLUG/history?recordsNumber=100"|jq -cr ".builds[]|select(.isTag)|select(.tag == \"$TAG\").version")
+# Search the history for the version built using our tag.
+# Ensure we only have one version selected - the first is the latest.
+APPVEYOR_BUILD_VERSION=$(curl -sSfL --header "Content-type: application/json" "$URL/projects/$PROJECT_SLUG/history?recordsNumber=100"|\
+    jq -cr "first(.builds[]|select(.isTag)|select(.tag == \"$TAG\")).version")
 APPVEYOR_BUILD_INFO=$(curl -sSfL --header "Content-type: application/json" "$URL/projects/$PROJECT_SLUG/build/${APPVEYOR_BUILD_VERSION}")
 
 # Assuming two jobs - Win32/64
