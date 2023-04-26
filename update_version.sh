b08c70692f918cf7981fa06b3e3f5c03613b82ca
Patrick Stephens <pat@calyptia.com>
2023-04-26 14:19:07 +0100

    packaging: handle failures in version script
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- update_version.sh
+++ update_version.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-set -eux
+set -e
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+function usage() {
+    echo "Usage: $0 2.1.2"
+    echo "Or set NEW_VERSION=2.1.2"
+}
 
 function sed_wrapper() {
   if sed --version >/dev/null 2>&1; then
@@ -10,6 +16,7 @@ function sed_wrapper() {
       gsed "$@"
     else
       echo "ERROR: No valid GNU compatible 'sed' found, if on macOS please run 'brew install gnu-sed'" >&2
+      exit 1
     fi
   fi
 }
@@ -17,7 +24,7 @@ function sed_wrapper() {
 NEW_VERSION=${NEW_VERSION:-$1}
 
 if [[ -z "$NEW_VERSION" ]]; then
-    echo "Usage: $0 2.1.2" >&2
+    usage
     exit 1
 fi
 
@@ -33,7 +40,7 @@ fi
 # Extract and verify each version
 major=$(echo "$NEW_VERSION" | cut -d. -f1)
 minor=$(echo "$NEW_VERSION" | cut -d. -f2)
-minor=$(echo "$NEW_VERSION" | cut -d. -f3)
+patch=$(echo "$NEW_VERSION" | cut -d. -f3)
 
 if [[ -z "$major" ]]; then
     echo "ERROR: major is empty, invalid version: $NEW_VERSION" >&2
@@ -49,20 +56,24 @@ if [[ -z "$patch" ]]; then
 fi
 
 # Build version
-sed_wrapper -i "s/FLB_VERSION_MAJOR  [0-9]/FLB_VERSION_MAJOR  $major/g" CMakeLists.txt
-sed_wrapper -i "s/FLB_VERSION_MINOR  [0-9]/FLB_VERSION_MINOR  $minor/g" CMakeLists.txt
-sed_wrapper -i "s/FLB_VERSION_PATCH  [0-9]/FLB_VERSION_PATCH  $patch/g" CMakeLists.txt
+sed_wrapper -i "s/FLB_VERSION_MAJOR  [0-9]/FLB_VERSION_MAJOR  $major/g" "$SCRIPT_DIR"/CMakeLists.txt
+sed_wrapper -i "s/FLB_VERSION_MINOR  [0-9]/FLB_VERSION_MINOR  $minor/g" "$SCRIPT_DIR"/CMakeLists.txt
+sed_wrapper -i "s/FLB_VERSION_PATCH  [0-9]/FLB_VERSION_PATCH  $patch/g" "$SCRIPT_DIR"/CMakeLists.txt
 
 # Dockerfile
-sed_wrapper -i "s/ARG RELEASE_VERSION=[0-9].[0-9].[0-9]/ARG RELEASE_VERSION=$NEW_VERSION/g" dockerfiles/Dockerfile
-sed_wrapper -i "s/ARG RELEASE_VERSION=[0-9].[0-9].[0-9]/ARG RELEASE_VERSION=$NEW_VERSION/g" dockerfiles/Dockerfile*
+sed_wrapper -i "s/ARG RELEASE_VERSION=[0-9].[0-9].[0-9]/ARG RELEASE_VERSION=$NEW_VERSION/g" "$SCRIPT_DIR"/dockerfiles/Dockerfile
+sed_wrapper -i "s/ARG RELEASE_VERSION=[0-9].[0-9].[0-9]/ARG RELEASE_VERSION=$NEW_VERSION/g" "$SCRIPT_DIR"/dockerfiles/Dockerfile*
 
 
 # Snap
-sed_wrapper -i "s/version: '[0-9].[0-9].[0-9]'/version: '$NEW_VERSION'/g" snap/snapcraft.yaml
+sed_wrapper -i "s/version: '[0-9].[0-9].[0-9]'/version: '$NEW_VERSION'/g" "$SCRIPT_DIR"/snap/snapcraft.yaml
 
 # Bitbake / Yocto
-sed_wrapper -i "s/PV = \"[0-9].[0-9].[0-9]\"/PV = \"$NEW_VERSION\"/g" fluent-bit_*.*.*.bb
+if [[ ! -f "fluent-bit-$NEW_VERSION.bb" ]]; then
+    sed_wrapper "s/PV = \"[0-9].[0-9].[0-9]\"/PV = \"$NEW_VERSION\"/g" "$SCRIPT_DIR"/fluent-bit-*.*.*.bb > "fluent-bit-$NEW_VERSION.bb"
+    echo "ERROR: existing fluent-bit-$NEW_VERSION.bb"
+    exit 1
+fi
 
 if [[ "${DISABLE_COMMIT:-no}" != "no" ]]; then
     if ! command -v git &> /dev/null ; then
@@ -73,7 +84,8 @@ if [[ "${DISABLE_COMMIT:-no}" != "no" ]]; then
     git commit -s -m "build: bump to v$NEW_VERSION" -- CMakeLists.txt
     git commit -s -m "dockerfile: bump to v$NEW_VERSION" -- dockerfiles/*
     git commit -s -m "snap: bump to v$NEW_VERSION" snap/snapcraft.yaml
-    git mv fluent-bit_*.*.*.bb "fluent-bit_$NEW_VERSION.bb"
+    # Handle renaming
+    git add "*.bb"
     git commit -a -s -m "bitbake: bump to v$NEW_VERSION"
 fi
 
