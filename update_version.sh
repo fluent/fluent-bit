e0ef93ce94e60363a928e0ebf0aa92f5804291ae
Patrick Stephens <pat@calyptia.com>
2023-04-26 12:10:02 +0100

    packaging: fix up versioning script
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- update_version.sh
+++ update_version.sh
@@ -1,31 +1,35 @@
 #!/bin/sh
+set -eux
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 1.7.0" >&2
+NEW_VERSION=${NEW_VERSION:-$1}
+
+if [ -z "$NEW_VERSION" ]; then
+    echo "Usage: $0 2.1.2" >&2
     exit 1
 fi
 
-major=$(echo "$1" | sed -e "s/^\([0-9]*\)\.[0-9]*\.[0-9]*/\1/")
-minor=$(echo "$1" | sed -e "s/^[0-9]*\.\([0-9]*\)\.[0-9]*/\1/")
-patch=$(echo "$1" | sed -e "s/^[0-9]*\.[0-9]*\.\([0-9]*\)/\1/")
+major=$(echo "$NEW_VERSION" | sed -e "s/^\([0-9]*\)\.[0-9]*\.[0-9]*/\1/")
+minor=$(echo "$NEW_VERSION" | sed -e "s/^[0-9]*\.\([0-9]*\)\.[0-9]*/\1/")
+patch=$(echo "$NEW_VERSION" | sed -e "s/^[0-9]*\.[0-9]*\.\([0-9]*\)/\1/")
 
 # Build version
 sed -i "s/FLB_VERSION_MAJOR  [0-9]/FLB_VERSION_MAJOR  $major/g" CMakeLists.txt
 sed -i "s/FLB_VERSION_MINOR  [0-9]/FLB_VERSION_MINOR  $minor/g" CMakeLists.txt
 sed -i "s/FLB_VERSION_PATCH  [0-9]/FLB_VERSION_PATCH  $patch/g" CMakeLists.txt
 
-git commit -s -m "build: bump to v$1" -- CMakeLists.txt
+git commit -s -m "build: bump to v$NEW_VERSION" -- CMakeLists.txt
 
 # Dockerfile
-sed -i "s/ARG RELEASE_VERSION=[0-9].[0-9].[0-9]/ARG RELEASE_VERSION=$1/g" dockerfiles/Dockerfile*
+sed -i "s/ARG RELEASE_VERSION=[0-9].[0-9].[0-9]/ARG RELEASE_VERSION=$NEW_VERSION/g" dockerfiles/Dockerfile
+sed -i "s/ARG RELEASE_VERSION=[0-9].[0-9].[0-9]/ARG RELEASE_VERSION=$NEW_VERSION/g" dockerfiles/Dockerfile*
 
-git commit -s -m "dockerfile: bump to v$1" -- dockerfiles/*
+git commit -s -m "dockerfile: bump to v$NEW_VERSION" -- dockerfiles/*
 
 # Snap
-sed -i "s/version: '[0-9].[0-9].[0-9]'/version: '$1'/g" snap/snapcraft.yaml
-git commit -s -m "snap: bump to v$1" snap/snapcraft.yaml
+sed -i "s/version: '[0-9].[0-9].[0-9]'/version: '$NEW_VERSION'/g" snap/snapcraft.yaml
+git commit -s -m "snap: bump to v$NEW_VERSION" snap/snapcraft.yaml
 
 # Bitbake / Yocto
-sed -i "s/PV = \"[0-9].[0-9].[0-9]\"/PV = \"$1\"/g" fluent-bit_*.*.*.bb
-git mv fluent-bit_*.*.*.bb "fluent-bit_$1.bb"
-git commit -a -s -m "bitbake: bump to v$1"
+sed -i "s/PV = \"[0-9].[0-9].[0-9]\"/PV = \"$NEW_VERSION\"/g" fluent-bit_*.*.*.bb
+git mv fluent-bit_*.*.*.bb "fluent-bit_$NEW_VERSION.bb"
+git commit -a -s -m "bitbake: bump to v$NEW_VERSION"
