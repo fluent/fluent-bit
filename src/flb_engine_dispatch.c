9cddf9f0745835e90007a52750df8ecb466f5c40
Leonardo Alminana <leonardo@calyptia.com>
2023-04-27 11:57:12 +0200

    engine_dispatch: added chunk withholding mechanism
    flb_engine_dispatch greedily tries to start flush tasks for all chunks
    that are not marked as busy but this can cause issues when we are close
    to exceeding the storage limits.
    
    Because of this a custom mechanism was added to delay the flushing of
    the oldest chunks when certain criteria is met.
    
    Signed-off-by: Leonardo Alminana <leonardo@calyptia.com>

--- src/flb_engine_dispatch.c
+++ src/flb_engine_dispatch.c
@@ -18,6 +18,7 @@
  *  limitations under the License.
  */
 
+#include <math.h>
 #include <stdlib.h>
 
 #include <fluent-bit/flb_info.h>
@@ -29,6 +30,28 @@
 #include <fluent-bit/flb_coro.h>
 #include <fluent-bit/flb_engine.h>
 #include <fluent-bit/flb_task.h>
+
+
+/* FLB_DISPATCHER_RESERVED_CHUNK_PERCENTAGE defines the percentage of chunks that
+ * will not be scheduled to be flushed if there are routes that risk running out
+ * of storage space.
+ *
+ * FLB_DISPATCHER_RESERVED_CHUNK_MINIMUM defines a lower boundary for the number
+ * of chunks we calculate using FLB_DISPATCHER_RESERVED_CHUNK_PERCENTAGE which
+ * in the real world shouldn't happen because target systems usually have
+ * limits above 500 megabytes.
+ *
+ * FLB_DISPATCHER_RESERVED_STORAGE_SPACE as defined ensures that the system is
+ * able to ingest N bytes without exceeding the limits (in this PoCs case 10 MB).
+ *
+ * In the real world these souldn't be constants but rather settings and the
+ * storage space setting should be higher than 10 megabytes, especially when
+ * the ingestion volume is rather large.
+*/
+
+#define FLB_DISPATCHER_RESERVED_CHUNK_PERCENTAGE 10
+#define FLB_DISPATCHER_RESERVED_CHUNK_MINIMUM    5
+#define FLB_DISPATCHER_RESERVED_STORAGE_SPACE    (10 * 1000000)
 
 /* It creates a new output thread using a 'Retry' context */
 int flb_engine_dispatch_retry(struct flb_task_retry *retry,
@@ -226,16 +249,44 @@ int flb_engine_dispatch(uint64_t id, struct flb_input_instance *in,
     struct flb_input_plugin *p;
     struct flb_input_chunk *ic;
     struct flb_task *task = NULL;
+    size_t chunk_count;
+    size_t chunk_index;
+    size_t availability_window;
+    int    overlimit_flag;
 
     p = in->p;
     if (!p) {
         return 0;
     }
 
+    chunk_count = mk_list_size(&in->chunks);
+    chunk_index = 0;
+
+    availability_window = ceil((FLB_DISPATCHER_RESERVED_CHUNK_PERCENTAGE * chunk_count) / 100);
+
+    if (availability_window < FLB_DISPATCHER_RESERVED_CHUNK_MINIMUM) {
+        availability_window = FLB_DISPATCHER_RESERVED_CHUNK_MINIMUM;
+    }
+
     /* Look for chunks ready to go */
     mk_list_foreach_safe(head, tmp, &in->chunks) {
         ic = mk_list_entry(head, struct flb_input_chunk, _head);
         if (ic->busy == FLB_TRUE) {
+            continue;
+        }
+
+        chunk_index++;
+
+        overlimit_flag = flb_input_chunk_has_overlimit_routes(
+                            ic, FLB_DISPATCHER_RESERVED_STORAGE_SPACE);
+
+        if (overlimit_flag &&
+            availability_window >= chunk_index) {
+            flb_debug("[task] skipping chunk %s task creation to ensure "
+                      "droppable chunk availability (%zu / %zu)",
+                      flb_input_chunk_get_name(ic),
+                      chunk_index, availability_window);
+
             continue;
         }
 
