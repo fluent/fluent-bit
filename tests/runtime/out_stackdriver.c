403362db4368c5a204dba0fb8751a582b4ec6dd9
Andre Silva <andreswebs@pm.me>
2023-05-08 23:33:56 -0300

    out_stackdriver: process spanId special field
    This adds support for processing the logging.googleapis.com/spanId
    special field from the jsonPayload
    
    Signed-off-by: Andre Silva <andreswebs@pm.me>

--- tests/runtime/out_stackdriver.c
+++ tests/runtime/out_stackdriver.c
@@ -35,6 +35,7 @@
 #include "data/stackdriver/stackdriver_test_k8s_resource.h"
 #include "data/stackdriver/stackdriver_test_labels.h"
 #include "data/stackdriver/stackdriver_test_trace.h"
+#include "data/stackdriver/stackdriver_test_span_id.h"
 #include "data/stackdriver/stackdriver_test_log_name.h"
 #include "data/stackdriver/stackdriver_test_resource_labels.h"
 #include "data/stackdriver/stackdriver_test_insert_id.h"
@@ -748,6 +749,24 @@ static void cb_check_trace_stackdriver_autoformat(void *ctx, int ffd,
 
     /* trace has been removed from jsonPayload */
     ret = mp_kv_exists(res_data, res_size, "$entries[0]['jsonPayload']['trace']");
+    TEST_CHECK(ret == FLB_FALSE);
+
+    flb_sds_destroy(res_data);
+}
+
+static void cb_check_span_id(void *ctx, int ffd,
+                                         int res_ret, void *res_data, size_t res_size,
+                                         void *data)
+{
+    int ret;
+
+    /* span id in the entries */
+    ret = mp_kv_cmp(res_data, res_size, "$entries[0]['spanId']", "000000000000004a");
+    TEST_CHECK(ret == FLB_TRUE);
+
+    /* span id has been removed from jsonPayload */
+    ret = mp_kv_exists(res_data, res_size,
+                       "$entries[0]['jsonPayload']['logging.googleapis.com/spanId']");
     TEST_CHECK(ret == FLB_FALSE);
 
     flb_sds_destroy(res_data);
@@ -2433,6 +2452,47 @@ void flb_test_trace_stackdriver_autoformat()
 
     /* Ingest data sample */
     flb_lib_push(ctx, in_ffd, (char *) TRACE_COMMON_CASE, size);
+
+    sleep(2);
+    flb_stop(ctx);
+    flb_destroy(ctx);
+}
+
+
+void flb_test_span_id()
+{
+    int ret;
+    int size = sizeof(SPAN_ID_COMMON_CASE) - 1;
+    flb_ctx_t *ctx;
+    int in_ffd;
+    int out_ffd;
+
+    /* Create context, flush every second (some checks omitted here) */
+    ctx = flb_create();
+    flb_service_set(ctx, "flush", "1", "grace", "1", NULL);
+
+    /* Lib input mode */
+    in_ffd = flb_input(ctx, (char *) "lib", NULL);
+    flb_input_set(ctx, in_ffd, "tag", "test", NULL);
+
+    /* Stackdriver output */
+    out_ffd = flb_output(ctx, (char *) "stackdriver", NULL);
+    flb_output_set(ctx, out_ffd,
+                   "match", "test",
+                   "resource", "gce_instance",
+                   NULL);
+
+    /* Enable test mode */
+    ret = flb_output_set_test(ctx, out_ffd, "formatter",
+                              cb_check_span_id,
+                              NULL, NULL);
+
+    /* Start */
+    ret = flb_start(ctx);
+    TEST_CHECK(ret == 0);
+
+    /* Ingest data sample */
+    flb_lib_push(ctx, in_ffd, (char *) SPAN_ID_COMMON_CASE, size);
 
     sleep(2);
     flb_stop(ctx);
@@ -5963,6 +6023,9 @@ TEST_LIST = {
     /* test trace */
     {"trace_no_autoformat", flb_test_trace_no_autoformat},
     {"trace_stackdriver_autoformat", flb_test_trace_stackdriver_autoformat},
+
+    /* test span id */
+    {"span_id", flb_test_span_id},
 
     /* test metadata server */
     {"set_metadata_server", flb_test_set_metadata_server},
