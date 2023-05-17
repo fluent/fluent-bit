bd713e7774675b370276504870bedc63e32116a5
Markus Meyer <hydrantanderwand@gmail.com>
2023-05-17 16:24:06 +0200

    out_opensearch: add support for gzip compression
    This PR adds optional support for gzip comprssion to the 
    opensearch output plugin.
    
    Signed-off-by: wndhydrnt <hydrantanderwand@gmail.com>

--- plugins/out_opensearch/opensearch.c
+++ plugins/out_opensearch/opensearch.c
@@ -25,6 +25,7 @@
 #include <fluent-bit/flb_time.h>
 #include <fluent-bit/flb_signv4.h>
 #include <fluent-bit/flb_aws_credentials.h>
+#include <fluent-bit/flb_gzip.h>
 #include <fluent-bit/flb_record_accessor.h>
 #include <fluent-bit/flb_ra_key.h>
 #include <fluent-bit/flb_log_event_decoder.h>
@@ -868,6 +869,9 @@ static void cb_opensearch_flush(struct flb_event_chunk *event_chunk,
     struct flb_connection *u_conn;
     struct flb_http_client *c;
     flb_sds_t signature = NULL;
+    int compressed = FLB_FALSE;
+    void *final_payload_buf = NULL;
+    size_t final_payload_size = 0;
 
     /* Get upstream connection */
     u_conn = flb_upstream_conn_get(ctx->u);
@@ -902,9 +906,26 @@ static void cb_opensearch_flush(struct flb_event_chunk *event_chunk,
     pack = (char *) out_buf;
     pack_size = out_size;
 
+    final_payload_buf = pack;
+    final_payload_size = pack_size;
+    /* Should we compress the payload ? */
+    if (ctx->compression == FLB_OS_COMPRESSION_GZIP) {
+        ret = flb_gzip_compress((void *) pack, pack_size,
+                                &out_buf, &out_size);
+        if (ret == -1) {
+            flb_plg_error(ctx->ins,
+                          "cannot gzip payload, disabling compression");
+        }
+        else {
+            compressed = FLB_TRUE;
+            final_payload_buf = out_buf;
+            final_payload_size = out_size;
+        }
+    }
+
     /* Compose HTTP Client request */
     c = flb_http_client(u_conn, FLB_HTTP_POST, ctx->uri,
-                        pack, pack_size, NULL, 0, NULL, 0);
+                        final_payload_buf, final_payload_size, NULL, 0, NULL, 0);
 
     flb_http_buffer_size(c, ctx->buffer_size);
 
@@ -929,6 +950,13 @@ static void cb_opensearch_flush(struct flb_event_chunk *event_chunk,
         flb_http_add_header(c, "User-Agent", 10, "Fluent-Bit", 10);
     }
 #endif
+
+    /* Set Content-Encoding of compressed payload */
+    if (compressed == FLB_TRUE) {
+        if (ctx->compression == FLB_OS_COMPRESSION_GZIP) {
+            flb_http_set_content_encoding_gzip(c);
+        }
+    }
 
     /* Map debug callbacks */
     flb_http_client_debug(c, ctx->ins->callback);
@@ -965,7 +993,7 @@ static void cb_opensearch_flush(struct flb_event_chunk *event_chunk,
                     /*
                      * If trace_error is set, trace the actual
                      * response from Elasticsearch explaining the problem.
-                     * Trace_Output can be used to see the request. 
+                     * Trace_Output can be used to see the request.
                      */
                     if (pack_size < 4000) {
                         flb_plg_debug(ctx->ins, "error caused by: Input\n%.*s\n",
@@ -998,6 +1026,11 @@ static void cb_opensearch_flush(struct flb_event_chunk *event_chunk,
     /* Cleanup */
     flb_http_client_destroy(c);
     flb_sds_destroy(pack);
+
+    if (final_payload_buf != pack) {
+        flb_free(final_payload_buf);
+    }
+
     flb_upstream_conn_release(u_conn);
     if (signature) {
         flb_sds_destroy(signature);
@@ -1008,6 +1041,11 @@ static void cb_opensearch_flush(struct flb_event_chunk *event_chunk,
  retry:
     flb_http_client_destroy(c);
     flb_sds_destroy(pack);
+
+    if (final_payload_buf != pack) {
+        flb_free(final_payload_buf);
+    }
+
     flb_upstream_conn_release(u_conn);
     FLB_OUTPUT_RETURN(FLB_RETRY);
 }
@@ -1217,6 +1255,13 @@ static struct flb_config_map config_map[] = {
      FLB_CONFIG_MAP_BOOL, "trace_error", "false",
      0, FLB_TRUE, offsetof(struct flb_opensearch, trace_error),
      "When enabled print the OpenSearch exception to stderr (for diag only)"
+    },
+
+    /* HTTP Compression */
+    {
+     FLB_CONFIG_MAP_STR, "compress", NULL,
+     0, FLB_TRUE, offsetof(struct flb_opensearch, compression_str),
+     "Set payload compression mechanism. Option available is 'gzip'"
     },
 
     /* EOF */
