ed302ee66b182f84fc69b93e20f837438ea03da5
Leonardo Alminana <leonardo@calyptia.com>
2023-05-24 11:34:04 +0200

    input_log: renamed function to ensure it clearly conveys its purpose
    Signed-off-by: Leonardo Alminana <leonardo@calyptia.com>

--- include/fluent-bit/flb_input_log.h
+++ include/fluent-bit/flb_input_log.h
@@ -27,13 +27,15 @@ int flb_input_log_append(struct flb_input_instance *ins,
                          const char *tag, size_t tag_len,
                          const void *buf, size_t buf_size);
 
-int flb_input_log_append_ex(struct flb_input_instance *ins,
-                            size_t processor_starting_stage,
-                            const char *tag, size_t tag_len,
-                            const void *buf, size_t buf_size);
-
 int flb_input_log_append_records(struct flb_input_instance *ins,
                                  size_t records,
                                  const char *tag, size_t tag_len,
                                  const void *buf, size_t buf_size);
+
+int flb_input_log_append_skip_processor_stages(struct flb_input_instance *ins,
+                                               size_t processor_starting_stage,
+                                               const char *tag,
+                                               size_t tag_len,
+                                               const void *buf,
+                                               size_t buf_size);
 #endif
