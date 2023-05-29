ffb3ee67dba80d922b767715144138c09d56814d
Thiago Padilha <thiago@calyptia.com>
2023-05-29 09:18:44 -0300

    in_kafka: Implement "data_format" config option.
    This option allows automatic parsing of kafka messages payload with the
    specified format. Right now only "json" is supported.
    
    Signed-off-by: Thiago Padilha <thiago@calyptia.com>

--- plugins/in_kafka/in_kafka.h
+++ plugins/in_kafka/in_kafka.h
@@ -28,13 +28,20 @@
 #include <fluent-bit/flb_log_event_encoder.h>
 
 
-#define FLB_IN_KAFKA_DEFAULT_POLL_MS  "500"
+#define FLB_IN_KAFKA_DEFAULT_POLL_MS       "500"
+#define FLB_IN_KAFKA_DEFAULT_DATA_FORMAT   "none"
+
+enum {
+    FLB_IN_KAFKA_DATA_FORMAT_NONE,
+    FLB_IN_KAFKA_DATA_FORMAT_JSON,
+};
 
 struct flb_in_kafka_config {
     struct flb_kafka kafka;
     struct flb_input_instance *ins;
     struct flb_log_event_encoder *log_encoder;
     int poll_ms;
+    int data_format;
 };
 
 #endif
