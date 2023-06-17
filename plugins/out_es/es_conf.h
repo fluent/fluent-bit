d456da760016c0e090104a82405478f5d40f12e0
Marat Abrarov <abrarov@gmail.com>
2023-06-18 00:36:37 +0300

    pipeline: outputs: es: support overriding the most plugin parameters with Upstream node configuration
    For Elastic cloud authentication these parameters are always taken from plugin configuration and never from Upstream node configuration: cloud_id.
    
    For AWS authentication these parameters are always taken from plugin configuration and never from Upstream node configuration: http_proxy, no_proxy, tls*.
    
    Signed-off-by: Marat Abrarov <abrarov@gmail.com>

--- plugins/out_es/es_conf.h
+++ plugins/out_es/es_conf.h
@@ -24,6 +24,11 @@
 
 #include "es.h"
 
+#define FLB_ES_WRITE_OP_INDEX  "index"
+#define FLB_ES_WRITE_OP_CREATE "create"
+#define FLB_ES_WRITE_OP_UPDATE "update"
+#define FLB_ES_WRITE_OP_UPSERT "upsert"
+
 struct flb_elasticsearch *flb_es_conf_create(struct flb_output_instance *ins,
                                              struct flb_config *config);
 
