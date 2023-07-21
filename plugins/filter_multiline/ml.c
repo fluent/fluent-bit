c86fab981153e100e9f3e3f0f9f60f13501836ea
Eduardo Silva <eduardo@calyptia.com>
2023-07-20 23:13:32 -0600

    filter_multiline: remove unused variable
    Signed-off-by: Eduardo Silva <eduardo@calyptia.com>

--- plugins/filter_multiline/ml.c
+++ plugins/filter_multiline/ml.c
@@ -551,7 +551,6 @@ static int ml_filter_partial(const void *data, size_t bytes,
     msgpack_sbuffer tmp_sbuf;
     msgpack_packer tmp_pck;
     int partial_records = 0;
-    int total_records = 0;
     int return_records = 0;
     int partial = FLB_FALSE;
     int is_last_partial = FLB_FALSE;
@@ -619,8 +618,6 @@ static int ml_filter_partial(const void *data, size_t bytes,
     while ((ret = flb_log_event_decoder_next(
                     &log_decoder,
                     &log_event)) == FLB_EVENT_DECODER_SUCCESS) {
-        total_records++;
-
         partial = ml_is_partial(log_event.body);
         if (partial == FLB_TRUE) {
             partial_records++;
