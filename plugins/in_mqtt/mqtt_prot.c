ccb9d7b1c5803af51d9156e010e96749b3a9da9e
Takahiro Yamashita <nokute78@gmail.com>
2023-08-27 09:06:31 +0900

    in_mqtt: support payload_key
    Signed-off-by: Takahiro Yamashita <nokute78@gmail.com>

--- plugins/in_mqtt/mqtt_prot.c
+++ plugins/in_mqtt/mqtt_prot.c
@@ -171,6 +171,13 @@ static int mqtt_data_append(char *topic, size_t topic_len,
                 FLB_LOG_EVENT_STRING_VALUE(topic, topic_len));
     }
 
+    if (ctx->payload_key) {
+        flb_log_event_encoder_append_body_string_length(ctx->log_encoder, flb_sds_len(ctx->payload_key));
+        flb_log_event_encoder_append_body_string_body(ctx->log_encoder, ctx->payload_key,
+                                                      flb_sds_len(ctx->payload_key));
+        flb_log_event_encoder_body_begin_map(ctx->log_encoder);
+    }
+
     /* Re-pack original KVs */
     for (i = 0;
          i < root.via.map.size &&
@@ -180,6 +187,10 @@ static int mqtt_data_append(char *topic, size_t topic_len,
                 ctx->log_encoder,
                 FLB_LOG_EVENT_MSGPACK_OBJECT_VALUE(&root.via.map.ptr[i].key),
                 FLB_LOG_EVENT_MSGPACK_OBJECT_VALUE(&root.via.map.ptr[i].val));
+    }
+
+    if (ctx->payload_key) {
+        flb_log_event_encoder_body_commit_map(ctx->log_encoder);
     }
 
     if (ret == FLB_EVENT_ENCODER_SUCCESS) {
