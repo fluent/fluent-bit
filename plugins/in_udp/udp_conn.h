9763d0b53aaa8901c10b2f6f528b29e7cab8cf9c
Hiroshi Hatake <hatake@calyptia.com>
2023-09-01 04:02:31 +0900

    in_udp: Add a capability to inject source IP (#7673)
    ---------
    
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- plugins/in_udp/udp_conn.h
+++ plugins/in_udp/udp_conn.h
@@ -25,6 +25,10 @@
 
 #define FLB_IN_UDP_CHUNK "32768"
 
+#define FLB_MAP_EXPAND_SUCCESS   0
+#define FLB_MAP_NOT_MODIFIED    -1
+#define FLB_MAP_EXPANSION_ERROR -2
+#define FLB_MAP_EXPANSION_INVALID_VALUE_TYPE -3
 
 struct udp_conn_stream {
     char *tag;
