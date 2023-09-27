941a7afab3808873871265fa72e956c58d042330
Phillip Whelan <phil@calyptia.com>
2023-09-27 10:57:02 -0300

    input: free flb_input libco parameters on input thread exit.
    Signed-off-by: Phillip Whelan <phil@calyptia.com>

--- src/flb_input_thread.c
+++ src/flb_input_thread.c
@@ -459,6 +459,7 @@ static void input_thread(void *data)
     flb_bucket_queue_destroy(evl_bktq);
     flb_sched_destroy(sched);
     input_thread_instance_destroy(thi);
+    flb_input_unprepare();
 }
 
 
