502a2098994951758d3682ee4735f85d3d999d9f
Hiroshi Hatake <hatake@calyptia.com>
2023-10-17 13:22:12 +0900

    in_node_exporter_metrics: Fix macOS build (#8048)
    To build on macOS, we have to provide skeleton functions which are not
    targeted OSes.
    
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- plugins/in_node_exporter_metrics/ne_processes.h
+++ plugins/in_node_exporter_metrics/ne_processes.h
@@ -22,8 +22,26 @@
 
 #include "ne.h"
 
+#ifdef __linux__
 int ne_processes_init(struct flb_ne *ctx);
 int ne_processes_update(struct flb_ne *ctx);
 int ne_processes_exit(struct flb_ne *ctx);
+#else
+static int ne_processes_init(struct flb_ne *ctx)
+{
+    return 0;
+}
+
+static int ne_processes_update(struct flb_ne *ctx)
+{
+    return 0;
+}
+
+static int ne_processes_exit(struct flb_ne *ctx)
+{
+    return 0;
+}
+
+#endif
 
 #endif
