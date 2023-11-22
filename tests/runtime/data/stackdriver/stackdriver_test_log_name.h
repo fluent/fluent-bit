60919e5b19450e565736cefcfa262b4e53de2f45
ryanohnemus <ryanohnemus@gmail.com>
2023-11-22 10:00:37 -0600

    out_stackdriver: add project_id_key override
    Closes #8195
    
    Signed-off-by: ryanohnemus <ryanohnemus@gmail.com>

--- tests/runtime/data/stackdriver/stackdriver_test_log_name.h
+++ tests/runtime/data/stackdriver/stackdriver_test_log_name.h
@@ -8,3 +8,15 @@
 	"1591111124," \
 	"{"	\
 	"}]"
+
+#define LOG_NAME_PROJECT_ID_OVERRIDE "[" \
+	"1591111124," \
+	"{"	\
+		"\"test_project_key\": \"fluent-bit-test-project-2\"" \
+	"}]"
+
+#define LOG_NAME_PROJECT_ID_NO_OVERRIDE	"[" \
+	"1591111124," \
+	"{"	\
+	    "\"logging.googleapis.com/projectId\": \"fluent-bit-test-project-2\"" \
+	"}]"
