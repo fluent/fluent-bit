1ca5136d4116f0e2b0264c5e302cd3fcce431dd4
nvima <p.mirwald@outlook.de>
2023-11-26 19:42:04 +0100

    config: added ipv6 dns result priorization option
    Signed-off-by: nvima <p.mirwald@outlook.de>

--- src/flb_config.c
+++ src/flb_config.c
@@ -125,6 +125,10 @@ struct flb_service_config service_configs[] = {
      FLB_CONF_TYPE_BOOL,
      offsetof(struct flb_config, dns_prefer_ipv4)},
 
+    {FLB_CONF_DNS_PREFER_IPV6,
+     FLB_CONF_TYPE_BOOL,
+     offsetof(struct flb_config, dns_prefer_ipv6)},
+
     /* Storage */
     {FLB_CONF_STORAGE_PATH,
      FLB_CONF_TYPE_STR,
