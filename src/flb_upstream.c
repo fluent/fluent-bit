6ec7b61c90028205cfc5b64c01ddec50ccea3f99
nvima <p.mirwald@outlook.de>
2023-11-26 19:44:26 +0100

    upstream: added ipv6 dns result priorization option
    Signed-off-by: nvima <p.mirwald@outlook.de>

--- src/flb_upstream.c
+++ src/flb_upstream.c
@@ -54,6 +54,12 @@ struct flb_config_map upstream_net[] = {
     },
 
     {
+     FLB_CONFIG_MAP_BOOL, "net.dns.prefer_ipv6", "false",
+     0, FLB_TRUE, offsetof(struct flb_net_setup, dns_prefer_ipv6),
+     "Prioritize IPv6 DNS results when trying to establish a connection"
+    },
+
+    {
      FLB_CONFIG_MAP_BOOL, "net.keepalive", "true",
      0, FLB_TRUE, offsetof(struct flb_net_setup, keepalive),
      "Enable or disable Keepalive support"
@@ -162,6 +168,12 @@ struct mk_list *flb_upstream_get_config_map(struct flb_config *config)
         if (config->dns_prefer_ipv4) {
             if (strcmp(upstream_net[config_index].name,
                        "net.dns.prefer_ipv4") == 0) {
+                upstream_net[config_index].def_value = "true";
+            }
+        }
+        if (config->dns_prefer_ipv6) {
+            if (strcmp(upstream_net[config_index].name,
+                       "net.dns.prefer_ipv6") == 0) {
                 upstream_net[config_index].def_value = "true";
             }
         }
