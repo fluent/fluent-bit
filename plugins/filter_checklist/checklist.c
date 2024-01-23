f81769be4c527531827152a7768cc0bb5f17e91a
Hiroshi Hatake <hatake@calyptia.com>
2024-01-23 19:51:18 +0900

    filter_checklist: Follow the signature change of flb_filter_do
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- plugins/filter_checklist/checklist.c
+++ plugins/filter_checklist/checklist.c
@@ -419,7 +419,8 @@ static int cb_checklist_filter(const void *data, size_t bytes,
                                struct flb_filter_instance *ins,
                                struct flb_input_instance *i_ins,
                                void *filter_context,
-                               struct flb_config *config)
+                               struct flb_config *config,
+                               int event_type)
 {
     int i;
     int id;
