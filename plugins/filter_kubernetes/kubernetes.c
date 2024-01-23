db439aa0849c55c5588fc9554f2e61bf45357e6f
Hiroshi Hatake <hatake@calyptia.com>
2024-01-23 19:53:16 +0900

    filter_kubernetes: Follow the signature change of flb_filter_do
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- plugins/filter_kubernetes/kubernetes.c
+++ plugins/filter_kubernetes/kubernetes.c
@@ -521,7 +521,8 @@ static int cb_kube_filter(const void *data, size_t bytes,
                           struct flb_filter_instance *f_ins,
                           struct flb_input_instance *i_ins,
                           void *filter_context,
-                          struct flb_config *config)
+                          struct flb_config *config,
+                          int event_type)
 {
     int ret;
     size_t pre = 0;
