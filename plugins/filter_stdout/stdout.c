c72e35804a63534e8f92a4d4561d631d5cd7c51e
Hiroshi Hatake <hatake@calyptia.com>
2024-01-23 20:03:31 +0900

    filter_stdout: Handle metrics and traces type of events
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- plugins/filter_stdout/stdout.c
+++ plugins/filter_stdout/stdout.c
@@ -25,6 +25,11 @@
 #include <fluent-bit/flb_time.h>
 #include <fluent-bit/flb_log_event_decoder.h>
 #include <fluent-bit/flb_log_event_encoder.h>
+#include <fluent-bit/flb_metrics.h>
+
+#include <ctraces/ctraces.h>
+#include <ctraces/ctr_decode_msgpack.h>
+
 #include <msgpack.h>
 
 static int cb_stdout_init(struct flb_filter_instance *f_ins,
@@ -40,6 +45,64 @@ static int cb_stdout_init(struct flb_filter_instance *f_ins,
         return -1;
     }
     return 0;
+}
+
+#ifdef FLB_HAVE_METRICS
+static void print_metrics_text(struct flb_filter_instance *f_ins,
+                               const void *data, size_t bytes)
+{
+    int ret;
+    size_t off = 0;
+    cfl_sds_t text;
+    struct cmt *cmt = NULL;
+
+    /* get cmetrics context */
+    ret = cmt_decode_msgpack_create(&cmt, (char *) data, bytes, &off);
+    if (ret != 0) {
+        flb_plg_error(f_ins, "could not process metrics payload");
+        return;
+    }
+
+    /* convert to text representation */
+    text = cmt_encode_text_create(cmt);
+
+    /* destroy cmt context */
+    cmt_destroy(cmt);
+
+    printf("%s", text);
+    fflush(stdout);
+
+    cmt_encode_text_destroy(text);
+}
+#endif
+
+static void print_traces_text(struct flb_filter_instance *f_ins,
+                              const void *data, size_t bytes)
+{
+    int ret;
+    size_t off = 0;
+    cfl_sds_t text;
+    struct ctrace *ctr = NULL;
+    int ok = CTR_DECODE_MSGPACK_SUCCESS;
+
+    /* Decode each ctrace context */
+    while ((ret = ctr_decode_msgpack_create(&ctr,
+                                            (char *) data,
+                                            bytes, &off)) == ok) {
+        /* convert to text representation */
+        text = ctr_encode_text_create(ctr);
+
+        /* destroy ctr context */
+        ctr_destroy(ctr);
+
+        printf("%s", text);
+        fflush(stdout);
+
+        ctr_encode_text_destroy(text);
+    }
+    if (ret != ok) {
+        flb_plg_debug(f_ins, "ctr decode msgpack returned : %d", ret);
+    }
 }
 
 static int cb_stdout_filter(const void *data, size_t bytes,
@@ -62,6 +125,19 @@ static int cb_stdout_filter(const void *data, size_t bytes,
     (void) i_ins;
     (void) filter_context;
     (void) config;
+
+#ifdef FLB_HAVE_METRICS
+    /* Check if the event type is metrics, handle the payload differently */
+    if (event_type == FLB_INPUT_METRICS) {
+        print_metrics_text(f_ins, (char *) data, bytes);
+        return FLB_FILTER_NOTOUCH;
+    }
+#endif
+
+    if (event_type == FLB_INPUT_TRACES) {
+        print_traces_text(f_ins, (char *) data, bytes);
+        return FLB_FILTER_NOTOUCH;
+    }
 
     ret = flb_log_event_decoder_init(&log_decoder, (char *) data, bytes);
 
@@ -104,5 +180,6 @@ struct flb_filter_plugin filter_stdout_plugin = {
     .cb_filter    = cb_stdout_filter,
     .cb_exit      = NULL,
     .config_map   = config_map,
+    .event_type   = FLB_FILTER_LOGS | FLB_FILTER_METRICS | FLB_FILTER_TRACES,
     .flags        = 0
 };
