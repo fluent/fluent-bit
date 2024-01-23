5d1986b726aa7300af1de12c74e61132b7252174
Hiroshi Hatake <hatake@calyptia.com>
2024-01-23 19:56:58 +0900

    filter_tensorflow: Follow the signature change of flb_filter_do
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- plugins/filter_tensorflow/tensorflow.c
+++ plugins/filter_tensorflow/tensorflow.c
@@ -249,7 +249,8 @@ static int cb_tensorflow_filter(const void *data, size_t bytes,
                                 struct flb_filter_instance *f_ins,
                                 struct flb_input_instance *i_ins,
                                 void *filter_context,
-                                struct flb_config *config)
+                                struct flb_config *config,
+                                int event_type)
 {
     int i;
     int j;
