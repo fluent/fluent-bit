fec7a127d748800add94202dab80a52286ae7f64
Hiroshi Hatake <hatake@calyptia.com>
2024-01-23 19:57:49 +0900

    filter_wasm: Follow the signature change of flb_filter_do
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- plugins/filter_wasm/filter_wasm.c
+++ plugins/filter_wasm/filter_wasm.c
@@ -43,7 +43,8 @@ static int cb_wasm_filter(const void *data, size_t bytes,
                           struct flb_filter_instance *f_ins,
                           struct flb_input_instance *i_ins,
                           void *filter_context,
-                          struct flb_config *config)
+                          struct flb_config *config,
+                          int event_type)
 {
     int ret;
     char *ret_val = NULL;
