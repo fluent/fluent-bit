8e080a10c556d8d4059de7250d2f8c61d3882613
Hiroshi Hatake <hatake@calyptia.com>
2024-01-23 19:52:08 +0900

    filter_expect: Follow the signature change of flb_filter_do
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- plugins/filter_expect/expect.c
+++ plugins/filter_expect/expect.c
@@ -401,7 +401,8 @@ static int cb_expect_filter(const void *data, size_t bytes,
                             struct flb_filter_instance *f_ins,
                             struct flb_input_instance *i_ins,
                             void *filter_context,
-                            struct flb_config *config)
+                            struct flb_config *config,
+                            int event_type)
 {
     int ret;
     int i;
