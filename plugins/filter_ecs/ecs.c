afd596e06fcd981e5f42b45188e3ee160a1e94a9
Hiroshi Hatake <hatake@calyptia.com>
2024-01-23 19:51:48 +0900

    filter_ecs: Follow the signature change of flb_filter_do
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- plugins/filter_ecs/ecs.c
+++ plugins/filter_ecs/ecs.c
@@ -1440,7 +1440,8 @@ static int cb_ecs_filter(const void *data, size_t bytes,
                          struct flb_filter_instance *f_ins,
                          struct flb_input_instance *i_ins,
                          void *context,
-                         struct flb_config *config)
+                         struct flb_config *config,
+                         int event_type)
 {
     struct flb_filter_ecs *ctx = context;
     int i = 0;
