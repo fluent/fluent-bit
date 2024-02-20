b7977efd9e1d486e3072af296122cce248f3643e
Hiroshi Hatake <hiroshi@chronosphere.io>
2024-02-20 18:26:20 +0900

    filter_type_converter: Follow the signature change
    Signed-off-by: Hiroshi Hatake <hiroshi@chronosphere.io>

--- plugins/filter_type_converter/type_converter.c
+++ plugins/filter_type_converter/type_converter.c
@@ -185,7 +185,8 @@ static int cb_type_converter_filter(const void *data, size_t bytes,
                                     struct flb_filter_instance *f_ins,
                                     struct flb_input_instance *i_ins,
                                     void *filter_context,
-                                    struct flb_config *config)
+                                    struct flb_config *config,
+                                    int event_type)
 {
     struct type_converter_ctx *ctx = filter_context;
     struct flb_time tm;
