f57682d91ca8513e33568c84612308fba942f90d
Eduardo Silva <eduardo@calyptia.com>
2024-03-15 12:49:20 -0600

    processor_sql: add wildcard support and code cleanup
    Signed-off-by: Eduardo Silva <eduardo@calyptia.com>

--- plugins/processor_sql/parser/sql_parser.c
+++ plugins/processor_sql/parser/sql_parser.c
@@ -14,119 +14,6 @@
 
 #include "sql.h"
 
-//#include "sql_parser.h"
-//#include "sql_lex.h"
-
-// void flb_sp_cmd_destroy(struct flb_sp_cmd *cmd)
-// {
-//     struct mk_list *head;
-//     struct mk_list *tmp;
-//     struct flb_sp_cmd_key *key;
-
-//     /* remove keys */
-//     mk_list_foreach_safe(head, tmp, &cmd->keys) {
-//         key = mk_list_entry(head, struct flb_sp_cmd_key, _head);
-//         mk_list_del(&key->_head);
-//         flb_sp_cmd_key_del(key);
-//     }
-
-//     flb_free(cmd->source_name);
-//     flb_free(cmd);
-// }
-
-// void flb_sp_cmd_key_del(struct flb_sp_cmd_key *key)
-// {
-//     if (key->name) {
-//         flb_sds_destroy(key->name);
-//     }
-//     if (key->alias) {
-//         flb_sds_destroy(key->alias);
-//     }
-//     flb_free(key);
-// }
-
-// int flb_sp_cmd_key_add(struct flb_sp_cmd *cmd, int aggr_func,
-//                        char *key_name, char *key_alias)
-// {
-//     struct flb_sp_cmd_key *key;
-
-//     key = flb_calloc(1, sizeof(struct flb_sp_cmd_key));
-//     if (!key) {
-//         flb_errno();
-//         return -1;
-//     }
-
-//     /* key name and aliases works when the selection is not a wildcard */
-//     if (key_name) {
-//         key->name = flb_sds_create(key_name);
-//         if (!key->name) {
-//             flb_sp_cmd_key_del(key);
-//             return -1;
-//         }
-//     }
-//     else {
-//         /*
-//          * This is a wildcard selection, make sure if any aggregation function
-//          * exists only apply for COUNT().
-//          */
-//         if (aggr_func > 0  && aggr_func != FLB_SP_COUNT) {
-//             flb_sp_cmd_key_del(key);
-//             return -1;
-//         }
-//     }
-
-//     if (key_alias) {
-//         key->alias = flb_sds_create(key_alias);
-//         if (!key->alias) {
-//             flb_sp_cmd_key_del(key);
-//             return -1;
-//         }
-//     }
-
-//     /* Aggregation function */
-//     if (aggr_func > 0) {
-//         key->aggr_func = aggr_func;
-//     }
-
-//     mk_list_add(&key->_head, &cmd->keys);
-//     return 0;
-// }
-
-// int flb_sp_cmd_source(struct flb_sp_cmd *cmd, int type, char *source)
-// {
-//     cmd->source_type = type;
-//     cmd->source_name = flb_strdup(source);
-//     if (!cmd->source_name) {
-//         flb_errno();
-//         return -1;
-//     }
-
-//     return 0;
-// }
-
-// void flb_sp_cmd_dump(struct flb_sp_cmd *cmd)
-// {
-//     struct mk_list *head;
-//     struct mk_list *tmp;
-//     struct flb_sp_cmd_key *key;
-
-//     /* Lookup keys */
-//     printf("== KEYS ==\n");
-//     mk_list_foreach_safe(head, tmp, &cmd->keys) {
-//         key = mk_list_entry(head, struct flb_sp_cmd_key, _head);
-//         printf("- '%s'\n", key->name);
-//     }
-//     printf("== SOURCE ==\n");
-//     if (cmd->source_type == FLB_SP_STREAM) {
-//         printf("stream => ");
-//     }
-//     else if (cmd->source_type == FLB_SP_TAG) {
-//         printf("tag match => ");
-//     }
-
-//     printf("'%s'\n", cmd->source_name);
-// }
-
 int sql_parser_query_key_add(struct sql_query *query, char *key_name, char *key_alias)
 {
     struct sql_key *key;
@@ -137,10 +24,20 @@ int sql_parser_query_key_add(struct sql_query *query, char *key_name, char *key_
         return -1;
     }
 
-    key->name = flb_sds_create(key_name);
-    if (!key->name) {
-        flb_free(key);
-        return -1;
+    if (key_name) {
+        key->name = flb_sds_create(key_name);
+        if (!key->name) {
+            flb_free(key);
+            return -1;
+        }
+    }
+    else {
+        /* wildcard case */
+        if (cfl_list_size(&query->keys) > 0) {
+            cfl_sds_destroy(key->name);
+            flb_free(key);
+            return -1;
+        }
     }
 
     if (key_alias) {
@@ -156,18 +53,56 @@ int sql_parser_query_key_add(struct sql_query *query, char *key_name, char *key_
     return 0;
 }
 
-void sql_parser_query_destroy(struct sql_query *query)
+static void condition_list_delete(struct cfl_list *cond_list)
 {
-    struct cfl_list *head;
     struct cfl_list *tmp;
+    struct cfl_list *head;
+    struct sql_expression *exp;
+    struct sql_expression_key *exp_key;
+    struct sql_expression_val *exp_val;
+
+    /* conditions */
+    cfl_list_foreach_safe(head, tmp, cond_list) {
+        exp = cfl_list_entry(head, struct sql_expression, _head);
+        if (exp->type == SQL_EXP_KEY) {
+            exp_key = (struct sql_expression_key *) exp;
+            cfl_sds_destroy(exp_key->name);
+        }
+        else if (exp->type == SQL_EXP_STRING) {
+            exp_val = (struct sql_expression_val *) exp;
+            cfl_sds_destroy(exp_val->val.string);
+        }
+        /* note: SQL_EXP_NULL is released directly */
+
+        cfl_list_del(&exp->_head);
+        flb_free(exp);
+    }
+}
+
+static void keys_list_delete(struct cfl_list *keys)
+{
+    struct cfl_list *tmp;
+    struct cfl_list *head;
     struct sql_key *key;
 
-    cfl_list_foreach_safe(head, tmp, &query->keys) {
+    cfl_list_foreach_safe(head, tmp, keys) {
         key = cfl_list_entry(head, struct sql_key, _head);
+        cfl_sds_destroy(key->name);
+        if (key->alias) {
+            cfl_sds_destroy(key->alias);
+        }
         cfl_list_del(&key->_head);
         flb_free(key);
     }
+}
 
+void sql_parser_query_destroy(struct sql_query *query)
+{
+    /* delete keys list items */
+    keys_list_delete(&query->keys);
+
+    /* delete condition list items */
+    condition_list_delete(&query->cond_list);
     flb_free(query);
 }
 
