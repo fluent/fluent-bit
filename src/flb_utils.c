f622e3c339f37e2549b8388d486ee2c7b123d07f
Phillip Whelan <phil@calyptia.com>
2024-04-16 16:43:18 -0400

    utils: use MachineGUID correctly.
    correctly copy the buffer from RegQuery in flb_utils_get_machine_id when
    querying the MachineGUID.
    
    Signed-off-by: Phillip Whelan <phil@calyptia.com>

--- src/flb_utils.c
+++ src/flb_utils.c
@@ -1415,6 +1415,7 @@ int flb_utils_get_machine_id(char **out_id, size_t *out_size)
 
     if (status == ERROR_SUCCESS) {
         *out_id = flb_calloc(1, dwBufSize+1);
+        memcpy(*out_id, buf, dwBufSize);
 
         if (*out_id == NULL) {
             return -1;
