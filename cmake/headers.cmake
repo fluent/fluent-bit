9fa05c725220594a2f54cd2842038a5d2cd03b70
Thomas Devoogdt <thomas@devoogdt.com>
2024-06-15 17:47:31 +0200

    build: use the system provided librdkafka if found
    e.g. buildroot has logic to build librdkafka,
    so if pkg_check_modules can find a suitable version,
    then use that one if -DFLB_PREFER_SYSTEM_LIB_KAFKA=Yes.
    
    Signed-off-by: Thomas Devoogdt <thomas@devoogdt.com>

--- cmake/headers.cmake
+++ cmake/headers.cmake
@@ -42,10 +42,6 @@ include_directories(
   ${CMAKE_CURRENT_BINARY_DIR}/lib/monkey/include/monkey/
   )
 
-if(FLB_IN_KAFKA OR FLB_OUT_KAFKA)
-    include_directories(${FLB_PATH_ROOT_SOURCE}/${FLB_PATH_LIB_RDKAFKA}/src/)
-endif()
-
 if(FLB_UTF8_ENCODER)
   include_directories(${FLB_PATH_ROOT_SOURCE}/${FLB_PATH_LIB_TUTF8E}/include)
 endif()
