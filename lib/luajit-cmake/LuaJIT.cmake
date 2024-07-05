7ee1b357cc0d80acefb0153aa69edc8e45b2110e
Hiroshi Hatake <hiroshi@chronosphere.io>
2024-07-05 14:15:57 +0900

    luajit-cmake: Permit absence of git command
    Signed-off-by: Hiroshi Hatake <hiroshi@chronosphere.io>

--- lib/luajit-cmake/LuaJIT.cmake
+++ lib/luajit-cmake/LuaJIT.cmake
@@ -408,10 +408,27 @@ if (CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
   set(GIT_FORMAT %%ct)
 endif()
 
-add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/luajit_relver.txt
-  COMMAND git show -s --format=${GIT_FORMAT} > ${CMAKE_CURRENT_BINARY_DIR}/luajit_relver.txt
-  WORKING_DIRECTORY ${LUAJIT_DIR}
+execute_process(
+  COMMAND git --version
+  RESULT_VARIABLE GIT_EXISTENCE
+  OUTPUT_VARIABLE GIT_VERSION
+  OUTPUT_STRIP_TRAILING_WHITESPACE
 )
+
+if (GIT_EXISTENCE EQUAL 0)
+  message(STATUS "Using Git: ${GIT_VERSION}")
+  add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/luajit_relver.txt
+    COMMAND git show -s --format=${GIT_FORMAT} > ${CMAKE_CURRENT_BINARY_DIR}/luajit_relver.txt
+    WORKING_DIRECTORY ${LUAJIT_DIR}
+  )
+else()
+  string(TIMESTAMP current_epoch "%s")
+  message(STATUS "Using current epoch: ${current_epoch}")
+  add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/luajit_relver.txt
+    COMMAND echo "${current_epoch}" > ${CMAKE_CURRENT_BINARY_DIR}/luajit_relver.txt
+    WORKING_DIRECTORY ${LUAJIT_DIR}
+   )
+endif()
 
 add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/luajit.h
   COMMAND ${HOST_WINE} ${MINILUA_PATH} ${LUAJIT_DIR}/src/host/genversion.lua
