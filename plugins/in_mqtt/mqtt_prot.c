140c71b7d1b4e6f9961793120ac309b8cdf3cfcb
Leonardo Alminana <leonardo.alminana@chronosphere.io>
2024-08-05 21:27:44 +0200

    in_mqtt: added removed debug message
    Signed-off-by: Leonardo Alminana <leonardo.alminana@chronosphere.io>

--- plugins/in_mqtt/mqtt_prot.c
+++ plugins/in_mqtt/mqtt_prot.c
@@ -144,8 +144,6 @@ static int mqtt_data_append(char *topic, size_t topic_len,
         return -1;
     }
 
-
-printf("PAYLOAD : ||||||%.*s|||||\n\n", msg_len, msg);
     off = 0;
     msgpack_unpacked_init(&result);
     if (msgpack_unpack_next(&result, pack, out, &off) != MSGPACK_UNPACK_SUCCESS) {
