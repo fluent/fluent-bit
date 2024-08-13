c95851a1e6a1d6ef573834ba4cf012ca3bfe36d4
metalfork <cwagner@cardfree.com>
2024-08-13 16:02:17 -0400

    Change to meet coding style rule.
    Signed-off-by: metalfork <cwagner@cardfree.com>

--- plugins/in_http/http_prot.c
+++ plugins/in_http/http_prot.c
@@ -524,7 +524,8 @@ static int process_payload(struct flb_http *ctx, struct http_conn *conn,
     }
 
     if ((header->val.len == 16 && strncasecmp(header->val.data, "application/json", 16) == 0) ||
-        (header->val.len > 16 && (strncasecmp(header->val.data, "application/json ", 17) == 0) || strncasecmp(header->val.data, "application/json;", 17) == 0)) {
+        (header->val.len > 16 && (strncasecmp(header->val.data, "application/json ", 17) == 0) || 
+        strncasecmp(header->val.data, "application/json;", 17) == 0)) {
         type = HTTP_CONTENT_JSON;
     }
 
