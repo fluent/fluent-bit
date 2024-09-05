e9155c54c9ae98062a12dd371bc3a11632ee6fbf
Leonardo Alminana <leonardo.alminana@chronosphere.io>
2024-09-05 16:01:11 +0200

    in_elasticsearch: improved buffer size check
    Signed-off-by: Leonardo Alminana <leonardo.alminana@chronosphere.io>

--- plugins/in_elasticsearch/in_elasticsearch_bulk_conn.c
+++ plugins/in_elasticsearch/in_elasticsearch_bulk_conn.c
@@ -116,9 +116,10 @@ static int in_elasticsearch_bulk_conn_event(void *data)
             }
 
             if (NULL != request_end) {
-                request_len = (size_t)(request_end - conn->buf_data);
+                request_len = (size_t) ((uintptr_t) request_end -
+                                        (uintptr_t) conn->buf_data);
 
-                if (0 < (conn->buf_len - request_len)) {
+                if (request_len < conn->buf_len) {
                     memmove(conn->buf_data, &conn->buf_data[request_len],
                             conn->buf_len - request_len);
 
