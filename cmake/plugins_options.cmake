58cc2d5944a00b0844e58b91b950ecf595e05063
Jorge Niedbalski <jnr@metaklass.org>
2024-09-20 17:39:09 +0200

    in_ebpf: initial version of the plugin (experimental)
    This is a proposal to implement a ebpf trace ingestor
    plugin to allow sending traces from in kernel functions and
    userland through uprobes.
    
    This initial implementation has 3 traces implemented:
    bind (tcp), malloc (uprobe) and signals (kernel trace).
    
    Events types are known and defined in the fluent-bit codebase and
    those has to be implemented by the ebpf program to follow when submitted
    into the ring buffer.
    
    Signed-off-by: Jorge Niedbalski <jnr@metaklass.org>

--- cmake/plugins_options.cmake
+++ cmake/plugins_options.cmake
@@ -62,6 +62,7 @@ DEFINE_OPTION(FLB_IN_WINLOG                   "Enable Windows Log input plugin" 
 DEFINE_OPTION(FLB_IN_WINDOWS_EXPORTER_METRICS "Enable windows exporter metrics input plugin" ON)
 DEFINE_OPTION(FLB_IN_WINEVTLOG                "Enable Windows EvtLog input plugin"           OFF)
 DEFINE_OPTION(FLB_IN_WINSTAT                  "Enable Windows Stat input plugin"             OFF)
+DEFINE_OPTION(FLB_IN_EBPF                     "Enable Linux eBPF input plugin"               OFF)
 
 # Processors
 # ==========
