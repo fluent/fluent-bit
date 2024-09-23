60872cc1a2ad5f194e1570e9607fdd772946983c
Phillip Adair Stewart Whelan <phillip.whelan@chronosphere.io>
2024-09-23 12:39:28 -0300

    in_calyptia_fleet: fix 'unable to find fleet by name' (backport of #9410).
    Signed-off-by: Phillip Adair Stewart Whelan <phillip.whelan@chronosphere.io>

--- plugins/in_calyptia_fleet/in_calyptia_fleet.c
+++ plugins/in_calyptia_fleet/in_calyptia_fleet.c
@@ -682,6 +682,10 @@ static msgpack_object *msgpack_lookup_map_key(msgpack_object *obj, const char *k
 
         key = &cur->key.via.str;
 
+        if (key->size != strlen(keyname)) {
+            continue;
+        }
+
         if (strncmp(key->ptr, keyname, key->size) == 0) {
             return &cur->val;
         }
@@ -799,7 +803,7 @@ static ssize_t parse_fleet_search_json(struct flb_in_calyptia_fleet_config *ctx,
             break;
         }
 
-        fleet = msgpack_lookup_map_key(map, "ID");
+        fleet = msgpack_lookup_map_key(map, "id");
         if (fleet == NULL) {
             flb_plg_error(ctx->ins, "unable to find fleet by name");
             break;
