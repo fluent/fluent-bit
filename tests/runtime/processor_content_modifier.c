c5ed2204109c1893ac8f73aadbc51dd639a350e8
Eduardo Silva <eduardo@calyptia.com>
2024-10-27 21:07:58 -0600

    tests: runtime: processor_content_modifier: fix copyright
    Signed-off-by: Eduardo Silva <eduardo@calyptia.com>

--- tests/runtime/processor_content_modifier.c
+++ tests/runtime/processor_content_modifier.c
@@ -2,8 +2,7 @@
 
 /*  Fluent Bit
  *  ==========
- *  Copyright (C) 2019-2024 The Fluent Bit Authors
- *  Copyright (C) 2015-2018 Treasure Data Inc.
+ *  Copyright (C) 2015-2024 The Fluent Bit Authors
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
