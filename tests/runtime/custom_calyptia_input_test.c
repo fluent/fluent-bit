353364ebc9e21b10d5a471a07ce6678fecd1c618
Patrick Stephens <pat@chronosphere.io>
2024-12-04 14:49:39 +0000

    calyptia: resolve function pointer usage
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>

--- tests/runtime/custom_calyptia_input_test.c
+++ tests/runtime/custom_calyptia_input_test.c
@@ -6,6 +6,7 @@
 #include <fluent-bit/flb_custom_plugin.h>
 #include <fluent-bit/flb_mem.h>
 #include <fluent-bit/flb_str.h>
+#include <fluent-bit/flb_utils.h>
 #include "flb_tests_runtime.h"
 #include "../../plugins/custom_calyptia/calyptia.h"
 
