3e96ec59609a3a6013fa74c945a5775b1a1530e5
Pat <pat@chronosphere.io>
2024-12-05 16:30:35 +0000

    calyptia: generate machine id for fleet agents (#9691)
    * calyptia: generate machine id for fleet agents
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    
    * calyptia: add missing include
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    
    * calyptia: add missing include
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    
    * tests: resolve conflicting definitions
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    
    * calyptia: resolve missing bool type
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    
    * calyptia: resolve missing header
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    
    * calyptia: resolve duplicate symbols for tests
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    
    * calyptia: resolve function pointer usage
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    
    * calyptia: update codeowners
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    
    * custom_calyptia: tests: Add missing declarations
    
    To link those missing declaration functions, we need to define for stub
    in the custom_calyptia testing files.
    
    Signed-off-by: Hiroshi Hatake <hiroshi@chronosphere.io>
    
    * custom_calyptia: Remove duplicated function declarations
    
    Signed-off-by: Hiroshi Hatake <hiroshi@chronosphere.io>
    
    * tests: add calyptia machine id generation tests
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    
    * tests: add missing header
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    
    * tests: fix typos
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    
    * tests: fix typos
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    
    * tests: fix memory issues
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    
    * tests: fix location
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    
    * tests: fix destroy usage
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    
    * tests: fix destroy usage
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    
    ---------
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    Signed-off-by: Hiroshi Hatake <hiroshi@chronosphere.io>
    Co-authored-by: Hiroshi Hatake <hiroshi@chronosphere.io>

--- lib/cprofiles/include/cprofiles/cprof_info.h
+++ lib/cprofiles/include/cprofiles/cprof_info.h
@@ -20,12 +20,9 @@
 #ifndef CPROF_INFO_H
 #define CPROF_INFO_H
 
-#define CPROF_SOURCE_DIR "/Users/leonardo/Work/Calyptia/fluent-bit"
+#define CPROF_SOURCE_DIR "/src/fluent-bit"
 
 /* General flags set by /CMakeLists.txt */
-#ifndef CPROF_HAVE_SANITIZE_ADDRESS
-#define CPROF_HAVE_SANITIZE_ADDRESS
-#endif
 #ifndef CPROF_HAVE_TIMESPEC_GET
 #define CPROF_HAVE_TIMESPEC_GET
 #endif
