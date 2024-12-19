f7ffbe5ef0fda073682ebe4613c0104eb6033cbb
Leonardo Alminana <leonardo.alminana@chronosphere.io>
2024-12-19 20:57:17 +0100

    out_opentelemetry: renamed option as requested
    Signed-off-by: Leonardo Alminana <leonardo.alminana@chronosphere.io>

--- plugins/in_opentelemetry/opentelemetry.h
+++ plugins/in_opentelemetry/opentelemetry.h
@@ -39,7 +39,7 @@ struct flb_opentelemetry {
     int  tag_from_uri;
     flb_sds_t logs_metadata_key;
     int profile_support_enabled;
-    int encode_profiles_as_text;
+    int encode_profiles_as_log;
 
     struct flb_input_instance *ins;
 
