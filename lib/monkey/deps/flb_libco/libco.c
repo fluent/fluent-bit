8199550aa70de9758dd9f23720e3d4031872a4ca
Sumit Dubey <sumit.dubey2@ibm.com>
2025-03-23 23:38:24 -0400

    engine: ppc64le support
    Signed-off-by: Sumit Dubey <sumit.dubey2@ibm.com>

--- lib/monkey/deps/flb_libco/libco.c
+++ lib/monkey/deps/flb_libco/libco.c
@@ -16,8 +16,9 @@
     #include "arm.c"
   #elif defined(__aarch64__)
     #include "aarch64.c"
-  #elif defined(_ARCH_PPC)
-    #include "ppc.c"
+  #elif defined(__powerpc64__) && defined(_CALL_ELF) && _CALL_ELF == 2
+    #include "ppc64le.c"
+  #elif defined(_ARCH_PPC) && !defined(__LITTLE_ENDIAN__)    #include "ppc.c"
   #elif defined(_WIN32)
     #include "fiber.c"
   #else
