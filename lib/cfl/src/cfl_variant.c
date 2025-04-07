74a748d5bd425e911c2a5a2030358919e36ce729
Eduardo Silva <eduardo@chronosphere.io>
2025-04-07 14:09:32 -0600

    lib: cfl: update to v0.6.0
    Signed-off-by: Eduardo Silva <eduardo@chronosphere.io>

--- lib/cfl/src/cfl_variant.c
+++ lib/cfl/src/cfl_variant.c
@@ -104,7 +104,7 @@ struct cfl_variant *cfl_variant_create_from_string_s(char *value, size_t value_s
         instance->data.as_string = cfl_sds_create_len(value, value_size);
         if (instance->data.as_string == NULL) {
             free(instance);
-            instance = NULL;
+            return NULL;
         }
 
     }
@@ -136,7 +136,7 @@ struct cfl_variant *cfl_variant_create_from_bytes(char *value, size_t length, in
         instance->data.as_bytes = cfl_sds_create_len(value, length);
         if (instance->data.as_bytes == NULL) {
             free(instance);
-            instance = NULL;
+            return NULL;
         }
     }
     cfl_variant_size_set(instance, length);
