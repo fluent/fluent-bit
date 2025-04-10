7049c07a8b83827c09305e771553756074e80bb7
Phillip Adair Stewart Whelan <phillip.whelan@chronosphere.io>
2025-04-10 16:01:48 -0400

    out_opentelemetry: add record accessor for scope.schema_url for scope_logs.
    Signed-off-by: Phillip Adair Stewart Whelan <phillip.whelan@chronosphere.io>

--- plugins/out_opentelemetry/opentelemetry.h
+++ plugins/out_opentelemetry/opentelemetry.h
@@ -172,6 +172,7 @@ struct opentelemetry_context {
     struct flb_record_accessor *ra_scope_name;
     struct flb_record_accessor *ra_scope_version;
     struct flb_record_accessor *ra_scope_attr;
+    struct flb_record_accessor *ra_scope_schema_url;
 
     /* log: metadata components coming from OTLP */
     struct flb_record_accessor *ra_log_meta_otlp_observed_ts;
