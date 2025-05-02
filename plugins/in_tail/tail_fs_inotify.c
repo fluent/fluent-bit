a7a43fdd502519785b0e62fbf86d5d0cd41391a0
deivid.garcia.garcia@gmail.com <deivid.garcia.garcia@gmail.com>
2025-05-02 10:34:35 +0200

    fix logic

--- plugins/in_tail/tail_fs_inotify.c
+++ plugins/in_tail/tail_fs_inotify.c
@@ -251,7 +251,7 @@ static int tail_fs_event(struct flb_input_instance *ins,
          */
 
         /* Check if the file was truncated */
-        if (file->offset > st.st_size || (st.st_size < file->offset && (file->offset - st.st_size) >= ctx->truncate_min_threshold)) {
+        if (st.st_size < file->offset && (file->offset - st.st_size) >= ctx->truncate_min_threshold) {
             offset = lseek(file->fd, 0, SEEK_SET);
             if (offset == -1) {
                 flb_errno();
