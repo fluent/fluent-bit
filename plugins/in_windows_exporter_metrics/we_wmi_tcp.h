339691c111a1796074156811b6780ff0c904b981
Hiroshi Hatake <hiroshi@chronosphere.io>
2025-07-17 00:51:16 +0900

    in_windows_exporter_metrics: Support TCPv4/TCPv6 metrics
    Signed-off-by: Hiroshi Hatake <hiroshi@chronosphere.io>

---
+++ plugins/in_windows_exporter_metrics/we_wmi_tcp.h
@@ -0,0 +1,30 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2019-2025 The Fluent Bit Authors
+ *  Copyright (C) 2015-2018 Treasure Data Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef FLB_WE_WMI_TCP_H
+#define FLB_WE_WMI_TCP_H
+
+#include "we.h"
+
+int we_wmi_tcp_init(struct flb_we *ctx);
+int we_wmi_tcp_update(struct flb_we *ctx);
+int we_wmi_tcp_exit(struct flb_we *ctx);
+
+#endif
