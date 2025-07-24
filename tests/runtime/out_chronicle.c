43e3f3027e69afbef4e3b62a088733312a49a29d
Hiroshi Hatake <hiroshi@chronosphere.io>
2025-07-24 18:23:20 +0900

    out_chronicle: tests: Add a test for partially suceeded case
    Signed-off-by: Hiroshi Hatake <hiroshi@chronosphere.io>
    Signed-off-by: Lucas Tembras <lucas.tembras@datadoghq.com>

--- tests/runtime/out_chronicle.c
+++ tests/runtime/out_chronicle.c
@@ -111,6 +111,26 @@ static void cb_check_format_multiple_records(void *ctx, int ffd,
     flb_sds_destroy(res_data);
 }
 
+static void cb_check_format_partially_succeeded_records(void *ctx, int ffd,
+                                                        int res_ret, void *res_data,
+                                                        size_t res_size, void *data)
+{
+    char *out_json = res_data;
+    char *p1, *p2;
+
+    set_output_invoked(1);
+
+    p1 = strstr(out_json, "\"log_text\":\"record one\"");
+    if (!TEST_CHECK(p1 != NULL)) {
+        TEST_MSG("Expected log_text with specific value not found. Got: %s", out_json);
+    }
+
+    p2 = strstr(out_json, "\"test\"");
+    TEST_CHECK(p2 == NULL);
+
+    flb_sds_destroy(res_data);
+}
+
 void test_format_no_log_key()
 {
     flb_ctx_t *ctx;
@@ -261,10 +281,53 @@ void test_format_multiple_records()
     flb_destroy(ctx);
 }
 
+void test_format_partially_suceeded_records()
+{
+    flb_ctx_t *ctx;
+    int in_ffd, out_ffd;
+    char record1[1024];
+    char record2[1024];
+    time_t now = time(NULL);
+
+    ctx = flb_create();
+    flb_service_set(ctx, "flush", "0.2", "grace", "1", "log_level", "error", NULL);
+
+    in_ffd = flb_input(ctx, (char *) "lib", NULL);
+    flb_input_set(ctx, in_ffd, "tag", "test", NULL);
+
+    out_ffd = flb_output(ctx, (char *) "chronicle", NULL);
+    flb_output_set(ctx, out_ffd,
+                   "match", "test",
+                   "customer_id", "test-customer",
+                   "project_id", "TESTING_FORMAT",
+                   "log_key", "message",
+                   "log_type", "TEST_LOG",
+                   NULL);
+
+    flb_output_set_test(ctx, out_ffd, "formatter", cb_check_format_partially_succeeded_records, NULL, NULL);
+
+    flb_start(ctx);
+    clear_output_invoked();
+
+    snprintf(record1, sizeof(record1) - 1, "[%ld, {\"message\": \"record one\"}]", (long) now);
+    snprintf(record2, sizeof(record2) - 1, "[%ld, {\"test\": \"record two\"}]", (long) now + 1);
+
+    flb_lib_push(ctx, in_ffd, record1, strlen(record1));
+    flb_lib_push(ctx, in_ffd, record2, strlen(record2));
+
+    sleep(1);
+
+    TEST_CHECK(get_output_invoked() == 1);
+    flb_stop(ctx);
+    flb_destroy(ctx);
+}
+
+
 TEST_LIST = {
     { "format_no_log_key",           test_format_no_log_key },
     { "format_with_log_key_found",   test_format_with_log_key_found },
     { "format_with_log_key_not_found", test_format_with_log_key_not_found },
     { "format_multiple_records",     test_format_multiple_records },
+    { "format_partially_suceeded_records", test_format_partially_suceeded_records },
     { NULL, NULL }
 };
