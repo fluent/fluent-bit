24971391d3f7bb746b05bf838130960566ae307b
Ryan Underwood <ryan.underwood@mongodb.com>
2025-07-25 11:18:45 -0500

    Introduce port parameter for Kinesis Firehose output plugin
    This mirrors the same for Kinesis Streams in
    https://github.com/fluent/fluent-bit/pull/9317
    
    Signed-off-by: Ryan Underwood <ryan.underwood@mongodb.com>

--- plugins/out_kinesis_firehose/firehose.h
+++ plugins/out_kinesis_firehose/firehose.h
@@ -29,6 +29,10 @@
 
 #define DEFAULT_TIME_KEY_FORMAT "%Y-%m-%dT%H:%M:%S"
 
+#define FLB_KINESIS_DEFAULT_HTTPS_PORT    443
+#define FLB_KINESIS_MIN_PORT              1
+#define FLB_KINESIS_MAX_PORT              65535
+
 /* buffers used for each flush */
 struct flush {
     /* temporary buffer for storing the serialized event messages */
@@ -87,6 +91,7 @@ struct flb_firehose {
     const char *log_key;
     const char *external_id;
     char *sts_endpoint;
+    int port;
     char *profile;
     int custom_endpoint;
     int retry_requests;
