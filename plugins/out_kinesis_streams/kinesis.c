0ef6dd1d191902dc037d48662e7d068dcb5e2410
Hiroshi Hatake <hiroshi@chronosphere.io>
2025-07-31 17:46:14 +0900

    out_kinesis_streams: Refer a config whether escaping unicode or not in JSON
    Signed-off-by: Hiroshi Hatake <hiroshi@chronosphere.io>

--- plugins/out_kinesis_streams/kinesis.c
+++ plugins/out_kinesis_streams/kinesis.c
@@ -361,7 +361,8 @@ static void cb_kinesis_flush(struct flb_event_chunk *event_chunk,
 
     ret = process_and_send_to_kinesis(ctx, buf,
                                       event_chunk->data,
-                                      event_chunk->size);
+                                      event_chunk->size,
+                                      config);
     if (ret < 0) {
         flb_plg_error(ctx->ins, "Failed to send records to kinesis");
         kinesis_flush_destroy(buf);
