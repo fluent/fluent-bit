7bceaa5fce2083dee4de3993abd7bb36d1095dee
Eduardo Silva <eduardo@chronosphere.io>
2025-08-06 17:21:05 -0600

    cmake: msgpack: increase stack size to 64 elements
    Signed-off-by: Eduardo Silva <eduardo@chronosphere.io>

--- cmake/msgpack.cmake
+++ cmake/msgpack.cmake
@@ -6,5 +6,9 @@ option(MSGPACK_BUILD_EXAMPLES         OFF)
 include_directories(
   ${FLB_PATH_ROOT_SOURCE}/${FLB_PATH_LIB_MSGPACK}/include
 )
+
+# define preprocessor MSGPACK_EMBED_STACK_SIZE to 1024
+add_compile_definitions(MSGPACK_EMBED_STACK_SIZE=64)
+
 add_subdirectory(${FLB_PATH_LIB_MSGPACK} EXCLUDE_FROM_ALL)
 set(MSGPACK_LIBRARIES "msgpack-c-static")
