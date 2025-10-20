ea0d7a02fb5b1eefbe5cf88be9bba1d82171e265
Eduardo Silva <eduardo@chronosphere.io>
2025-10-20 09:25:39 -0600

    in_node_exporter_metrics: add netstat linux collector
    Extend node_exporter_metrics by exposing netstat metrics by parsing /proc/net/snmp data and
    aligning the output with Prometheus node_exporter conventions.
    
    Example parsed values from a live system:
    
      node_netstat_Tcp_ActiveOpens = 26581
      node_netstat_Tcp_PassiveOpens = 129
      node_netstat_Tcp_RetransSegs = 9254
      node_netstat_Tcp_CurrEstab = 6
      node_netstat_Udp_InDatagrams = 1292553
      node_netstat_Udp_OutDatagrams = 1601161
      node_netstat_Udp_NoPorts = 2047
      node_netstat_Udp_InErrors = 0
    
    Signed-off-by: Eduardo Silva <eduardo@chronosphere.io>

---
+++ plugins/in_node_exporter_metrics/ne_netstat.c
@@ -0,0 +1,33 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2015-2025 The Fluent Bit Authors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifdef __linux__
+#include "ne_netstat_linux.c"
+#else
+
+#include "ne.h"
+
+struct flb_ne_collector netstat_collector = {
+    .name = "netstat",
+    .cb_init = NULL,
+    .cb_update = NULL,
+    .cb_exit = NULL
+};
+
+#endif
