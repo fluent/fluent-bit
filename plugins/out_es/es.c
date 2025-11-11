0463f15c3b8e4f3d986ab23c7c5c6ca68fad4b63
Marat Abrarov <abrarov@gmail.com>
2025-11-12 00:01:18 +0300

    pipeline: outputs: es: fix of memory leak when using AWS authentication and retrying buffer flush
    Signed-off-by: Marat Abrarov <abrarov@gmail.com>

--- plugins/out_es/es.c
+++ plugins/out_es/es.c
@@ -1048,6 +1048,9 @@ static void cb_es_flush(struct flb_event_chunk *event_chunk,
     }
 
     flb_upstream_conn_release(u_conn);
+    if (signature) {
+        flb_sds_destroy(signature);
+    }
     FLB_OUTPUT_RETURN(FLB_RETRY);
 }
 
