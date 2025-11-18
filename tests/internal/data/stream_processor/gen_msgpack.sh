c69c95cd7c6553ddb8561a2012c2b1505e37b9c6
Hank Leininger <hlein@korelogic.com>
2025-11-18 11:51:27 -0700

    config_format: Convert many parser*.conf references to parser*.yaml
    While it's still possible to build without libyaml, it is included by
    default and none of our canned builds/distro-packagings disable it. So,
    update all clear-cut references to conf/parser*.conf files to the
    corresponding .yaml. Also updated some occurances in documentation and
    examples. Generally did not touch most of tests/{internal,runtime}/,
    unless they were referencing the toplevel conf/ dir.
    
    Signed-off-by: Hank Leininger <hlein@korelogic.com>

--- tests/internal/data/stream_processor/gen_msgpack.sh
+++ tests/internal/data/stream_processor/gen_msgpack.sh
@@ -7,7 +7,7 @@ echo
 rm -rf samples.mp samples-subkeys.mp
 
 # Generate new msgpack file using as a source samples.json
-../../../../build/bin/fluent-bit -R ../../../../conf/parsers.conf              \
+../../../../build/bin/fluent-bit -R ../../../../conf/parsers.yaml              \
  -i tail -t samples         -p path=samples.json          -p parser=json       \
  -i tail -t samples-subkeys -p path=samples-subkeys.json  -p parser=json       \
  -o file -m samples         -p format=msgpack  -p file=samples.mp              \
@@ -31,7 +31,7 @@ rm -rf samples.mp samples-subkeys.mp
      # Generate new msgpack files for hopping window
      echo ""
      echo "=== Generating MessagePack file for $file ... ==="
-     ../../../../build/bin/fluent-bit -R ../../../../conf/parsers.conf   \
+     ../../../../build/bin/fluent-bit -R ../../../../conf/parsers.yaml   \
       -i tail -p path=$file  -p parser=json                              \
       -o file -p format=msgpack -p file=$(echo $file | sed s/json/mp/) -f 1 &
       pid=$!
