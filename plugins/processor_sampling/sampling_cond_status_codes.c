a981f6681c4b4efd962f8f4c81307db26fa4b74d
Eduardo Silva <eduardo@chronosphere.io>
2025-12-31 18:10:35 -0600

    plugins: update copyright year to 2026
    Signed-off-by: Eduardo Silva <eduardo@chronosphere.io>

--- plugins/processor_sampling/sampling_cond_status_codes.c
+++ plugins/processor_sampling/sampling_cond_status_codes.c
@@ -2,7 +2,7 @@
 
 /*  Fluent Bit
  *  ==========
- *  Copyright (C) 2015-2025 The Fluent Bit Authors
+ *  Copyright (C) 2015-2026 The Fluent Bit Authors
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
