d7032dcc5697eacb07f6cb1abf66ae1730f06504
Hiroshi Hatake <hiroshi@chronosphere.io>
2026-02-17 17:49:14 +0900

    multiline: ml_stream: Clean up stream's metadata
    Signed-off-by: Hiroshi Hatake <hiroshi@chronosphere.io>

--- src/multiline/flb_ml_stream.c
+++ src/multiline/flb_ml_stream.c
@@ -147,6 +147,7 @@ static void stream_group_destroy(struct flb_ml_stream_group *group)
     msgpack_sbuffer_destroy(&group->mp_sbuf);
 
     mk_list_del(&group->_head);
+    flb_ml_stream_group_purge_metadata(group);
     flb_free(group);
 }
 
