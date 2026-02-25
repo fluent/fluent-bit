ed52699f668f6349ec1ac469ab92bedb464e0981
Eduardo Silva <eduardo@chronosphere.io>
2026-02-25 12:38:42 -0600

    tests: runtime: content_modifier: add test for otel_log_attributes context
    Signed-off-by: Eduardo Silva <eduardo@chronosphere.io>

--- tests/runtime/processor_content_modifier.c
+++ tests/runtime/processor_content_modifier.c
@@ -19,6 +19,8 @@
 
 #include <fluent-bit.h>
 #include <fluent-bit/flb_time.h>
+#include <fluent-bit/flb_log_event_decoder.h>
+#include <fluent-bit/flb_pack.h>
 #include <msgpack.h>
 #include "flb_tests_runtime.h"
 
@@ -81,6 +83,70 @@ static int cb_check_result(void *record, size_t size, void *data)
     }
 
     flb_free(record);
+    return 0;
+}
+
+static int cb_check_metadata_result(void *record, size_t size, void *data)
+{
+    int ret;
+    char *p;
+    flb_sds_t result;
+    size_t result_size = 1024;
+    struct expect_str *expected;
+    struct flb_log_event event;
+    struct flb_log_event_decoder decoder;
+
+    expected = (struct expect_str *) data;
+    result = NULL;
+
+    ret = flb_log_event_decoder_init(&decoder, (char *) record, size);
+    if (!TEST_CHECK(ret == FLB_EVENT_DECODER_SUCCESS)) {
+        return -1;
+    }
+
+    flb_log_event_decoder_read_groups(&decoder, FLB_TRUE);
+
+    ret = flb_log_event_decoder_next(&decoder, &event);
+    if (!TEST_CHECK(ret == FLB_EVENT_DECODER_SUCCESS)) {
+        flb_log_event_decoder_destroy(&decoder);
+        return -1;
+    }
+
+    result = flb_sds_create_size(result_size);
+    if (!TEST_CHECK(result != NULL)) {
+        flb_log_event_decoder_destroy(&decoder);
+        return -1;
+    }
+
+    ret = flb_msgpack_to_json(result, result_size, event.metadata, FLB_TRUE);
+    if (!TEST_CHECK(ret >= 0)) {
+        flb_sds_destroy(result);
+        flb_log_event_decoder_destroy(&decoder);
+        return -1;
+    }
+
+    while(expected != NULL && expected->str != NULL) {
+        if (expected->found == FLB_TRUE) {
+            p = strstr(result, expected->str);
+            if(!TEST_CHECK(p != NULL)) {
+                flb_error("Expected to find: '%s' in metadata '%s'",
+                          expected->str, result);
+            }
+        }
+        else {
+            p = strstr(result, expected->str);
+            if(!TEST_CHECK(p == NULL)) {
+                flb_error("'%s' should be removed from metadata '%s'",
+                          expected->str, result);
+            }
+        }
+
+        expected++;
+    }
+
+    flb_sds_destroy(result);
+    flb_log_event_decoder_destroy(&decoder);
+
     return 0;
 }
 
@@ -244,7 +310,7 @@ static void flb_logs_action_insert()
         return;
     }
 
-    p = "[0, {\"k\":\"sample\"}]";
+    p = "[[0, {}], {\"k\":\"sample\"}]";
     len = strlen(p);
     bytes = flb_lib_push(ctx->flb, ctx->i_ffd, p, len);
     TEST_CHECK(bytes == len);
@@ -1424,6 +1490,321 @@ static void flb_logs_action_convert_from_null_to_double()
     processor_test_destroy(ctx);
 }
 
+static void flb_logs_otel_log_attributes_insert()
+{
+    struct processor_test *ctx;
+    struct flb_lib_out_cb cb_data;
+    int ret;
+    char *p;
+    int bytes;
+    size_t len;
+    struct expect_str expect[] = {
+      {"\"otlp\":{\"attributes\":{\"my_otlp_attr\":\"my_otlp_value\"}}", FLB_TRUE},
+      {NULL, FLB_TRUE}
+    };
+
+    struct cfl_variant action = {
+        .type = CFL_VARIANT_STRING,
+        .data.as_string = "insert",
+    };
+    struct cfl_variant context = {
+        .type = CFL_VARIANT_STRING,
+        .data.as_string = "otel_log_attributes",
+    };
+    struct cfl_variant key = {
+        .type = CFL_VARIANT_STRING,
+        .data.as_string = "my_otlp_attr",
+    };
+    struct cfl_variant value = {
+        .type = CFL_VARIANT_STRING,
+        .data.as_string = "my_otlp_value",
+    };
+
+    cb_data.cb = cb_check_metadata_result;
+    cb_data.data = &expect;
+
+    ctx = processor_test_create(FLB_PROCESSOR_LOGS, &cb_data);
+    if (!TEST_CHECK(ctx != NULL)) {
+        TEST_MSG("failed to create ctx");
+        return;
+    }
+
+    ret = flb_output_set(ctx->flb, ctx->o_ffd,
+                         "data_mode", "chunk",
+                         NULL);
+    TEST_CHECK(ret == 0);
+
+    ret = flb_processor_unit_set_property(ctx->pu, "action", &action);
+    TEST_CHECK(ret == 0);
+    ret = flb_processor_unit_set_property(ctx->pu, "context", &context);
+    TEST_CHECK(ret == 0);
+    ret = flb_processor_unit_set_property(ctx->pu, "key", &key);
+    TEST_CHECK(ret == 0);
+    ret = flb_processor_unit_set_property(ctx->pu, "value", &value);
+    TEST_CHECK(ret == 0);
+
+    ret = flb_start(ctx->flb);
+    if (!TEST_CHECK(ret == 0)) {
+        TEST_MSG("flb_start failed");
+        return;
+    }
+
+    p = "[[0, {}], {\"k\":\"sample\"}]";
+    len = strlen(p);
+    bytes = flb_lib_push(ctx->flb, ctx->i_ffd, p, len);
+    TEST_CHECK(bytes == len);
+
+    processor_test_destroy(ctx);
+}
+
+static void flb_logs_otel_log_attributes_upsert()
+{
+    struct processor_test *ctx;
+    struct flb_lib_out_cb cb_data;
+    struct flb_processor_unit *pu_upsert;
+    int ret;
+    char *p;
+    int bytes;
+    size_t len;
+    struct expect_str expect[] = {
+      {"\"my_otlp_attr\":\"old_value\"", FLB_FALSE},
+      {"\"my_otlp_attr\":\"new_value\"", FLB_TRUE},
+      {NULL, FLB_TRUE}
+    };
+
+    struct cfl_variant action_insert = {
+        .type = CFL_VARIANT_STRING,
+        .data.as_string = "insert",
+    };
+    struct cfl_variant action_upsert = {
+        .type = CFL_VARIANT_STRING,
+        .data.as_string = "upsert",
+    };
+    struct cfl_variant context = {
+        .type = CFL_VARIANT_STRING,
+        .data.as_string = "otel_log_attributes",
+    };
+    struct cfl_variant key = {
+        .type = CFL_VARIANT_STRING,
+        .data.as_string = "my_otlp_attr",
+    };
+    struct cfl_variant value_insert = {
+        .type = CFL_VARIANT_STRING,
+        .data.as_string = "old_value",
+    };
+    struct cfl_variant value_upsert = {
+        .type = CFL_VARIANT_STRING,
+        .data.as_string = "new_value",
+    };
+
+    cb_data.cb = cb_check_metadata_result;
+    cb_data.data = &expect;
+
+    ctx = processor_test_create(FLB_PROCESSOR_LOGS, &cb_data);
+    if (!TEST_CHECK(ctx != NULL)) {
+        TEST_MSG("failed to create ctx");
+        return;
+    }
+
+    ret = flb_output_set(ctx->flb, ctx->o_ffd,
+                         "data_mode", "chunk",
+                         NULL);
+    TEST_CHECK(ret == 0);
+
+    ret = flb_processor_unit_set_property(ctx->pu, "action", &action_insert);
+    TEST_CHECK(ret == 0);
+    ret = flb_processor_unit_set_property(ctx->pu, "context", &context);
+    TEST_CHECK(ret == 0);
+    ret = flb_processor_unit_set_property(ctx->pu, "key", &key);
+    TEST_CHECK(ret == 0);
+    ret = flb_processor_unit_set_property(ctx->pu, "value", &value_insert);
+    TEST_CHECK(ret == 0);
+
+    pu_upsert = flb_processor_unit_create(ctx->proc, ctx->type, "content_modifier");
+    if (!TEST_CHECK(pu_upsert != NULL)) {
+        TEST_MSG("failed to create second processor unit");
+        processor_test_destroy(ctx);
+        return;
+    }
+
+    ret = flb_processor_unit_set_property(pu_upsert, "action", &action_upsert);
+    TEST_CHECK(ret == 0);
+    ret = flb_processor_unit_set_property(pu_upsert, "context", &context);
+    TEST_CHECK(ret == 0);
+    ret = flb_processor_unit_set_property(pu_upsert, "key", &key);
+    TEST_CHECK(ret == 0);
+    ret = flb_processor_unit_set_property(pu_upsert, "value", &value_upsert);
+    TEST_CHECK(ret == 0);
+
+    ret = flb_start(ctx->flb);
+    if (!TEST_CHECK(ret == 0)) {
+        TEST_MSG("flb_start failed");
+        return;
+    }
+
+    p = "[[0, {}], {\"k\":\"sample\"}]";
+    len = strlen(p);
+    bytes = flb_lib_push(ctx->flb, ctx->i_ffd, p, len);
+    TEST_CHECK(bytes == len);
+
+    processor_test_destroy(ctx);
+}
+
+static void flb_logs_otel_log_attributes_delete()
+{
+    struct processor_test *ctx;
+    struct flb_lib_out_cb cb_data;
+    struct flb_processor_unit *pu_delete;
+    int ret;
+    char *p;
+    int bytes;
+    size_t len;
+    struct expect_str expect[] = {
+      {"\"my_otlp_attr\"", FLB_FALSE},
+      {"\"otlp\":{\"attributes\":{}}", FLB_TRUE},
+      {NULL, FLB_TRUE}
+    };
+
+    struct cfl_variant action_insert = {
+        .type = CFL_VARIANT_STRING,
+        .data.as_string = "insert",
+    };
+    struct cfl_variant action_delete = {
+        .type = CFL_VARIANT_STRING,
+        .data.as_string = "delete",
+    };
+    struct cfl_variant context = {
+        .type = CFL_VARIANT_STRING,
+        .data.as_string = "otel_log_attributes",
+    };
+    struct cfl_variant key = {
+        .type = CFL_VARIANT_STRING,
+        .data.as_string = "my_otlp_attr",
+    };
+    struct cfl_variant value_insert = {
+        .type = CFL_VARIANT_STRING,
+        .data.as_string = "my_otlp_value",
+    };
+
+    cb_data.cb = cb_check_metadata_result;
+    cb_data.data = &expect;
+
+    ctx = processor_test_create(FLB_PROCESSOR_LOGS, &cb_data);
+    if (!TEST_CHECK(ctx != NULL)) {
+        TEST_MSG("failed to create ctx");
+        return;
+    }
+
+    ret = flb_output_set(ctx->flb, ctx->o_ffd,
+                         "data_mode", "chunk",
+                         NULL);
+    TEST_CHECK(ret == 0);
+
+    ret = flb_processor_unit_set_property(ctx->pu, "action", &action_insert);
+    TEST_CHECK(ret == 0);
+    ret = flb_processor_unit_set_property(ctx->pu, "context", &context);
+    TEST_CHECK(ret == 0);
+    ret = flb_processor_unit_set_property(ctx->pu, "key", &key);
+    TEST_CHECK(ret == 0);
+    ret = flb_processor_unit_set_property(ctx->pu, "value", &value_insert);
+    TEST_CHECK(ret == 0);
+
+    pu_delete = flb_processor_unit_create(ctx->proc, ctx->type, "content_modifier");
+    if (!TEST_CHECK(pu_delete != NULL)) {
+        TEST_MSG("failed to create second processor unit");
+        processor_test_destroy(ctx);
+        return;
+    }
+
+    ret = flb_processor_unit_set_property(pu_delete, "action", &action_delete);
+    TEST_CHECK(ret == 0);
+    ret = flb_processor_unit_set_property(pu_delete, "context", &context);
+    TEST_CHECK(ret == 0);
+    ret = flb_processor_unit_set_property(pu_delete, "key", &key);
+    TEST_CHECK(ret == 0);
+
+    ret = flb_start(ctx->flb);
+    if (!TEST_CHECK(ret == 0)) {
+        TEST_MSG("flb_start failed");
+        return;
+    }
+
+    p = "[[0, {}], {\"k\":\"sample\"}]";
+    len = strlen(p);
+    bytes = flb_lib_push(ctx->flb, ctx->i_ffd, p, len);
+    TEST_CHECK(bytes == len);
+
+    processor_test_destroy(ctx);
+}
+
+static void flb_logs_otel_log_attributes_invalid_otlp_metadata()
+{
+    struct processor_test *ctx;
+    struct flb_lib_out_cb cb_data;
+    int ret;
+    char *p;
+    int bytes;
+    size_t len;
+    struct expect_str expect[] = {
+      {"\"otlp\":\"broken\"", FLB_TRUE},
+      {"\"my_otlp_attr\"", FLB_FALSE},
+      {NULL, FLB_TRUE}
+    };
+
+    struct cfl_variant action = {
+        .type = CFL_VARIANT_STRING,
+        .data.as_string = "insert",
+    };
+    struct cfl_variant context = {
+        .type = CFL_VARIANT_STRING,
+        .data.as_string = "otel_log_attributes",
+    };
+    struct cfl_variant key = {
+        .type = CFL_VARIANT_STRING,
+        .data.as_string = "my_otlp_attr",
+    };
+    struct cfl_variant value = {
+        .type = CFL_VARIANT_STRING,
+        .data.as_string = "my_otlp_value",
+    };
+
+    cb_data.cb = cb_check_metadata_result;
+    cb_data.data = &expect;
+
+    ctx = processor_test_create(FLB_PROCESSOR_LOGS, &cb_data);
+    if (!TEST_CHECK(ctx != NULL)) {
+        TEST_MSG("failed to create ctx");
+        return;
+    }
+
+    ret = flb_output_set(ctx->flb, ctx->o_ffd,
+                         "data_mode", "chunk",
+                         NULL);
+    TEST_CHECK(ret == 0);
+
+    ret = flb_processor_unit_set_property(ctx->pu, "action", &action);
+    TEST_CHECK(ret == 0);
+    ret = flb_processor_unit_set_property(ctx->pu, "context", &context);
+    TEST_CHECK(ret == 0);
+    ret = flb_processor_unit_set_property(ctx->pu, "key", &key);
+    TEST_CHECK(ret == 0);
+    ret = flb_processor_unit_set_property(ctx->pu, "value", &value);
+    TEST_CHECK(ret == 0);
+
+    ret = flb_start(ctx->flb);
+    if (!TEST_CHECK(ret == 0)) {
+        TEST_MSG("flb_start failed");
+        return;
+    }
+
+    p = "[[0, {\"otlp\":\"broken\"}], {\"k\":\"sample\"}]";
+    len = strlen(p);
+    bytes = flb_lib_push(ctx->flb, ctx->i_ffd, p, len);
+    TEST_CHECK(bytes == len);
+
+    processor_test_destroy(ctx);
+}
+
 TEST_LIST = {
     {"logs.action.insert"           , flb_logs_action_insert },
     {"logs.action.delete"           , flb_logs_action_delete },
@@ -1443,5 +1824,10 @@ TEST_LIST = {
     {"logs.action.convert_from_null_to_string" , flb_logs_action_convert_from_null_to_string },
     {"logs.action.convert_from_null_to_int" , flb_logs_action_convert_from_null_to_int },
     {"logs.action.convert_from_null_to_double" , flb_logs_action_convert_from_null_to_double },
+    {"logs.otel_log_attributes.insert" , flb_logs_otel_log_attributes_insert },
+    {"logs.otel_log_attributes.upsert" , flb_logs_otel_log_attributes_upsert },
+    {"logs.otel_log_attributes.delete" , flb_logs_otel_log_attributes_delete },
+    {"logs.otel_log_attributes.invalid_otlp_metadata",
+     flb_logs_otel_log_attributes_invalid_otlp_metadata },
     {NULL, NULL}
 };
