f3ebc3c8f4845064182fdf770092a975825efb32
Eduardo Silva <eduardo@chronosphere.io>
2026-03-11 11:24:49 -0600

    in_elasticsearch: unify HTTP server path
    Signed-off-by: Eduardo Silva <eduardo@chronosphere.io>

--- plugins/in_elasticsearch/in_elasticsearch_bulk_prot.h
+++ plugins/in_elasticsearch/in_elasticsearch_bulk_prot.h
@@ -20,25 +20,12 @@
 #ifndef FLB_IN_ELASTICSEARCH_BULK_PROT
 #define FLB_IN_ELASTICSEARCH_BULK_PROT
 
-#include "in_elasticsearch_bulk_conn.h"
-
 #define ES_VERSION_RESPONSE_TEMPLATE \
     "{\"version\":{\"number\":\"%s\",\"build_flavor\":\"Fluent Bit OSS\"},\"tagline\":\"Fluent Bit's Bulk API compatible endpoint\"}"
 
 #define ES_NODES_TEMPLATE "{\"_nodes\":{\"total\":1,\"successful\":1,\"failed\":0}," \
     "\"nodes\":{\"%s\":{\"name\":\"%s\",\"version\":\"8.0.0\"," \
     "\"http\":{\"publish_address\":\"%s:%s\",\"max_content_length_in_bytes\":%ld}}}}"
-
-int in_elasticsearch_bulk_prot_handle(struct flb_in_elasticsearch *ctx,
-                                      struct in_elasticsearch_bulk_conn *conn,
-                                      struct mk_http_session *session,
-                                      struct mk_http_request *request);
-
-int in_elasticsearch_bulk_prot_handle_error(struct flb_in_elasticsearch *ctx,
-                                            struct in_elasticsearch_bulk_conn *conn,
-                                            struct mk_http_session *session,
-                                            struct mk_http_request *request);
-
 
 int in_elasticsearch_bulk_prot_handle_ng(struct flb_http_request *request,
                                          struct flb_http_response *response);
