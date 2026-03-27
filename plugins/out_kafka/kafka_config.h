f6800affc638ce80f332dd9fd03e491936688cdb
Eduardo Silva <eduardo@chronosphere.io>
2026-03-27 04:15:28 -0600

    out_kafka: add support for OTLP json
    Signed-off-by: Eduardo Silva <eduardo@chronosphere.io>

--- plugins/out_kafka/kafka_config.h
+++ plugins/out_kafka/kafka_config.h
@@ -36,6 +36,8 @@
 #define FLB_KAFKA_FMT_AVRO            3
 #endif
 #define FLB_KAFKA_FMT_RAW             4
+#define FLB_KAFKA_FMT_OTLP_JSON       5
+#define FLB_KAFKA_FMT_OTLP_PROTO      6
 #define FLB_KAFKA_TS_KEY              "@timestamp"
 #define FLB_KAFKA_QUEUE_FULL_RETRIES  "10"
 
