97507b13e7a3a572eaaa05f954e57fc1c9a0732e
Hiroshi Hatake <hiroshi@chronosphere.io>
2026-05-01 14:16:38 +0900

    output: Unwind instance on the new alias-rewrite on exception
    Signed-off-by: Hiroshi Hatake <hiroshi@chronosphere.io>

--- src/flb_output.c
+++ src/flb_output.c
@@ -832,8 +832,16 @@ struct flb_output_instance *flb_output_new(struct flb_config *config,
     if (plugin->flags & FLB_OUTPUT_NET) {
         output_uri = flb_plugin_alias_rewrite(FLB_PLUGIN_OUTPUT, output_name);
         if (output_uri == FLB_PLUGIN_ALIAS_ERR) {
-            if (instance->flags & FLB_OUTPUT_SYNCHRONOUS) {
+            if ((instance->flags & FLB_OUTPUT_SYNCHRONOUS) &&
+                 instance->singleplex_queue != NULL) {
                 flb_task_queue_destroy(instance->singleplex_queue);
+            }
+            if (instance->callback != NULL) {
+                flb_callback_destroy(instance->callback);
+            }
+            if (plugin->type != FLB_OUTPUT_PLUGIN_CORE &&
+                instance->context != NULL) {
+                flb_free(instance->context);
             }
             flb_free(instance->http_server_config);
             flb_free(instance);
@@ -849,8 +857,16 @@ struct flb_output_instance *flb_output_new(struct flb_config *config,
         }
 
         if (ret != 0) {
-            if (instance->flags & FLB_OUTPUT_SYNCHRONOUS) {
+            if ((instance->flags & FLB_OUTPUT_SYNCHRONOUS) &&
+                 instance->singleplex_queue != NULL) {
                 flb_task_queue_destroy(instance->singleplex_queue);
+            }
+            if (instance->callback != NULL) {
+                flb_callback_destroy(instance->callback);
+            }
+            if (plugin->type != FLB_OUTPUT_PLUGIN_CORE &&
+                instance->context != NULL) {
+                flb_free(instance->context);
             }
             flb_free(instance->http_server_config);
             flb_free(instance);
