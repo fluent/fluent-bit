10d244363bff4c8ca93471a452fea550776ab709
Hiroshi Hatake <hiroshi@chronosphere.io>
2026-07-14 15:56:24 +0900

    tests: runtime: Save with byte order mark for non-ASCII environment of Windows
    Signed-off-by: Hiroshi Hatake <hiroshi@chronosphere.io>

--- tests/runtime/in_tail.c
+++ tests/runtime/in_tail.c
@@ -1,4 +1,4 @@
-/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+﻿/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
 /*  Fluent Bit
  *  ==========
