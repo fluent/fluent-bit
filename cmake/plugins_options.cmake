7dff44a626ab66ed6ef01c85c57bb9597a5d5d86
Stas <tomat1979@gmail.com>
2026-09-14 22:10:48 +0200

    build: register Manticore output plugin
    Signed-off-by: Stas <tomat1979@gmail.com>

--- cmake/plugins_options.cmake
+++ cmake/plugins_options.cmake
@@ -119,6 +119,7 @@ DEFINE_OPTION(FLB_OUT_CLOUDWATCH_LOGS         "Enable AWS CloudWatch output plug
 DEFINE_OPTION(FLB_OUT_COUNTER                 "Enable Counter output plugin"                 ON)
 DEFINE_OPTION(FLB_OUT_DATADOG                 "Enable DataDog output plugin"                 ON)
 DEFINE_OPTION(FLB_OUT_ES                      "Enable Elasticsearch output plugin"           ON)
+DEFINE_OPTION(FLB_OUT_MANTICORE               "Enable Manticore Search output plugin"        ON)
 DEFINE_OPTION(FLB_OUT_EXIT                    "Enable Exit output plugin"                    ON)
 DEFINE_OPTION(FLB_OUT_FILE                    "Enable file output plugin"                    ON)
 DEFINE_OPTION(FLB_OUT_FLOWCOUNTER             "Enable flowcount output plugin"               ON)
