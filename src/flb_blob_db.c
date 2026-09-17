d044afc4efb3ce8fa5e38b12d8e63ea8570b622c
Eduardo Silva <eduardo@chronosphere.io>
2026-09-16 20:53:40 -0600

    blob_db: support the browser WebAssembly runtime
    Signed-off-by: Eduardo Silva <eduardo@chronosphere.io>

--- src/flb_blob_db.c
+++ src/flb_blob_db.c
@@ -17,10 +17,10 @@
  *  limitations under the License.
  */
 
-#ifdef FLB_HAVE_SQLDB
-
-#include <fluent-bit/flb_sqldb.h>
 #include <fluent-bit/flb_blob_db.h>
+
+#ifdef FLB_HAVE_SQLDB
+#include <fluent-bit/flb_sqldb.h>
 
 static int prepare_stmts(struct flb_blob_db *context)
 {
@@ -1512,7 +1512,8 @@ int flb_blob_db_file_part_get_next(struct flb_blob_db *context,
                                    cfl_sds_t *file_path,
                                    cfl_sds_t *destination,
                                    cfl_sds_t *remote_file_id,
-                                   cfl_sds_t *tag)
+                                   cfl_sds_t *tag,
+                                   int *part_count)
 {
     return FLB_BLOB_DB_ERROR_NO_BACKEND_AVAILABLE;
 }
