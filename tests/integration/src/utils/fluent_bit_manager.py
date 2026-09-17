37366fe98d24cc5cd133964f205433d79a9602e6
Eduardo Silva <eduardo@chronosphere.io>
2026-09-16 20:53:43 -0600

    tests: integration: retry transient startup health timeouts
    Signed-off-by: Eduardo Silva <eduardo@chronosphere.io>

--- tests/integration/src/utils/fluent_bit_manager.py
+++ tests/integration/src/utils/fluent_bit_manager.py
@@ -430,8 +430,9 @@ class FluentBitManager:
                     if uptime > 1:
                         logger.info("Fluent Bit is running, health check OK")
                         return True
-            except requests.ConnectionError:
-                # it's ok to fail, we are testing
+            except (requests.ConnectionError, requests.Timeout):
+                # Slow instrumented startup may accept a connection before
+                # answering. Retry within the existing overall deadline.
                 pass
 
             time.sleep(1)
